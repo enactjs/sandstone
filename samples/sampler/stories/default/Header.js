be93a96871131e52e512edea35850f12c5a8d059
Blake Stephens <blake.stephens@lge.com>
2020-01-29 12:32:47 -0800

    Updated Header component and related other pieces
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/sampler/stories/default/Header.js
+++ samples/sampler/stories/default/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 
 import {Header, HeaderBase} from '@enact/sandstone/Panels';
-import Button from '@enact/sandstone/Button';
+import Icon from '@enact/sandstone/Icon';
 import IconButton from '@enact/sandstone/IconButton';
 import Input from '@enact/sandstone/Input';
 
@@ -13,60 +13,65 @@ const Config = mergeComponentMetadata('Header', HeaderBase, Header);
 
 // Set up some defaults for info and knobs
 const prop = {
-	children: {
-		'no buttons': null,
-		'1 button': <IconButton>gear</IconButton>,
-		'2 buttons': <React.Fragment>
-			<Button>A Button</Button>
-			<IconButton>gear</IconButton>
+	above: {
+		none: null,
+		steps: <React.Fragment>
+			<Icon>check</Icon>
+			<Icon>check</Icon>
+			<Icon>circle</Icon>
+			<Icon>4</Icon>
+			<Icon>5</Icon>
 		</React.Fragment>
 	},
-	controls: {
+	buttons: {
 		'no buttons': null,
-		'1 button': <IconButton>gear</IconButton>,
+		'1 button': <IconButton>ellipsis</IconButton>,
 		'2 buttons': <React.Fragment>
-			<IconButton>star</IconButton>
-			<IconButton>gear</IconButton>
+			<IconButton>search</IconButton>
+			<IconButton>ellipsis</IconButton>
 		</React.Fragment>
 	},
+	buttonsSelection: ['no buttons', '1 button', '2 buttons'],
 	marqueeOn: ['', 'hover', 'render'],
-	type: ['compact', 'dense', 'standard']
+	type: ['standard', 'compact', 'walkthrough']
 };
 
 storiesOf('Sandstone', module)
 	.add(
 		'Header',
-		context => {
-			context.noHeader = true;
-
+		() => {
 			const headerInput = boolean('headerInput', Config) ? <Input placeholder="placeholder text" /> : null;
-			const childrenSelection = select('children', ['no buttons', '1 button', '2 buttons'], Config);
-			const children = prop.children[childrenSelection];
+			const slotAboveSelection = select('slotAbove', ['none', 'steps'], Config);
+			const slotAbove = prop.above[slotAboveSelection];
+			const slotBeforeSelection = select('slotBefore', prop.buttonsSelection, Config);
+			const slotBefore = prop.buttons[slotBeforeSelection];
+			const slotAfterSelection = select('slotAfter', prop.buttonsSelection, Config);
+			const slotAfter = prop.buttons[slotAfterSelection];
+			const childrenSelection = select('children', prop.buttonsSelection, Config);
+			const children = prop.buttons[childrenSelection];
 
 			const story = (
 				<Header
 					title={text('title', Config, 'The Matrix')}
-					titleBelow={text('titleBelow', Config, 'Free your mind')}
-					subTitleBelow={text('subTitleBelow', Config, 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.')}
+					subtitle={text('subtitle', Config, 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.')}
 					type={select('type', prop.type, Config)}
 					centered={boolean('centered', Config)}
-					fullBleed={boolean('fullBleed', Config)}
 					headerInput={headerInput}
-					hideLine={boolean('hideLine', Config)}
 					marqueeOn={select('marqueeOn', prop.marqueeOn, Config)}
+					slotAbove={slotAbove}
+					slotBefore={slotBefore}
+					slotAfter={slotAfter}
 				>
 					{children}
 				</Header>
 			);
 
-			context.panelsProps = {
-				controls: prop.controls[select('controls', ['no buttons', '1 button', '2 buttons'], {displayName: 'Panels'})],
-				noCloseButton: boolean('noCloseButton', {displayName: 'Panels'}) || false
-			};
-
 			return story;
 		},
 		{
+			props: {
+				noHeader: true
+			},
 			info: {
 				text: 'A block to use as a screen\'s title and description. Supports additional buttons and up to two subtitles.'
 			}
