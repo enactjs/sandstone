enactjs/moonstone

PLAT-98768: Add UI Tests (#3)
* add existing ui-tests

* add npm run task

* fix test references

* temp add @babel/preset-env to fix build error

* fix date picker, expandable input, and checkbox item specs

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* update existing testcase with new methods and param

* remove enact_moonstone prefixes

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* update existing testcase with new methods and param

* add VL tests

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* refine selectors in FormCheckboxItem

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* updated ActivityPanels

* update name of ui-test-utils

* move preset-env to dev dependency

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* add addl test-ui commands

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* Relocate eslint config, add newlines

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* Add linting of tests again

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* Update linting

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* Update eslint to ignore nested node_modules

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* Don't need to ignore _resources_ anymore

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* ui tests is public now

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* ui tests is public now, but use develop

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* ui tests is public now, but use develop and right name

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

* update lint command and ui-test-utils version

Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

* Switch to local lint config

Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

Co-authored-by: RenukaAtale <renuka.atale@lge.com>
Co-authored-by: Roy Sutton <pre101@pre101.com>

tests/ui/apps/ExpandableItem/ExpandableItem-View.js
@@ -1,0 +1,89 @@
+import ExpandableItem from '../../../../ExpandableItem';
+import Item from '../../../../Item';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const children = <Item>The Expanded Item</Item>;
+
+const app = (props) => <div {...props}>
+	<div>
+		<ExpandableItem
+			id="expandableItemDefaultClosedWithoutNoneText"
+			title="ExpandableItem Default"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemDefaultClosedWithNoneText"
+			noneText="Nothing Selected"
+			title="ExpandableItem Default With noneText"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			defaultOpen
+			id="expandableItemDefaultOpenWithNoneText"
+			noneText="Nothing Selected"
+			title="ExpandableItem Default Open"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			autoClose
+			id="expandableItemWithAutoClose"
+			title="ExpandableItem With autoClose"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemWithLockBottom"
+			lockBottom
+			title="ExpandableItem With lockBottom"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemWithoutChildren"
+			title="ExpandableItem Without Children"
+		/>
+		<ExpandableItem
+			id="expandableItemAutoLabel"
+			label="Labeled Item"
+			noneText="Nothing Selected"
+			title="ExpandableItem With Label (auto)"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemAlwaysLabel"
+			label="Labeled Item"
+			showLabel="always"
+			title="ExpandableItem With Label (always)"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemNeverLabel"
+			label="Labeled Item"
+			showLabel="never"
+			title="ExpandableItem With Label (never)"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			disabled
+			id="expandableItemDisabledWithNoneText"
+			noneText="Nothing Selected"
+			title="ExpandableItem Disabled"
+		>
+			{children}
+		</ExpandableItem>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);

tests/ui/apps/ExpandableList/ExpandableList-View.js
@@ -1,0 +1,83 @@
+import ExpandableList from '../../../../ExpandableList';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const children = ['option1', 'option2', 'option3'];
+
+// ExpandableList options:
+// select, disabled, noneText, title, closeOnSelect noAutoClose, noLockBottom,
+// defaultOpen, defaultSelected
+
+const app = (props) => <div {...props}>
+	<div>
+		<ExpandableList
+			id="expandable1"
+			title="ExpandableList Radio Select"
+			select="radio"
+			noneText="Nothing Selected"
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable2"
+			title="ExpandableList Multi Select"
+			select="multiple"
+			noneText="Nothing Selected"
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable3"
+			title="ExpandableList Single Select"
+			select="single"
+			noneText="Nothing Selected"
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable4"
+			title="ExpandableList No Lock Bottom"
+			select="radio"
+			noneText="Nothing Selected"
+			noLockBottom
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable5"
+			title="ExpandableList No Auto Close"
+			select="radio"
+			noneText="Nothing Selected"
+			noAutoClose
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable6"
+			title="ExpandableList Default Open"
+			select="radio"
+			noneText="Nothing Selected"
+			defaultOpen
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable7"
+			title="ExpandableList Disabled"
+			select="radio"
+			noneText="Nothing Selected"
+			disabled
+		>
+			{children}
+		</ExpandableList>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);
+
+

tests/ui/apps/FormCheckboxItem/FormCheckboxItem-View.js
@@ -1,0 +1,63 @@
+import FormCheckboxItem from '../../../../FormCheckboxItem';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<FormCheckboxItem
+			id="formCheckboxItem1"
+		>
+			FormCheckbox Item
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem2"
+			defaultSelected
+		>
+			FormCheckbox Item selected
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem3"
+			iconPosition="after"
+			defaultSelected
+		>
+			FormCheckbox Item after
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem4"
+			defaultSelected
+			inline
+		>
+			FormCheckbox Item inline
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem5"
+			iconPosition="after"
+			defaultSelected
+			inline
+		>
+			FormCheckbox Item inline after
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem6"
+			defaultSelected
+			disabled
+		>
+			FormCheckbox Item disabled
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem7"
+			defaultSelected
+			inline
+			disabled
+		>
+			FormCheckbox Item inline disabled
+		</FormCheckboxItem>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);

tests/ui/apps/RadioItem/RadioItem-View.js
@@ -1,0 +1,48 @@
+import RadioItem from '../../../../RadioItem';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<RadioItem
+			id="radioItem1"
+		>
+			Radio Item1
+		</RadioItem>
+		<RadioItem
+			id="radioItem2"
+			defaultSelected
+		>
+			Radio Item selected
+		</RadioItem>
+		<RadioItem
+			id="radioItem3"
+			defaultSelected
+			inline
+		>
+			Radio Item inline
+		</RadioItem>
+		<RadioItem
+			id="radioItem4"
+			defaultSelected
+			disabled
+		>
+			Radio Item disabled
+		</RadioItem>
+		<RadioItem
+			id="radioItem5"
+			defaultSelected
+			inline
+			disabled
+		>
+			Radio Item inline disabled
+		</RadioItem>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);

tests/ui/apps/SelectableItem/SelectableItem-View.js
@@ -1,0 +1,48 @@
+import SelectableItem from '../../../../SelectableItem';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<SelectableItem
+			id="selectableItem1"
+		>
+			Selectable Item1
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem2"
+			defaultSelected
+		>
+			Selectable Item selected
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem3"
+			defaultSelected
+			inline
+		>
+			Selectable Item inline
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem4"
+			defaultSelected
+			disabled
+		>
+			Selectable Item disabled
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem5"
+			defaultSelected
+			inline
+			disabled
+		>
+			Selectable Item inline disabled
+		</SelectableItem>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);

tests/ui/apps/SwitchItem/SwitchItem-View.js
@@ -1,0 +1,48 @@
+import SwitchItem from '../../../../SwitchItem';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<SwitchItem
+			id="switchItem1"
+		>
+			Switch Item1
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem2"
+			defaultSelected
+		>
+			Switch Item selected
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem3"
+			defaultSelected
+			inline
+		>
+			Switch Item inline
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem4"
+			defaultSelected
+			disabled
+		>
+			Switch Item disabled
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem5"
+			defaultSelected
+			inline
+			disabled
+		>
+			Switch Item inline disabled
+		</SwitchItem>
+	</div>
+</div>;
+
+export default MoonstoneDecorator(app);

tests/ui/apps/VirtualList/VirtualList-View.js
@@ -1,0 +1,163 @@
+import {Button} from '../../../../Button';
+import ri from '@enact/ui/resolution';
+import {Row, Column, Cell} from '@enact/ui/Layout';
+import SwitchItem from '../../../../SwitchItem';
+import ToggleButton from '../../../../ToggleButton';
+import VirtualList from '../../../../VirtualList';
+import MoonstoneDecorator from '../../../../MoonstoneDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+import SpotlightContainerDecorator from '@enact/spotlight/SpotlightContainerDecorator';
+
+const ListContainer = SpotlightContainerDecorator({leaveFor: {up: ''}}, 'div');
+const OptionsContainer = SpotlightContainerDecorator({leaveFor: {down: '#left'}}, 'div');
+const getScrollbarVisibility = (hidden) => hidden ? 'hidden' : 'visible';
+const fullHeightStyle = {
+	height: '100%'
+};
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const items = [],
+	itemSize = 72,
+	itemStyle = {
+		borderBottom: ri.unit(3, 'rem') + ' solid #202328',
+		boxSizing: 'border-box'
+	},
+	numItems = 100;
+
+const renderItem = (size) => ({index, ...rest}) => {
+	const style = {height: size + 'px', ...itemStyle};
+	return (
+		<StatefulSwitchItem index={index} style={style} {...rest} id={`item${index}`}>
+			{items[index].item}
+		</StatefulSwitchItem>
+	);
+};
+
+const updateDataSize = (dataSize) => {
+	const
+		itemNumberDigits = dataSize > 0 ? ((dataSize - 1) + '').length : 0,
+		headingZeros = Array(itemNumberDigits).join('0');
+
+	items.length = 0;
+
+	for (let i = 0; i < dataSize; i++) {
+		items.push({item :'Item ' + (headingZeros + i).slice(-itemNumberDigits), selected: false});
+	}
+
+	return dataSize;
+};
+
+updateDataSize(numItems);
+
+class StatefulSwitchItem extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			prevIndex: props.index,
+			selected: items[props.index].selected
+		};
+	}
+
+	static getDerivedStateFromProps (props, state) {
+		if (state.prevIndex !== props.index) {
+			return {
+				prevIndex: props.index,
+				selected: items[props.index].selected
+			};
+		}
+
+		return null;
+	}
+
+	onToggle = () => {
+		items[this.props.index].selected = !items[this.props.index].selected;
+		this.setState(({selected}) => ({
+			selected: !selected
+		}));
+	}
+
+	render () {
+		const props = Object.assign({}, this.props);
+		delete props.index;
+
+		return (
+			<SwitchItem {...props} onToggle={this.onToggle} selected={this.state.selected}>
+				{this.props.children}
+			</SwitchItem>
+		);
+	}
+}
+
+class app extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			focusableScrollbar: false,
+			hideScrollbar: false,
+			keyDownEvents: 0,
+			wrap: false
+		};
+	}
+
+	onKeyDown = () => {
+		this.setState(({keyDownEvents}) => ({keyDownEvents: keyDownEvents + 1}));
+	}
+
+	onToggle = ({currentTarget}) => {
+		const key = currentTarget.getAttribute('id');
+		this.setState((state) => ({[key]: !state[key]}));
+	}
+
+	render () {
+		const {focusableScrollbar, hideScrollbar, keyDownEvents, wrap} = this.state;
+		return (
+			<div {...this.props} data-keydown-events={keyDownEvents} id="list" style={fullHeightStyle}>
+				<Column>
+					<Cell component={OptionsContainer} shrink>
+						<ToggleButton id="focusableScrollbar" onClick={this.onToggle} selected={focusableScrollbar}>focusableScrollbar</ToggleButton>
+						<ToggleButton id="hideScrollbar" onClick={this.onToggle} selected={hideScrollbar}>hide scrollbar</ToggleButton>
+						<ToggleButton id="wrap" onClick={this.onToggle} selected={wrap}>wrap</ToggleButton>
+					</Cell>
+					<Cell component={ListContainer}>
+						<Row align="center" style={fullHeightStyle}>
+							<Cell component={Button} shrink id="left">
+								Left
+							</Cell>
+							<Cell align="stretch">
+								<Column align="center" style={fullHeightStyle}>
+									<Cell component={Button} shrink id="top">
+										Top
+									</Cell>
+									<Cell>
+										<VirtualList
+											dataSize={numItems}
+											focusableScrollbar={focusableScrollbar}
+											itemRenderer={renderItem(itemSize)}
+											itemSize={itemSize}
+											onKeyDown={this.onKeyDown}
+											spacing={0}
+											verticalScrollbar={getScrollbarVisibility(hideScrollbar)}
+											wrap={wrap}
+										/>
+									</Cell>
+									<Cell component={Button} shrink id="bottom">
+										Bottom
+									</Cell>
+								</Column>
+							</Cell>
+							<Cell component={Button} shrink id="right">
+								Right
+							</Cell>
+						</Row>
+					</Cell>
+				</Column>
+			</div>
+		);
+	}
+}
+
+export default MoonstoneDecorator(app);

enactjs/sandstone

tests/ui/apps/ExpandableItem/ExpandableItem-View.js
@@ -1,0 +1,89 @@
+import ExpandableItem from '../../../../ExpandableItem';
+import Item from '../../../../Item';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const children = <Item>The Expanded Item</Item>;
+
+const app = (props) => <div {...props}>
+	<div>
+		<ExpandableItem
+			id="expandableItemDefaultClosedWithoutNoneText"
+			title="ExpandableItem Default"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemDefaultClosedWithNoneText"
+			noneText="Nothing Selected"
+			title="ExpandableItem Default With noneText"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			defaultOpen
+			id="expandableItemDefaultOpenWithNoneText"
+			noneText="Nothing Selected"
+			title="ExpandableItem Default Open"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			autoClose
+			id="expandableItemWithAutoClose"
+			title="ExpandableItem With autoClose"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemWithLockBottom"
+			lockBottom
+			title="ExpandableItem With lockBottom"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemWithoutChildren"
+			title="ExpandableItem Without Children"
+		/>
+		<ExpandableItem
+			id="expandableItemAutoLabel"
+			label="Labeled Item"
+			noneText="Nothing Selected"
+			title="ExpandableItem With Label (auto)"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemAlwaysLabel"
+			label="Labeled Item"
+			showLabel="always"
+			title="ExpandableItem With Label (always)"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			id="expandableItemNeverLabel"
+			label="Labeled Item"
+			showLabel="never"
+			title="ExpandableItem With Label (never)"
+		>
+			{children}
+		</ExpandableItem>
+		<ExpandableItem
+			disabled
+			id="expandableItemDisabledWithNoneText"
+			noneText="Nothing Selected"
+			title="ExpandableItem Disabled"
+		>
+			{children}
+		</ExpandableItem>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);

tests/ui/apps/ExpandableList/ExpandableList-View.js
@@ -1,0 +1,83 @@
+import ExpandableList from '../../../../ExpandableList';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const children = ['option1', 'option2', 'option3'];
+
+// ExpandableList options:
+// select, disabled, noneText, title, closeOnSelect noAutoClose, noLockBottom,
+// defaultOpen, defaultSelected
+
+const app = (props) => <div {...props}>
+	<div>
+		<ExpandableList
+			id="expandable1"
+			title="ExpandableList Radio Select"
+			select="radio"
+			noneText="Nothing Selected"
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable2"
+			title="ExpandableList Multi Select"
+			select="multiple"
+			noneText="Nothing Selected"
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable3"
+			title="ExpandableList Single Select"
+			select="single"
+			noneText="Nothing Selected"
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable4"
+			title="ExpandableList No Lock Bottom"
+			select="radio"
+			noneText="Nothing Selected"
+			noLockBottom
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable5"
+			title="ExpandableList No Auto Close"
+			select="radio"
+			noneText="Nothing Selected"
+			noAutoClose
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable6"
+			title="ExpandableList Default Open"
+			select="radio"
+			noneText="Nothing Selected"
+			defaultOpen
+		>
+			{children}
+		</ExpandableList>
+		<ExpandableList
+			id="expandable7"
+			title="ExpandableList Disabled"
+			select="radio"
+			noneText="Nothing Selected"
+			disabled
+		>
+			{children}
+		</ExpandableList>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);
+
+

tests/ui/apps/FormCheckboxItem/FormCheckboxItem-View.js
@@ -1,0 +1,63 @@
+import FormCheckboxItem from '../../../../FormCheckboxItem';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<FormCheckboxItem
+			id="formCheckboxItem1"
+		>
+			FormCheckbox Item
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem2"
+			defaultSelected
+		>
+			FormCheckbox Item selected
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem3"
+			iconPosition="after"
+			defaultSelected
+		>
+			FormCheckbox Item after
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem4"
+			defaultSelected
+			inline
+		>
+			FormCheckbox Item inline
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem5"
+			iconPosition="after"
+			defaultSelected
+			inline
+		>
+			FormCheckbox Item inline after
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem6"
+			defaultSelected
+			disabled
+		>
+			FormCheckbox Item disabled
+		</FormCheckboxItem>
+		<FormCheckboxItem
+			id="formCheckboxItem7"
+			defaultSelected
+			inline
+			disabled
+		>
+			FormCheckbox Item inline disabled
+		</FormCheckboxItem>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);

tests/ui/apps/RadioItem/RadioItem-View.js
@@ -1,0 +1,48 @@
+import RadioItem from '../../../../RadioItem';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<RadioItem
+			id="radioItem1"
+		>
+			Radio Item1
+		</RadioItem>
+		<RadioItem
+			id="radioItem2"
+			defaultSelected
+		>
+			Radio Item selected
+		</RadioItem>
+		<RadioItem
+			id="radioItem3"
+			defaultSelected
+			inline
+		>
+			Radio Item inline
+		</RadioItem>
+		<RadioItem
+			id="radioItem4"
+			defaultSelected
+			disabled
+		>
+			Radio Item disabled
+		</RadioItem>
+		<RadioItem
+			id="radioItem5"
+			defaultSelected
+			inline
+			disabled
+		>
+			Radio Item inline disabled
+		</RadioItem>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);

tests/ui/apps/SelectableItem/SelectableItem-View.js
@@ -1,0 +1,48 @@
+import SelectableItem from '../../../../SelectableItem';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<SelectableItem
+			id="selectableItem1"
+		>
+			Selectable Item1
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem2"
+			defaultSelected
+		>
+			Selectable Item selected
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem3"
+			defaultSelected
+			inline
+		>
+			Selectable Item inline
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem4"
+			defaultSelected
+			disabled
+		>
+			Selectable Item disabled
+		</SelectableItem>
+		<SelectableItem
+			id="selectableItem5"
+			defaultSelected
+			inline
+			disabled
+		>
+			Selectable Item inline disabled
+		</SelectableItem>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);

tests/ui/apps/SwitchItem/SwitchItem-View.js
@@ -1,0 +1,48 @@
+import SwitchItem from '../../../../SwitchItem';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const app = (props) => <div {...props}>
+	<div>
+		<SwitchItem
+			id="switchItem1"
+		>
+			Switch Item1
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem2"
+			defaultSelected
+		>
+			Switch Item selected
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem3"
+			defaultSelected
+			inline
+		>
+			Switch Item inline
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem4"
+			defaultSelected
+			disabled
+		>
+			Switch Item disabled
+		</SwitchItem>
+		<SwitchItem
+			id="switchItem5"
+			defaultSelected
+			inline
+			disabled
+		>
+			Switch Item inline disabled
+		</SwitchItem>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);

tests/ui/apps/VirtualList/VirtualList-View.js
@@ -1,0 +1,163 @@
+import {Button} from '../../../../Button';
+import ri from '@enact/ui/resolution';
+import {Row, Column, Cell} from '@enact/ui/Layout';
+import SwitchItem from '../../../../SwitchItem';
+import ToggleButton from '../../../../ToggleButton';
+import VirtualList from '../../../../VirtualList';
+import ThemeDecorator from '../../../../ThemeDecorator';
+import React from 'react';
+import spotlight from '@enact/spotlight';
+import SpotlightContainerDecorator from '@enact/spotlight/SpotlightContainerDecorator';
+
+const ListContainer = SpotlightContainerDecorator({leaveFor: {up: ''}}, 'div');
+const OptionsContainer = SpotlightContainerDecorator({leaveFor: {down: '#left'}}, 'div');
+const getScrollbarVisibility = (hidden) => hidden ? 'hidden' : 'visible';
+const fullHeightStyle = {
+	height: '100%'
+};
+
+// NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
+// runs the same way
+spotlight.setPointerMode(false);
+
+const items = [],
+	itemSize = 72,
+	itemStyle = {
+		borderBottom: ri.unit(3, 'rem') + ' solid #202328',
+		boxSizing: 'border-box'
+	},
+	numItems = 100;
+
+const renderItem = (size) => ({index, ...rest}) => {
+	const style = {height: size + 'px', ...itemStyle};
+	return (
+		<StatefulSwitchItem index={index} style={style} {...rest} id={`item${index}`}>
+			{items[index].item}
+		</StatefulSwitchItem>
+	);
+};
+
+const updateDataSize = (dataSize) => {
+	const
+		itemNumberDigits = dataSize > 0 ? ((dataSize - 1) + '').length : 0,
+		headingZeros = Array(itemNumberDigits).join('0');
+
+	items.length = 0;
+
+	for (let i = 0; i < dataSize; i++) {
+		items.push({item :'Item ' + (headingZeros + i).slice(-itemNumberDigits), selected: false});
+	}
+
+	return dataSize;
+};
+
+updateDataSize(numItems);
+
+class StatefulSwitchItem extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			prevIndex: props.index,
+			selected: items[props.index].selected
+		};
+	}
+
+	static getDerivedStateFromProps (props, state) {
+		if (state.prevIndex !== props.index) {
+			return {
+				prevIndex: props.index,
+				selected: items[props.index].selected
+			};
+		}
+
+		return null;
+	}
+
+	onToggle = () => {
+		items[this.props.index].selected = !items[this.props.index].selected;
+		this.setState(({selected}) => ({
+			selected: !selected
+		}));
+	}
+
+	render () {
+		const props = Object.assign({}, this.props);
+		delete props.index;
+
+		return (
+			<SwitchItem {...props} onToggle={this.onToggle} selected={this.state.selected}>
+				{this.props.children}
+			</SwitchItem>
+		);
+	}
+}
+
+class app extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			focusableScrollbar: false,
+			hideScrollbar: false,
+			keyDownEvents: 0,
+			wrap: false
+		};
+	}
+
+	onKeyDown = () => {
+		this.setState(({keyDownEvents}) => ({keyDownEvents: keyDownEvents + 1}));
+	}
+
+	onToggle = ({currentTarget}) => {
+		const key = currentTarget.getAttribute('id');
+		this.setState((state) => ({[key]: !state[key]}));
+	}
+
+	render () {
+		const {focusableScrollbar, hideScrollbar, keyDownEvents, wrap} = this.state;
+		return (
+			<div {...this.props} data-keydown-events={keyDownEvents} id="list" style={fullHeightStyle}>
+				<Column>
+					<Cell component={OptionsContainer} shrink>
+						<ToggleButton id="focusableScrollbar" onClick={this.onToggle} selected={focusableScrollbar}>focusableScrollbar</ToggleButton>
+						<ToggleButton id="hideScrollbar" onClick={this.onToggle} selected={hideScrollbar}>hide scrollbar</ToggleButton>
+						<ToggleButton id="wrap" onClick={this.onToggle} selected={wrap}>wrap</ToggleButton>
+					</Cell>
+					<Cell component={ListContainer}>
+						<Row align="center" style={fullHeightStyle}>
+							<Cell component={Button} shrink id="left">
+								Left
+							</Cell>
+							<Cell align="stretch">
+								<Column align="center" style={fullHeightStyle}>
+									<Cell component={Button} shrink id="top">
+										Top
+									</Cell>
+									<Cell>
+										<VirtualList
+											dataSize={numItems}
+											focusableScrollbar={focusableScrollbar}
+											itemRenderer={renderItem(itemSize)}
+											itemSize={itemSize}
+											onKeyDown={this.onKeyDown}
+											spacing={0}
+											verticalScrollbar={getScrollbarVisibility(hideScrollbar)}
+											wrap={wrap}
+										/>
+									</Cell>
+									<Cell component={Button} shrink id="bottom">
+										Bottom
+									</Cell>
+								</Column>
+							</Cell>
+							<Cell component={Button} shrink id="right">
+								Right
+							</Cell>
+						</Row>
+					</Cell>
+				</Column>
+			</div>
+		);
+	}
+}
+
+export default ThemeDecorator(app);