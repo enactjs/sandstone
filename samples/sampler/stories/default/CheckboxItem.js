f0b935e444b90fbeabc0543b29bb88f55198b18f
Renuka Atale <renuka.atale@lge.com>
2020-01-30 14:59:25 -0800

    Update checkboxItem component

--- samples/sampler/stories/default/CheckboxItem.js
+++ samples/sampler/stories/default/CheckboxItem.js
@@ -1,36 +1,25 @@
 import {action} from '@enact/storybook-utils/addons/actions';
 import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
-import {mergeComponentMetadata, nullify} from '@enact/storybook-utils';
-import UiToggleItem, {ToggleItemBase as UiToggleItemBase} from '@enact/ui/ToggleItem';
-import UiItem from '@enact/ui/Item';
-import {listIcons} from './icons';
+import {mergeComponentMetadata} from '@enact/storybook-utils';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
 import CheckboxItem from '@enact/sandstone/CheckboxItem';
-import ToggleItem from '@enact/sandstone/ToggleItem';
-import Item, {ItemBase} from '@enact/sandstone/Item';
-import Icon from '@enact/sandstone/Icon';
 
 CheckboxItem.displayName = 'CheckboxItem';
-const Config = mergeComponentMetadata('CheckboxItem', UiItem, ItemBase, Item, UiToggleItemBase, UiToggleItem, ToggleItem, CheckboxItem);
+const Config = mergeComponentMetadata('CheckboxItem', CheckboxItem);
 
 storiesOf('Sandstone', module)
 	.add(
 		'CheckboxItem',
 		() => {
-			const iconPosition = select('iconPosition', ['before', 'after'], Config);
-			const icon = select('itemIcon', ['', ...listIcons], Config);
-			const itemIcon = nullify(icon ? <Icon>{icon}</Icon> : null);
-			const itemIconPosition = select('itemIconPosition', [null, 'before', 'beforeChildren', 'afterChildren', 'after'], Config);
 			return (
 				<CheckboxItem
 					// disabled and inline have problems when set to `null` from the internal nullify...
 					disabled={boolean('disabled', Config)}
-					iconPosition={iconPosition}
 					inline={boolean('inline', Config)}
-					itemIcon={itemIcon}
-					itemIconPosition={itemIconPosition}
+					label={text('label', Config, '')}
+					labelPosition={select('labelPosition', ['', 'above', 'after', 'before', 'below'], Config, '')}
 					onToggle={action('onToggle')}
 				>
 					{text('children', Config, 'Hello CheckboxItem')}
