b35ab8fcaba05c6dbde8d4da0bd22f93b2fd6d28
Ryan Duffy <ryan.duffy@lge.com>
2020-02-05 10:27:30 -0800

    move some ui tests to unit tests
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- tests/ui/specs/Button/Button-specs.js
+++ tests/ui/specs/Button/Button-specs.js
@@ -9,63 +9,9 @@ describe('Button', function () {
 	const {
 		buttonDefault,
 		buttonDisabled,
-		buttonTransparent,
-		buttonWithCheckIcon,
-		buttonWithIconAfter,
-		buttonFalseMinWidth,
 		buttonSizeSmall,
 		iconButton
 	} = Page.components;
-
-	it('should have default backgroundOpacity opaque', function () {
-		expect(buttonDefault.isOpaque).to.be.true();
-	});
-
-	it('should have default minWidth', function () {
-		expect(buttonDefault.isMinWidth).to.be.true();
-	});
-
-	it('should have default size large', function () {
-		expect(buttonDefault.isLarge).to.be.true();
-	});
-
-	describe('with no minWidth', function () {
-		it('should not have minWidth class', function () {
-			expect(buttonFalseMinWidth.isMinWidth).to.be.false();
-		});
-	});
-
-	describe('with transparent backgroundOpacity', function () {
-		it('should have transparent class', function () {
-			expect(buttonTransparent.isTransparent).to.be.true();
-		});
-
-		it('should not have have opaque class', function () {
-			expect(buttonTransparent.isOpaque).to.be.false();
-		});
-	});
-
-	describe('with icon', function () {
-		it('should have check icon when specified', function () {
-			expect(buttonWithCheckIcon.iconSymbol).to.equal('✓');
-		});
-
-		it('should display icon before the text by default', function () {
-			expect(buttonWithCheckIcon.isIconBefore).to.be.true();
-		});
-
-		it('should display icon after the text when iconPosition is after', function () {
-			expect(buttonWithIconAfter.isIconAfter).to.be.true();
-		});
-
-		it('should not have minWidth class with only icon', function () {
-			expect(iconButton.isMinWidth).to.be.false();
-		});
-
-		it('should have iconOnly class when there is no children', function () {
-			expect(iconButton.isIconButton).to.be.true();
-		});
-	});
 
 	describe('5-way', function () {
 		it('should focus disabled button on 5-way right', function () {
