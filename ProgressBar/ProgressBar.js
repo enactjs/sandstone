3a30eaddb07e9a80a64d90f57f1e120799382f43
Jeremy Thomas <thomas.jeremy.e@gmail.com>
2020-02-14 11:47:25 -0500

    inherit from ui/ProgressBar

--- ProgressBar/ProgressBar.js
+++ ProgressBar/ProgressBar.js
@@ -38,17 +38,6 @@ const ProgressBarBase = kind({
 
 	propTypes: /** @lends sandstone/ProgressBar.ProgressBarBase.prototype */ {
 		/**
-		 * The proportion of the loaded portion of the progress bar.
-		 *
-		 * * Valid values are between `0` and `1`.
-		 *
-		 * @type {Number}
-		 * @default 0
-		 * @public
-		 */
-		backgroundProgress: PropTypes.number,
-
-		/**
 		 * Customizes the component by mapping the supplied collection of CSS class names to the
 		 * corresponding internal elements and states of this component.
 		 *
@@ -130,7 +119,6 @@ const ProgressBarBase = kind({
 	},
 
 	defaultProps: {
-		backgroundProgress: 0,
 		orientation: 'horizontal',
 		progress: 0
 	},
@@ -141,11 +129,9 @@ const ProgressBarBase = kind({
 	},
 
 	computed: {
-		className: ({highlighted, orientation, progress, backgroundProgress, styler}) => styler.append({
+		className: ({highlighted, orientation, styler}) => styler.append({
 			highlighted,
-			radial: (orientation === 'radial'),
-			fillHalfWay: (progress > 0.5),
-			loadHalfWay: (backgroundProgress > 0.5)
+			//radial: (orientation === 'radial')
 		}),
 		tooltip: ({tooltip}) => tooltip === true ? ProgressBarTooltip : tooltip
 	},
