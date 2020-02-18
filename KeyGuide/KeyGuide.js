869ad2ecffc8376254450f6da1aabd6db65d9ee0
Hyeok Jo <hyeok.jo@lge.com>
2020-02-18 12:03:49 +0900

    Initial implementation
    Enact-DCO-1.0-Signed-off-by: Hyeok Jo <hyeok.jo@lge.com>

---
+++ KeyGuide/KeyGuide.js
@@ -0,0 +1,116 @@
+/**
+ * Sandstone styled key guide component and behaviors.
+ *
+ * @example
+ * <KeyGuide
+ *		open
+ * >
+ * 	{[
+ *		{icon: 'star', children: 'start label', key: 1},
+ *		{icon: 'plus', children: 'plus label', key: 2},
+ *		{icon: 'minus', children: 'minus label', key: 3}
+ *	]}
+ * </KeyGuide>
+ *
+ * @module sandstone/KeyGuide
+ * @exports KeyGuide
+ * @exports KeyGuideBase
+ * @exports KeyGuideDecorator
+ */
+
+import kind from '@enact/core/kind';
+import EnactPropTypes from '@enact/core/internal/prop-types';
+import FloatingLayer from '@enact/ui/FloatingLayer';
+import Pure from '@enact/ui/internal/Pure';
+import PropTypes from 'prop-types';
+import compose from 'ramda/src/compose';
+import React from 'react';
+
+import LabeledIcon from '../LabeledIcon';
+import Skinnable from '../Skinnable';
+import componentCss from './KeyGuide.module.less';
+
+/**
+ * A Key Guide component.
+ *
+ * This component is most often not used directly but may be composed within another component as it
+ * is within [KeyGuide]{@link sandstone/KeyGuide.KeyGuide}.
+ *
+ * @class KeyGuideBase
+ * @memberof sandstone/KeyGuide
+ * @ui
+ * @public
+ */
+const KeyGuideBase = kind({
+	name: 'KeyGuide',
+
+	propTypes: /** @lends sandstone/MediaOverlay.MediaOverlayBase.prototype */ {
+		/**
+		 * The items to be displayed in the `KeyGuide` when `open`.
+		 *
+		 * Takes an array of objects. The properties will be passed onto an `LabeledIcon` component
+		 * and `children` as well as a unique `key` property are required.
+		 *
+		 * @type {Array.<{children: (String|Component), key: (Number|String), icon: (String)}>}
+		 * @public
+		 */
+		children: PropTypes.arrayOf(PropTypes.shape({
+			children: EnactPropTypes.renderable.isRequired,
+			key: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+			icon: PropTypes.string
+		})).isRequired,
+
+		/**
+		 * Controls the visibility of the KeyGuide.
+		 *
+		 * @type {Boolean}
+		 * @default false
+		 * @public
+		 */
+		open: PropTypes.bool
+	},
+
+	computed: {
+		children: ({children}) => {
+			if (!Array.isArray(children)) return [];
+
+			return children.map((child) => {
+				return <LabeledIcon {...child} css={componentCss} labelPosition="after" size="small" />;
+			});
+		}
+	},
+
+	styles: {
+		css: componentCss,
+		className: 'KeyGuide',
+		publicClassNames: ['KeyGuide']
+	},
+
+	render: ({open, children, ...rest}) => {
+		return (
+			<FloatingLayer
+				noAutoDismiss
+				open={open}
+				scrimType="none"
+			>
+				<div {...rest}>
+					{children}
+				</div>
+			</FloatingLayer>
+		);
+	}
+});
+
+const KeyGuideDecorator = compose(
+	Pure,
+	Skinnable
+);
+
+const KeyGuide = KeyGuideDecorator(KeyGuideBase);
+
+export default KeyGuide;
+export {
+	KeyGuide,
+	KeyGuideBase,
+	KeyGuideDecorator
+};
