7d46362c62f17b452179632b3ae12849cc9e8806
Dave Freeman <dave.freeman@lge.com>
2020-02-19 17:22:29 -0600

    Adds all `size` prop combinations for each test

--- tests/screenshot/apps/components/Icon.js
+++ tests/screenshot/apps/components/Icon.js
@@ -4,19 +4,41 @@ import React from 'react';
 import buttonstate from '../../images/button-state.svg';
 
 const IconTests = [
-	<Icon>minus</Icon>,
+	<Icon>minus</Icon>, // default `size` prop is "small"
+	<Icon size="tiny">minus</Icon>,
+	<Icon size="medium">minus</Icon>,
+	<Icon size="large">minus</Icon>,
 	<Icon>💣</Icon>,  // testing 'custom-icon' using unicode character
+	<Icon size="tiny">💣</Icon>,
+	<Icon size="medium">💣</Icon>,
 	<Icon size="large">💣</Icon>,
 	<Icon>{buttonstate}</Icon>,
+	<Icon size="tiny">{buttonstate}</Icon>,
+	<Icon size="medium">{buttonstate}</Icon>,
 	<Icon size="large">{buttonstate}</Icon>,
 	<Icon flip="vertical">question</Icon>,
+	<Icon flip="vertical" size="tiny">question</Icon>,
+	<Icon flip="vertical" size="medium">question</Icon>,
+	<Icon flip="vertical" size="large">question</Icon>,
 	<Icon flip="horizontal">question</Icon>,
+	<Icon flip="horizontal" size="tiny">question</Icon>,
+	<Icon flip="horizontal" size="medium">question</Icon>,
+	<Icon flip="horizontal" size="large">question</Icon>,
 	<Icon flip="both">repeat</Icon>,
+	<Icon flip="both" size="tiny">repeat</Icon>,
+	<Icon flip="both" size="medium">repeat</Icon>,
 	<Icon flip="both" size="large">repeat</Icon>,
-	<Icon size="large">plus</Icon>,
 	{
 		textSize: 'large',
 		component: <Icon>plus</Icon>
+	},
+	{
+		textSize: 'large',
+		component: <Icon size="tiny">plus</Icon>
+	},
+	{
+		textSize: 'large',
+		component: <Icon size="medium">plus</Icon>
 	},
 	{
 		textSize: 'large',
@@ -33,7 +55,30 @@ const IconTests = [
 
 	{
 		locale: 'ar-SA',
+		component: <Icon size="tiny">minus</Icon>
+	},
+
+	{
+		locale: 'ar-SA',
+		component: <Icon size="medium">minus</Icon>
+	},
+
+	{
+		locale: 'ar-SA',
+		component: <Icon size="large">minus</Icon>
+	},
+
+	{
+		locale: 'ar-SA',
 		component: <Icon>💣</Icon>  // testing 'custom-icon' using unicode character
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon size="tiny">💣</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon size="medium">💣</Icon>
 	},
 	{
 		locale: 'ar-SA',
@@ -42,6 +87,14 @@ const IconTests = [
 	{
 		locale: 'ar-SA',
 		component: <Icon>{buttonstate}</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon size="tiny">{buttonstate}</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon size="medium">{buttonstate}</Icon>
 	},
 	{
 		locale: 'ar-SA',
@@ -54,7 +107,31 @@ const IconTests = [
 	},
 	{
 		locale: 'ar-SA',
+		component: <Icon flip="vertical" size="tiny">question</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon flip="vertical" size="medium">question</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon flip="vertical" size="large">question</Icon>
+	},
+	{
+		locale: 'ar-SA',
 		component: <Icon flip="horizontal">question</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon flip="horizontal" size="tiny">question</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon flip="horizontal" size="medium">question</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon flip="horizontal" size="large">question</Icon>
 	},
 	{
 		locale: 'ar-SA',
@@ -62,16 +139,30 @@ const IconTests = [
 	},
 	{
 		locale: 'ar-SA',
-		component: <Icon flip="both" size="large">repeat</Icon>
+		component: <Icon flip="both" size="tiny">repeat</Icon>
 	},
 	{
 		locale: 'ar-SA',
-		component: <Icon size="large">plus</Icon>
+		component: <Icon flip="both" size="medium">repeat</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		component: <Icon flip="both" size="large">repeat</Icon>
 	},
 	{
 		locale: 'ar-SA',
 		textSize: 'large',
 		component: <Icon>plus</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		textSize: 'large',
+		component: <Icon size="tiny">plus</Icon>
+	},
+	{
+		locale: 'ar-SA',
+		textSize: 'large',
+		component: <Icon size="medium">plus</Icon>
 	},
 	{
 		locale: 'ar-SA',
