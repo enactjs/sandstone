53b35715c4d4d53874d6c5c254c8c4f1019d1f90
Teck Liew <teck.liew@lge.com>
2020-02-21 15:50:04 -0800

    add unit tests

--- Popup/tests/Popup-specs.js
+++ Popup/tests/Popup-specs.js
@@ -8,7 +8,7 @@ import css from '../Popup.module.less';
 const FloatingLayerController = FloatingLayerDecorator('div');
 
 describe('Popup specs', () => {
-	test('should be rendered opened if open is set to true', () => {
+	it('should be rendered opened if open is set to true', () => {
 		const popup = mount(
 			<FloatingLayerController>
 				<Popup open><div>popup</div></Popup>
@@ -21,7 +21,7 @@ describe('Popup specs', () => {
 		expect(actual).toBe(expected);
 	});
 
-	test('should not be rendered if open is set to false', () => {
+	it('should not be rendered if open is set to false', () => {
 		const popup = mount(
 			<FloatingLayerController>
 				<Popup><div>popup</div></Popup>
@@ -34,7 +34,7 @@ describe('Popup specs', () => {
 		expect(actual).toBe(expected);
 	});
 
-	test('should set role to alert by default', () => {
+	it('should set role to alert by default', () => {
 		const popup = shallow(
 			<PopupBase><div>popup</div></PopupBase>
 		);
@@ -45,7 +45,7 @@ describe('Popup specs', () => {
 		expect(actual).toBe(expected);
 	});
 
-	test('should allow role to be overridden', () => {
+	it('should allow role to be overridden', () => {
 		const popup = shallow(
 			<PopupBase role="dialog"><div>popup</div></PopupBase>
 		);
@@ -56,7 +56,7 @@ describe('Popup specs', () => {
 		expect(actual).toBe(expected);
 	});
 
-	test('should set "data-webos-voice-exclusive" when popup is open', () => {
+	it('should set "data-webos-voice-exclusive" when popup is open', () => {
 		const popup = mount(
 			<FloatingLayerController>
 				<Popup open><div>popup</div></Popup>
@@ -69,7 +69,7 @@ describe('Popup specs', () => {
 		expect(actual).toBe(expected);
 	});
 
-	test('should set "data-webos-voice-disabled" when voice control is disabled', () => {
+	it('should set "data-webos-voice-disabled" when voice control is disabled', () => {
 		const popup = shallow(
 			<PopupBase open data-webos-voice-disabled><div>popup</div></PopupBase>
 		);
@@ -78,5 +78,113 @@ describe('Popup specs', () => {
 		const actual = popup.find(`.${css.popup}`).prop('data-webos-voice-disabled');
 
 		expect(actual).toBe(expected);
+	});
+
+	describe('with position bottom', function () {
+		it('should have bottom class', () => {
+			const popup = shallow(
+				<PopupBase open position="bottom"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.find(`.${css.popup}`).prop('className').split(' ')).toContain('bottom');
+		});
+
+		it('should have bottom class in popup transition container', () => {
+			const popup = shallow(
+				<PopupBase open position="bottom"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.prop('className').split(' ')).toContain('bottom');
+		});
+	});
+
+	describe('with position left', function () {
+		it('should have left class', () => {
+			const popup = shallow(
+				<PopupBase open position="left"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.find(`.${css.popup}`).prop('className').split(' ')).toContain('left');
+		});
+
+		it('should have left class in popup transition container', () => {
+			const popup = shallow(
+				<PopupBase open position="left"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.prop('className').split(' ')).toContain('left');
+		});
+	});
+
+	describe('with position top', function () {
+		it('should have right class', () => {
+			const popup = shallow(
+				<PopupBase open position="right"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.find(`.${css.popup}`).prop('className').split(' ')).toContain('right');
+		});
+
+		it('should have right class in popup transition container', () => {
+			const popup = shallow(
+				<PopupBase open position="right"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.prop('className').split(' ')).toContain('right');
+		});
+	});
+
+	describe('with position top', function () {
+		it('should have top class', () => {
+			const popup = shallow(
+				<PopupBase open position="top"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.find(`.${css.popup}`).prop('className').split(' ')).toContain('top');
+		});
+
+		it('should have top class popup transition container', () => {
+			const popup = shallow(
+				<PopupBase open position="top"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.prop('className').split(' ')).toContain('top');
+		});
+	});
+
+	describe('with position center', function () {
+		it('should have center class', () => {
+			const popup = shallow(
+				<PopupBase open position="center"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.find(`.${css.popup}`).prop('className').split(' ')).toContain('center');
+		});
+
+		it('should have center class popup transition container', () => {
+			const popup = shallow(
+				<PopupBase open position="center"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.prop('className').split(' ')).toContain('center');
+		});
+	});
+
+	describe('with position fullscreen', function () {
+		it('should have fullscreen class', () => {
+			const popup = shallow(
+				<PopupBase open position="fullscreen"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.find(`.${css.popup}`).prop('className').split(' ')).toContain('fullscreen');
+		});
+
+		it('should have fullscreen class popup transition container', () => {
+			const popup = shallow(
+				<PopupBase open position="fullscreen"><div>popup</div></PopupBase>
+			);
+
+			expect(popup.prop('className').split(' ')).toContain('fullscreen');
+		});
 	});
 });
