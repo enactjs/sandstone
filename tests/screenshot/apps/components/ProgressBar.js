0de76b71a61669d2ae13a541bee6f3852c030764
Lucie Roy <lucie.roy@lge.com>
2020-02-21 13:29:58 -0800

    added comments to match Jira and tests

--- tests/screenshot/apps/components/ProgressBar.js
+++ tests/screenshot/apps/components/ProgressBar.js
@@ -2,7 +2,7 @@ import ProgressBar from '../../../../ProgressBar';
 import React from 'react';
 
 // TODO: RTL
-
+// [GT-28227] - generic to test Progress Bar and Background Progress Bar Colors - no end marked for this test
 const ProgressBarTests = [
 	<ProgressBar />,
 	<ProgressBar highlighted />,
@@ -12,11 +12,14 @@ const ProgressBarTests = [
 	<ProgressBar highlighted progress={1} />,
 	<ProgressBar backgroundProgress={0.5} />,
 	<ProgressBar backgroundProgress={1} />,
-	<ProgressBar backgroundProgress={0.25} progress={0.5} />,
-	<ProgressBar backgroundProgress={0.5} progress={0.25} />,
+	<ProgressBar backgroundProgress={0.25} progress={0.5} />,  // [GT-28229] - Step 6
+	<ProgressBar backgroundProgress={0.5} progress={0.25} />,  // [GT-28229] - Step 5
+	<ProgressBar backgroundProgress={0.5} progress={0.5} />,
 	<ProgressBar highlighted backgroundProgress={0.5} />,
 	<ProgressBar highlighted backgroundProgress={1} />,
+	// [GT-28224]
 	<ProgressBar highlighted backgroundProgress={0.5} progress={0.25} />,
+	// end of [GT-28224]
 	<ProgressBar tooltip />,
 	<ProgressBar tooltip progress={0.5} />,
 	<ProgressBar tooltip percent progress={0.5} />,
@@ -28,13 +31,13 @@ const ProgressBarTests = [
 	<ProgressBar progress={0.25} progressAnchor={0.5} tooltip />,
 	<ProgressBar orientation="vertical" />,
 	<ProgressBar orientation="vertical" highlighted />,
-	<ProgressBar orientation="vertical" progress={0.5} />,
+	<ProgressBar orientation="vertical" progress={0.5} />, // [GT-28230]
 	<ProgressBar orientation="vertical" progress={1} />,
 	<ProgressBar orientation="vertical" highlighted progress={0.5} />,
 	<ProgressBar orientation="vertical" highlighted progress={1} />,
 	<ProgressBar orientation="vertical" backgroundProgress={0.5} />,
 	<ProgressBar orientation="vertical" backgroundProgress={1} />,
-	<ProgressBar orientation="vertical" backgroundProgress={0.25} progress={0.5} />,
+	<ProgressBar orientation="vertical" backgroundProgress={0.25} progress={0.5} />, // [GT-28230]
 	<ProgressBar orientation="vertical" backgroundProgress={0.5} progress={0.25} />,
 	<ProgressBar orientation="vertical" highlighted backgroundProgress={0.5} />,
 	<ProgressBar orientation="vertical" highlighted backgroundProgress={1} />,
@@ -45,6 +48,98 @@ const ProgressBarTests = [
 	<ProgressBar orientation="vertical" backgroundProgress={0.75} progress={0.25} progressAnchor={0.5} />,
 	<ProgressBar orientation="vertical" backgroundProgress={0.1} progress={0.25} progressAnchor={0.2} />,
 	<ProgressBar orientation="vertical" progress={0.25} progressAnchor={0.5} tooltip />,
+	// *************************************************************
+	// tooltip - all positions
+	// *************************************************************
+	// Change 'position' (location and direction) dynamically of Tooltip - [GT-28225]
+	{
+		component: <ProgressBar tooltip position="above" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="above left" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="above right" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="above before" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="above after" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="before" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="left" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="right" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="after" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="below" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="below left" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="below right" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="below before" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	{
+		component: <ProgressBar tooltip position="below after" progress={0.4} backgroundProgress={0.5} />,
+		wrapper: {
+			padded: true
+		}
+	},
+	// end of [GT-28225]
+	// *************************************************************
+	// end of tooltip - all positions
+	// *************************************************************
 	<ProgressBar orientation="radial" />,
 	<ProgressBar orientation="radial" highlighted />,
 	<ProgressBar orientation="radial" progress={0.5} />,
@@ -63,6 +158,15 @@ const ProgressBarTests = [
 	<ProgressBar orientation="radial" backgroundProgress={0.25} progress={0.75} progressAnchor={0.5} />,
 	<ProgressBar orientation="radial" backgroundProgress={0.75} progress={0.25} progressAnchor={0.5} />,
 	<ProgressBar orientation="radial" backgroundProgress={0.1} progress={0.25} progressAnchor={0.2} />,
-	<ProgressBar orientation="radial" progress={0.25} progressAnchor={0.5} tooltip />
+	<ProgressBar orientation="radial" progress={0.25} progressAnchor={0.5} tooltip />,
+	// *************************************************************
+	// locale = 'ar-SA'
+	// *************************************************************
+	// [GT-28226]
+	{
+		locale: 'ar-SA',
+		component: <ProgressBar progress={0.4} backgroundProgress={0.5} />
+	}
+	// end of [GT-28226]
 ];
 export default ProgressBarTests;
