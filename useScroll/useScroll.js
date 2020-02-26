243b2178a07c40b3ca40619811d785e937de296b
YunBum Sung <yb.sung@lge.com>
2020-02-26 11:10:09 +0900

    Remove redundant padding
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- useScroll/useScroll.js
+++ useScroll/useScroll.js
@@ -388,7 +388,9 @@ const useScroll = (props) => {
 		className: [
 			css.scroll,
 			scrollContainerHandle.current.rtl ? css.rtl : null,
-			overscrollCss.scroll
+			overscrollCss.scroll,
+			(props.direction === 'horizontal' || props.direction === 'both') && (props.horizontalScrollbar !== 'hidden') ? css.horizontal : null,
+			(props.direction === 'vertical' || props.direction === 'both') && (props.verticalScrollbar !== 'hidden') ? css.vertical : null
 		],
 		'data-spotlight-container': spotlightContainer,
 		'data-spotlight-container-disabled': spotlightContainerDisabled,
