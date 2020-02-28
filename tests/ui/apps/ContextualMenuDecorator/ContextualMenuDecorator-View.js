676dd2a7347f919c1984c1f771e8ba3897467f2f
Lucie Roy <lucie.roy@lge.com>
2020-02-28 14:57:37 -0800

    added tests and updated comments to ContextualMenuDecorator

--- tests/ui/apps/ContextualMenuDecorator/ContextualMenuDecorator-View.js
+++ tests/ui/apps/ContextualMenuDecorator/ContextualMenuDecorator-View.js
@@ -1,21 +1,22 @@
+
 import Button from '../../../../Button';
 import ContextualMenuDecorator from '../../../../ContextualMenuDecorator';
 import ThemeDecorator from '../../../../ThemeDecorator';
 import React, {Component} from 'react';
 import spotlight from '@enact/spotlight';
-
 const ContextualButton = ContextualMenuDecorator(Button);
 const menuItems = ['Option 1', 'Option 2', 'Option 3'];
-
 // NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
 // runs the same way
 spotlight.setPointerMode(false);
-
 class app extends Component {
-
+	state = {
+		open: true
+	};
+	clickHandler = (st) =>  this.setState(st);
 	render () {
 		return (
-			<div>
+			<div {...this.props}>
 				<ContextualButton
 					id="button1"
 					menuItems={menuItems}
@@ -26,7 +27,9 @@ class app extends Component {
 				<ContextualButton
 					id="button2"
 					menuItems={menuItems}
-					open
+					onClose={() => this.clickHandler({open: false})}
+					onOpen={() => this.clickHandler({open: true})}
+					open={this.state.open}
 					popupSpotlightId="menu2"
 				>
 					Contextual Menu Button
@@ -35,5 +38,4 @@ class app extends Component {
 		);
 	}
 }
-
 export default ThemeDecorator(app);
--- tests/ui/specs/ContextualMenuDecorator/ContextualMenuDecorator-specs.js
+++ tests/ui/specs/ContextualMenuDecorator/ContextualMenuDecorator-specs.js
@@ -8,6 +8,7 @@ describe('ContextualMenuDecorator', function () {
 
 	const {
 		button1,
+		button2,
 		menu1,
 		menu2
 	} = Page.components;
@@ -27,38 +28,49 @@ describe('ContextualMenuDecorator', function () {
 		});
 
 		describe('using 5-way', function () {
+			// The menu is open and the first item has focus
 			beforeEach(function () {
 				Page.spotlightSelect();
 			});
 
+			// [GT-28276] - The menu expands (verify step 3 - part 1)
 			it('should have the menu on select', function () {
 				expect(menu1.isMenuExist).to.be.true();
 			});
 
+			// [GT-28276] - Spotlight is on the first item (verify step 3 - part 2)
 			it('should move focus to first menu item on select', function () {
 				expect(menu1.item(0).hasFocus()).to.be.true();
 			});
 
+			// [GT-28284] - The *Contextual Button* button closes. Spotlight is on *Contextual Button* button (verify step 4)
 			it('should dismiss the menu on 5-way left from menu item', function () {
 				Page.spotlightLeft();
 				expect(menu1.isMenuExist).to.be.false();
+				expect(button1.self.hasFocus()).to.be.true();
 			});
 
+			// [GT-28284] - The *Contextual Button* button closes. Spotlight is on *Contextual Button* button (verify step 5)
 			it('should dismiss the menu on 5-way right from menu item', function () {
 				Page.spotlightRight();
 				expect(menu1.isMenuExist).to.be.false();
+				expect(button1.self.hasFocus()).to.be.true();
 			});
 
+			// [GT-28284] - The *Contextual Button* button closes. Spotlight is on *Contextual Button* button (verify step 6)
 			it('should dismiss the menu on 5-way up from first menu item', function () {
 				Page.spotlightUp();
 				expect(menu1.isMenuExist).to.be.false();
+				expect(button1.self.hasFocus()).to.be.true();
 			});
 
+			// [GT-28285] - Spotlight is on the second item. (verify step 4)
 			it('should move focus to the next menu item on 5-way down', function () {
 				Page.spotlightDown();
 				expect(menu1.item(1).hasFocus()).to.be.true();
 			});
 
+			// [GT-28285] - The *Contextual Button* button closes. Spotlight is on *Contextual Button* button. (verify step 5)
 			it('should dismiss the menu and move focus back to activator on close', function () {
 				Page.spotlightDown();
 				Page.spotlightDown();
@@ -67,8 +79,29 @@ describe('ContextualMenuDecorator', function () {
 				expect(button1.self.hasFocus()).to.be.true();
 			});
 		});
+
+		describe('using pointer', function () {
+			// [GT-28282]
+			// The 3 steps below are needed for the test to pass
+			it('should keep Spotlight on button when menu opens with pointer', function () {
+				button1.focus();
+				button1.self.click(); // this will close menu2 and open menu1
+				button1.focus();
+				button1.self.click(); // this will close menu1
+				button1.focus();
+				button1.self.click(); // this will open menu1
+				expect(button1.self.hasFocus()).to.be.true();  // (verify step 3)
+				expect(menu1.isMenuExist).to.be.true();  // (verify step 3)
+				expect(menu1.item(0).hasFocus()).to.be.false();  // Spotlight is Not on the first item. (verify step 3)
+				button1.self.click(); // this will close menu1
+				button1.focus();
+				expect(menu1.isMenuExist).to.be.false(); 	// (verify step 4)
+				expect(button1.self.hasFocus()).to.be.true();	// (verify step 4)
+			});
+		});
 	});
 
+	// Menu with Button 2 is explicitly set as opened. It is using an open prop. It is there to test that if a user specifies that prop, they can programmatically control when it closes.
 	describe('using open', function () {
 		it('should have the menu on start', function () {
 			expect(menu2.isMenuExist).to.be.true();
@@ -78,4 +111,5 @@ describe('ContextualMenuDecorator', function () {
 			expect(menu2.item(0).hasFocus()).to.be.true();
 		});
 	});
+
 });
