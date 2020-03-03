17b66eba06174cbbf2cd0923cf1f3353dc5b443d
Blake Stephens <blake.stephens@lge.com>
2020-03-03 13:52:12 -0800

    Updated story to use `View` export
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/sampler/stories/default/WizardPanel.js
+++ samples/sampler/stories/default/WizardPanel.js
@@ -8,7 +8,7 @@ import CheckboxItem from '@enact/sandstone/CheckboxItem';
 import Icon from '@enact/sandstone/Icon';
 import Item from '@enact/sandstone/Item';
 import {Scroller} from '@enact/sandstone/Scroller';
-import {WizardPanel} from '@enact/sandstone/Panels';
+import {WizardPanel, View} from '@enact/sandstone/Panels';
 
 WizardPanel.displayName = 'WizardPanel';
 
@@ -19,7 +19,7 @@ storiesOf('Sandstone', module)
 			<WizardPanel
 				onSelect={action('onSelect')}
 			>
-				<WizardPanel.View footer="Footer in View 1" subtitle="A subtitle for View 1" title="WizardPanel View 1">
+				<View footer="Footer in View 1" subtitle="A subtitle for View 1" title="WizardPanel View 1">
 					<Scroller>
 						<BodyText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus faucibus ornare suspendisse sed nisi. Vestibulum sed arcu non odio euismod lacinia at quis. Elementum eu facilisis sed odio morbi quis commodo. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Neque ornare aenean euismod elementum. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Tincidunt augue i\nterdum velit euismod. Nunc sed augue lacus viverra vitae congue eu consequat. Ultricies integer quis auctor elit sed vulputate. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Elit sed vulputate mi sit amet mauris commodo. Ipsum consequat nisl vel pretium lectus. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Nulla facilisi morbi tempus iaculis urna id volutpat. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus faucibus ornare suspendisse sed nisi. Vestibulum sed arcu non odio euismod lacinia at quis. Elementum eu facilisis sed odio morbi quis commodo. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Neque ornare aenean euismod elementum. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Tincidunt augue interdum velit euismod. Nunc sed augue lacus viverra vitae congue eu consequat. Ultricies integer quis auctor elit sed vulputate. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Elit sed vulputate mi sit amet mauris commodo. Ipsum consequat nisl vel pretium lectus. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Nulla facilisi morbi tempus iaculis urna id volutpat. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus faucibus ornare suspendisse sed nisi. Vestibulum sed arcu non odio euismod lacinia at quis. Elementum eu facilisis sed odio morbi quis commodo. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Neque ornare aenean euismod elementum. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Tincidunt augue interdum velit euismod. Nunc sed augue lacus viverra vitae congue eu consequat. Ultricies integer quis auctor elit sed vulputate. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Elit sed vulputate mi sit amet mauris commodo. Ipsum consequat nisl vel pretium lectus. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Nulla facilisi morbi tempus iaculis urna id volutpat. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus faucibus ornare suspendisse sed nisi. Vestibulum sed arcu non odio euismod lacinia at quis. Elementum eu facilisis sed odio morbi quis commodo. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Neque ornare aenean euismod elementum. Iaculis nunc sed augue lacus viverra vitae congue eu consequat. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Tincidunt augue interdum velit euismod. Nunc sed augue lacus viverra vitae congue eu consequat. Ultricies integer quis auctor elit sed vulputate. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Elit sed vulputate mi sit amet mauris commodo. Ipsum consequat nisl vel pretium lectus. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Nulla facilisi morbi tempus iaculis urna id volutpat. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus.</BodyText>
 					</Scroller>
@@ -27,8 +27,8 @@ storiesOf('Sandstone', module)
 						<Button>OK</Button>
 						<Button>Cancel</Button>
 					</buttons>
-				</WizardPanel.View>
-				<WizardPanel.View subtitle="A subtitle for View 2" title="WizardPanel View 2">
+				</View>
+				<View subtitle="A subtitle for View 2" title="WizardPanel View 2">
 					<BodyText>Several buttons!</BodyText>
 					<Button icon="list">Button A</Button>
 					<Button icon="gear">Button B</Button>
@@ -41,23 +41,23 @@ storiesOf('Sandstone', module)
 					<footer>
 						<CheckboxItem inline>Confirm</CheckboxItem>
 					</footer>
-				</WizardPanel.View>
-				<WizardPanel.View footer="Footer in View 3" subtitle="A subtitle for View 3" title="WizardPanel View 3">
+				</View>
+				<View footer="Footer in View 3" subtitle="A subtitle for View 3" title="WizardPanel View 3">
 					<Item>
 						<slotBefore>
 							<Icon>resumeplay</Icon>
 						</slotBefore>
 						Hello Item
 					</Item>
-				</WizardPanel.View>
-				<WizardPanel.View footer="Footer in View 4" subtitle="A subtitle for View 4" title="WizardPanel View 4">
+				</View>
+				<View footer="Footer in View 4" subtitle="A subtitle for View 4" title="WizardPanel View 4">
 					<Icon>resumeplay</Icon>
 					<BodyText>A simple view</BodyText>
 					<buttons>
 						<Button>OK</Button>
 						<Button>Cancel</Button>
 					</buttons>
-				</WizardPanel.View>
+				</View>
 			</WizardPanel>
 		),
 		{
