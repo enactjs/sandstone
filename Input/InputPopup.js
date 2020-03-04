a431841f7770e29204935b1dd43272eab0569ef9
Blake Stephens <blake.stephens@lge.com>
2020-03-04 14:56:06 -0800

    It's not necessary to specify type
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- Input/InputPopup.js
+++ Input/InputPopup.js
@@ -1,6 +1,5 @@
 import kind from '@enact/core/kind';
 import deprecate from '@enact/core/internal/deprecate';
-import PropTypes from 'prop-types';
 import React from 'react';
 
 import Input from './Input';
@@ -8,19 +7,15 @@ import Input from './Input';
 const InputPopupBase = kind({
 	name: 'InputPopup',
 
-	propTypes: {
-		type: PropTypes.oneOf(['number', 'password'])
-	},
-
-	render: ({type, ...rest}) => {
+	render: ({...rest}) => {
 		deprecate({
 			name: 'sandstone/InputPopup',
 			replacedBy: 'sandstone/Input',
-			message: 'Use `sandstone/Input` with `type="number"` or `type="passwordnumber"'
+			message: 'Use `sandstone/Input`'
 		});
 
 		return (
-			<Input {...rest} type={type === 'password' ? 'passwordnumber' : type} />
+			<Input {...rest} />
 		);
 	}
 });
@@ -31,7 +26,6 @@ const InputPopupBase = kind({
  * Usage:
  * ```
  * <InputPopup
- *   length={4}
  *   onComplete={this.handleInputComplete}
  *   placeholder="Placeholder"
  *   popupType="overlay"
