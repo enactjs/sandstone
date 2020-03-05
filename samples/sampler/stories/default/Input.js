df80624d3e756cbb2a82c63a5b8dd20e99e9658a
Blake Stephens <blake.stephens@lge.com>
2020-03-05 15:00:39 -0800

    Rearrange stories
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/sampler/stories/default/Input.js
+++ samples/sampler/stories/default/Input.js
@@ -1,44 +1,39 @@
 import {action} from '@enact/storybook-utils/addons/actions';
-import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
+import {boolean, number, select, text} from '@enact/storybook-utils/addons/knobs';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import {InputField, InputFieldBase} from '@enact/sandstone/Input';
+import Input, {InputBase} from '@enact/sandstone/Input';
 
-import icons from './icons';
+Input.displayName = 'Input';
+const Config = mergeComponentMetadata('Input', InputBase, Input);
 
-const iconNames = ['', ...icons];
-
-InputField.displayName = 'InputField';
-const Config = mergeComponentMetadata('InputField', InputFieldBase, InputField);
-
-// Set up some defaults for info and knobs
 const prop = {
-	type: ['text', 'number', 'password']
+	popupType: ['fullscreen', 'overlay'],
+	type: ['text', 'password', 'number', 'passwordnumber']
 };
 
 storiesOf('Sandstone', module)
 	.add(
-		'InputField',
+		'Input',
 		() => (
-			<InputField
-				autoFocus={boolean('autoFocus', Config)}
-				onChange={action('onChange')}
-				disabled={boolean('disabled', Config)}
-				dismissOnEnter={boolean('dismissOnEnter', Config)}
-				iconAfter={select('iconAfter', iconNames, Config)}
-				iconBefore={select('iconBefore', iconNames, Config)}
-				invalid={boolean('invalid', Config)}
-				invalidMessage={text('invalidMessage', Config)}
-				placeholder={text('placeholder', Config)}
-				size={select('size', ['small', 'large'], Config)}
-				type={select('type', prop.type, Config, prop.type[0])}
-			/>
+			<div>
+				<Input
+					disabled={boolean('disabled', Config)}
+					onComplete={action('onComplete')}
+					length={number('length', Config, {range: true, min: 1, max: 10}, 4)}
+					placeholder={text('placeholder', Config, 'placeholder text')}
+					subtitle={text('subtitle', Config, 'Title Below Text')}
+					title={text('title', Config, 'Title Text')}
+					type={select('type', prop.type, Config)}
+					popupType={select('popupType', prop.popupType, Config)}
+				/>
+			</div>
 		),
 		{
 			info: {
-				text: 'The basic InputField'
+				text: 'Basic usage of Input'
 			}
 		}
 	);
---
+++ samples/sampler/stories/default/InputField.js
@@ -0,0 +1,44 @@
+import {action} from '@enact/storybook-utils/addons/actions';
+import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
+import {mergeComponentMetadata} from '@enact/storybook-utils';
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import {InputField, InputFieldBase} from '@enact/sandstone/Input';
+
+import icons from './icons';
+
+const iconNames = ['', ...icons];
+
+InputField.displayName = 'InputField';
+const Config = mergeComponentMetadata('InputField', InputFieldBase, InputField);
+
+// Set up some defaults for info and knobs
+const prop = {
+	type: ['text', 'number', 'password']
+};
+
+storiesOf('Sandstone', module)
+	.add(
+		'InputField',
+		() => (
+			<InputField
+				autoFocus={boolean('autoFocus', Config)}
+				onChange={action('onChange')}
+				disabled={boolean('disabled', Config)}
+				dismissOnEnter={boolean('dismissOnEnter', Config)}
+				iconAfter={select('iconAfter', iconNames, Config)}
+				iconBefore={select('iconBefore', iconNames, Config)}
+				invalid={boolean('invalid', Config)}
+				invalidMessage={text('invalidMessage', Config)}
+				placeholder={text('placeholder', Config)}
+				size={select('size', ['small', 'large'], Config)}
+				type={select('type', prop.type, Config, prop.type[0])}
+			/>
+		),
+		{
+			info: {
+				text: 'The basic InputField'
+			}
+		}
+	);
