66a09ed7651c6f01a03acc799951b9d3726873af
Ryan Duffy <ryan.duffy@lge.com>
2020-03-06 13:17:04 -0800

    add center popup sst
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- tests/screenshot/apps/components/Popup.js
+++ tests/screenshot/apps/components/Popup.js
@@ -5,6 +5,7 @@ const PopupTests = [
 	<Popup open>Popup!</Popup>,
 	//GT-28270,
 	<Popup open position="fullscreen">Fullscreen Popup!</Popup>,
+	<Popup open position="center">Center Popup!</Popup>,
 	<Popup open position="left">Left Popup!</Popup>,
 	<Popup open position="right">Right Popup!</Popup>,
 	<Popup open position="top">Top Popup!</Popup>,
