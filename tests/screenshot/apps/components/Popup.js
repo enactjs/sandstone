f6aef015bdb3015c7ff5af27741f96190fb302be
Teck Liew <teck.liew@lge.com>
2020-03-05 16:39:07 -0800

    add screenshot test for position prop

--- tests/screenshot/apps/components/Popup.js
+++ tests/screenshot/apps/components/Popup.js
@@ -4,6 +4,11 @@ import React from 'react';
 const PopupTests = [
 	<Popup>Popup!</Popup>,
 	<Popup open>Popup!</Popup>,
+	//GT-28270,
+	<Popup open position="fullscreen">Fullscreen Popup!</Popup>,
+	<Popup open position="left">Left Popup!</Popup>,
+	<Popup open position="right">Right Popup!</Popup>,
+	<Popup open position="top">Top Popup!</Popup>,
 	// *************************************************************
 	// locale = 'ar-SA'
 	// *************************************************************
