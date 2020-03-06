2ae241a5abbd38b8818909f1e8b51d63ae419d89
Teck Liew <teck.liew@lge.com>
2020-03-06 15:40:45 -0800

    fix comment

--- tests/screenshot/apps/components/Popup.js
+++ tests/screenshot/apps/components/Popup.js
@@ -3,12 +3,15 @@ import React from 'react';
 
 const PopupTests = [
 	<Popup open>Popup!</Popup>,
-	//GT-28270,
+
+	// [GT-28270]
 	<Popup open position="fullscreen">Fullscreen Popup!</Popup>,
 	<Popup open position="center">Center Popup!</Popup>,
 	<Popup open position="left">Left Popup!</Popup>,
 	<Popup open position="right">Right Popup!</Popup>,
 	<Popup open position="top">Top Popup!</Popup>,
+	// End of [GT-28270]
+
 	// *************************************************************
 	// locale = 'ar-SA'
 	// *************************************************************
