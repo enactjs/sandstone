8003fd6c4da7eabcecb62a127a39d8871252cc68
YunBum Sung <yb.sung@lge.com>
2020-03-10 10:53:26 +0900

    Cleanup
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- useScroll/useScroll.js
+++ useScroll/useScroll.js
@@ -294,7 +294,36 @@ const useScroll = (props) => {
 
 	const [themeScrollContentHandle, setThemeScrollContentHandle] = useThemeScrollContentHandle();
 
-	const scrollContainerHandle = useRef();
+	const scrollContainerHandle = useRef({
+		animator: null,
+		applyOverscrollEffect: null,
+		bounds: null,
+		calculateDistanceByWheel: null,
+		canScrollHorizontally: null,
+		canScrollVertically: null,
+		checkAndApplyOverscrollEffect: null,
+		getScrollBounds: null,
+		isDragging: null,
+		isScrollAnimationTargetAccumulated: null,
+		isUpdatedScrollThumb: null,
+		lastInputType: null,
+		rtl: null,
+		scrollBounds: null,
+		scrollHeight: null,
+		scrolling: null,
+		scrollLeft: null,
+		scrollPos: null,
+		scrollTo: null,
+		scrollToAccumulatedTarget: null,
+		scrollToInfo: null,
+		scrollTop: null,
+		setOverscrollStatus: null,
+		showThumb: null,
+		start: null,
+		startHidingThumb: null,
+		stop: null,
+		wheelDirection: null
+	});
 
 	const setScrollContainerHandle = (handle) => {
 		scrollContainerHandle.current = handle;
