863ad72144fca33048b60a085dc6abd422d6e5a7
YunBum Sung <yb.sung@lge.com>
2020-03-12 07:49:31 +0900

    Cleanup
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- useScroll/useScroll.js
+++ useScroll/useScroll.js
@@ -188,9 +188,6 @@ const useThemeScroll = (props, instances, context, assignProperties) => {
 
 	// Callback for scroller updates; calculate and, if needed, scroll to new position based on focused item.
 	function handleScrollerUpdate () {
-		// const bounds = scrollContainerHandle.current.getScrollBounds();
-		// const scrollHeight = bounds && bounds.scrollHeight || 0;
-
 		if (scrollContainerHandle.current.scrollToInfo === null) {
 			const scrollHeight = scrollContainerHandle.current.getScrollBounds().scrollHeight;
 
