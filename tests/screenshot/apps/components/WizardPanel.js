7b0f477956d21fa8a9257586646b4d1dce4f382b
Teck Liew <teck.liew@lge.com>
2020-03-16 10:19:47 -0700

    set wrapper to full

--- tests/screenshot/apps/components/WizardPanel.js
+++ tests/screenshot/apps/components/WizardPanel.js
@@ -1,42 +1,60 @@
 import {WizardPanel, View} from '../../../../Panels';
 import React from 'react';
 
-const wizardPanelStyle = {height: '1080px', width: '1920px'};
-
 const WizardPanelTests = [
-	<WizardPanel style={wizardPanelStyle}>
-		<View>
-			View 1
-		</View>
-		<View>
-			View 2
-		</View>
-	</WizardPanel>,
-	<WizardPanel index={1} style={wizardPanelStyle}>
-		<View>
-			View 1
-		</View>
-		<View>
-			View 2
-		</View>
-	</WizardPanel>,
+	{
+		component: (
+			<WizardPanel>
+				<View>
+					View 1
+				</View>
+				<View>
+					View 2
+				</View>
+			</WizardPanel>
+		),
+		wrapper: {
+			full: true
+		}
+	},
+	{
+		component: (
+			<WizardPanel index={1}>
+				<View>
+					View 1
+				</View>
+				<View>
+					View 2
+				</View>
+			</WizardPanel>
+		),
+		wrapper: {
+			full: true
+		}
+	},
 	// [GT-28274] - 'nextButtonText' and 'prevButtonText' to show properly
-	<WizardPanel
-		index={1}
-		nextButtonText="nextButtonText"
-		prevButtonText="prevButtonText"
-		style={wizardPanelStyle}
-		title="WizardPanel"
-	>
-		<View>
-			View 1
-		</View>
-		<View>
-			View 2
-		</View>
-		<View>
-			View 3
-		</View>
-	</WizardPanel>
+	{
+		component: (
+			<WizardPanel
+				index={1}
+				nextButtonText="nextButtonText"
+				prevButtonText="prevButtonText"
+				title="WizardPanel"
+			>
+				<View>
+					View 1
+				</View>
+				<View>
+					View 2
+				</View>
+				<View>
+					View 3
+				</View>
+			</WizardPanel>
+		),
+		wrapper: {
+			full: true
+		}
+	}
 ];
 export default WizardPanelTests;
