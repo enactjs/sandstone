e7d71604b9f9e2475701e8d5e0a8a626eddb5b53
Mikyung Kim <mikyung27.kim@lge.com>
2020-03-19 14:26:16 +0900

    PLAT-101269: Add ImageItem story to sampler

---
+++ samples/sampler/stories/default/ImageItem.js
@@ -0,0 +1,42 @@
+import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
+import {mergeComponentMetadata} from '@enact/storybook-utils';
+import {ImageItem as UiImageItem} from '@enact/ui/ImageItem';
+import ri from '@enact/ui/resolution';
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import {ImageItem, ImageItemBase, ImageItemDecorator} from '@enact/sandstone/ImageItem';
+
+const Config = mergeComponentMetadata('ImageItem', UiImageItem, ImageItem, ImageItemBase, ImageItemDecorator);
+ImageItem.displayName = 'ImageItem';
+
+const prop = {
+	orientation: ['horizontal', 'vertical']
+};
+
+storiesOf('Sandstone', module)
+	.add(
+		'ImageItem',
+		() => (
+			<ImageItem
+				caption={text('caption', Config, 'ImageItem Caption')}
+				disabled={boolean('disabled', Config)}
+				imageIconSource={text('imageIconSource', Config, 'http://placehold.it/300x300/ffffff/ffffff&text=Icon')}
+				orientation={select('orientation', prop.orientation, Config)}
+				selected={boolean('selected', Config)}
+				selectionOverlayShowing={boolean('selectionOverlayShowing', Config)}
+				source={text('source', Config, 'http://placehold.it/600x600/7ed31d/ffffff&text=Image')}
+				style={{
+					position: 'absolute',
+					width: ri.scale(select('orientation', prop.orientation, Config) === 'vertical' ? 640 : 1020),
+					height: ri.scale(select('orientation', prop.orientation, Config) === 'vertical' ? 516 : 240)
+				}}
+				subCaption={text('subCaption', Config, 'ImageItem subCaption')}
+			/>
+		),
+		{
+			info: {
+				text: 'The basic ImageItem'
+			}
+		}
+	);
