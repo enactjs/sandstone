82fe01a21deb046234d529604c133d520842bab3
Renuka Atale <renuka.atale@lge.com>
2020-03-24 12:14:06 -0700

    fix the console error for index

--- samples/sampler/stories/default/Panels.js
+++ samples/sampler/stories/default/Panels.js
@@ -2,14 +2,32 @@ import {number} from '@enact/storybook-utils/addons/knobs';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import {Header, Panels, Panel, WizardPanel, TabLayout} from '@enact/sandstone/Panels';
+import {Header, Panels, Panel} from '@enact/sandstone/Panels';
 import Button from '@enact/sandstone/Button';
 import Item from '@enact/sandstone/Item';
+import Picker from '@enact/sandstone/Picker';
 import {Row} from '@enact/ui/Layout';
 
-import Icon from '@enact/sandstone/Icon';
+const pickerList = {
+	vegetables: [
+		'Celery',
+		'Carrot',
+		'Tomato',
+		'Onion',
+		'Broccoli',
+		'Spinach'
+	],
+	airports: [
+		'San Francisco International Airport Terminal 1',
+		'Milan Malpensa Airport Terminal 2',
+		'Paris-Charles De Gaulle Airport Terminal 3',
+		'Boston Logan Airport Terminal D',
+		'Tokyo Narita Airport Terminal 5',
+		'Heathrow Terminal 6',
+		'נמל התעופה בן גוריון טרמינל הבינלאומי'
+	]
+};
 
-const KsIcon = <Icon size="small">flag</Icon>;
 
 Header.displayName = 'Panels';
 
@@ -19,7 +37,7 @@ storiesOf('Sandstone', module)
 		'Panels',
 		() => {
 			const story = (
-				<Panels index={number('index', 0)}>
+				<Panels index={number('index', {}, {range: true, min: 0, max: 2, step: 1}, 0)}>
 					<Panel>
 						<Header title="Panel with Item view" />
 						<Item>
@@ -43,17 +61,25 @@ storiesOf('Sandstone', module)
 					</Panel>
 					<Panel>
 						<Header type="compact" title="Button Panel" />
-						<Row wrap>
+						<Row>
 							<Button
 								backgroundOpacity="transparent"
 								icon="flag"
 								minWidth={false}
 							/>
 							<Button size="small" alt="Normal">Button</Button>
-							<Button size="small" alt="Selected" selected>Button</Button>
 							<Button size="small" alt="Disabled" disabled>Button</Button>
 							<Button size="small" alt="Long Text">Super-duper long text string inside a button</Button>
-							<Button size="small" alt="With Icon" icon={KsIcon}>Button</Button>
+							<Button size="small" alt="With Icon" icon="flag">Button</Button>
+						</Row>
+					</Panel>
+					<Panel>
+						<Header type="compact" title="Picker Panel" />
+						<Row wrap>
+							<Picker alt="Basic" width="medium">{pickerList.airports}</Picker>
+							<Picker orientation="vertical" alt="Basic" width="medium">{pickerList.vegetables}</Picker>
+							<Picker joined alt="Basic" width="medium">{pickerList.vegetables}</Picker>
+							<Picker joined orientation="vertical" alt="Basic" width="medium">{pickerList.vegetables}</Picker>
 						</Row>
 					</Panel>
 				</Panels>
