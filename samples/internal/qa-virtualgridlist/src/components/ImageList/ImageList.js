c9b91d207d0998fe093c61f623cc189eb568d7aa
Dave Freeman <dave.freeman@lge.com>
2020-04-02 17:15:57 -0500

    migrate qa-virtualgridlist

---
+++ samples/internal/qa-virtualgridlist/src/components/ImageList/ImageList.js
@@ -0,0 +1,53 @@
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import React from 'react';
+import ri from '@enact/ui/resolution';
+import {VirtualGridList} from '../../../../../../VirtualList';
+
+import ImageItem from '../ImageItem';
+
+class ImageList extends React.Component {
+	static propTypes = {
+		dispatch: PropTypes.func,
+		imageitems: PropTypes.array,
+		minHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+		minWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+		spacing: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+	}
+
+	calculateOfSize = (size) => ri.scale(parseInt(size) || 0)
+
+	renderItem = ({...rest}) => (<ImageItem {...rest} />)
+
+	render = () => {
+		const
+			rest = Object.assign({}, this.props),
+			{imageitems, spacing, minHeight, minWidth} = this.props;
+
+		delete rest.dispatch;
+		delete rest.imageitems;
+		delete rest.minHeight;
+		delete rest.minWidth;
+		delete rest.spacing;
+
+		return (
+			<VirtualGridList
+				{...rest}
+				dataSize={imageitems.length}
+				itemRenderer={this.renderItem}
+				itemSize={{minHeight: this.calculateOfSize(minHeight), minWidth: this.calculateOfSize(minWidth)}}
+				scrollMode="translate"
+				spacing={this.calculateOfSize(spacing)}
+			/>
+		);
+	}
+}
+
+const mapStateToProps = ({data}) => ({
+	imageitems: data.dataOrder,
+	minHeight: data.minHeight,
+	minWidth: data.minWidth,
+	spacing: data.spacing
+});
+
+export default connect(mapStateToProps)(ImageList);
