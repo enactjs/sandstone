b107cb0cef60673aa4de05257e293420177089ed
Dave Freeman <dave.freeman@lge.com>
2020-04-02 13:49:55 -0500

    migrate perf-enact-virtuallist-native-with-different-item-size

---
+++ samples/internal/perf-enact-virtuallist-native-with-different-item-size/src/App.js
@@ -0,0 +1,56 @@
+import Dropdown from '../../../../Dropdown';
+import React, {Component} from 'react';
+import ThemeDecorator from '../../../../ThemeDecorator';
+
+import HorizontalDifferentWidthItemList from './views/HorizontalDifferentWidthItemList';
+import VerticalDifferentHeightItemList from './views/VerticalDifferentHeightItemList';
+import VerticalExpandableDifferentHeightItemList from './views/VerticalExpandableDifferentHeightItemList';
+
+const views = [
+	HorizontalDifferentWidthItemList,
+	VerticalDifferentHeightItemList,
+	VerticalExpandableDifferentHeightItemList
+];
+
+const viewNames = [
+	'HorizontalDifferentWidthItemList',
+	'VerticalDifferentHeightItemList',
+	'VerticalExpandableDifferentHeightItemList'
+];
+
+const defaultViewIndex = 0;
+
+class VirtualListSample extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			view: views[defaultViewIndex]
+		};
+	}
+
+	onSelect = ({selected}) => {
+		this.setState({view: views[selected]});
+	}
+
+	render () {
+		const View = this.state.view;
+
+		return (
+			<div {...this.props}>
+				<Dropdown
+					direction="down"
+					onSelect={this.onSelect}
+					size="large"
+					title={viewNames[defaultViewIndex]}
+					width="large"
+				>
+					{viewNames}
+				</Dropdown>
+				<View style={{height: '600px'}} />
+			</div>
+		);
+	}
+}
+
+export default ThemeDecorator(VirtualListSample);
---
+++ samples/internal/perf-enact-virtuallist-native-with-different-item-size/src/views/HorizontalDifferentWidthItemList.js
@@ -0,0 +1,111 @@
+import Item from '../../../../../Item';
+import React, {Component} from 'react';
+import ri from '@enact/ui/resolution';
+import {VirtualList} from '../../../../../VirtualList';
+
+const
+	languages = [
+		'한국어 - 한국',
+		'English - United States',
+		'Português - Brasil',
+		'Português - Portugal',
+		'Čeština - Česká republika',
+		'Dansk - Danmark',
+		'Deutsch - Deutschland',
+		'Ελληνική γλώσσα - Ελλάδα',
+		'Español - España',
+		'Suomi - Suomi'
+	],
+	numOfItems = 100,
+	fontSize = ri.scale(20),
+	oneLineSize = ri.scale(50),
+	lineHeight = `${oneLineSize - 10}px`,
+	spacing = 50;
+
+class DifferenctWidthItem extends Component {
+	itemStyleDefault = {
+		position: 'absolute',
+		height: '100%',
+		borderRight: 'solid 10px gray',
+		boxSizing: 'border-box',
+		fontSize,
+		lineHeight,
+		whiteSpace: 'pre'
+	}
+
+	innerItemStyleDefault = {
+		height: '100%',
+		writingMode: 'vertical-rl'
+	};
+
+	render () {
+		const
+			{index, items, style: itemStyleFromList, ...rest} = this.props,
+			{title: children, width} = items[index],
+			itemStyle = {...this.itemStyleDefault, ...itemStyleFromList, width: width + 'px'};
+
+
+		return (
+			<Item {...rest} style={itemStyle}>
+				<div style={this.innerItemStyleDefault}>
+					{children}
+				</div>
+			</Item>
+		);
+	}
+}
+
+class HorizontalDifferenctWidthItemList extends Component {
+	constructor (props) {
+		let
+			position = 0,
+			itemSize = [],
+			items = [];
+
+		super(props);
+
+		for (let i = 0; i < numOfItems; i++) {
+			const
+				numOfLines = Math.ceil(Math.random() * 6),
+				width = numOfLines * oneLineSize;
+
+			items.push({
+				title: (`${('00' + i).slice(-3)} | ${position}px | ${languages[i % 10]}\n`).repeat(numOfLines),
+				width
+			});
+			itemSize.push(width);
+			position += (width + spacing);
+		}
+
+		this.state = {
+			items,
+			itemSize
+		};
+	}
+
+	renderItem = (props) => {
+		return <DifferenctWidthItem {...props} />;
+	}
+
+	render () {
+		return (
+			<VirtualList
+				{...this.props}
+				childProps={{
+					items: this.state.items
+				}}
+				dataSize={this.state.items.length}
+				direction="horizontal"
+				focusableScrollbar
+				itemRenderer={this.renderItem}
+				itemSize={{
+					minSize: oneLineSize,
+					size: this.state.itemSize
+				}}
+				spacing={spacing}
+			/>
+		);
+	}
+}
+
+export default HorizontalDifferenctWidthItemList;
