9327d6d800a6be9c58426e237fceb293f4086602
Blake Stephens <blake.stephens@lge.com>
2020-04-03 13:52:14 -0700

    Fixed clicking in the negative space so events are sent to the scrim when expected; simplified the story and fixed button sizes.
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/sampler/stories/default/MiniPanels.js
+++ samples/sampler/stories/default/MiniPanels.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-no-bind */
+
 import {action} from '@enact/storybook-utils/addons/actions';
-import {boolean, number, select} from '@enact/storybook-utils/addons/knobs';
+import {boolean, select} from '@enact/storybook-utils/addons/knobs';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
@@ -7,7 +9,6 @@ import {storiesOf} from '@storybook/react';
 import {MiniPanels, Panel, Header} from '@enact/sandstone/Panels';
 import Scroller from '@enact/sandstone/Scroller';
 import ri from '@enact/ui/resolution';
-import BodyText from '@enact/sandstone/BodyText';
 import Button from '@enact/sandstone/Button';
 import Item from '@enact/sandstone/Item';
 import Slider from '@enact/sandstone/Slider';
@@ -34,7 +35,7 @@ storiesOf('Sandstone', module)
 					noAnimation={boolean('noAnimation', Config)}
 					noAutoDismiss={boolean('noAutoDismiss', Config)}
 					onBack={action('onBack')}
-					onClose={toggleOpen} // eslint-disable-line react/jsx-no-bind
+					onClose={toggleOpen}
 					onHide={action('onHide')}
 					onShow={action('onShow')}
 					scrimType={select('scrimType', ['none', 'translucent', 'transparent'], Config, 'translucent')}
@@ -44,58 +45,58 @@ storiesOf('Sandstone', module)
 						<Header title="List of options" type="mini">
 							{boolean('include close button', Config) ?
 								<slotBefore>
-									<Button icon="closex" onClick={toggleOpen} /* eslint-disable-line react/jsx-no-bind */ />
+									<Button icon="closex" onClick={toggleOpen} size="small" />
 								</slotBefore> :
 								null
 							}
 						</Header>
 						<div style={{width: ri.scaleToRem(900)}}>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 1</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 2</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 3</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 4</Item>
+							<Item onClick={nextPanel}>Item 1</Item>
+							<Item onClick={nextPanel}>Item 2</Item>
+							<Item onClick={nextPanel}>Item 3</Item>
+							<Item onClick={nextPanel}>Item 4</Item>
 						</div>
 					</Panel>
 					<Panel>
 						<Header title="Way way way too many Items for this list to handle, plus a wicked long title" type="mini">
 							<slotBefore>
-								<Button icon="arrowhookleft" onClick={prevPanel} /* eslint-disable-line react/jsx-no-bind */ />
+								<Button icon="arrowhookleft" onClick={prevPanel} size="small" />
 							</slotBefore>
 						</Header>
 						<Scroller style={{width: ri.scaleToRem(900)}}>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 1</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 2</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 3</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 4</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 5</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 6</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 7</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 8</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 9</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 10</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 11</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 12</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 13</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 14</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 15</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 16</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 17</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 18</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 19</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 20</Item>
-							<Item onClick={nextPanel} /* eslint-disable-line react/jsx-no-bind */>Item 21</Item>
+							<Item onClick={nextPanel}>Item 1</Item>
+							<Item onClick={nextPanel}>Item 2</Item>
+							<Item onClick={nextPanel}>Item 3</Item>
+							<Item onClick={nextPanel}>Item 4</Item>
+							<Item onClick={nextPanel}>Item 5</Item>
+							<Item onClick={nextPanel}>Item 6</Item>
+							<Item onClick={nextPanel}>Item 7</Item>
+							<Item onClick={nextPanel}>Item 8</Item>
+							<Item onClick={nextPanel}>Item 9</Item>
+							<Item onClick={nextPanel}>Item 10</Item>
+							<Item onClick={nextPanel}>Item 11</Item>
+							<Item onClick={nextPanel}>Item 12</Item>
+							<Item onClick={nextPanel}>Item 13</Item>
+							<Item onClick={nextPanel}>Item 14</Item>
+							<Item onClick={nextPanel}>Item 15</Item>
+							<Item onClick={nextPanel}>Item 16</Item>
+							<Item onClick={nextPanel}>Item 17</Item>
+							<Item onClick={nextPanel}>Item 18</Item>
+							<Item onClick={nextPanel}>Item 19</Item>
+							<Item onClick={nextPanel}>Item 20</Item>
+							<Item onClick={nextPanel}>Item 21</Item>
 						</Scroller>
 					</Panel>
 					<Panel>
 						<Header title="Vertical Slider" type="mini">
 							<slotBefore>
-								<Button icon="arrowhookleft" onClick={prevPanel} /* eslint-disable-line react/jsx-no-bind */ />
+								<Button icon="arrowhookleft" onClick={prevPanel} size="small" />
 							</slotBefore>
 						</Header>
 						<Slider orientation="vertical" defaultValue={50} style={{height: ri.scaleToRem(600)}} />
 					</Panel>
 				</MiniPanels>
-				<Button onClick={toggleOpen} /* eslint-disable-line react/jsx-no-bind */>Open MiniPanels</Button>
+				<Button onClick={toggleOpen}>Open MiniPanels</Button>
 			</div>);
 		},
 		{
