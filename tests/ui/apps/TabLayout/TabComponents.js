a90fe44115c8cb59dd448e30271a3bf29aba3ce9
Roy Sutton <roy.sutton@lge.com>
2020-04-08 21:43:50 -0400

    Remove key
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- tests/ui/apps/TabLayout/TabComponents.js
+++ tests/ui/apps/TabLayout/TabComponents.js
@@ -5,42 +5,42 @@ import {TabLayoutItem} from '../../../../TabLayout'
 
 export const tabs = [
 	<TabLayoutItem title="One" icon="">
-		<div id="view1" key={1}>View One</div>
+		<div id="view1">View One</div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Two" icon="">
-		<div id="view2" key={2}><Button id="button2">Button Two</Button></div>
+		<div id="view2"><Button id="button2">Button Two</Button></div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Three" icon="">
-		<div id="view3" key={3}>View Three</div>
+		<div id="view3">View Three</div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Four" icon="">
-		<div id="view4" key={4}><Button id="button4">Button Four</Button></div>
+		<div id="view4"><Button id="button4">Button Four</Button></div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Five" icon="">
-		<div id="view5" key={5}>View Five</div>
+		<div id="view5">View Five</div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Six" icon="">
-		<div id="view6" key={6}><Button id="button6">Button Six</Button></div>
+		<div id="view6"><Button id="button6">Button Six</Button></div>
 	</TabLayoutItem>
 ];
 
 export const tabsWithIcons = [
 	<TabLayoutItem title="One" icon="star">
-		<div id="view1" key={1}>View One</div>
+		<div id="view1">View One</div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Two" icon="home">
-		<div id="view2" key={2}><Button id="button2">Button Two</Button></div>
+		<div id="view2"><Button id="button2">Button Two</Button></div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Three" icon="plug">
-		<div id="view3" key={3}>View Three</div>
+		<div id="view3">View Three</div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Four" icon="lock">
-		<div id="view4" key={4}><Button id="button4">Button Four</Button></div>
+		<div id="view4"><Button id="button4">Button Four</Button></div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Five" icon="picture">
-		<div id="view5" key={5}>View Five</div>
+		<div id="view5">View Five</div>
 	</TabLayoutItem>,
 	<TabLayoutItem title="Six" icon="search">
-		<div id="view6" key={6}><Button id="button6">Button Six</Button></div>
+		<div id="view6"><Button id="button6">Button Six</Button></div>
 	</TabLayoutItem>
 ];
