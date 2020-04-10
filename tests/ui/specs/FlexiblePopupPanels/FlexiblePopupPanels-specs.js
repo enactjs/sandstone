23012283f1dc27b5f11e173325e06776f3a367bd
Blake Stephens <blake.stephens@lge.com>
2020-04-10 14:31:37 -0700

    Updated the tests to be a bit more efficient/accurate, even though they still don't work
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- tests/ui/specs/FlexiblePopupPanels/FlexiblePopupPanels-specs.js
+++ tests/ui/specs/FlexiblePopupPanels/FlexiblePopupPanels-specs.js
@@ -19,35 +19,30 @@ describe('FixedPopupPanels', function () {
 			expect(Interface.nextButton.isFocused(), 'focus Next button').to.be.true();
 			Page.spotlightSelect();
 
-			Interface.waitForLeave(1);
+			Interface.waitForEnter(2);
 			expect(Interface.panel2.isExisting(), 'navigate to the second Panel').to.be.true();
 
 			Interface.focusPrevButton();
 			Page.spotlightSelect();
 
-			Interface.waitForLeave(2);
+			Interface.waitForEnter(1);
 			expect(Interface.panel1.isExisting(), 'navigate back to the first Panel').to.be.true();
 		});
 	});
 
 	describe('Pointer', function () {
 		it('should open FlexiblePopupPanels and navigate to Panel', function () {
-			browser.moveToElement(Interface.openButton.ELEMENT);
-			expect(Interface.openButton.isFocused(), 'focus Open button').to.be.true();
 			Interface.openButton.click();
 			Interface.waitForOpen();
 
-			browser.moveToElement(Interface.nextButton.ELEMENT);
-			expect(Interface.nextButton.isFocused(), 'focus Next button').to.be.true();
 			Interface.nextButton.click();
+			Interface.waitForEnter(2);
 
-			Interface.waitForLeave(1);
 			expect(Interface.panel2.isExisting(), 'navigate to the second Panel').to.be.true();
 
-			browser.moveToElement(Interface.prevButton.ELEMENT);
 			Interface.prevButton.click();
+			Interface.waitForEnter(1);
 
-			Interface.waitForLeave(2);
 			expect(Interface.panel1.isExisting(), 'navigate back to the first Panel').to.be.true();
 		});
 	});
