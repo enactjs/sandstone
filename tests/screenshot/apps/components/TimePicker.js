7eb1bebd63fa34442b084112e477cf5c8023f3fa
Renuka Atale <renuka.atale@lge.com>
2020-04-13 16:24:00 -0700

    add default time for the time picker

--- tests/screenshot/apps/components/TimePicker.js
+++ tests/screenshot/apps/components/TimePicker.js
@@ -2,6 +2,7 @@ import TimePicker from '../../../../TimePicker';
 import React from 'react';
 
 const TimePickerTests = [
-	<TimePicker />
+	<TimePicker />,
+	<TimePicker defaultValue={new Date(2009, 5, 6)} />
 ];
 export default TimePickerTests;
