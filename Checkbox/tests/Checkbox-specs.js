f918376a78e49768fb304585049a1011ff8d3256
Blake Stephens <blake.stephens@lge.com>
2020-04-13 15:49:28 -0700

    Added unit tests for checkbox.indeterminate
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- Checkbox/tests/Checkbox-specs.js
+++ Checkbox/tests/Checkbox-specs.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 
 import {CheckboxBase} from '../Checkbox';
-import css from '../Item.module.less';
+import css from '../Checkbox.module.less';
 
 describe('CheckboxItem Specs', () => {
 	test('should not include the selected class when not selected', () => {
 		const subject = shallow(
-			<CheckboxBase>
-				check
-			</CheckboxBase>
+			<CheckboxBase />
 		);
 
 		const expected = css.selected;
@@ -20,14 +18,45 @@ describe('CheckboxItem Specs', () => {
 
 	test('should add the selected class when given the selected prop', () => {
 		const subject = shallow(
-			<CheckboxBase selected>
-				check
-			</CheckboxBase>
+			<CheckboxBase selected />
 		);
 
 		const expected = css.selected;
 		const actual = subject.first().prop('className');
 
 		expect(actual).toContain(expected);
+	});
+
+	test('should not include the indeterminate class when not indeterminate', () => {
+		const subject = shallow(
+			<CheckboxBase />
+		);
+
+		const expected = css.indeterminate;
+		const actual = subject.first().prop('className');
+
+		expect(actual).not.toContain(expected);
+	});
+
+	test('should add the indeterminate class when given the indeterminate prop', () => {
+		const subject = shallow(
+			<CheckboxBase indeterminate />
+		);
+
+		const expected = css.indeterminate;
+		const actual = subject.first().prop('className');
+
+		expect(actual).toContain(expected);
+	});
+
+	test('should prioritize indeterminate over selected', () => {
+		const subject = mount(
+			<CheckboxBase indeterminate selected indeterminateIcon="Ind">Sel</CheckboxBase>
+		);
+
+		const expected = 'Ind';
+		const actual = subject.first().text();
+
+		expect(actual).toBe(expected);
 	});
 });
