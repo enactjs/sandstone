2ad0d358193df8915e020ce3a5d7214d07c9672f
Roy Sutton <roy.sutton@lge.com>
2020-04-13 20:08:24 -0400

    Add screenshot tests
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

---
+++ tests/screenshot/apps/components/CollapsingHeaderPanel.js
@@ -0,0 +1,50 @@
+import Panels, {CollapsingHeaderPanel, Header} from '../../../../Panels';
+import Scroller from '../../../../Scroller';
+import React from 'react';
+
+class CallbackScroller extends React.Component {
+
+	componentDidMount () {
+		this.scrollTo({position: {y: 2000}});
+	}
+
+	cbScrollTo = (fn) => {
+		this.scrollTo = fn;
+	}
+
+	render () {
+		return (
+			<Scroller cbScrollTo={this.cbScrollTo}>
+				<div style={{height: 4000}}>
+					Some content
+				</div>
+			</Scroller>
+		);
+	}
+}
+// Panel components to show in the Panels
+const uncollapsed =
+	<CollapsingHeaderPanel>
+		<Header title="Title" subtitle="Sub Title" />
+	</CollapsingHeaderPanel>;
+
+const collapsed =
+	<CollapsingHeaderPanel>
+		<Header title="Title" subtitle="Sub Title" />
+		<CallbackScroller />
+	</CollapsingHeaderPanel>;
+
+const PanelsTests = [
+	{
+		title: 'with uncollapsed header',
+		component: <Panels>{uncollapsed}</Panels>,
+		wrapper: {full: true}
+	},
+	{
+		title: 'with collapsed header',
+		component: <Panels>{collapsed}</Panels>,
+		wrapper: {full: true}
+	}
+];
+
+export default PanelsTests;
