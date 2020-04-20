d58d510fd0afbe28a044ed157d321674189116b8
Ryan Duffy <ryan.duffy@lge.com>
2020-04-20 14:03:26 -0700

    add SST for unbalanced buttons
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- tests/screenshot/apps/components/WizardPanel.js
+++ tests/screenshot/apps/components/WizardPanel.js
@@ -1,60 +1,48 @@
 import {WizardPanel, View} from '../../../../Panels';
 import React from 'react';
 
-const WizardPanelTests = [
-	{
-		component: (
-			<WizardPanel>
-				<View>
-					View 1
-				</View>
-				<View>
-					View 2
-				</View>
-			</WizardPanel>
-		),
-		wrapper: {
-			full: true
-		}
-	},
-	{
-		component: (
-			<WizardPanel index={1}>
-				<View>
-					View 1
-				</View>
-				<View>
-					View 2
-				</View>
-			</WizardPanel>
-		),
-		wrapper: {
-			full: true
-		}
-	},
-	// [GT-28274] - 'nextButtonText' and 'prevButtonText' to show properly
-	{
-		component: (
-			<WizardPanel
-				index={1}
-				nextButtonText="nextButtonText"
-				prevButtonText="prevButtonText"
-				title="WizardPanel"
-			>
-				<View>
-					View 1
-				</View>
-				<View>
-					View 2
-				</View>
-				<View>
-					View 3
-				</View>
-			</WizardPanel>
-		),
-		wrapper: {
-			full: true
-		}
+import {withConfig} from './utils';
+
+const WizardPanelTests = withConfig({
+	wrapper: {
+		full: true
 	}
-];
+}, [
+	<WizardPanel>
+		<View>View 1</View>
+		<View>View 2</View>
+	</WizardPanel>,
+	<WizardPanel index={1}>
+		<View>View 1</View>
+		<View>View 2</View>
+	</WizardPanel>,
+	// [GT-28274] - 'nextButtonText' and 'prevButtonText' to show properly
+	<WizardPanel index={1} nextButtonText="nextButtonText" prevButtonText="prevButtonText" title="WizardPanel">
+		<View>View 1</View>
+		<View>View 2</View>
+		<View>View 3</View>
+	</WizardPanel>,
+	{
+		component: (
+			<WizardPanel index={1} nextButtonText="nextButtonText" prevButtonText="prevButtonText" title="WizardPanel">
+				<View>View 1</View>
+				<View>View 2</View>
+				<View>View 3</View>
+			</WizardPanel>
+		),
+		locale: 'ar-SA'
+	},
+	// Test unbalanced next/prev button text
+	<WizardPanel index={0} prevButtonText="prevButtonText" title="WizardPanel">
+		<View>View 1</View>
+		<View>View 2</View>
+		<View>View 3</View>
+	</WizardPanel>,
+	<WizardPanel index={0} nextButtonText="nextButtonText" title="WizardPanel">
+		<View>View 1</View>
+		<View>View 2</View>
+		<View>View 3</View>
+	</WizardPanel>
+]);
+
 export default WizardPanelTests;
