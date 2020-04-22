20a0651ec21c4cbde2696f1037208571a16e0543
Lucie <lucie.roy@lge.com>
2020-04-22 16:58:11 -0700

    PLAT-105285: added screenshot tests to CheckboxItem and FormCheckboxItem (#242)
    * added screenshot tests to CheckboxItem and FormCheckboxItem
    
    * Remove unused import
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Add tests for indeterminateIcon
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    Co-authored-by: Roy Sutton <roy.sutton@lge.com>

--- tests/screenshot/apps/components/FormCheckboxItem.js
+++ tests/screenshot/apps/components/FormCheckboxItem.js
@@ -2,6 +2,97 @@ import FormCheckboxItem from '../../../../FormCheckboxItem';
 import React from 'react';
 
 const FormCheckboxItemTests = [
-	<FormCheckboxItem />
+	<FormCheckboxItem />,
+	<FormCheckboxItem>FormCheckboxItem</FormCheckboxItem>, 					// not selected
+	<FormCheckboxItem disabled>FormCheckboxItem</FormCheckboxItem>,	// not selected
+	<FormCheckboxItem inline>FormCheckboxItem</FormCheckboxItem>,		// not selected
+	<FormCheckboxItem disabled inline>FormCheckboxItem</FormCheckboxItem>,	// not selected
+	<FormCheckboxItem selected>FormCheckboxItem Checked</FormCheckboxItem>,
+	<FormCheckboxItem selected disabled>FormCheckboxItem Checked</FormCheckboxItem>,
+	<FormCheckboxItem selected inline>FormCheckboxItem Checked</FormCheckboxItem>,
+	<FormCheckboxItem selected disabled inline>FormCheckboxItem Checked</FormCheckboxItem>,
+
+	<FormCheckboxItem indeterminate>FormCheckboxItem</FormCheckboxItem>, 					// not selected
+	<FormCheckboxItem disabled indeterminate>FormCheckboxItem</FormCheckboxItem>,	// not selected
+	<FormCheckboxItem inline indeterminate>FormCheckboxItem</FormCheckboxItem>,		// not selected
+	<FormCheckboxItem disabled inline indeterminate>FormCheckboxItem</FormCheckboxItem>, // not selected
+	<FormCheckboxItem selected indeterminate>FormCheckboxItem</FormCheckboxItem>,
+	<FormCheckboxItem selected disabled indeterminate>FormCheckboxItem</FormCheckboxItem>,
+	<FormCheckboxItem selected inline indeterminate>FormCheckboxItem</FormCheckboxItem>,
+	<FormCheckboxItem selected disabled inline indeterminate>FormCheckboxItem</FormCheckboxItem>,
+	<FormCheckboxItem indeterminate indeterminateIcon="lock">FormCheckboxItem</FormCheckboxItem>, 					// not selected
+
+	// *************************************************************
+	// locale = 'ar-SA'
+	// *************************************************************
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem>FormCheckboxItem</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem disabled>FormCheckboxItem</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem inline>FormCheckboxItem</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem disabled inline>FormCheckboxItem</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected disabled>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected inline>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected disabled inline>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem disabled indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem inline indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem disabled inline indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected disabled indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected inline indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem selected disabled inline indeterminate>FormCheckboxItem Checked</FormCheckboxItem>
+	},
+	{
+		locale: 'ar-SA',
+		component: <FormCheckboxItem indeterminate indeterminateIcon="lock">FormCheckboxItem Checked</FormCheckboxItem>
+	}
 ];
 export default FormCheckboxItemTests;
