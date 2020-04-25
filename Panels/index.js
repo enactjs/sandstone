64b14511000bc4370afc28e7b857ed1f4991e49c
Dave Freeman <sugardave@users.noreply.github.com>
2020-04-24 22:42:20 -0500

    remove deprecated Panels components (#254)
    ActivityPanels, AlwaysViewingPanels, Breadcrumb, and associated inline docs, less, and less vars
    
    Co-authored-by: Blake Stephens <blake.stephens@lge.com>

--- Panels/index.js
+++ Panels/index.js
@@ -2,9 +2,6 @@
  * Panels provides a way to manage different screens of an app.
  *
  * @module sandstone/Panels
- * @exports ActivityPanels
- * @exports AlwaysViewingPanels
- * @exports Breadcrumb
  * @exports Header
  * @exports Panel
  * @exports Panels
@@ -15,9 +12,6 @@
 
 import Routable, {Route} from '@enact/ui/Routable';
 
-import ActivityPanels from './ActivityPanels';
-import AlwaysViewingPanels from './AlwaysViewingPanels';
-import Breadcrumb from './Breadcrumb';
 import Header, {HeaderBase} from './Header';
 import FlexiblePopupPanels from './FlexiblePopupPanels';
 import FixedPopupPanels from './FixedPopupPanels';
@@ -27,9 +21,6 @@ import WizardPanel, {View, WizardPanelBase} from './WizardPanel';
 
 export default Panels;
 export {
-	ActivityPanels,
-	AlwaysViewingPanels,
-	Breadcrumb,
 	Header,
 	HeaderBase,
 	FlexiblePopupPanels,
