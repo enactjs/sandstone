c5b169f04638df0a59aa592588d4c645a0a9068d
Roy Sutton <roy.sutton@lge.com>
2020-04-28 20:07:48 -0400

    Add test for collapsing header
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- tests/ui/specs/Panel/Panel-specs.js
+++ tests/ui/specs/Panel/Panel-specs.js
@@ -8,12 +8,14 @@ describe('Panel', function () {
 
 	describe('Collapsing Header', function () {
 		it('should collapse header on page down', function () {
+			const y = Page.panel.getHeaderTop();
 
 			// Get focus in scroller
 			Page.panel.content.moveTo();
 			Page.pageDown();
+			Page.delay(500);
 
-
+			expect(Page.panel.getHeaderTop()).to.be.lessThan(y);
 		})
 	})
 });
