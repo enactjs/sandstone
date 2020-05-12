3ffb72b2931b0767be3dccf6aa87e51d906c47de
Teck Liew <teck.liew@lge.com>
2020-05-12 08:19:14 -0700

    import alert tests

--- tests/screenshot/apps/components/Alert.js
+++ tests/screenshot/apps/components/Alert.js
@@ -3,23 +3,34 @@ import Button from '../../../../Button';
 import React from 'react';
 
 const AlertTests = [
+	// Fullscreen alert with only children should be blank
 	<Alert open>Alert!</Alert>,
 	<Alert open subtitle="Subtitle" title="Title">Alert with title and subtitle!</Alert>,
-	<Alert open subtitle="Subtitle" title="Title">
+	<Alert open subtitle="Subtitle" title="Alert with buttons!">
 		<buttons>
 			<Button>Yes</Button>
 			<Button>No</Button>
 		</buttons>
-		Alert with buttons!
 	</Alert>,
-	<Alert open subtitle="Subtitle" title="Title">
+	<Alert open subtitle="Subtitle" title="Alert with image!">
 		<image>
 			<AlertImage
 				src="https://via.placeholder.com/240.png?text=image"
 				type="thumbnail"
 			/>
 		</image>
-		Alert with image!
+	</Alert>,
+	<Alert open subtitle="Subtitle" title="Alert with image and buttons!">
+		<image>
+			<AlertImage
+				src="https://via.placeholder.com/240.png?text=image"
+				type="thumbnail"
+			/>
+		</image>
+		<buttons>
+			<Button>Yes</Button>
+			<Button>No</Button>
+		</buttons>
 	</Alert>,
 	<Alert open subtitle="Subtitle" title="Title" type="overlay">Overlay Alert with title!</Alert>,
 	<Alert open subtitle="Subtitle" title="Title" type="overlay">
@@ -37,6 +48,19 @@ const AlertTests = [
 			/>
 		</image>
 		Overlay Alert with image!
+	</Alert>,
+	<Alert open subtitle="Subtitle" title="Title" type="overlay">
+		<image>
+			<AlertImage
+				src="https://via.placeholder.com/240.png?text=image"
+				type="thumbnail"
+			/>
+		</image>
+		<buttons>
+			<Button>Yes</Button>
+			<Button>No</Button>
+		</buttons>
+		Overlay Alert with buttons!
 	</Alert>
 ];
 
