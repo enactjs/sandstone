9340ffa12dd9f553cb9cd71ceaf0616bfcc27839
Teck Liew <teck.liew@lge.com>
2020-05-12 10:07:47 -0700

    update tests

--- tests/screenshot/apps/components/Alert.js
+++ tests/screenshot/apps/components/Alert.js
@@ -1,67 +1,67 @@
 import Alert, {AlertImage}  from '../../../../Alert';
 import Button from '../../../../Button';
 import React from 'react';
+import {withConfig, withProps} from './utils';
+
+const baseTests = [
+	<Alert open>Alert!</Alert>,
+	<Alert open title="Title" />,
+	<Alert open title="Title Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut nunc dolor." />,
+	<Alert open title="Title" subtitle="Subtitle" />,
+	<Alert open title="Title Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut nunc dolor." subtitle="Subtitle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut nunc dolor." />
+];
+
+const dropIn = {
+	image: (
+		<AlertImage
+			src="https://via.placeholder.com/240.png?text=image"
+			type="thumbnail"
+		/>
+	),
+	oneButton: (
+		<React.Fragment>
+			<buttons>
+				<Button>Yes</Button>
+			</buttons>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut nunc dolor.
+		</React.Fragment>
+	),
+	twoButtons: (
+		<React.Fragment>
+			<buttons>
+				<Button>Yes</Button>
+				<Button>No</Button>
+			</buttons>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut nunc dolor.
+		</React.Fragment>
+	)
+};
+
+const LtrTests = [
+	// Initial
+	...withProps({type: 'fullscreen'}, baseTests),
+	...withProps({type: 'overlay'}, baseTests),
+
+	// With Buttons
+	...withProps({type: 'fullscreen', children: dropIn.oneButton}, baseTests),
+	...withProps({type: 'fullscreen', children: dropIn.twoButtons}, baseTests),
+	...withProps({type: 'overlay', children: dropIn.oneButton}, baseTests),
+	...withProps({type: 'overlay', children: dropIn.twoButtons}, baseTests),
+
+	// With image
+	...withProps({type: 'fullscreen', image: dropIn.image}, baseTests),
+	...withProps({type: 'overlay', image: dropIn.image}, baseTests),
+
+	// With image and button
+	...withProps({type: 'fullscreen', children: dropIn.oneButton, image: dropIn.image}, baseTests),
+	...withProps({type: 'fullscreen', children: dropIn.twoButtons, image: dropIn.image}, baseTests),
+	...withProps({type: 'overlay', children: dropIn.oneButton, image: dropIn.image}, baseTests),
+	...withProps({type: 'overlay', children: dropIn.twoButtons, image: dropIn.image}, baseTests)
+];
 
 const AlertTests = [
-	// Fullscreen alert with only children should be blank
-	<Alert open>Alert!</Alert>,
-	<Alert open subtitle="Subtitle" title="Title">Alert with title and subtitle!</Alert>,
-	<Alert open subtitle="Subtitle" title="Alert with buttons!">
-		<buttons>
-			<Button>Yes</Button>
-			<Button>No</Button>
-		</buttons>
-	</Alert>,
-	<Alert open subtitle="Subtitle" title="Alert with image!">
-		<image>
-			<AlertImage
-				src="https://via.placeholder.com/240.png?text=image"
-				type="thumbnail"
-			/>
-		</image>
-	</Alert>,
-	<Alert open subtitle="Subtitle" title="Alert with image and buttons!">
-		<image>
-			<AlertImage
-				src="https://via.placeholder.com/240.png?text=image"
-				type="thumbnail"
-			/>
-		</image>
-		<buttons>
-			<Button>Yes</Button>
-			<Button>No</Button>
-		</buttons>
-	</Alert>,
-	<Alert open subtitle="Subtitle" title="Title" type="overlay">Overlay Alert with title!</Alert>,
-	<Alert open subtitle="Subtitle" title="Title" type="overlay">
-		<buttons>
-			<Button>Yes</Button>
-			<Button>No</Button>
-		</buttons>
-		Overlay Alert with buttons!
-	</Alert>,
-	<Alert open subtitle="Subtitle" title="Title" type="overlay">
-		<image>
-			<AlertImage
-				src="https://via.placeholder.com/240.png?text=image"
-				type="thumbnail"
-			/>
-		</image>
-		Overlay Alert with image!
-	</Alert>,
-	<Alert open subtitle="Subtitle" title="Title" type="overlay">
-		<image>
-			<AlertImage
-				src="https://via.placeholder.com/240.png?text=image"
-				type="thumbnail"
-			/>
-		</image>
-		<buttons>
-			<Button>Yes</Button>
-			<Button>No</Button>
-		</buttons>
-		Overlay Alert with buttons!
-	</Alert>
+	...LtrTests,
+	...withConfig({locale: 'ar-SA'}, LtrTests)
 ];
 
 export default AlertTests;
