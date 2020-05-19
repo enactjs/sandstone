ce15f1d800b302e5c82ac311d0dba928d8c14a21
jeonghee27 <jeonghee27.ahn@lge.com>
2020-05-19 14:59:21 +0900

    Assign diffrent padding for thin scrollbar and scrollbar hidden.
    Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- samples/sampler/stories/default/Scroller.js
+++ samples/sampler/stories/default/Scroller.js
@@ -26,45 +26,51 @@ const ScrollerConfig = mergeComponentMetadata('Scroller', UiScrollerBasic, Scrol
 
 storiesOf('Sandstone', module)
 	.add(
-		'Scroller',
-		() => (
-			<Scroller
-				className={
-					{
-						[css.verticalPadding]: select('direction', prop.direction, ScrollerConfig) !== 'horizontal',
-						[css.horizontalPadding]: select('direction', prop.direction, ScrollerConfig) !== 'vertical'
-					}
-				}
-				direction={select('direction', prop.direction, ScrollerConfig)}
-				fadeOut={boolean('fadeOut', ScrollerConfig, false)}
-				focusableScrollbar={select('focusableScrollbar', prop.focusableScrollbarOption, ScrollerConfig)}
-				horizontalScrollbar={select('horizontalScrollbar', prop.scrollbarOption, ScrollerConfig)}
-				key={select('scrollMode', prop.scrollModeOption, ScrollerConfig)}
-				noScrollByWheel={boolean('noScrollByWheel', ScrollerConfig)}
-				onScrollStart={action('onScrollStart')}
-				onScrollStop={action('onScrollStop')}
-				scrollMode={select('scrollMode', prop.scrollModeOption, ScrollerConfig)}
-				spotlightDisabled={boolean('spotlightDisabled', ScrollerConfig, false)}
-				verticalScrollbar={select('verticalScrollbar', prop.scrollbarOption, ScrollerConfig)}
-			>
-				<div
-					style={{
-						height: ri.scaleToRem(2004),
-						width: ri.scaleToRem(4002)
-					}}
+		'Scroller', () => {
+			const
+				direction = select('direction', prop.direction, ScrollerConfig),
+				focusableScrollbar = select('focusableScrollbar', prop.focusableScrollbarOption, ScrollerConfig),
+				horizontalScrollbar = select('horizontalScrollbar', prop.scrollbarOption, ScrollerConfig),
+				verticalScrollbar = select('verticalScrollbar', prop.scrollbarOption, ScrollerConfig),
+				className = {
+					[css.verticalPadding]: direction !== 'horizontal' && verticalScrollbar !== 'hidden',
+					[css.horizontalPadding]: direction !== 'vertical' && horizontalScrollbar !== 'hidden',
+					[css.bodyText]: focusableScrollbar || null
+				};
+			return (
+				<Scroller
+					className={className}
+					direction={direction}
+					fadeOut={boolean('fadeOut', ScrollerConfig)}
+					focusableScrollbar={focusableScrollbar}
+					horizontalScrollbar={horizontalScrollbar}
+					key={select('scrollMode', prop.scrollModeOption, ScrollerConfig)}
+					noScrollByWheel={boolean('noScrollByWheel', ScrollerConfig)}
+					onScrollStart={action('onScrollStart')}
+					onScrollStop={action('onScrollStop')}
+					scrollMode={select('scrollMode', prop.scrollModeOption, ScrollerConfig)}
+					spotlightDisabled={boolean('spotlightDisabled', ScrollerConfig, false)}
+					verticalScrollbar={verticalScrollbar}
 				>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-					Aenean id blandit nunc. Donec lacinia nisi vitae mi dictum, eget pulvinar nunc tincidunt. Integer vehicula tempus rutrum. Sed efficitur neque in arcu dignissim cursus.
 					<div
 						style={{
-							marginTop: ri.scaleToRem(1602)
+							height: ri.scaleToRem(2004),
+							width: ri.scaleToRem(4002)
 						}}
 					>
-						Mauris blandit sollicitudin mattis. Fusce commodo arcu vitae risus consectetur sollicitudin. Aliquam eget posuere orci. Cras pellentesque lobortis sapien non lacinia.
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+						Aenean id blandit nunc. Donec lacinia nisi vitae mi dictum, eget pulvinar nunc tincidunt. Integer vehicula tempus rutrum. Sed efficitur neque in arcu dignissim cursus.
+						<div
+							style={{
+								marginTop: ri.scaleToRem(1602)
+							}}
+						>
+							Mauris blandit sollicitudin mattis. Fusce commodo arcu vitae risus consectetur sollicitudin. Aliquam eget posuere orci. Cras pellentesque lobortis sapien non lacinia.
+						</div>
 					</div>
-				</div>
-			</Scroller>
-		),
+				</Scroller>
+			);
+		},
 		{
 			info: {
 				text: 'Basic usage of Scroller'
