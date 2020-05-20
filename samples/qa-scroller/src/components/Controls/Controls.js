b7ea7a7d5eef29d073d834b5b8358aceec4953b7
Mikyung Kim <mikyung27.kim@lge.com>
2020-05-20 16:52:51 +0900

    Fix qa-scroller

---
+++ samples/qa-scroller/src/components/Controls/Controls.js
@@ -0,0 +1,53 @@
+import {Cell, Row} from '@enact/ui/Layout';
+import CheckboxItem from '@enact/sandstone/CheckboxItem';
+import Input from '@enact/sandstone/Input';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import LocaleSwitch from '../LocaleSwitch';
+import ScrollModeSwitch from '../ScrollModeSwitch';
+
+class Controls extends React.Component {
+	static propTypes = {
+		handleFocusableScrollbar: PropTypes.func,
+		handleHeight: PropTypes.func,
+		handleScrollMode: PropTypes.func,
+		handleWidth: PropTypes.func,
+		height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+		nativeScroll: PropTypes.bool,
+		width: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+	}
+
+	render () {
+		const
+			{handleFocusableScrollbar, handleHeight, handleScrollMode, handleWidth, height, nativeScroll, width} = this.props,
+			inputWidth = {width: '5em'};
+
+		return (
+			<Row style={{direction: 'ltr'}}>
+				<Cell>
+					<label>height:</label>
+					<Input size="small" onChange={handleHeight} style={inputWidth} type="number" value={height} />
+				</Cell>
+				<Cell>
+					<label>width:</label>
+					<Input size="small" onChange={handleWidth} style={inputWidth} type="number" value={width} />
+				</Cell>
+				<Cell
+					component={CheckboxItem}
+					onClick={handleFocusableScrollbar}
+				>
+					Focusable Scrollbar
+				</Cell>
+				<Cell>
+					<ScrollModeSwitch defaultSelected={nativeScroll} onToggle={handleScrollMode} />
+				</Cell>
+				<Cell>
+					<LocaleSwitch />
+				</Cell>
+			</Row>
+		);
+	}
+}
+
+export default Controls;
--- samples/qa-scroller/src/views/MainView.js
+++ samples/qa-scroller/src/views/MainView.js
@@ -2,6 +2,7 @@ import DatePicker from '@enact/sandstone/DatePicker';
 import Group from '@enact/ui/Group';
 import Input from '@enact/sandstone/Input';
 import Item from '@enact/sandstone/Item';
+import {Panel, Header} from '@enact/sandstone/Panels';
 import Picker from '@enact/sandstone/Picker';
 import RadioItem from '@enact/sandstone/RadioItem';
 import React from 'react';
@@ -9,9 +10,7 @@ import ri from '@enact/ui/resolution';
 import Scroller from '@enact/sandstone/Scroller';
 import TimePicker from '@enact/sandstone/TimePicker';
 
-import PanelHeader from '../components/PanelHeader';
-
-import css from './MainView.module.less';
+import Controls from '../components/Controls';
 
 const
 	airports = [
@@ -60,49 +59,47 @@ class MainView extends React.Component {
 		const {focusableScrollbar, height, nativeScroll, width} = this.state;
 
 		return (
-			<div className={css.mainView}>
-				<PanelHeader
-					handleFocusableScrollbar={this.handleFocusableScrollbar}
-					handleHeight={this.handleHeight}
-					handleScrollMode={this.handleScrollMode}
-					handleWidth={this.handleWidth}
-					height={height}
-					nativeScroll={nativeScroll}
-					title="Scroller"
-					type="mini"
-					width={width}
-				/>
-				<div className={css.content}>
-					<Scroller
-						focusableScrollbar={focusableScrollbar}
-						scrollMode={nativeScroll ? 'native' : 'translate'}
-					>
-						<div style={{height: `${this.getScaledSize(height)}px`, width: `${this.getScaledSize(width)}px`}}>
-							<Input
-								defaultValue="Initial value"
-								title="Input with defaultValue"
-							/>
-							<Picker
-								orientation="vertical"
-								width="medium"
-							>
-								{airports}
-							</Picker>
-							<DatePicker
-								title="DatePicker"
-							/>
-							<RadioItem> FirstLongTextWithSpace FirstLongTextWithSpace FirstLongTextWithSpace FirstLongTextWithSpace </RadioItem>
-							<RadioItem disabled> Default disabled Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Text </RadioItem>
-							<Group childComponent={Item}>
-								{itemData}
-							</Group>
-							<TimePicker
-								title="TimePicker"
-							/>
-						</div>
-					</Scroller>
-				</div>
-			</div>
+			<Panel>
+				<Header title="Scroller" type="mini">
+					<Controls
+						handleFocusableScrollbar={this.handleFocusableScrollbar}
+						handleHeight={this.handleHeight}
+						handleScrollMode={this.handleScrollMode}
+						handleWidth={this.handleWidth}
+						height={height}
+						nativeScroll={nativeScroll}
+						width={width}
+					/>
+				</Header>
+				<Scroller
+					focusableScrollbar={focusableScrollbar}
+					scrollMode={nativeScroll ? 'native' : 'translate'}
+				>
+					<div style={{height: `${this.getScaledSize(height)}px`, width: `${this.getScaledSize(width)}px`}}>
+						<Input
+							defaultValue="Initial value"
+							title="Input with defaultValue"
+						/>
+						<Picker
+							orientation="vertical"
+							width="medium"
+						>
+							{airports}
+						</Picker>
+						<DatePicker
+							title="DatePicker"
+						/>
+						<RadioItem> FirstLongTextWithSpace FirstLongTextWithSpace FirstLongTextWithSpace FirstLongTextWithSpace </RadioItem>
+						<RadioItem disabled> Default disabled Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Text </RadioItem>
+						<Group childComponent={Item}>
+							{itemData}
+						</Group>
+						<TimePicker
+							title="TimePicker"
+						/>
+					</div>
+				</Scroller>
+			</Panel>
 		);
 	}
 }
