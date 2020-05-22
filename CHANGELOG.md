71ca4d69b472864e2080fb00dfdf9f6a4c6ff7dc
Roy Sutton <roy.sutton@lge.com>
2020-05-22 16:33:31 -0400

    PLAT-107750: Remove featureContent support (#347)
    * Remove collapsingHeaderPanel and initialHiddenHeight
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Remove useScrollPosition
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Add CHANGELOG entry
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Address review feedback
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The following is a curated list of changes in the Enact sandstone module, newest
 
 ### Removed
 
+- `sandstone/Panels` prop `featureContent`
+- `sandstone/Scroller` and `sandstone/VirtualList` prop `initialHiddenHeight`
 - `sandstone/WizardPanels` prop `footer`
 - `sandstone/Panels.FixedPopupPanels`, `sandstone/Panels.FlexiblePopupPanels`, `sandstone/Panels.View` and `sandstone/Panels.WizardPanels` aliases
 
--- tests/ui/apps/Panel/Panel-View.js
+++ tests/ui/apps/Panel/Panel-View.js
@@ -3,7 +3,6 @@ import React from 'react';
 import spotlight from '@enact/spotlight';
 
 import {Panel, Panels, Header} from '../../../../Panels';
-import Scroller from '../../../../Scroller';
 
 // NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
 // runs the same way
@@ -11,15 +10,9 @@ spotlight.setPointerMode(false);
 
 const app = (props) => (
 	<Panels {...props}>
-		<Panel id="Panel" featureContent>
+		<Panel id="Panel">
 			<Header id="header" title="Title" subtitle="Sub Title" />
-			<Scroller id="scroller">
-				<div style={{height: 4000}}>
-					<div id="content">
-						Some content
-					</div>
-				</div>
-			</Scroller>
+			<div>Panel Body</div>
 		</Panel>
 	</Panels>
 );
