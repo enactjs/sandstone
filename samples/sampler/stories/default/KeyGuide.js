fbba597c4157ab5b97af8e9193b669e7fe1ce31c
Blake Stephens <blake.stephens@lge.com>
2020-06-01 16:51:05 -0700

    Added editable text to the sample
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/sampler/stories/default/KeyGuide.js
+++ samples/sampler/stories/default/KeyGuide.js
@@ -1,4 +1,4 @@
-import {boolean, number, select} from '@enact/storybook-utils/addons/knobs';
+import {boolean, number, select, text} from '@enact/storybook-utils/addons/knobs';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import KeyGuide from '@enact/sandstone/KeyGuide';
 import React from 'react';
@@ -18,7 +18,7 @@ storiesOf('Sandstone', module)
 			const itemCount = number('items', Config, {range: true, min: 0, max: 4}, 3);
 			const icon = select('first item icon', prop.icon, Config, prop.icon[0]);
 			const items = [
-				{icon, children: 'This is long name item', key: 1},
+				{icon, children: text('Item 1 children', Config, 'This is long name item'), key: 1},
 				{icon: 'plus', children: 'Item 1', key: 2},
 				{icon: 'minus', children: 'Item 2', key: 3},
 				{icon: 'music', children: 'Item 3', key: 4}
