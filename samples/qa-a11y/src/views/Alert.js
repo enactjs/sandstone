1c04e9518cf4a5bce4aa736d6b27f4f591c3272e
sangwook1203.lee <sangwook1203.lee@lge.com>
2020-06-03 10:51:09 +0900

    updated qa-a11y samples

--- samples/qa-a11y/src/views/Alert.js
+++ samples/qa-a11y/src/views/Alert.js
@@ -1,5 +1,6 @@
+import Alert, {AlertImage} from '@enact/sandstone/Alert';
 import Button from '@enact/sandstone/Button';
-import Alert from '@enact/sandstone/Alert';
+import Heading from '@enact/sandstone/Heading';
 import React from 'react';
 
 class AlertView extends React.Component {
@@ -7,14 +8,32 @@ class AlertView extends React.Component {
 		super(props);
 		this.state = {
 			open1: false,
-			open2: false
+			open2: false,
+			open3: false,
+			open4: false,
+			open5: false,
+			open6: false,
+			open7: false,
+			open8: false
 		};
 
 		this.handleOpen1 = this.handleOpen(1);
 		this.handleOpen2 = this.handleOpen(2);
+		this.handleOpen3 = this.handleOpen(3);
+		this.handleOpen4 = this.handleOpen(4);
+		this.handleOpen5 = this.handleOpen(5);
+		this.handleOpen6 = this.handleOpen(6);
+		this.handleOpen7 = this.handleOpen(7);
+		this.handleOpen8 = this.handleOpen(8);
 
 		this.handleClose1 = this.handleClose(1);
 		this.handleClose2 = this.handleClose(2);
+		this.handleClose3 = this.handleClose(3);
+		this.handleClose4 = this.handleClose(4);
+		this.handleClose5 = this.handleClose(5);
+		this.handleClose6 = this.handleClose(6);
+		this.handleClose7 = this.handleClose(7);
+		this.handleClose8 = this.handleClose(8);
 	}
 
 	handleOpen = (expNum) => () => this.setState({['open' + expNum]: true})
@@ -22,18 +41,28 @@ class AlertView extends React.Component {
 	handleClose = (expNum) => () => this.setState({['open' + expNum]: false})
 
 	render () {
-		const {open1, open2} = this.state;
+		const {open1, open2, open3, open4, open5, open6, open7, open8} = this.state;
 
 		return (
 			<div>
-				<Button size="small" onClick={this.handleOpen1}>Small Alert</Button>
-				<Button size="small" onClick={this.handleOpen2}>Large Alert</Button>
+				<Heading showLine>Alert Fullscreen</Heading>
+				<Button size="small" onClick={this.handleOpen1}>Default</Button>
+				<Button size="small" onClick={this.handleOpen2}>With 3(max) buttons</Button>
+				<Button size="small" onClick={this.handleOpen3}>With thumbnail</Button>
+				<Button size="small" onClick={this.handleOpen4}>Without title</Button>
+				<Button size="small" onClick={this.handleOpen5}>Without content</Button>
+
+				<Heading showLine>Alert Overlay</Heading>
+				<Button size="small" onClick={this.handleOpen6}>Default</Button>
+				<Button size="small" onClick={this.handleOpen7}>With 3(max) buttons</Button>
+				<Button size="small" onClick={this.handleOpen8}>With thumbnail</Button>
 
 				<Alert
 					open={open1}
 					onClose={this.handleClose1}
+					title="this is title"
 				>
-					<span>Hello</span>
+					<span>this is content</span>
 					<buttons>
 						<Button size="small" onClick={this.handleClose1}>Howdy</Button>
 					</buttons>
@@ -42,6 +71,7 @@ class AlertView extends React.Component {
 				<Alert
 					open={open2}
 					onClose={this.handleClose2}
+					title="good morning"
 				>
 					<span>{`Not to worry, this message isn't going to be very long.
 					It just has to be long enough to show what a long message looks like.
@@ -50,7 +80,92 @@ class AlertView extends React.Component {
 						<Button size="small" onClick={this.handleClose2}>First Button!</Button>
 						<Button size="small" onClick={this.handleClose2}>Oh My Yes, Kitten</Button>
 						<Button size="small" onClick={this.handleClose2}>Hide And Show</Button>
-						<Button size="small" onClick={this.handleClose2}>Close</Button>
+					</buttons>
+				</Alert>
+
+				<Alert
+					open={open3}
+					onClose={this.handleClose3}
+					title="this is title"
+				>
+					<image>
+						<AlertImage src={'https://via.placeholder.com/250.png?text=image'} type="thumbnail" />
+					</image>
+					<span>this is content</span>
+					<buttons>
+						<Button size="small" onClick={this.handleClose3}>Howdy</Button>
+						<Button size="small" onClick={this.handleClose3}>Hide And Show</Button>
+					</buttons>
+				</Alert>
+
+				<Alert
+					open={open4}
+					onClose={this.handleClose4}
+				>
+					<span>{`Not to worry, this message isn't going to be very long.
+					It just has to be long enough to show what a long message looks like.
+					That's all; have a nice day.`}</span>
+					<buttons>
+						<Button size="small" onClick={this.handleClose4}>First Button!</Button>
+						<Button size="small" onClick={this.handleClose4}>Oh My Yes, Kitten</Button>
+						<Button size="small" onClick={this.handleClose4}>Hide And Show</Button>
+					</buttons>
+				</Alert>
+
+				<Alert
+					open={open5}
+					onClose={this.handleClose5}
+					title="good afternoon"
+				>
+					<buttons>
+						<Button size="small" onClick={this.handleClose5}>First Button!</Button>
+						<Button size="small" onClick={this.handleClose5}>Oh My Yes, Kitten</Button>
+					</buttons>
+				</Alert>
+
+				<Alert
+					open={open6}
+					onClose={this.handleClose6}
+					type="overlay"
+				>
+					<span>{`Not to worry, this message isn't going to be very long.
+					It just has to be long enough to show what a long message looks like.
+					That's all; have a nice day.`}</span>
+					<buttons>
+						<Button size="small" onClick={this.handleClose6}>First Button!</Button>
+						<Button size="small" onClick={this.handleClose6}>Oh My Yes, Kitten</Button>
+					</buttons>
+				</Alert>
+
+				<Alert
+					open={open7}
+					onClose={this.handleClose7}
+					type="overlay"
+				>
+					<span>{`Not to worry, this message isn't going to be very long.
+					It just has to be long enough to show what a long message looks like.
+					That's all; have a nice day.`}</span>
+					<buttons>
+						<Button size="small" onClick={this.handleClose7}>First Button!</Button>
+						<Button size="small" onClick={this.handleClose7}>Oh My Yes, Kitten</Button>
+						<Button size="small" onClick={this.handleClose7}>Hide And Show</Button>
+					</buttons>
+				</Alert>
+
+				<Alert
+					open={open8}
+					onClose={this.handleClose8}
+					type="overlay"
+				>
+					<image>
+						<AlertImage src={'https://via.placeholder.com/250.png?text=image'} type="thumbnail" />
+					</image>
+					<span>{`Not to worry, this message isn't going to be very long.
+					It just has to be long enough to show what a long message looks like.
+					That's all; have a nice day.`}</span>
+					<buttons>
+						<Button size="small" onClick={this.handleClose8}>First Button!</Button>
+						<Button size="small" onClick={this.handleClose8}>Oh My Yes, Kitten</Button>
 					</buttons>
 				</Alert>
 			</div>
