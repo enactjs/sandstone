2ade9de9b7332204534f28f42418941ee603b323
YunBum Sung <yb.sung@lge.com>
2020-06-09 08:12:42 +0900

    Add unit tests for AccessibilityDecorator
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- ThemeDecorator/AccessibilityDecorator.js
+++ ThemeDecorator/AccessibilityDecorator.js
@@ -33,6 +33,44 @@ const AccessibilityDecorator = hoc((config, Wrapped) => {	// eslint-disable-line
 			highContrast: PropTypes.bool,
 
 			/**
+			 * The variant(s) on a skin that a component should use when rendering. These will
+			 * typically alter the appearance of a skin's existing definition in a way that does not
+			 * override that skin's general styling.
+			 *
+			 * Multiple data types are supported by this prop, which afford different conveniences
+			 * and abilities. String and Array are effectively the same, supporting just additions
+			 * to the variants being applied to a component, and are much more convenient. Objects
+			 * may also be used, and have the ability to disable variants being passed by their
+			 * ancestors. Objects take the format of a basic hash, with variants as key names and
+			 * true/false Booleans as values, depicting their state. If a variant is excluded from
+			 * any version of data type used to set this prop, that variant will ignored, falling
+			 * back to the defaultVariant or parent variant, in that order.
+			 *
+			 * skinVariants examples:
+			 * ```
+			 *  // String
+			 *  skinVariants="highContrast"
+			 *
+			 *  // Array
+			 *  skinVariants={['highContrast']}
+			 *
+			 *  // Object
+			 *  skinVariants={{
+			 *  	highContrast: true,
+			 *  	grayscale: false
+			 *  }}
+			 * ```
+			 *
+			 * @type {String|String[]|Object}
+			 * @public
+			 */
+			skinVariants: PropTypes.oneOfType([
+				PropTypes.string,
+				PropTypes.array,
+				PropTypes.object
+			]),
+
+			/**
 			 * Sets the goal size of the text.
 			 *
 			 * The UI library will be responsible for using this
