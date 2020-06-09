8433efe015648bdb22fae479337ae3b91e787197
Seungcheon Baek <sc.baek@lge.com>
2020-06-09 15:15:01 +0900

    Cleared code
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- ContextualPopupDecorator/ContextualPopupDecorator.js
+++ ContextualPopupDecorator/ContextualPopupDecorator.js
@@ -282,7 +282,7 @@ const Decorator = hoc(defaultConfig, (config, Wrapped) => {
 		}
 
 		componentDidUpdate (prevProps, prevState, snapshot) {
-			if (prevProps.direction !== this.props.direction || snapshot && snapshot.containerWidth !== this.getContainerNodeWidth()) {
+			if (prevProps.direction !== this.props.direction || snapshot.containerWidth !== this.getContainerNodeWidth()) {
 				this.adjustedDirection = this.props.direction;
 				// NOTE: `setState` is called and will cause re-render
 				this.positionContextualPopup();
