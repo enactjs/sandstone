a5c8d3ee43556dd05740e30532c1a8a55b8c3fbc
dongsu.won <dongsu.won@lgepartner.com>
2020-06-10 13:01:17 +0900

    PLAT-1110343: Add UI TC(GT-28936)
           * Add UI Test(GT-28936)
    
            Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>

--- tests/ui/specs/VirtualList/VirtualList/VirtualList-specs.js
+++ tests/ui/specs/VirtualList/VirtualList/VirtualList-specs.js
@@ -433,6 +433,7 @@ describe('VirtualList', function () {
 				// Step3 : datasize Knobs setting '4'
 				Page.spotlightRight();
 				Page.spotlightRight();
+				Page.spotlightRight();
 				Page.spotlightSelect();
 				Page.backSpace();
 				Page.backSpace();
@@ -567,6 +568,32 @@ describe('VirtualList', function () {
 				expectFocusedItem(4);
 				const newSpotlightSize = Page.spotlightSize();
 				expect(newSpotlightSize).to.equal(25);
+			});
+		});
+		describe('cbScrollTo VirtualList Samples', function () {
+			beforeEach(function () {
+				Page.open();
+			});
+
+			// Since it is not the same as the view of samples, we made a button to go directly to the corresponding item.
+			it('should Jump to item when press cbScrollTo button [GT-28936]', function () {
+				// Step2-1: 5-way Spot the '010 - 한국어 - 한국'(item10 for this test) item.
+				// Check if item10 is located at the top of the list.
+				Page.spotlightRight();
+				Page.spotlightRight();
+				Page.spotlightSelect();
+				const topId = Page.topVisibleItemId();
+				expect(topId).to.equal('item10');
+				expectFocusedItem(10);
+				// Step2-2: 5-way Spot the '035 - Čeština - Česká republika'(item35 for this test) item.
+				Page.fiveWayToItem(35);
+				expectFocusedItem(35);
+				// Step3-1: 5-way Spot the '034 - Čeština - Česká republika'(item34 for this test) item.
+				Page.spotlightUp();
+				expectFocusedItem(34);
+				// Step3-2: 5-way Up until Spotlight displays on the '000 - 한국어 - 한국'(item0 for this test) item.
+				Page.fiveWayToItem(0);
+				expectFocusedItem(0);
 			});
 		});
 	});
