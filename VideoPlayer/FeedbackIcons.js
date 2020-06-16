9e846a67ed9099203ecf1235e91a37f1316a42d2
Hyeok Jo <hyeok.jo@lge.com>
2020-06-16 10:05:39 +0900

    Update GUI
    Enact-DCO-1.0-Signed-off-by: Hyeok Jo <hyeok.jo@lge.com>

--- VideoPlayer/FeedbackIcons.js
+++ VideoPlayer/FeedbackIcons.js
@@ -6,9 +6,9 @@ export default {
 	pause         : {icon: 'pause',              position: 'after',   allowHide: false,  message: null},
 	rewind        : {icon: 'backward',           position: 'before',  allowHide: false,  message: 'x'},
 	fastForward   : {icon: 'forward',            position: 'after',   allowHide: false,  message: 'x'},
-	jumpBackward  : {icon: 'jumpBackward',       position: 'before',  allowHide: false,  message: ' '},
-	jumpForward   : {icon: 'jumpForward',        position: 'after',   allowHide: false,  message: ' '},
-	jumpToStart   : {icon: 'jumpBackward',       position: 'before',  allowHide: true,   message: null},
-	jumpToEnd     : {icon: 'jumpForward',        position: 'after',   allowHide: true,   message: null},
+	jumpBackward  : {icon: 'jumpbackward',       position: 'before',  allowHide: false,  message: ' '},
+	jumpForward   : {icon: 'jumpforward',        position: 'after',   allowHide: false,  message: ' '},
+	jumpToStart   : {icon: 'jumpbackward',       position: 'before',  allowHide: true,   message: null},
+	jumpToEnd     : {icon: 'jumpforward',        position: 'after',   allowHide: true,   message: null},
 	stop          : {icon: 'stop',               position: null,      allowHide: true,   message: null}
 };
