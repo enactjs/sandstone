9577fb5404135a40c671c4c434a4764e02b6931f
Roy Sutton <roy.sutton@lge.com>
2020-06-17 15:08:41 -0400

    Add support for Item size prop
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ The following is a curated list of changes in the Enact sandstone module, newest
 
 ## [unreleased]
 
+### Added
+
+- `sandstone/Item` prop `size` to support small items
+
 ### Fixed
 
 - `sandstone/Input` to support accessibility features
--- Item/Item.js
+++ Item/Item.js
@@ -174,6 +174,15 @@ const ItemBase = kind({
 		selected: PropTypes.bool,
 
 		/**
+		 * The size of the item.
+		 *
+		 * @type {('large'|'small')}
+		 * @default 'large'
+		 * @public
+		 */
+		size: PropTypes.oneOf(['large', 'small']),
+
+		/**
 		 * Nodes to be inserted after `children`.
 		 *
 		 * For LTR locales, the nodes are inserted to the right of the primary content. For RTL
@@ -199,7 +208,8 @@ const ItemBase = kind({
 	},
 
 	defaultProps: {
-		labelPosition: 'below'
+		labelPosition: 'below',
+		size: 'large'
 	},
 
 	styles: {
@@ -208,7 +218,7 @@ const ItemBase = kind({
 	},
 
 	computed: {
-		className: ({centered, label, selected, styler}) => styler.append({centered, selected, hasLabel: label != null}),
+		className: ({centered, label, selected, size, styler}) => styler.append({centered, selected, hasLabel: label != null}, size),
 		label: ({label}) => (typeof label === 'number' ? label.toString() : label)
 	},
 
