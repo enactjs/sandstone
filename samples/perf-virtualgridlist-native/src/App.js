321b9ac617b39bab448e16fadfd89fcfeaf22e8a
jeonghee27 <jeonghee27.ahn@lge.com>
2020-06-18 16:34:09 +0900

    feature/PLAT-109849: Simulation wheel and get fps
    Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- samples/perf-virtualgridlist-native/src/App.js
+++ samples/perf-virtualgridlist-native/src/App.js
@@ -16,7 +16,50 @@ for (let i = 0; i < 1000; i++) {
 
 class VirtualGridListNativeSample extends Component {
 	componentDidMount () {
-		this.scrollTo({animate: false, focus: true, index: 19});
+		window.elapsedTimes = [];
+		let timerCount = 0;
+
+		const setIntervalId = setInterval(function () {
+			timerCount++;
+
+			// Wait for 5 Sec (1~10)
+
+			// Execute scrollTo for 10 seconds
+			if (timerCount > 10 && (timerCount <= 30)) {
+				// Make wheel event
+				const evt = document.createEvent('MouseEvents');
+				evt.initEvent('wheel', true, true);
+				evt.deltaY = 2000;
+
+				// Send wheel event to Scroller div
+				document.querySelector('root div div').dispatchEvent(evt);
+			} else if (timerCount === 41) {
+				// Wait for 5 Sec (31~40)
+				// Stop and show result
+				clearInterval(setIntervalId);
+
+				// Show FPS Result
+				if (window.elapsedTimes) {
+					const avgElapsedTimePerFrame = window.elapsedTimes[window.elapsedTimes.length - 1] / window.elapsedTimes.length;
+					console.log(window.elapsedTimes);
+					console.log('Average Elapsed Time per 1 frame : ' + avgElapsedTimePerFrame);
+					console.log('Average FPS : ' + (1000 / avgElapsedTimePerFrame));
+				}
+			}
+		}, 500);
+
+		function onRAF (timestamp) {
+			// During wheel
+			if (timestamp > 5000 && timestamp < 15000) {
+				if (!this.startTime) {
+					this.startTime = timestamp;
+				}
+				window.elapsedTimes.push(timestamp - this.startTime);
+				window.requestAnimationFrame(onRAF);
+			}
+		}
+
+		window.requestAnimationFrame(onRAF);
 	}
 
 	getScrollTo = (scrollTo) => {
