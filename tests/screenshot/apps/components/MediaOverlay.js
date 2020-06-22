7dc49b06fa1daadf8c93847f9b0722c059161a2b
Ryan Duffy <ryan.duffy@lge.com>
2020-06-22 09:31:28 -0700

    add a few more SSTs
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- tests/screenshot/apps/components/MediaOverlay.js
+++ tests/screenshot/apps/components/MediaOverlay.js
@@ -1,12 +1,25 @@
 import MediaOverlay from '../../../../MediaOverlay';
 import React from 'react';
 
+import img from '../../images/600x600.png';
+
 const MediaOverlayTests = [
+	// Base Tests
 	<MediaOverlay />,
-	<MediaOverlay caption="DTV 7-1" imageOverlay="https://picsum.photos/1280/720?image=1080" subtitle="07:00 AM - 08:00 AM" title="Program Name">
-		<source src="http://media.w3.org/2010/05/sintel/trailer.mp4" />
-	</MediaOverlay>,
-	<MediaOverlay disabled />
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" disabled />,
+	{
+		locale: 'ar-SA',
+		component: <MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" />
+	},
+
+	// Caption / Title / Subtitle combinations
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" />,
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" />,
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" subtitle="07:00 AM - 08:00 AM" />,
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
+	<MediaOverlay imageOverlay={img} title="Program Name" />,
+	<MediaOverlay imageOverlay={img} title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
+	<MediaOverlay imageOverlay={img} subtitle="07:00 AM - 08:00 AM" />
 
 ];
 export default MediaOverlayTests;
