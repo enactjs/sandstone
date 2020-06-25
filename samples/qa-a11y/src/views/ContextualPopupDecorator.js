6f6e4775acc50f7833d08b83dbe03af151c30433
Baekwoo Jung <baekwoo.jung@lge.com>
2020-06-25 11:53:42 +0900

    Clean up
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)

--- samples/qa-a11y/src/views/ContextualPopupDecorator.js
+++ samples/qa-a11y/src/views/ContextualPopupDecorator.js
@@ -54,7 +54,7 @@ class ContextualPopupDecoratorView extends React.Component {
 				<ContextualButton popupComponent={this.renderPopup2} spotlightRestrict="self-only">
 					Spotlight Modal
 				</ContextualButton>
-				<ContextualButton direction="above" popupComponent={this.renderPopup3}>
+				<ContextualButton direction="below" popupComponent={this.renderPopup3}>
 					Nested Radio
 				</ContextualButton>
 			</>
