c860ada37027d3526e6f0c312cc09b7f15cfab55
Baekwoo Jung <baekwoo.jung@lge.com>
2020-06-25 11:35:26 +0900

    Clean up
    Enact-DCO-1.0-Signed-off-by: Baekwoo Jung (baekwoo.jung@lge.com)

--- samples/qa-a11y/src/views/Checkbox.js
+++ samples/qa-a11y/src/views/Checkbox.js
@@ -2,7 +2,7 @@ import Checkbox from '@enact/sandstone/Checkbox';
 import React from 'react';
 
 const CheckboxView = () => (
-	<Checkbox />
+	<Checkbox style={{marginLeft: 20 + 'px', marginTop: 20 + 'px'}} />
 );
 
 export default CheckboxView;
