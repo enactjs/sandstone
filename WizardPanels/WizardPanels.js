ef43fdf08867307d40ddf80fe99d70b963220293
Roy Sutton <roy.sutton@lge.com>
2020-06-25 19:27:31 -0400

    Fix lint
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- WizardPanels/WizardPanels.js
+++ WizardPanels/WizardPanels.js
@@ -415,14 +415,14 @@ const WizardPanelsBase = kind({
 });
 
 // From https://usehooks.com/usePrevious/
-function usePrevious(value) {
+function usePrevious (value) {
 	// The ref object is a generic container whose current property is mutable ...
 	// ... and can hold any value, similar to an instance property on a class
 	const ref = React.useRef();
 
 	// Store current value in ref
 	React.useEffect(() => {
-	  ref.current = value;
+		ref.current = value;
 	}, [value]); // Only re-run if value changes
 
 	// Return previous value (happens before update in useEffect above)
