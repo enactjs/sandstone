5fe68a8036b323fcbcc4dea7e79210ee30eee46d
Ryan Duffy <ryan.duffy@lge.com>
2020-07-01 15:46:27 -0700

    fix ref usage
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- WizardPanels/useToggleRole.js
+++ WizardPanels/useToggleRole.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function useToggleRole ({role = 'region', event = 'onWillTransition'} = {}) {
-	const {current: ref} = React.useRef(null);
+	const ref = React.useRef(null);
 
 	const handler = React.useCallback(() => {
-		// To workaround not reading title when panel transition ends
-		ref.setAttribute('role', null);
-		ref.setAttribute('role', role);
+		if (ref.current) {
+			// To workaround not reading title when panel transition ends
+			ref.current.setAttribute('role', null);
+			ref.current.setAttribute('role', role);
+		}
 	}, [ref, role]);
 
 	return {
