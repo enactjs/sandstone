c779409b8a63be7f1fbf9b81dbe02d2e439f3331
Blake Stephens <blake.stephens@lge.com>
2020-07-08 16:09:03 -0700

    Cleaner implementation of the holepunch, with variables and some comments
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- ContextualPopupDecorator/HolePunchScrim.js
+++ ContextualPopupDecorator/HolePunchScrim.js
@@ -8,11 +8,18 @@ import Skinnable from '../Skinnable';
 
 import css from './HolePunchScrim.module.less';
 
+const autoUnit = (size) => (typeof size === 'number' ? unit(size, 'rem') : size);
+
 const HolePunchScrimBase = kind({
 	name: 'HolePunchScrim',
 
 	propTypes: {
-		holeBounds: PropTypes.object
+		holeBounds: PropTypes.shape({
+			height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+			right: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+			top: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+			width: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+		})
 	},
 
 	defaultProps: {
@@ -28,10 +35,10 @@ const HolePunchScrimBase = kind({
 		style: ({holeBounds: {top = 0, left = 0, width = 0, height = 0}, style}) => {
 			return {
 				...style,
-				'--hole-height': unit(height, 'rem'),
-				'--hole-width': unit(width, 'rem'),
-				'--hole-top': unit(top, 'rem'),
-				'--hole-left': unit(left, 'rem')
+				'--hole-height': autoUnit(height),
+				'--hole-width': autoUnit(width),
+				'--hole-top': autoUnit(top),
+				'--hole-left': autoUnit(left)
 			};
 		}
 	},
