b9499695598909ec0c63d6a73b2ed581397fbf94
YunBum Sung <yb.sung@lge.com>
2020-07-09 12:06:25 +0900

    Cleanup
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge)

--- ImageItem/AsyncRenderChildren.js
+++ ImageItem/AsyncRenderChildren.js
@@ -6,7 +6,9 @@ import React from 'react';
 const delayToRenderChildren = 600;
 
 /**
- * Render the `children` prop asynchronously when the `index` prop is defined and changes.
+ * This component introduced to enhance scroll performance when `ImageItem` used in `VirtualGridList`.
+ * Basically it renders `children` asynchronously when the `index` prop is defined and changes.
+ * When `ImageItem` used in other components, it will render immediately.
  *
  * @class AsyncRenderChildren
  * @memberof sandstone/ImageItem
@@ -17,7 +19,7 @@ function AsyncRenderChildren ({children: cachedChildren, fallback = '', index}) 
 	const [children, setChildren] = React.useState(cachedChildren);
 	const prevIndexRef = React.useRef(index);
 	const timerRef = React.useRef(null);
-	const aync = (children !== cachedChildren && typeof index !== 'undefined' && index !== prevIndexRef.current);
+	const aync = (typeof index !== 'undefined' && index !== prevIndexRef.current);
 
 	prevIndexRef.current = index;
 
@@ -42,7 +44,7 @@ function AsyncRenderChildren ({children: cachedChildren, fallback = '', index}) 
 
 AsyncRenderChildren.propTypes = /** @lends sandstone/ImageItem.AsyncRenderChildren.prototype */ {
 	/**
-	 * Render while waiting for the `children` prop to render. It could be any React elements.
+	 * The fallback element that shows before `children` render.
 	 *
 	 * @type {*}
 	 * @private
@@ -50,7 +52,8 @@ AsyncRenderChildren.propTypes = /** @lends sandstone/ImageItem.AsyncRenderChildr
 	fallback: PropTypes.any,
 
 	/**
-	 * Render the `children` prop asynchronously when the `index` is defined and changes.
+	 * `data-index` of [ImageItem]{@link sandstone/ImageItem}.
+	 * Renders `children` asynchronously when this value changed.
 	 *
 	 * @type {Number}
 	 * @private
