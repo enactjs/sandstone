f1e25cad063569654ed7478911acc38ef8eddc59
Ryan Duffy <ryan.duffy@lge.com>
2020-07-10 15:04:22 -0700

    PLAT-109392: Fix dynamic height panels to reflect changing height (#573)
    * wip - remove calc height
    
    * Additional tuning to get back to the beginning.
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * more flex!
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * spacing
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * change log
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * remove unused fn
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix noAnimation case
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix onfinish/oncancel to not override local handler
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    Co-authored-by: Blake Stephens <blake.stephens@lge.com>
    Co-authored-by: Roy Sutton <roy.sutton@lge.com>

--- internal/Panels/Arrangers.js
+++ internal/Panels/Arrangers.js
@@ -1,5 +1,4 @@
 import {arrange} from '@enact/ui/ViewManager';
-import ri from '@enact/ui/resolution';
 
 const quadInOut = 'cubic-bezier(0.455, 0.030, 0.515, 0.955)';
 const animationOptions = {easing: quadInOut};
@@ -27,13 +26,27 @@ class AnimateOnIdle {
 
 		this._onfinish = null;
 		this._oncancel = null;
-		this._finish = false;
-		this._cancel = false;
 		this._reverse = false;
+		this._playState = 'idle';
 
 		this.fill(node, firstKeyframe);
 
+		this.handleFinish = () => {
+			this._playState = 'finished';
+			this.fill(node, lastKeyframe);
+			if (this._onfinish) this._onfinish();
+		};
+
+		this.handleCancel = () => {
+			this._playState = 'finished';
+			this.fill(node, firstKeyframe);
+			if (this._oncancel) this._oncancel();
+		};
+
 		idle(() => {
+			// if the animation was finsihed/cancelled before the idle callback occurs, bail out
+			if (this._playState === 'finished') return;
+
 			this.animation = node.animate(keyframes, {
 				duration,
 				direction: reverse ? 'reverse' : 'normal',
@@ -41,23 +54,11 @@ class AnimateOnIdle {
 				...options
 			});
 
-			this.animation.onfinish = () => {
-				this.fill(node, lastKeyframe);
-				if (this._onfinish) this._onfinish();
-			};
-			this.animation.oncancel = () => {
-				this.fill(node, firstKeyframe);
-				if (this._oncancel) this._oncancel();
-			};
+			this.animation.onfinish = this.handleFinish;
+			this.animation.oncancel = this.handleCancel;
 
 			if (this._reverse) {
 				this.animation.reverse();
-			}
-
-			if (this._cancel) {
-				this.animation.cancel();
-			} else if (this._finish) {
-				this.animation.finish();
 			}
 		});
 	}
@@ -67,27 +68,23 @@ class AnimateOnIdle {
 		node.style.transform = keyframe.transform;
 	}
 
+	get playState () {
+		return this.animation ? this.animation.playState : this._playState;
+	}
+
 	set onfinish (value) {
-		if (this.animation) {
-			this.animation.onfinish = value;
-		} else {
-			this._onfinish = value;
-		}
+		this._onfinish = value;
 	}
 
 	set oncancel (value) {
-		if (this.animation) {
-			this.animation.oncancel = value;
-		} else {
-			this._oncancel = value;
-		}
+		this._oncancel = value;
 	}
 
 	finish () {
 		if (this.animation) {
 			this.animation.finish();
 		} else {
-			this._finish = true;
+			this.handleFinish();
 		}
 	}
 
@@ -95,7 +92,7 @@ class AnimateOnIdle {
 		if (this.animation) {
 			this.animation.cancel();
 		} else {
-			this._cancel = true;
+			this.handleCancel();
 		}
 	}
 
@@ -107,22 +104,6 @@ class AnimateOnIdle {
 			this._reverse = true;
 		}
 	}
-}
-
-function setHeightVariable (node, height) {
-	// If height is exists (and isn't 0) convert it to a REM for safe screen scaling.
-	if (height) height = ri.unit(height, 'rem');
-
-	// Relay the height value up to the Panels instance (parent of ViewManager, grand-parent of the
-	// Panel) so the background element can animate without "blanking out" (resetting to null)
-	// between panel measurement. Panel must be measured at "native" layout size, then set to fixed
-	// to allow a transition to work as expected, since transitions to/from "auto" are impossible
-	// at this time.
-	node.parentNode.parentNode.style.setProperty('--sand-panels-measured-height', height);
-
-	// Re-assign the measured height back to the panel as a fixed value, to enable proper DOM
-	// bounding rectangle clipping, to support native scrollable region detection.
-	node.style.setProperty('--sand-panel-measured-height', height);
 }
 
 const deferArrange = (config, keyframes, options) => {
@@ -142,40 +123,13 @@ const deferArrange = (config, keyframes, options) => {
  * @private
  */
 const BasicArranger = {
-	stay: (config) => {
-		// Set the initial size of the panel, before any transitions take place
-		const {node} = config;
-		const height = node.getBoundingClientRect().height;
-		setHeightVariable(node, height);
-
-		return deferArrange(config, [
-			{transform: 'none'},
-			{transform: 'none'}
-		], animationOptions);
-	},
 	enter: (config) => {
-		const {node, reverse} = config;
-
-		// Only assign values for the view entering the screen
-		if (!reverse) {
-			const height = node.getBoundingClientRect().height;
-			setHeightVariable(node, height);
-		}
-
 		return deferArrange(config, [
 			{transform: 'translateX(100%)', offset: 0},
 			{transform: 'none', offset: 1}
 		], animationOptions);
 	},
 	leave: (config) => {
-		const {node, reverse} = config;
-
-		// Only assign values for the view entering the screen
-		if (reverse) {
-			const height = node.getBoundingClientRect().height;
-			setHeightVariable(node, height);
-		}
-
 		return deferArrange(config, [
 			{transform: 'none', offset: 0},
 			{transform: 'translateX(-100%)', offset: 1}
