844ff6c36e6d1f7ad0dfa81ebdf6a4fdea23a505
Blake Stephens <blake.stephens@lge.com>
2020-07-28 13:09:07 -0700

    Simpler sample, with a comment, and commented out knobs.
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- samples/sampler/stories/default/Sprite.js
+++ samples/sampler/stories/default/Sprite.js
@@ -27,16 +27,27 @@ storiesOf('Sandstone', module)
 						fhd: spriteGear2k,
 						uhd: spriteGear4k
 					}}
+					columns={6}
+					rows={5}
+					height={120}
+					width={120}
+					orientation="horizontal"
 					paused={boolean('paused', Config)}
 					duration={number('duration', Config, {range: true, min: 500, max: 10000, step: 250}, 1000)}
 					iterations={Number(select('iterations', prop.iterations, Config))}
-					columns={number('columns', Config, {range: true, min: 5, max: 10}, 6)}
-					rows={number('rows', Config, {range: true, min: 3, max: 7}, 5)}
-					height={number('height', Config, {min: 30, max: 300}, 120)}
-					width={number('height', Config, {min: 30, max: 300}, 120)}
-					offsetTop={number('offsetTop', Config, {min: 0, max: 300}, 0)}
-					offsetLeft={number('offsetLeft', Config, {min: 0, max: 300}, 0)}
-					orientation={select('orientation', prop.orientation, Config)}
+
+				// These are design-time props, which don't make sense to be modified
+				// at runtime, but would be useful to someone trying to learn what the
+				// options are and how the props affect the component.More customizability
+				// (at a future time) could open them up to make them useful for this sample.
+				//
+				// columns={number('columns', DesignTimeConfig, {range: true, min: 5, max: 10}, 6)}
+				// rows={number('rows', DesignTimeConfig, {range: true, min: 3, max: 7}, 5)}
+				// height={number('height', DesignTimeConfig, {min: 30, max: 300}, 120)}
+				// width={number('height', DesignTimeConfig, {min: 30, max: 300}, 120)}
+				// offsetTop={number('offsetTop', DesignTimeConfig, {min: 0, max: 300}, 0)}
+				// offsetLeft={number('offsetLeft', DesignTimeConfig, {min: 0, max: 300}, 0)}
+				// orientation={select('orientation', prop.orientation, DesignTimeConfig)}
 				/>
 			);
 		},
