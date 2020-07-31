4dfe2b11952e64bd906358713b695f70f137f591
Ryan Duffy <ryan.duffy@lge.com>
2020-07-31 13:19:55 -0700

    remove earlier work
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- internal/Panels/index.js
+++ internal/Panels/index.js
@@ -5,11 +5,9 @@ import NavigationButton from './NavigationButton';
 import PopupDecorator from './PopupDecorator';
 import Viewport, {PanelsStateContext} from './Viewport';
 import useAutoFocus, {AutoFocusDecorator} from './useAutoFocus';
-import useSuppressKeysDuringTransition from './useSuppressKeysDuringTransition';
 
 export {
 	useAutoFocus,
-	useSuppressKeysDuringTransition,
 	AutoFocusDecorator,
 	BasicArranger,
 	CancelDecorator,
