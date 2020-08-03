aa81d16f57b98bd5246e95dc031bcedd13d4ba48
Ryan Duffy <ryan.duffy@lge.com>
2020-08-03 16:47:27 -0700

    restrict to back/5-way keys
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- internal/Panels/captureKeys.js
+++ internal/Panels/captureKeys.js
@@ -1,11 +1,22 @@
-import handle, {preventDefault, stop, stopImmediate} from '@enact/core/handle';
+import handle, {forKey, preventDefault, stop, stopImmediate} from '@enact/core/handle';
 import onWindowReady from '@enact/core/snapshot';
+
+// Utility to return true if any of the handlers returns true
+const any = (...handlers) => (...args) => handlers.find(fn => fn(...args)) != null;
 
 let capturing = false;
 
 // If capturing, prevent default behaviors and stop all event propagation
 const handleGlobalKeyDownCapture = handle(
 	() => capturing,
+	any(
+		forKey('cancel'),
+		forKey('left'),
+		forKey('right'),
+		forKey('up'),
+		forKey('down'),
+		forKey('enter')
+	),
 	preventDefault,
 	stop,
 	stopImmediate
