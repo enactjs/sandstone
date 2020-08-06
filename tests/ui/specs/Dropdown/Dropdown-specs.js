25d99caff82c3e1f0ee7bf6a82e178e7a34854f4
Lucie Roy <lucie.roy@lge.com>
2020-08-06 14:39:04 -0700

    Updated code

--- tests/ui/specs/Dropdown/Dropdown-specs.js
+++ tests/ui/specs/Dropdown/Dropdown-specs.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undefined */
 const Page = require('./DropdownPage');
 
 function waitForFocusedText (dropdown, text, timeout, timeoutMsg = `timed out waiting for ${text}`, interval = 250) {
@@ -19,8 +20,7 @@ describe('Dropdown', function () {
 
 			Page.openDropdown(dropdown);
 
-			// eslint-disable-next-line no-undefined
-			waitForFocusedText(dropdown, 'four', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'four', 500, undefined, 100);
 
 			waitForFocusedText(dropdown, 'one', 750);
 		});
@@ -30,8 +30,7 @@ describe('Dropdown', function () {
 
 			Page.openDropdown(dropdown);
 
-			// eslint-disable-next-line no-undefined
-			waitForFocusedText(dropdown, 'one', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
 
 			waitForFocusedText(dropdown, 'three', 750);
 		});
@@ -41,8 +40,7 @@ describe('Dropdown', function () {
 
 			Page.openDropdown(dropdown);
 
-			// eslint-disable-next-line no-undefined
-			waitForFocusedText(dropdown, '28', 500, 'some text', 100);
+			waitForFocusedText(dropdown, '28', 500, undefined, 100);
 
 			waitForFocusedText(dropdown, '3', 2250);
 		});
@@ -53,47 +51,50 @@ describe('Dropdown', function () {
 			const dropdown = Page.components.dropdownDefault;
 			const dropdownList = $('.Dropdown_Dropdown_dropdownList');
 			// Step 3: 5-way Spot and 5-way Select the 'Default' Dropdown.
-			Page.dropdown.moveTo();
-			Page.spotlightSelect();
+			// dropdown.self.moveTo();
+			// Page.spotlightLeft();
+			// Page.spotlightSelect();
+			Page.openDropdown(dropdown);
+			// Page.delay(10000);
 
 			// Verify Step 3.1: The 'Default' Dropdown opens.
 			// Verify Step 3.2: Spotlight is on *Option 1* on the left *Dropdown*.
-			waitForFocusedText(dropdown, 'one', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
 
 			// Step 4: 5-way Down to *Option 5*
 			// Verify Step 4.1: Spotlight is on each option with each 5-way Down.
 			Page.spotlightDown();
-			waitForFocusedText(dropdown, 'two', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'two', 500, undefined, 100);
 			Page.spotlightDown();
-			waitForFocusedText(dropdown, 'three', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'three', 500, undefined, 100);
 			Page.spotlightDown();
-			waitForFocusedText(dropdown, 'four', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'four', 500, undefined, 100);
 			// Verify Step 4.2:Spotlight is on *Option 5*.
 			Page.spotlightDown();
-			waitForFocusedText(dropdown, 'five', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'five', 500, undefined, 100);
 
 			// Step 5: 5-way Down one more time.
 			// Verify Step 5.1: Spotlight stays on *Option 5*.
 			Page.spotlightDown();
-			waitForFocusedText(dropdown, 'five', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'five', 500, undefined, 100);
 			// ????? Verify Step 5.2: Spotlight stays *inside* the 'Default' Dropdown.
 
 			// Step 6: 5-way Up to *Option 1*.`
 			// Verify Step 6.1: Spotlight is on each option with each 5-way Up.
 			Page.spotlightUp();
-			waitForFocusedText(dropdown, 'four', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'four', 500, undefined, 100);
 			Page.spotlightUp();
-			waitForFocusedText(dropdown, 'three', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'three', 500, undefined, 100);
 			Page.spotlightUp();
-			waitForFocusedText(dropdown, 'two', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'two', 500, undefined, 100);
 			// Verify Step 6.2:Spotlight is on *Option 1*.
 			Page.spotlightUp();
-			waitForFocusedText(dropdown, 'one', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
 
 			// Step 7: 5-way Up one more time.
 			// Verify Step 7.1: Spotlight stays on *Option 1*.
 			Page.spotlightUp();
-			waitForFocusedText(dropdown, 'one', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
 			// ????? Verify Step 7.2: Spotlight stays *inside* the 'Default' Dropdown.
 
 			// Step 8: Click on the empty space created by the wrapper (Pointer mode).
@@ -104,11 +105,11 @@ describe('Dropdown', function () {
 			expect(dropdownList.isExisting).to.not.be.true();
 
 			// Step 9: 5-way Spot the 'Default' Dropdown.
-			Page.dropdown.moveTo(); // ???  is it Pointer mode ? we need 5-way mode
-			Page.spotlightSelect();
+			// dropdown.self.moveTo(); // ???  is it Pointer mode ? we need 5-way mode
+			// Page.spotlightSelect();
 			// Verify Step 9: Spotlight is on the closed 'Default' Dropdown.
 			// ???
-			expect(dropdownList.isExisting).to.not.be.true(); // ???
+			// expect(dropdownList.isExisting).to.not.be.true(); // ???
 
 			// Step 10: 5-way Right to the *right* Dropdown.
 			// Verify Step 10: Spotlight is on the 'Selected' Dropdown.
@@ -121,7 +122,7 @@ describe('Dropdown', function () {
 
 			// open the first dropdown and wait for the first list item to be focused
 			Page.openDropdown(dropdown);
-			waitForFocusedText(dropdown, 'one', 500, 'some text', 100);
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
 
 			// click in the empty space created by the wrapper
 			const wrapper = $('#wrapper');
