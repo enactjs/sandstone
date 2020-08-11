30f26eedd8b9e2a7e0e1871576533f4254e8b3d5
Dave Freeman <dave.freeman@lge.com>
2020-08-11 15:11:34 -0500

    latest @enact/cli doesn't like this at all

--- samples/qa-keypress-monitor/src/hooks/useEventListener.js
+++ samples/qa-keypress-monitor/src/hooks/useEventListener.js
@@ -8,6 +8,7 @@ import {useEffect} from 'react';
 
 const useEventListener = (target, type, listener, ...options) => {
 	useEffect(() => {
+		// eslint-disable-next-line
 		const targetIsRef = target.hasOwnProperty('current');
 		const currentTarget = targetIsRef ? target.current : target;
 		if (currentTarget) {
