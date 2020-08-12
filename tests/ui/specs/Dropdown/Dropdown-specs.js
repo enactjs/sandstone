0d127df68c1d403aa5920c4ed3253bdb83016c3a
Lucie <lucie.roy@lge.com>
2020-08-12 12:56:14 -0700

    PLAT-116019: UI Tests Dropdown - automate GT-28644 and GT-28646 (#681)
    * automated GT-28644 and GT-28646
    
    * Updated code
    
    * updated comments
    
    * removed one space in comment

--- tests/ui/specs/Dropdown/Dropdown-specs.js
+++ tests/ui/specs/Dropdown/Dropdown-specs.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undefined */
 const Page = require('./DropdownPage');
 
 function waitForFocusedText (dropdown, text, timeout, timeoutMsg = `timed out waiting for ${text}`, interval = 250) {
@@ -19,7 +20,6 @@ describe('Dropdown', function () {
 
 			Page.openDropdown(dropdown);
 
-			// eslint-disable-next-line no-undefined
 			waitForFocusedText(dropdown, 'four', 500, undefined, 100);
 
 			waitForFocusedText(dropdown, 'one', 750);
@@ -30,7 +30,6 @@ describe('Dropdown', function () {
 
 			Page.openDropdown(dropdown);
 
-			// eslint-disable-next-line no-undefined
 			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
 
 			waitForFocusedText(dropdown, 'three', 750);
@@ -41,11 +40,79 @@ describe('Dropdown', function () {
 
 			Page.openDropdown(dropdown);
 
-			// eslint-disable-next-line no-undefined
 			waitForFocusedText(dropdown, '28', 500, undefined, 100);
 
 			waitForFocusedText(dropdown, '3', 2250);
 		});
 	});
 
+	describe('5-way', function () {
+		it('should lock Spotlight inside the Dropdown - [GT-28646]', function () {
+			const dropdown = Page.components.dropdownDefault;
+			const dropdownList = $('.Dropdown_Dropdown_dropdownList');
+			// Step 3: 5-way Spot and 5-way Select the 'Default' Dropdown.
+			Page.openDropdown(dropdown);
+			// Verify Step 3.1: The 'Default' Dropdown opens.
+			// Verify Step 3.2: Spotlight is on *Option 1* on the left *Dropdown*.
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
+
+			// Step 4: 5-way Down to *Option 5*
+			// Verify Step 4.1: Spotlight is on each option with each 5-way Down.
+			Page.spotlightDown();
+			waitForFocusedText(dropdown, 'two', 500, undefined, 100);
+			Page.spotlightDown();
+			waitForFocusedText(dropdown, 'three', 500, undefined, 100);
+			Page.spotlightDown();
+			waitForFocusedText(dropdown, 'four', 500, undefined, 100);
+			// Verify Step 4.2:Spotlight is on *Option 5*.
+			Page.spotlightDown();
+			waitForFocusedText(dropdown, 'five', 500, undefined, 100);
+
+			// Step 5: 5-way Down one more time.
+			// Verify Step 5.1: Spotlight stays on *Option 5*.
+			Page.spotlightDown();
+			waitForFocusedText(dropdown, 'five', 500, undefined, 100);
+
+			// Step 6: 5-way Up to *Option 1*.`
+			// Verify Step 6.1: Spotlight is on each option with each 5-way Up.
+			Page.spotlightUp();
+			waitForFocusedText(dropdown, 'four', 500, undefined, 100);
+			Page.spotlightUp();
+			waitForFocusedText(dropdown, 'three', 500, undefined, 100);
+			Page.spotlightUp();
+			waitForFocusedText(dropdown, 'two', 500, undefined, 100);
+			// Verify Step 6.2:Spotlight is on *Option 1*.
+			Page.spotlightUp();
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
+
+			// Step 7: 5-way Up one more time.
+			// Verify Step 7.1: Spotlight stays on *Option 1*.
+			Page.spotlightUp();
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
+
+			// Step 8: Click  on the empty space created by the wrapper (Pointer mode).
+			const wrapper = $('#wrapper');
+			wrapper.click({x: 0, y: 0});
+			// Verify Step 8: The 'Default' Dropdoan is closed.
+			expect(dropdownList.isExisting).to.not.be.true();
+		});
+	});
+
+	describe('pointer', function () {
+		it('should dismiss dropdown when clicking outside - [GT-28644]', function () {
+			const dropdown = Page.components.dropdownDefault;
+
+			// open the first dropdown and wait for the first list item to be focused
+			Page.openDropdown(dropdown);
+			waitForFocusedText(dropdown, 'one', 500, undefined, 100);
+
+			// Step 3: Click in the area outside the Dropdown (in the empty space created by the wrapper)
+			const wrapper = $('#wrapper');
+			wrapper.click({x: 0, y: 0});
+
+			// Verify Step 3: that the floating list no longer exists (Dropdown is closed)
+			const dropdownList = $('.Dropdown_Dropdown_dropdownList');
+			expect(dropdownList.isExisting).to.not.be.true();
+		});
+	});
 });
