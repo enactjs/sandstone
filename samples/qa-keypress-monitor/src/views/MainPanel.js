47414398c15f43d5df3377a69576f47fba0b2a26
Dave Freeman <dave.freeman@lge.com>
2020-08-13 16:37:00 -0500

    move key map filtering up to the main view

--- samples/qa-keypress-monitor/src/views/MainPanel.js
+++ samples/qa-keypress-monitor/src/views/MainPanel.js
@@ -2,30 +2,49 @@ import {Cell, Row} from '@enact/ui/Layout';
 import {Header, Panel} from '@enact/sandstone/Panels';
 import {Heading} from '@enact/sandstone/Heading';
 import kind from '@enact/core/kind';
-import React from 'react';
+import React, {useContext} from 'react';
 
 import KeyLogger from '../components/KeyLogger/KeyLogger';
+import PressedKeysContext from '../contexts/PressedKeysContext';
 
-const MainPanel = kind({
+const MainPanelBase = kind({
 	name: 'MainPanel',
 
-	render: (props) => (
-		<Panel {...props} noCloseButton>
+	render: ({keys: {modifiers, nonModifiers}, ...rest}) => (
+		<Panel {...rest} noCloseButton>
 			<Header>
 				<title>KeyPress Monitor</title>
 			</Header>
 			<Row>
 				<Cell>
 					<Heading>keydown Events</Heading>
-					<KeyLogger />
+					<KeyLogger keys={nonModifiers} />
 				</Cell>
 				<Cell>
 					<Heading>Modifier Keys</Heading>
-					<KeyLogger modifiers />
+					<KeyLogger keys={modifiers} />
 				</Cell>
 			</Row>
 		</Panel>
 	)
 });
+
+const MainPanel = (props) => {
+	const {pressedKeys} = useContext(PressedKeysContext);
+
+	const modifierKeys = ['alt', 'control', 'shift', 'meta'];
+	const isModifier = ({key} = {}) => modifierKeys.includes(key.toLowerCase());
+
+	const keys = [...pressedKeys.values()];
+	const filteredKeys = {
+		modifiers: keys.length ? keys.filter((key) => isModifier(key)) : [],
+		nonModifiers: keys.length ? keys.filter((key) => !isModifier(key)) : []
+	};
+
+	return (
+		<MainPanelBase {...props} keys={filteredKeys} />
+	);
+
+};
 
 export default MainPanel;
