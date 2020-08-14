23c93eb6027f1ed0b6f1c9c724573ea5586cdf5c
dongsu.won <dongsu.won@lgepartner.com>
2020-08-14 11:05:35 +0900

    *Remove unnessary delay and add comment(GT-28464).
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>

--- tests/ui/specs/VirtualList/VirtualList/onScroll/VirtualList-GT-28464-specs.js
+++ tests/ui/specs/VirtualList/VirtualList/onScroll/VirtualList-GT-28464-specs.js
@@ -1,23 +1,23 @@
 const Page = require('../VirtualListPage');
-const {expectFocusedItem, waitUntilFocused} = require('../../VirtualList-utils');
+const {expectFocusedItem} = require('../../VirtualList-utils');
 
 describe('Item Animates', function () {
 	beforeEach(function () {
 		Page.open();
 	});
-
+	// TODO: Need to api for press holding page down/up key.
 	it('should animate Items via Channel Down [GT-28464]', function () {
 		// Step 3: Position the pointer on the first item('Item 000)
 		Page.showPointerByKeycode();
 		Page.item(0).moveTo();
 		expectFocusedItem(0);
-		// Step 4: Press Channel Down
+		// Step 4: Press Channel Down 2 times.
+		// Step 4 Verify: The list Scrolls Up page by page.
 		Page.checkScrollbyPagekey('down');
-		// Step 5: Press Channel Down again.
 		Page.checkScrollbyPagekey('down');
+		// Step 6: Channel Up 2 times
+		// Step 6 Verify: The list Scrolls Up page by page.
 		Page.checkScrollbyPagekey('up');
-		Page.delay(200);
 		Page.checkScrollbyPagekey('up');
-		Page.delay(200);
 	});
 });
