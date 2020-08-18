bf888429f6d0f72072f52bde92075c0888fb36a1
dongsuwon <61816767+dongsuwon@users.noreply.github.com>
2020-08-19 06:16:24 +0900

    PLAT-116278: Modify check to scrolling event (#690)
    * PLAT-116278: Modify check to scrolling event
    
    *Modify api for check to scrolling event.(GT-28491, GT-28464)
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Add delay for check to scrolling event.(GT-28470)
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Remove unnessary step(GT-28464).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Change checking api for item(GT-28464, GT-28491).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Remove unnessary checking item(GT-28464).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Remove unnessary checking item(GT-28464).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Change checking api for scrollevent(GT-28470).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Change api for checking item(GT-28491).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Remove unnessary delay and add comment(GT-28464).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Remove unnessary delay and add comment(GT-28464).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Fixed wrong checking for scrolling event(GT-28470)
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Change way for delay(GT-28491)
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Add waitUntilVisible
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    * *Unskip Test(GT-28491).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>

--- tests/ui/specs/VirtualList/VirtualList/onScroll/VirtualList-GT-28464-specs.js
+++ tests/ui/specs/VirtualList/VirtualList/onScroll/VirtualList-GT-28464-specs.js
@@ -5,21 +5,19 @@ describe('Item Animates', function () {
 	beforeEach(function () {
 		Page.open();
 	});
-
+	// TODO: Need to api for press holding page down/up key.
 	it('should animate Items via Channel Down [GT-28464]', function () {
 		// Step 3: Position the pointer on the first item('Item 000)
 		Page.showPointerByKeycode();
 		Page.item(0).moveTo();
 		expectFocusedItem(0);
-		// Step 4: Press Channel Down
-		Page.pageDown();
-		Page.delay(700);
-		// Check to scrolling event on VirtualList
-		expect(Page.list.getAttribute('data-scrolling-events')).to.equal('1');
-		// Step 5: Press Channel Down again.
-		Page.pageDown();
-		Page.delay(700);
-		// Check to scrolling event on VirtualList
-		expect(Page.list.getAttribute('data-scrolling-events')).to.equal('2');
+		// Step 4: Press Channel Down 2 times.
+		// Step 4 Verify: The list Scrolls Up page by page.
+		Page.checkScrollbyPagekey('down');
+		Page.checkScrollbyPagekey('down');
+		// Step 6: Channel Up 2 times
+		// Step 6 Verify: The list Scrolls Up page by page.
+		Page.checkScrollbyPagekey('up');
+		Page.checkScrollbyPagekey('up');
 	});
 });
