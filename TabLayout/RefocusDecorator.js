acffbeb6c5a43efe719774c2dfed61bc9b0ddf9a
Roy Sutton <roy.sutton@lge.com>
2020-08-20 22:41:52 -0400

    Respot Tab contents when index changes
    Problem: When the `index` changes programmatically, focus is lost.
    
    Fix: Detect `index` change and, if necessary, ask Spotlight to focus.
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- TabLayout/RefocusDecorator.js
+++ TabLayout/RefocusDecorator.js
@@ -9,13 +9,19 @@ function getTabsSpotlightId (spotlightId, collapsed) {
 
 const RefocusDecorator = Wrapped => {
 	// eslint-disable-next-line no-shadow
-	function RefocusDecorator ({collapsed, onTabAnimationEnd, orientation, spotlightId, ...rest}) {
+	function RefocusDecorator ({collapsed, index, onTabAnimationEnd, orientation, spotlightId, ...rest}) {
 		const {generateId} = useId({prefix: 'sand-tablayout-'});
 
 		// generate an id for the component (and a derived id for the tabs) so we can refocus them
 		// generating a different ID by orientation so swapping orientations doesn't clear container
 		// config before the new one is mounted
 		spotlightId = spotlightId || generateId(orientation || 'vertical');
+
+		React.useLayoutEffect(() => {
+			if (collapsed && !Spotlight.getPointerMode() && !Spotlight.getCurrent()) {
+				Spotlight.focus();
+			}
+		}, [index]);	// eslint-disable-line react-hooks/exhaustive-deps
 
 		const handleTabAnimationEnd = React.useCallback((ev) => {
 			if (onTabAnimationEnd) {
@@ -36,6 +42,7 @@ const RefocusDecorator = Wrapped => {
 			<Wrapped
 				{...rest}
 				collapsed={collapsed}
+				index={index}
 				onTabAnimationEnd={handleTabAnimationEnd}
 				orientation={orientation}
 				spotlightId={spotlightId}
@@ -45,6 +52,7 @@ const RefocusDecorator = Wrapped => {
 
 	RefocusDecorator.propTypes = {
 		collapsed: PropTypes.bool,
+		index: PropTypes.number,
 		onTabAnimationEnd: PropTypes.func,
 		orientation: PropTypes.string,
 		spotlightId: PropTypes.string
