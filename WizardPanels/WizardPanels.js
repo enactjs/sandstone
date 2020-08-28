32811559c30c063392338150b138ecea9a4fa722
Jeremy Thomas <thomas.jeremy.e@gmail.com>
2020-08-28 15:14:42 -0400

    use rtl over reverseTransition

--- WizardPanels/WizardPanels.js
+++ WizardPanels/WizardPanels.js
@@ -233,12 +233,12 @@ const WizardPanelsBase = kind({
 		prevButtonVisibility: PropTypes.oneOf(['auto', 'always', 'never']),
 
 		/**
-		 * Explicitly sets the ViewManager transition direction.
+		 * Used to determine the transition direction.
 		 *
 		 * @type {Boolean}
 		 * @private
 		 */
-		reverseTransition: PropTypes.bool,
+		rtl: PropTypes.bool,
 
 		/**
 		* The subtitle to display.
@@ -357,7 +357,7 @@ const WizardPanelsBase = kind({
 		onWillTransition,
 		prevButton,
 		prevButtonVisibility,
-		reverseTransition,
+		rtl,
 		steps,
 		subtitle,
 		title,
@@ -425,7 +425,7 @@ const WizardPanelsBase = kind({
 								onTransition={onTransition}
 								onWillTransition={onWillTransition}
 								noAnimation={noAnimation}
-								reverseTransition={reverseTransition}
+								rtl={rtl}
 							>
 								{children}
 							</ViewManager>
@@ -440,21 +440,6 @@ const WizardPanelsBase = kind({
 		);
 	}
 });
-
-// single-index ViewManagers need some help knowing when the transition direction needs to change
-// because the index is always 0 from its perspective.
-function useReverseTransition (index = -1, rtl) {
-	const [prevIndex, setPrevIndex] = React.useState(-1);
-	let [reverse, setReverse] = React.useState(rtl);
-
-	if (prevIndex !== index) {
-		reverse = rtl ? (index > prevIndex) : (index < prevIndex);
-		setReverse(reverse);
-		setPrevIndex(index);
-	}
-
-	return reverse;
-}
 
 /**
  * WizardPanelsRouter passes the children, footer, subtitle, and title from
@@ -475,14 +460,12 @@ const WizardPanelsRouter = (Wrapped) => {
 		onTransition,
 		onWillTransition,
 		title,
-		rtl,
 		...rest
 	}) => {
 		const [panel, setPanel] = React.useState(null);
 		const {ref: a11yRef, onWillTransition: a11yOnWillTransition} = useToggleRole();
 		const autoFocus = useAutoFocus({autoFocus: 'default-element', hideChildren: panel == null});
 		const ref = useChainRefs(autoFocus, a11yRef, componentRef);
-		const reverseTransition = useReverseTransition(index, rtl);
 		const {
 			onWillTransition: focusOnWillTransition,
 			...transition
@@ -511,7 +494,6 @@ const WizardPanelsRouter = (Wrapped) => {
 					onWillTransition={handleWillTransition}
 					title={currentTitle}
 					totalPanels={totalPanels}
-					reverseTransition={reverseTransition}
 				>
 					{children}
 				</Wrapped>
