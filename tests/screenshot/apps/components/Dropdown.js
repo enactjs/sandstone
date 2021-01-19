5da170e9618a504251a62e4251413d622725eed4
sangwook1203.lee <sangwook1203.lee@lge.com>
2021-01-19 13:41:54 +0900

    updated screenshot tc for Dropdown

--- tests/screenshot/apps/components/Dropdown.js
+++ tests/screenshot/apps/components/Dropdown.js
@@ -3,10 +3,7 @@ import React from 'react';
 
 import {withProps} from './utils';
 
-// TO DO: add children options
-// TO DO: add direction up/down
-// TO DO: add open/close
-// TO DO: add RTL tests
+const children = (new Array(5)).fill().map((i, index) => `Option ${index + 1}`);
 
 const Widths = [
 	<Dropdown placeholder="Dropdown" width="tiny" />,
@@ -37,7 +34,26 @@ const DropdownTests = [
 	// size="large"
 	...withProps({
 		size: 'large'
-	}, Widths)
+	}, Widths),
 
+	// open with children
+	<Dropdown open title="Select an option below">
+		{children}
+	</Dropdown>,
+
+	// direction - 'above', 'below'
+	// 'above' is default
+	<Dropdown title="Select an option above" direction="above">
+		{children}
+	</Dropdown>,
+	<Dropdown title="Select an option below">
+		{children}
+	</Dropdown>,
+
+	// locale = 'ar-SA'
+	{
+		locale: 'ar-SA',
+		component: <Dropdown title="Select an option below">{children}</Dropdown>
+	}
 ];
 export default DropdownTests;
