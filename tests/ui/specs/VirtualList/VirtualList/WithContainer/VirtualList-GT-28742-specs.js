1ee5ad7ea863c6325758354864314d6ceff5d652
dongsu.won <dongsu.won@lgepartner.com>
2021-01-20 16:15:27 +0900

    PLAT-126350: Add UI Test for VirtualList.
    *Add UI Test for VirtualList(GT-28742, 32169).
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Create new view for VirtualList Test.
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Add button in VirtualList View for VirtualList Test.
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Add api for  VirtualList Test.
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>
    
    *Remove api that not work.
    
    Enact-DCO-1.0-Signed-off-by: dongsuwon <dongsu.won@lgepartner.com>

---
+++ tests/ui/specs/VirtualList/VirtualList/WithContainer/VirtualList-GT-28742-specs.js
@@ -0,0 +1,23 @@
+const Page = require('../VirtualListPage');
+
+describe('With Container', function () {
+	beforeEach(function () {
+		Page.open('WithContainer');
+	});
+
+	it('should Spotlight Moves between Spottable Controls of Items [GT-28742]', function () {
+		// Step 1: 5-way Spot a star icon (★) of the first ('Item 000').
+		Page.spotlightDown();
+		Page.spotlightRight();
+		Page.spotlightRight();
+		// STep 1 Verify: Spotlight displays on a star icon(★) of the first item ('item 000').
+		expect(Page.getElementAttribute('id')).to.equal('starIcon');
+		expect(Number(Page.getElementAttribute('data-index'))).to.equal(0);
+		// 5-way Up hold for 1 second.
+		browser.keys('Down Arrow');
+		// Spotlight still on any item's starIcon.
+		// As Jenkins'result is unexpectable by performance, make sure that spotlight is in starIcon only.
+		expect(Page.getElementAttribute('id')).to.equal('starIcon');
+		expect(Number(Page.getElementAttribute('data-index'))).to.above(0);
+	});
+});
