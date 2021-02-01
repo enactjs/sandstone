b377b7a5c431dc63923c92d018e08533bc642c0e
Yunho Lee0 <yunho2.lee@lge.com>
2021-02-01 18:09:31 +0900

    Register existing addons (knobs, actions)

--- samples/sampler/.storybook/main.js
+++ samples/sampler/.storybook/main.js
@@ -3,7 +3,11 @@
 const webpack = require('@enact/storybook-utils/configs/webpack');
 
 module.exports = {
-	stories: ['./../stories/default/*.js'],
+    stories: ['./../stories/default/*.js'],
+    addons: [
+		'@enact/storybook-utils/addons/knobs/register',
+		'@enact/storybook-utils/addons/actions/register'
+	],
 	webpackFinal: async (config, {configType}) => {
 		return webpack(config, configType, __dirname);
 	}
--- samples/sampler/.storybook/preview.js
+++ samples/sampler/.storybook/preview.js
@@ -1,3 +1,14 @@
+import {addDecorator} from '@storybook/react';
+import {configureActions} from '@enact/storybook-utils/addons/actions';
+import {withKnobs} from '@enact/storybook-utils/addons/knobs';
+
 import ThemeEnvironment from '../src/ThemeEnvironment'
 
+configureActions();
+addDecorator(withKnobs);
+export const parameters = {
+	knobs: {
+		timestamps: true,
+	}
+};
 export const decorators = [ThemeEnvironment];
