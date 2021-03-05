3114369a39e28c9c6a00c07d8f66818e1cb51e38
jeonghee27 <jeonghee27.ahn@lge.com>
2021-03-05 16:48:25 +0900

    Add Slider ui-test (migrated from agate) Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

---
+++ tests/ui/apps/Slider/Slider-View.js
@@ -0,0 +1,32 @@
+import {scaleToRem} from '@enact/ui/resolution';
+
+import Heading from '../../../../Heading';
+import Scroller from '../../../../Scroller';
+import Slider from '../../../../Slider';
+import ThemeDecorator from '../../../../ThemeDecorator';
+
+const app = (props) => <div {...props}>
+	<div>
+		<Scroller style={{height: scaleToRem(900)}}>
+			<Heading>Slider default</Heading>
+			<Slider defaultValue={0} id="sliderDefault"  />
+
+			<Heading>Slider activate on slect</Heading>
+			<Slider activateOnSelect id="sliderActivateOnSelect"  />
+
+			<Heading>Slider custom progressAnchor</Heading>
+			<Slider activateOnSelect id="sliderCustomProgressAnchor" progressAnchor={0.7} />
+
+			<Heading>Slider vertical</Heading>
+			<Slider id="sliderVertical" max={10} orientation="vertical" />
+
+			<Heading>Slider disabled</Heading>
+			<Slider disabled id="sliderDisabled"  />
+
+			<Heading>Slider vertical disabled</Heading>
+			<Slider disabled id="sliderVerticalDisabled" max={10} orientation="vertical" />
+		</Scroller>
+	</div>
+</div>;
+
+export default ThemeDecorator(app);
---
+++ tests/ui/specs/Slider/SliderPage.js
@@ -0,0 +1,53 @@
+'use strict';
+const {Page} = require('@enact/ui-test-utils/utils');
+
+class SliderInterface {
+	constructor (id) {
+		this.id = id;
+		this.selector = `#${this.id}`;
+	}
+
+	focus () {
+		return browser.execute((el) => el.focus(), $(`#${this.id}`));
+	}
+
+	get self () {
+		return $(this.selector);
+	}
+
+	get knob () {
+		return $(this.selector + ' .Slider_Slider_knob');
+	}
+
+	get knobPositionHorizontal () {
+		return parseInt(this.knob.getCSSProperty('left').value);
+	}
+
+	get knobPositionVertical () {
+		return parseInt(this.knob.getCSSProperty('bottom').value);
+	}
+
+	get sliderFillWidth () {
+		return parseInt($(this.selector + ' .Slider_Slider_fill').getCSSProperty('width').value);
+	}
+}
+
+class SliderPage extends Page {
+	constructor () {
+		super();
+		this.title = 'Slider Test';
+		const sliderDefault = new SliderInterface('sliderDefault');
+		const sliderDisabled = new SliderInterface('sliderDisabled');
+		const sliderCustomProgressAnchor = new SliderInterface('sliderCustomProgressAnchor');
+		const sliderVertical = new SliderInterface('sliderVertical');
+		const sliderVerticalDisabled = new SliderInterface('sliderVerticalDisabled');
+		const sliderActivateOnSelect = new SliderInterface('sliderActivateOnSelect');
+		this.components = {sliderDefault, sliderDisabled, sliderCustomProgressAnchor, sliderVertical, sliderVerticalDisabled, sliderActivateOnSelect};
+	}
+
+	open (urlExtra) {
+		super.open('Slider-View', urlExtra);
+	}
+}
+
+module.exports = new SliderPage();
