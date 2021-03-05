65b2d5b07060a9d98cbfc348b908f25977a08de2
Mikyung Kim <mikyung27.kim@lge.com>
2021-03-05 16:32:33 +0900

    fix
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- Dropdown/DropdownList.js
+++ Dropdown/DropdownList.js
@@ -197,7 +197,6 @@ const DropdownListSpotlightDecorator = hoc((config, Wrapped) => {
 		componentDidMount () {
 			// eslint-disable-next-line react/no-find-dom-node
 			this.node = ReactDOM.findDOMNode(this);
-
 			Spotlight.set(this.node.dataset.spotlightId, {
 				defaultElement: '[data-selected="true"]',
 				enterTo: 'default-element'
