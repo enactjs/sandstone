4ae4bd17a3aaa695b264efe23f698b67435e4eae
jeonghee27 <jeonghee27.ahn@lge.com>
2021-04-15 09:38:10 +0900

    Apply review

--- Scroller/useThemeScroller.js
+++ Scroller/useThemeScroller.js
@@ -30,6 +30,8 @@ const getFocusableBodyProps = (scrollContainerRef, contentId, isScrollbarVisible
 			});
 			return true;
 		}
+
+		return false;
 	};
 
 	const getNavigableFilterTarget = (ev) => {
