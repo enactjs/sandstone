a2ee30717ce751a89217ae0deb3ac08173ec0c47
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-05-18 18:16:17 +0300

    added ss tests for MediaOverlay

--- tests/screenshot/apps/components/MediaOverlay.js
+++ tests/screenshot/apps/components/MediaOverlay.js
@@ -2,7 +2,7 @@ import MediaOverlay from '../../../../MediaOverlay';
 
 import img from '../../images/600x600.png';
 
-import {withConfig} from "./utils";
+import {withConfig} from './utils';
 
 const MediaOverlayTests = [
 	// Base Tests
@@ -18,9 +18,9 @@ const MediaOverlayTests = [
 	<MediaOverlay imageOverlay={img} title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
 	<MediaOverlay imageOverlay={img} subtitle="07:00 AM - 08:00 AM" />,
 
-	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start"/>,
-	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center"/>,
-	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end"/>,
+	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start" />,
+	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center" />,
+	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end" />,
 
 	<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress />,
 	<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"  />,
@@ -40,12 +40,12 @@ const MediaOverlayTests = [
 		<MediaOverlay imageOverlay={img} title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
 		<MediaOverlay imageOverlay={img} subtitle="07:00 AM - 08:00 AM" />,
 
-		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start"/>,
-		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center"/>,
-		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end"/>,
+		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start" />,
+		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center" />,
+		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end" />,
 
 		<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress />,
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"  />,
+		<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"  />
 	])
 
 ];
