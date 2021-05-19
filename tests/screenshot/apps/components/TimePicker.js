d0eeddbcf2986740dc49353f0742ec061afc14c0
Stanca Pop <stanca.pop@lgepartner.com>
2021-05-19 14:54:27 +0300

    added extra ss tests

--- tests/screenshot/apps/components/TimePicker.js
+++ tests/screenshot/apps/components/TimePicker.js
@@ -2,10 +2,20 @@ import TimePicker from '../../../../TimePicker';
 
 const TimePickerTests = [
 	<TimePicker defaultValue={new Date(2009, 5, 6)} />,
+	<TimePicker disabled value={new Date()} />,
+	// RTL
 	{
 		locale: 'ar-SA',
 		component: <TimePicker defaultValue={new Date(2009, 5, 6)} />
+	},
+	{
+		locale: 'ar-SA',
+		component: <TimePicker disabled value={new Date()} />
+	},
+	// long meridiem characters
+	{
+		locale: 'ta-IN',
+		component: <TimePicker disabled value={new Date()} />
 	}
-
 ];
 export default TimePickerTests;
