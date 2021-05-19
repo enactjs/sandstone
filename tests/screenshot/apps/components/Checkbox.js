fc66bc9f19bfdd8e534a01e183e8166367086313
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-05-19 16:31:26 +0300

    updated screenshot tests for Checkbox and CheckboxItem

--- tests/screenshot/apps/components/Checkbox.js
+++ tests/screenshot/apps/components/Checkbox.js
@@ -3,7 +3,10 @@ import Checkbox from '../../../../Checkbox';
 const CheckboxTests = [
 	<Checkbox />,
 	<Checkbox selected />,
+	<Checkbox selected disabled />,
 	<Checkbox>star</Checkbox>,
-	<Checkbox selected>star</Checkbox>
+	<Checkbox selected>star</Checkbox>,
+	<Checkbox indeterminate />,
+	<Checkbox indeterminate disabled />
 ];
 export default CheckboxTests;
