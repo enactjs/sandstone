70056eebd3b7564cb1406e1729a6d264576291f4
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-05-20 13:08:54 +0300

    Created the defaultImageItemTests. Added : <ImageItem src={img} orientation="horizontal" imageIconSrc={img} /> <ImageItem src={img} orientation="horizontal" label="Short" imageIconSrc={img}>Short</ImageItem> to defaultImageItemTests. Added screenshots tests for centered, disabled.

--- tests/screenshot/apps/components/ImageItem.js
+++ tests/screenshot/apps/components/ImageItem.js
@@ -2,12 +2,16 @@ import ri from '@enact/ui/resolution';
 
 import ImageItem from '../../../../ImageItem';
 
+import {withProps} from './utils';
+
+
 import img from '../../images/600x600.png';
 
 // vertical ImageItem doesn't render well without defined styles right now.
 const verticalStyle = {height: ri.scale(540), width: ri.scale(640)};
 
-const ImageItemTests = [
+const defaultImageItemTests = [
+	// Vertical
 	<ImageItem src={img} style={verticalStyle} orientation="vertical" />,
 	<ImageItem src={img} style={verticalStyle} orientation="vertical">Short</ImageItem>,
 	<ImageItem src={img} style={verticalStyle} orientation="vertical" label="Short" />,
@@ -17,11 +21,28 @@ const ImageItemTests = [
 	<ImageItem src={img} style={verticalStyle} orientation="vertical" label="Short" showSelection>Short</ImageItem>,
 	<ImageItem src={img} style={verticalStyle} orientation="vertical" label="Short" selected showSelection>Short</ImageItem>,
 
+	// Horizontal
 	<ImageItem src={img} orientation="horizontal" />,
 	<ImageItem src={img} orientation="horizontal">Short</ImageItem>,
 	<ImageItem src={img} orientation="horizontal" label="Short" />,
+	<ImageItem src={img} orientation="horizontal" imageIconSrc={img} />,
 	<ImageItem src={img} orientation="horizontal" label="Short">Short</ImageItem>,
+	<ImageItem src={img} orientation="horizontal" label="Short" imageIconSrc={img}>Short</ImageItem>,
 	<ImageItem src={img} orientation="horizontal" label="Short" showSelection>Short</ImageItem>,
 	<ImageItem src={img} orientation="horizontal" label="Short" selected showSelection>Short</ImageItem>
 ];
+
+const ImageItemTests = [
+	...defaultImageItemTests,
+
+	// Centered
+	...withProps({centered:true}, defaultImageItemTests),
+
+	// Disabled
+	...withProps({disabled: true}, defaultImageItemTests),
+
+	// Centered and disabled
+	...withProps({centered: true, disabled: true}, defaultImageItemTests)
+];
+
 export default ImageItemTests;
