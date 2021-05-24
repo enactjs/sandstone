d77cab942cbe75bdb009f26c00cb750743baeb30
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-05-24 11:00:44 +0300

    Fix.

--- tests/screenshot/apps/components/Sprite.js
+++ tests/screenshot/apps/components/Sprite.js
@@ -5,8 +5,8 @@ import spriteGear2k from '../../images/sprite-gear-2k.png';
 import spriteGear4k from '../../images/sprite-gear-4k.png';
 
 const LTR = [
-    <Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="horizontal" stopped={true}/>,
-    <Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="vertical" stopped={true}/>
+    <Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="horizontal" stopped/>,
+    <Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="vertical" stopped/>
 ];
 
 const SpriteTests = [
