04ca87bd758d582b33190b58042d24d018f21231
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-05-24 11:12:53 +0300

    Fix.

--- tests/screenshot/apps/components/Sprite.js
+++ tests/screenshot/apps/components/Sprite.js
@@ -5,18 +5,18 @@ import spriteGear2k from '../../images/sprite-gear-2k.png';
 import spriteGear4k from '../../images/sprite-gear-4k.png';
 
 const LTR = [
-    <Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="horizontal" stopped/>,
-    <Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="vertical" stopped/>
+	<Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="horizontal" stopped />,
+	<Sprite src={{fhd: spriteGear2k, uhd: spriteGear4k}} columns={6} rows={5} height={120} width={120} orientation="vertical" stopped />
 ];
 
 const SpriteTests = [
-    ...LTR,
+	...LTR,
 
-    // RTL
-    ...withConfig({locale: 'ar-SA'}, LTR),
+	// RTL
+	...withConfig({locale: 'ar-SA'}, LTR),
 
-    // Tallglyphs
-    ...withConfig({locale: 'vi-VN'}, LTR)
+	// Tallglyphs
+	...withConfig({locale: 'vi-VN'}, LTR)
 ];
 
 export default SpriteTests;
