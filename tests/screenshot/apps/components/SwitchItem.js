e476a6d0e79d3269676538fd6aa5bf68584a2733
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-05-24 17:30:13 +0300

    added ss tests for SwitchItem

--- tests/screenshot/apps/components/SwitchItem.js
+++ tests/screenshot/apps/components/SwitchItem.js
@@ -9,6 +9,8 @@ const SwitchItemTests = [
 	<SwitchItem selected>Hello SwitchItem</SwitchItem>,
 	<SwitchItem disabled>Hello SwitchItem</SwitchItem>,
 	<SwitchItem disabled selected>Hello SwitchItem</SwitchItem>,
+	<SwitchItem inline>Hello SwitchItem</SwitchItem>,
+	<SwitchItem inline selected>Hello SwitchItem</SwitchItem>,
 
 	// Icon slotAfter
 	<SwitchItem>SwitchItem<Icon slot="slotAfter">home</Icon></SwitchItem>,
@@ -25,6 +27,8 @@ const SwitchItemTests = [
 		<SwitchItem selected>Hello SwitchItem</SwitchItem>,
 		<SwitchItem disabled>Hello SwitchItem</SwitchItem>,
 		<SwitchItem disabled selected>Hello SwitchItem</SwitchItem>,
+		<SwitchItem inline>Hello SwitchItem</SwitchItem>,
+		<SwitchItem inline selected>Hello SwitchItem</SwitchItem>,
 		<SwitchItem>SwitchItem<Icon slot="slotAfter">home</Icon></SwitchItem>,
 		<SwitchItem centered>Hello SwitchItem</SwitchItem>,
 		<SwitchItem centered>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam dapibus imperdiet. Morbi diam ex, vulputate eget luctus eu, gravida at ligula. Sed tristique eros sit amet iaculis varius. Phasellus rutrum augue id nulla consectetur, a vulputate velit dictum. Vestibulum ultrices tellus ac cursus condimentum. Aliquam sit amet consectetur nulla, viverra bibendum metus.</SwitchItem>
