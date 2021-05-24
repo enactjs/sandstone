ddc23110e43ed2874fbaf8a5f127ce694d540a53
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-05-24 14:44:16 +0300

    added extra ss tests

--- tests/screenshot/apps/components/Tooltip.js
+++ tests/screenshot/apps/components/Tooltip.js
@@ -4,7 +4,7 @@ import {withConfig} from './utils';
 
 const TooltipDisplay = (props) => (
 	<div {...props}>
-		<Tooltip type={props.type} direction={props.direction} arrowAnchor={props.arrowAnchor} style={{top: '50%', left: '50%'}}>{`View ${props.type} ${props.direction} ${props.arrowAnchor}`}</Tooltip>
+		<Tooltip type={props.type} direction={props.direction} arrowAnchor={props.arrowAnchor} style={{top: '50%', left: '50%'}} marquee={props.marquee} width={props.tooltipWidth}>{`View ${props.type} ${props.direction} ${props.arrowAnchor}`}</Tooltip>
 	</div>
 );
 
@@ -19,11 +19,32 @@ const ToolTipTests = withConfig({
 
 	TooltipDisplay({type: 'balloon', direction:'right', arrowAnchor:'middle'}),
 
+	TooltipDisplay({type: 'balloon', direction:'left', arrowAnchor:'middle'}),
+
 	TooltipDisplay({type: 'transparent', direction:'below', arrowAnchor:'center'}),
 
 	TooltipDisplay({type: 'transparent', direction:'above', arrowAnchor:'center'}),
 
-	TooltipDisplay({type: 'transparent', direction:'left', arrowAnchor:'middle'})
+	TooltipDisplay({type: 'transparent', direction:'left', arrowAnchor:'middle'}),
+
+	// Custom width
+	TooltipDisplay({type: 'balloon', direction:'above', arrowAnchor:'center', tooltipWidth: 200}),
+
+	TooltipDisplay({type: 'balloon', direction:'below', arrowAnchor:'center', tooltipWidth: 200}),
+
+	TooltipDisplay({type: 'balloon', direction:'right', arrowAnchor:'middle', tooltipWidth: 200}),
+
+	TooltipDisplay({type: 'balloon', direction:'left', arrowAnchor:'middle', tooltipWidth: 200}),
+
+	// Testing marquee
+	TooltipDisplay({type: 'balloon', direction:'above', arrowAnchor:'center', tooltipWidth: 200, marquee: true}),
+
+	TooltipDisplay({type: 'balloon', direction:'below', arrowAnchor:'center', tooltipWidth: 200, marquee: true}),
+
+	TooltipDisplay({type: 'balloon', direction:'right', arrowAnchor:'middle', tooltipWidth: 200, marquee: true}),
+
+	TooltipDisplay({type: 'balloon', direction:'left', arrowAnchor:'middle', tooltipWidth: 200, marquee: true})
+
 ]);
 
 export default ToolTipTests;
