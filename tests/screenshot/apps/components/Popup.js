c0989c34d684c89ffdaa888f8033296d7a61f6b9
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-06-29 10:31:55 +0300

    added tests for each value of scrimType

--- tests/screenshot/apps/components/Popup.js
+++ tests/screenshot/apps/components/Popup.js
@@ -1,10 +1,8 @@
 import Popup from '../../../../Popup';
-import {withConfig} from './utils';
+import {withConfig, withProps} from './utils';
 
-const PopupTests = [
+const PopupBaseTests = [
 	<Popup open>Popup!</Popup>,
-	<Popup open scrimType="transparent">Popup!</Popup>,
-	<Popup open scrimType="none">Popup!</Popup>,
 
 	// [GT-28270]
 	<Popup open position="fullscreen">Fullscreen Popup!</Popup>,
@@ -13,35 +11,22 @@ const PopupTests = [
 	<Popup open position="right">Right Popup!</Popup>,
 	<Popup open position="top">Top Popup!</Popup>,
 	// End of [GT-28270]
+];
+
+const PopupTests = [
+	...withProps({scrimType: "translucent"}, PopupBaseTests),
+	...withProps({scrimType: "transparent"}, PopupBaseTests),
+	...withProps({scrimType: "none"}, PopupBaseTests),
 
 	// *************************************************************
 	// locale = 'ar-SA'
 	// *************************************************************
 	// [GT-28262]
-	{
-		locale: 'ar-SA',
-		component: <Popup open>Popup!</Popup>
-	},
-	{
-		locale: 'ar-SA',
-		component: <Popup open position="fullscreen">Fullscreen Popup!</Popup>
-	},
-	{
-		locale: 'ar-SA',
-		component: <Popup open position="center">Center Popup!</Popup>
-	},
-	{
-		locale: 'ar-SA',
-		component: <Popup open position="left">Left Popup!</Popup>
-	},
-	{
-		locale: 'ar-SA',
-		component: <Popup open position="right">Right Popup!</Popup>
-	},
-	{
-		locale: 'ar-SA',
-		component: <Popup open position="top">Top Popup!</Popup>
-	}
+	...withConfig({locale: 'ar-SA'}, [
+		...withProps({scrimType: "translucent"}, PopupBaseTests),
+		...withProps({scrimType: "transparent"}, PopupBaseTests),
+		...withProps({scrimType: "none"}, PopupBaseTests),
+	])
 ];
 
 export default withConfig({wrapper: {full: true}}, PopupTests);
