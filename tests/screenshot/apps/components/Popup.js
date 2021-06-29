ccd903537d92bd5b784004ed0ba3060a1b6d1adf
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-06-29 10:45:37 +0300

    added tests for each value of scrimType

--- tests/screenshot/apps/components/Popup.js
+++ tests/screenshot/apps/components/Popup.js
@@ -9,23 +9,23 @@ const PopupBaseTests = [
 	<Popup open position="center">Center Popup!</Popup>,
 	<Popup open position="left">Left Popup!</Popup>,
 	<Popup open position="right">Right Popup!</Popup>,
-	<Popup open position="top">Top Popup!</Popup>,
+	<Popup open position="top">Top Popup!</Popup>
 	// End of [GT-28270]
 ];
 
 const PopupTests = [
-	...withProps({scrimType: "translucent"}, PopupBaseTests),
-	...withProps({scrimType: "transparent"}, PopupBaseTests),
-	...withProps({scrimType: "none"}, PopupBaseTests),
+	...withProps({scrimType: 'translucent'}, PopupBaseTests),
+	...withProps({scrimType: 'transparent'}, PopupBaseTests),
+	...withProps({scrimType: 'none'}, PopupBaseTests),
 
 	// *************************************************************
 	// locale = 'ar-SA'
 	// *************************************************************
 	// [GT-28262]
 	...withConfig({locale: 'ar-SA'}, [
-		...withProps({scrimType: "translucent"}, PopupBaseTests),
-		...withProps({scrimType: "transparent"}, PopupBaseTests),
-		...withProps({scrimType: "none"}, PopupBaseTests),
+		...withProps({scrimType: 'translucent'}, PopupBaseTests),
+		...withProps({scrimType: 'transparent'}, PopupBaseTests),
+		...withProps({scrimType: 'none'}, PopupBaseTests)
 	])
 ];
 
