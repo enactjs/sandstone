53c82c0e2933c3bd9bfd80de15a5aa5b1e59f1d6
alexandrumorariu <84004128+alexandrumorariu@users.noreply.github.com>
2021-07-06 14:39:36 +0300

    Changed the spacing

--- ProgressButton/tests/ProgressButton-specs.js
+++ ProgressButton/tests/ProgressButton-specs.js
@@ -4,76 +4,82 @@ import {ProgressButtonBase} from '../ProgressButton';
 
 describe('ProgressButton Specs', () => {
 	test('should show text', () => {
-		const expected = 'Progress Button';
 		const {getByRole} = render(
 			<ProgressButtonBase>
 				Progress Button
 			</ProgressButtonBase>
 		);
-
+		
+		const expected = 'Progress Button';
 		const actual = getByRole('button').textContent;
+		
 		expect(actual).toBe(expected);
 	});
 
 	test('should have a root className `progressButton`', () => {
-		const expected = 'progressButton';
 		const {getByRole} = render(
 			<ProgressButtonBase>
 				Progress Button
 			</ProgressButtonBase>
 		);
 
+		const expected = 'progressButton';
 		const actual = getByRole('button').className;
+		
 		expect(actual).toContain(expected);
 	});
 
 	test('should show radial progress', () => {
-		const expected = 'radial';
 		const {getByRole} = render(
 			<ProgressButtonBase showProgress>
 				Progress Button
 			</ProgressButtonBase>
 		);
 
+		const expected = 'radial';
 		const actual = getByRole('progressbar').className;
+		
 		expect(actual).toContain(expected);
 	});
 
 	test('should show 0.5 progress', () => {
-		const expected = 0.5;
 		const {getByRole} = render(
 			<ProgressButtonBase showProgress progress={0.5}>
 				Progress Button
 			</ProgressButtonBase>
 		);
 
+		const expected = 0.5;
 		const styleValues = getByRole('progressbar').style._values;
 		const {'--ui-progressbar-proportion-end':actual} = styleValues;
+	
 		expect(actual).toBe(expected.toString());
 	});
 
 	test('should show default icon `stop`', () => {
-		const expectedCode = 983004;
 		const {getByRole} = render(
 			<ProgressButtonBase showProgress progress={0.5}>
 				Progress Button
 			</ProgressButtonBase>
 		);
 
+		const expectedCode = 983004;
 		const actualCode = getByRole('button').textContent.codePointAt();
+
 		expect(actualCode).toBe(expectedCode);
 	});
 
 	test('should support a custom icon', () => {
-		const expected = 'star';
-		const expectedCode = 983080;
+		const customIcon = 'star';
 		const {getByRole} = render(
-			<ProgressButtonBase showProgress progress={0.5} icon={expected}>
+			<ProgressButtonBase showProgress progress={0.5} icon={customIcon}>
 				Progress Button
 			</ProgressButtonBase>
 		);
 
+		const expectedCode = 983080;
 		const actualCode = getByRole('button').textContent.codePointAt();
+
 		expect(actualCode).toBe(expectedCode);
 	});
 });
