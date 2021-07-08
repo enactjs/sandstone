c4fbb92890918cf6311610bfd391672b9c6afccd
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-07-08 15:32:45 +0300

    Init

--- Picker/tests/Picker-specs.js
+++ Picker/tests/Picker-specs.js
@@ -1,16 +1,19 @@
-import {mount} from 'enzyme';
+import {render} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import {Picker, PickerBase} from '../Picker';
 
 describe('Picker Specs', () => {
+	console.log('----------------------------------');
 	test('should render selected child wrapped with <PickerItem/>', () => {
-		const picker = mount(
-			<Picker value={1}>
+		const {getByTestId} = render(
+			<Picker data-testid='picker' value={1}>
 				{[1, 2, 3, 4]}
 			</Picker>
 		);
 
+		const picker = getByTestId('picker');
 		const expected = '2';
-		const actual = picker.find('PickerItem').text();
+		const actual = picker.children.item(1).textContent;
 
 		expect(actual).toBe(expected);
 	});
@@ -18,83 +21,93 @@ describe('Picker Specs', () => {
 	test(
 		'should set the max of <Picker> to be one less than the number of children',
 		() => {
-			const picker = mount(
-				<Picker value={1}>
+			const {getAllByRole, getByTestIdt} = render(
+				<Picker data-testid='picker' value={3}>
 					{[1, 2, 3, 4]}
 				</Picker>
 			);
 
-			const expected = 3;
-			const actual = picker.find('Picker').last().prop('max');
+			const arrowForward = getAllByRole('button')[1];
 
-			expect(actual).toBe(expected);
+			console.log(arrowForward.getAttributeNames());
+			userEvent.click(arrowForward);
+			userEvent.click(arrowForward);
+			console.log(arrowForward.getAttributeNames());
+			console.log(arrowForward.attributes.getNamedItem('aria-controls').textContent);
+			// const expected = 3;
+			// const actual = picker.find('Picker').last().prop('max');
+			//
+			// expect(actual).toBe(expected);
 		}
 	);
 
-	test('should be disabled when empty', () => {
-		const picker = mount(
-			<PickerBase>
-				{[]}
-			</PickerBase>
-		);
-
-		const actual = picker.find('Picker').last().prop('disabled');
-		expect(actual).toBe(true);
-	});
-
-	test('should set "data-webos-voice-disabled" to decrement button when voice control is disabled', () => {
-		const picker = mount(
-			<PickerBase data-webos-voice-disabled>
-				{[1, 2, 3, 4]}
-			</PickerBase>
-		);
-
-		const expected = true;
-		const actual = picker.find('PickerButton').at(0).prop('data-webos-voice-disabled');
-
-		expect(actual).toBe(expected);
-	});
-
-	test('should set "data-webos-voice-disabled" to increment button when voice control is disabled', () => {
-		const picker = mount(
-			<PickerBase data-webos-voice-disabled>
-				{[1, 2, 3, 4]}
-			</PickerBase>
-		);
-
-		const expected = true;
-		const actual = picker.find('PickerButton').at(1).prop('data-webos-voice-disabled');
-
-		expect(actual).toBe(expected);
-	});
-
-	test('should have an heading element when \'title\'', () => {
-		const subject = mount(
-			<PickerBase title="title text">
-				{[1, 2, 3, 4]}
-			</PickerBase>
-		);
-
-		expect(subject.find('Heading')).toHaveLength(1);
-
-		const expected = 'title';
-		const actual = subject.find('Heading').prop('className');
-
-		expect(actual).toContain(expected);
-	});
-
-	test('should have an heading element with inline class when \'title\' and \'inlineTitle\'', () => {
-		const subject = mount(
-			<PickerBase title="title text" inlineTitle>
-				{[1, 2, 3, 4]}
-			</PickerBase>
-		);
-
-		expect(subject.find('Heading')).toHaveLength(1);
-
-		const expected = 'inline';
-		const actual = subject.find('Heading').prop('className');
-
-		expect(actual).toContain(expected);
-	});
+	// test('should be disabled when empty', () => {
+	// 	const {getByTestId} = render(
+	// 		<PickerBase data-testid='picker'>
+	// 			{[]}
+	// 		</PickerBase>
+	// 	);
+	//
+	// 	const picker = getByTestId('picker');
+	//
+	// 	const actual = picker.find('Picker').last().prop('disabled');
+	// 	expect(actual).toBe(true);
+	// });
+	//
+	// test('should set "data-webos-voice-disabled" to decrement button when voice control is disabled', () => {
+	// 	const picker = mount(
+	// 		<PickerBase data-webos-voice-disabled>
+	// 			{[1, 2, 3, 4]}
+	// 		</PickerBase>
+	// 	);
+	//
+	// 	const expected = true;
+	// 	const actual = picker.find('PickerButton').at(0).prop('data-webos-voice-disabled');
+	//
+	// 	expect(actual).toBe(expected);
+	// });
+	//
+	// test('should set "data-webos-voice-disabled" to increment button when voice control is disabled', () => {
+	// 	const picker = mount(
+	// 		<PickerBase data-webos-voice-disabled>
+	// 			{[1, 2, 3, 4]}
+	// 		</PickerBase>
+	// 	);
+	//
+	// 	const expected = true;
+	// 	const actual = picker.find('PickerButton').at(1).prop('data-webos-voice-disabled');
+	//
+	// 	expect(actual).toBe(expected);
+	// });
+	//
+	// test('should have an heading element when \'title\'', () => {
+	// 	const subject = mount(
+	// 		<PickerBase title="title text">
+	// 			{[1, 2, 3, 4]}
+	// 		</PickerBase>
+	// 	);
+	//
+	// 	expect(subject.find('Heading')).toHaveLength(1);
+	//
+	// 	const expected = 'title';
+	// 	const actual = subject.find('Heading').prop('className');
+	//
+	// 	expect(actual).toContain(expected);
+	// });
+	//
+	// test('should have an heading element with inline class when \'title\' and \'inlineTitle\'', () => {
+	// 	const subject = mount(
+	// 		<PickerBase title="title text" inlineTitle>
+	// 			{[1, 2, 3, 4]}
+	// 		</PickerBase>
+	// 	);
+	//
+	// 	expect(subject.find('Heading')).toHaveLength(1);
+	//
+	// 	const expected = 'inline';
+	// 	const actual = subject.find('Heading').prop('className');
+	//
+	// 	expect(actual).toContain(expected);
+	// });
+	console.log('----------------------------------');
 });
