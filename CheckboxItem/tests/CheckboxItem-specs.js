ba47b03db8cdbd6fa0cd9dc411178fae6290ac7c
adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>
2021-07-09 15:20:47 +0300

    WRN-1346: Migrate Checkbox and CheckboxItem unit tests to Testing Library (#985)
    * migrate Checkbox and CheckboxItem unit tests to react-testing-library
    
    * fixed lint warnings
    
    * review fixes

--- CheckboxItem/tests/CheckboxItem-specs.js
+++ CheckboxItem/tests/CheckboxItem-specs.js
@@ -1,18 +1,42 @@
-import {shallow} from 'enzyme';
-import {CheckboxItemBase} from '../CheckboxItem';
+import '@testing-library/jest-dom';
+import {render, fireEvent} from '@testing-library/react';
+
+import CheckboxItem, {CheckboxItemBase} from '../CheckboxItem';
 
 describe('CheckboxItem Specs', () => {
+
 	test('should support a custom icon', () => {
-		const expected = 'trash';
-
-		const subject = shallow(
-			<CheckboxItemBase icon={expected}>
-				Hello CheckboxItem
-			</CheckboxItemBase>
-		);
-
-		const actual = subject.find('Checkbox').prop('children');
+		const {getAllByRole} = render(<CheckboxItemBase icon="trash">Hello CheckboxItem</CheckboxItemBase>);
+		const checkboxItemElement = getAllByRole('checkbox');
+		const actual = checkboxItemElement[1].textContent.codePointAt();
+		const expected = 983077; // decimal converted charCode of Unicode 'trash' character
 
 		expect(actual).toBe(expected);
+	});
+
+	test('should have correct text', () => {
+		const {getByText} = render(<CheckboxItemBase>Hello CheckboxItem</CheckboxItemBase>);
+		const element = getByText(/Hello CheckboxItem/i);
+
+		expect(element).toBeInTheDocument();
+	});
+
+	test('should select with click', () => {
+		const {getAllByRole} = render(<CheckboxItem>Hello CheckboxItem</CheckboxItem>);
+		const checkboxItemElement = getAllByRole('checkbox');
+
+		fireEvent.click(checkboxItemElement[0]);
+
+		expect(checkboxItemElement[0].className).toContain('selected');
+	});
+
+	test('should select with click', () => {
+		const {getAllByRole} = render(<CheckboxItem>Hello CheckboxItem</CheckboxItem>);
+		const checkboxItemElement = getAllByRole('checkbox');
+
+		fireEvent.click(checkboxItemElement[0]);
+		fireEvent.click(checkboxItemElement[0]);
+
+		expect(checkboxItemElement[0].className).not.toContain('selected');
 	});
 });
