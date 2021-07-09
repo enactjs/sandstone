4197e427acbbec87c751999d8127e934d9158ab5
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2021-07-09 15:21:23 +0300

    WRN-1360: Migrate Switch and SwitchItem unit tests to Testing Library (#987)
    * migrated tests to react-testing library
    
    * fixed lint warning
    
    * deleted some empty lines
    
    * deleted some empty lines
    
    * refactored tests
    
    * fixed lint warning
    
    * minor refactoring

--- Switch/tests/Switch-specs.js
+++ Switch/tests/Switch-specs.js
@@ -1,43 +1,63 @@
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import {SwitchBase} from '../Switch';
-import css from '../Switch.module.less';
+import Switch, {SwitchBase} from '../Switch';
 
 describe('Switch Specs', () => {
+	test('should not have `selected` className', () => {
+		const {getByRole} = render(<SwitchBase />);
+		const SwitchNotSelected = getByRole('button');
 
-	test('should have selected', () => {
+		const unexpected = 'selected';
+		const actual = SwitchNotSelected.className;
 
-		const switchIcon = shallow(
-			<SwitchBase selected />
-		);
+		expect(actual).not.toContain(unexpected);
+	});
 
-		const expected = css.selected;
-		const actual = switchIcon.prop('className');
+	test('should have `selected` className', () => {
+		const {getByRole} = render(<SwitchBase selected />);
+		const SwitchSelected = getByRole('button');
+
+		const expected = 'selected';
+		const actual = SwitchSelected.className;
 
 		expect(actual).toContain(expected);
 	});
 
-	test('should have animated', () => {
+	test('should have `animated` className', () => {
+		const {getByRole} = render(<SwitchBase />);
+		const SwitchAnimated = getByRole('button');
 
-		const switchIcon = shallow(
-			<SwitchBase />
-		);
-
-		const expected = css.animated;
-		const actual = switchIcon.prop('className');
+		const expected = 'animated';
+		const actual = SwitchAnimated.className;
 
 		expect(actual).toContain(expected);
 	});
 
-	test('should have not animated', () => {
+	test('should not have `animated` className', () => {
+		const {getByRole} = render(<SwitchBase noAnimation />);
+		const SwitchNotAnimated = getByRole('button');
 
-		const switchIcon = shallow(
-			<SwitchBase noAnimation />
-		);
+		const unexpected = 'animated';
+		const actual = SwitchNotAnimated.className;
 
-		const expected = css.animated;
-		const actual = switchIcon.prop('className');
+		expect(actual).not.toContain(unexpected);
+	});
 
-		expect(actual).not.toContain(expected);
+	test('toggle Switch', () => {
+		const handleToggle = jest.fn();
+		const {getByRole} = render(<Switch onToggle={handleToggle} />);
+		const SwitchElement = getByRole('button');
+
+		userEvent.click(SwitchElement);
+
+		const expected = 'selected';
+		const actual = SwitchElement.className;
+
+		expect(actual).toContain(expected);
+
+		const expectedTimes = 1;
+
+		expect(handleToggle).toBeCalledTimes(expectedTimes);
 	});
 });
--- SwitchItem/tests/SwitchItem-specs.js
+++ SwitchItem/tests/SwitchItem-specs.js
@@ -1,33 +1,54 @@
-import {shallow} from 'enzyme';
-import {SwitchItemBase} from '../SwitchItem';
+import '@testing-library/jest-dom';
+import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import SwitchItem, {SwitchItemBase} from '../SwitchItem';
 
 describe('SwitchItem Specs', () => {
-
 	test('should contain a Switch', () => {
+		const {getAllByRole} = render(<SwitchItemBase />);
+		const Buttons = getAllByRole('button');
 
-		const subject = shallow(
-			<SwitchItemBase>
-				SwitchItem
-			</SwitchItemBase>
-		);
+		const expected = 'switch';
+		const actual = Buttons[1].className;
 
-		const expected = 1;
-		const actual = subject.find('Switch').length;
-
-		expect(actual).toBe(expected);
+		expect(actual).toContain(expected);
 	});
 
-	test('should pass selected to Switch element', () => {
+	test('should pass `selected` to Switch element', () => {
+		const {getAllByRole} = render(<SwitchItemBase selected />);
+		const Buttons = getAllByRole('button');
 
-		const subject = shallow(
-			<SwitchItemBase selected>
-				SwitchItem
-			</SwitchItemBase>
-		);
+		const expected = 'selected';
+		const SwitchItemElement = Buttons[0].className;
+		const SwitchElement = Buttons[1].className;
 
-		const expected = true;
-		const actual = subject.find('Switch').prop('selected');
+		expect(SwitchItemElement).toContain(expected);
+		expect(SwitchElement).toContain(expected);
+	});
 
-		expect(actual).toBe(expected);
+	test('should toggle Switch', () => {
+		const handleToggle = jest.fn();
+		const {getAllByRole} = render(<SwitchItem onToggle={handleToggle} />);
+		const Buttons = getAllByRole('button');
+		const Switch = Buttons[1];
+
+		userEvent.click(Switch);
+
+		const expected = 'selected';
+		const actual = Switch.className;
+
+		expect(actual).toContain(expected);
+
+		const expectedTimes = 1;
+
+		expect(handleToggle).toBeCalledTimes(expectedTimes);
+	});
+
+	test('should render correct children', () => {
+		const {getByText} = render(<SwitchItem>Hello SwitchItem</SwitchItem>);
+		const Child = getByText(/Hello SwitchItem/i);
+
+		expect(Child).toBeInTheDocument();
 	});
 });
