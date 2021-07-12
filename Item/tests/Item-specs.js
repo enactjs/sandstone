5e989baccd510c41b2f6a16e076aabeaf2915c94
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-07-12 17:20:23 +0300

    migrated unit tests for Item to testing-library

--- Item/tests/Item-specs.js
+++ Item/tests/Item-specs.js
@@ -1,60 +1,44 @@
-import {shallow, mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render} from '@testing-library/react';
 
 import {ItemBase} from '../Item';
+
 import css from '../Item.module.less';
+
 
 describe('Item Specs', () => {
 	test('should support adding text as a child', () => {
 		const expected = 'Hello Item';
 
-		const subject = mount(
-			<ItemBase>
-				{expected}
-			</ItemBase>
-		);
+		const {getByText} = render(<ItemBase>{expected}</ItemBase>);
+		const Child = getByText(expected);
 
-		const actual = subject.text();
-
-		expect(actual).toBe(expected);
+		expect(Child).toBeInTheDocument();
 	});
 
 	test('should support adding a `label`', () => {
 		const expected = 'Example Label';
 
-		const subject = mount(
-			<ItemBase label={expected}>
-				Hello Item
-			</ItemBase>
-		);
-
-		const actual = subject.find(`.${css.label}`).first().text();
+		const {getByTestId} = render(<ItemBase data-testid="item" label={expected}>Hello Item</ItemBase>);
+		const actual = getByTestId('item').children.item(1).children.item(0).children.item(1).textContent;
 
 		expect(actual).toBe(expected);
 	});
 
 	test('should support label with 0', () => {
-		const subject = mount(
-			<ItemBase label={0}>
-				Hello Item
-			</ItemBase>
-		);
-
 		const expected = '0';
-		const actual = subject.find(`.${css.label}`).first().text();
 
-		expect(actual).toContain(expected);
+		const {getByTestId} = render(<ItemBase data-testid="item" label={expected}>Hello Item</ItemBase>);
+		const actual = getByTestId('item').children.item(1).children.item(0).children.item(1).textContent;
+
+		expect(actual).toBe(expected);
 	});
 
 	test('should support adding text as a child when a label is also set', () => {
 		const expected = 'Hello Item';
 
-		const subject = mount(
-			<ItemBase label="Example Label">
-				{expected}
-			</ItemBase>
-		);
-
-		const actual = subject.find(`.${css.content}`).first().text();
+		const {getByTestId} = render(<ItemBase data-testid="item" label="Example Label">{expected}</ItemBase>);
+		const actual = getByTestId('item').children.item(1).children.item(0).children.item(0).textContent;
 
 		expect(actual).toBe(expected);
 	});
@@ -62,13 +46,8 @@ describe('Item Specs', () => {
 	test('should support `slotBefore`', () => {
 		const expected = 'slot before';
 
-		const subject = mount(
-			<ItemBase slotBefore={expected}>
-				Hello Item
-			</ItemBase>
-		);
-
-		const actual = subject.find(`.${css.slotBefore}`).first().text();
+		const {getByTestId} = render(<ItemBase data-testid="item" slotBefore={expected}>Hello Item</ItemBase>);
+		const actual = getByTestId('item').children.item(1).textContent;
 
 		expect(actual).toBe(expected);
 	});
@@ -76,70 +55,52 @@ describe('Item Specs', () => {
 	test('should support `slotAfter`', () => {
 		const expected = 'slot after';
 
-		const subject = mount(
-			<ItemBase slotAfter={expected}>
-				Hello Item
-			</ItemBase>
-		);
-
-		const actual = subject.find(`.${css.slotAfter}`).first().text();
+		const {getByTestId} = render(<ItemBase data-testid="item" slotAfter={expected}>Hello Item</ItemBase>);
+		const actual = getByTestId('item').children.item(2).textContent;
 
 		expect(actual).toBe(expected);
+
 	});
 
 	test('should support repositioning of the label', () => {
-		const subject = mount(
-			<ItemBase labelPosition="above" label="my label">
-				Hello Item
-			</ItemBase>
-		);
+		const {getByTestId} = render(<ItemBase data-testid="item" labelPosition="above" label="my label">Hello Item</ItemBase>);
 
-		const expected = css.labelAbove;
-		const actual = subject.find(`.${css.itemContent}`).first().prop('className');
-
-		expect(actual).toContain(expected);
+		expect(getByTestId('item').children.item(1).className).toContain(css.labelAbove);
 	});
 
 	test('should not include the selected class when not selected', () => {
-		const subject = shallow(
-			<ItemBase>
-				Hello Item
-			</ItemBase>
-		);
+		const {getByTestId} = render(<ItemBase data-testid="item">Hello Item</ItemBase>);
 
 		const expected = css.selected;
-		const actual = subject.first().prop('className');
+		const actual = getByTestId('item').className;
 
 		expect(actual).not.toContain(expected);
 	});
 
 	test('should add the selected class when given the selected prop', () => {
-		const subject = shallow(
-			<ItemBase selected>
-				Hello Item
-			</ItemBase>
-		);
+		const {getByTestId} = render(<ItemBase data-testid="item" selected>Hello Item</ItemBase>);
 
 		const expected = css.selected;
-		const actual = subject.first().prop('className');
+		const actual = getByTestId('item').className;
 
 		expect(actual).toContain(expected);
 	});
 
-	it('should have apply small class when small', function () {
-		const subject = shallow(<ItemBase size="small" />);
+	test('should have apply small class when small', function () {
+		const {getByTestId} = render(<ItemBase data-testid="item" size="small">Hello Item</ItemBase>);
 
 		const expected = 'small';
-		const actual = subject.first().prop('className');
+		const actual = getByTestId('item').className;
 
 		expect(actual).toContain(expected);
 	});
 
 	test('should support RTL text', () => {
-		const subject = mount(<ItemBase>Hello מצב תמונה</ItemBase>);
+		const text = 'Hello מצב תמונה';
+		const {getByTestId} = render(<ItemBase data-testid="item">{text}</ItemBase>);
 
 		const expected = 'rtl';
-		const actual = subject.find('.text').prop('style');
+		const actual = getByTestId('item').children.item(1).children.item(0).children.item(0).style;
 
 		expect(actual).toHaveProperty('direction', expected);
 	});
