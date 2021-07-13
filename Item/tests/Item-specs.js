4ae4566f65b8f7b5f0b0a008c3eaaadaff7e2c2f
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-07-13 16:40:30 +0300

    changes based on code review

--- Item/tests/Item-specs.js
+++ Item/tests/Item-specs.js
@@ -18,47 +18,46 @@ describe('Item Specs', () => {
 	test('should support adding a `label`', () => {
 		const expected = 'Example Label';
 
-		const {getByTestId} = render(<ItemBase data-testid="item" label={expected}>Hello Item</ItemBase>);
-		const actual = getByTestId('item').children.item(1).children.item(0).children.item(1).textContent;
+		const {getByText} = render(<ItemBase label={expected}>Hello Item</ItemBase>);
+		const label = getByText('Hello Item');
 
-		expect(actual).toBe(expected);
+		expect(label).toBeInTheDocument();
 	});
 
 	test('should support label with 0', () => {
 		const expected = '0';
 
-		const {getByTestId} = render(<ItemBase data-testid="item" label={expected}>Hello Item</ItemBase>);
-		const actual = getByTestId('item').children.item(1).children.item(0).children.item(1).textContent;
+		const {getByText} = render(<ItemBase label={expected}>Hello Item</ItemBase>);
+		const label = getByText(expected);
 
-		expect(actual).toBe(expected);
+		expect(label).toBeInTheDocument();
 	});
 
 	test('should support adding text as a child when a label is also set', () => {
 		const expected = 'Hello Item';
 
-		const {getByTestId} = render(<ItemBase data-testid="item" label="Example Label">{expected}</ItemBase>);
-		const actual = getByTestId('item').children.item(1).children.item(0).children.item(0).textContent;
+		const {getByText} = render(<ItemBase label="Example Label">{expected}</ItemBase>);
+		const actual = getByText(expected);
 
-		expect(actual).toBe(expected);
+		expect(actual).toBeInTheDocument();
 	});
 
 	test('should support `slotBefore`', () => {
 		const expected = 'slot before';
 
-		const {getByTestId} = render(<ItemBase data-testid="item" slotBefore={expected}>Hello Item</ItemBase>);
-		const actual = getByTestId('item').children.item(1).textContent;
+		const {getByText} = render(<ItemBase data-testid="item" slotBefore={expected}>Hello Item</ItemBase>);
+		const actual = getByText(expected);
 
-		expect(actual).toBe(expected);
+		expect(actual).toBeInTheDocument();
 	});
 
 	test('should support `slotAfter`', () => {
 		const expected = 'slot after';
 
-		const {getByTestId} = render(<ItemBase data-testid="item" slotAfter={expected}>Hello Item</ItemBase>);
-		const actual = getByTestId('item').children.item(2).textContent;
+		const {getByText} = render(<ItemBase data-testid="item" slotAfter={expected}>Hello Item</ItemBase>);
+		const actual = getByText(expected);
 
-		expect(actual).toBe(expected);
-
+		expect(actual).toBeInTheDocument();
 	});
 
 	test('should support repositioning of the label', () => {
