927c68cd6322a799a9762da8556e6e338aab1823
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-07-14 14:55:47 +0300

    migrated Button unit tests to testing-library

--- Button/tests/Button-specs.js
+++ Button/tests/Button-specs.js
@@ -2,200 +2,210 @@ import '@testing-library/jest-dom';
 import {fireEvent, render} from '@testing-library/react';
 
 import Button, {ButtonBase} from '../Button';
+
 import css from '../Button.module.less';
 
 describe('Button', () => {
-	test(
-		'should have \'disabled\' HTML attribute when \'disabled\' prop is provided',
-		() => {
-			const {getByTestId} = render(<Button data-testid="button" disabled>I am a disabled Button</Button>);
+	test('should have \'disabled\' HTML attribute when \'disabled\' prop is provided', () => {
+		const {getByRole} = render(<Button disabled>I am a disabled Button</Button>);
+		const button = getByRole('button');
 
-			expect(getByTestId('button')).toHaveAttribute('disabled', '');
-		}
-	);
+		expect(button).toHaveAttribute('disabled');
+	});
 
-	it('should have default backgroundOpacity opaque', function () {
-		const {getByTestId} = render(<ButtonBase data-testid="button" />);
+	test('should have default backgroundOpacity opaque', () => {
+		const {getByRole} = render(<ButtonBase />);
+		const button = getByRole('button');
 
 		const expected = css.opaque;
 
-		expect(getByTestId('button')).toHaveClass(expected);
+		expect(button).toHaveClass(expected);
 	});
 
-	it('should expand by default', function () {
-		const {getByTestId} = render(<ButtonBase data-testid="button" />);
+	test('should expand by default', function () {
+		const {getByRole} = render(<ButtonBase />);
+		const button = getByRole('button');
 
 		const expected = 'focusExpand';
 
-		expect(getByTestId('button')).toHaveClass(expected);
+		expect(button).toHaveClass(expected);
 	});
 
-	it('should be able to disable the expand focus effect', function () {
-		const {getByTestId} = render(<ButtonBase data-testid="button" focusEffect="static" />);
+	test('should be able to disable the expand focus effect', () => {
+		const {getByRole} = render(<ButtonBase focusEffect="static" />);
+		const button = getByRole('button');
 
 		const expected = 'focusStatic';
 
-		expect(getByTestId('button')).toHaveClass(expected);
+		expect(button).toHaveClass(expected);
 	});
 
-	it('should have default minWidth', function () {
-		const {getByTestId} = render(<ButtonBase data-testid="button" />);
+	test('should have default minWidth', function () {
+		const {getByRole} = render(<ButtonBase />);
+		const button = getByRole('button');
 
 		const expected = css.minWidth;
 
-		expect(getByTestId('button')).toHaveClass(expected);
+		expect(button).toHaveClass(expected);
 	});
 
-	it('should have default size large', function () {
-		const {getByTestId} = render(<ButtonBase data-testid="button" />);
+	test('should have default size large', function () {
+		const {getByRole} = render(<ButtonBase />);
+		const button = getByRole('button');
 
 		const expected = css.large;
 
-		expect(getByTestId('button')).toHaveClass(expected);
+		expect(button).toHaveClass(expected);
 	});
 
 	describe('with no minWidth', function () {
-		it('should not have minWidth class', function () {
-			const {getByTestId} = render(<ButtonBase data-testid="button" minWidth={false} />);
+		test('should not have minWidth class', () => {
+			const {getByRole} = render(<ButtonBase minWidth={false} />);
+			const button = getByRole('button');
 
 			const expected = css.minWidth;
 
-			expect(getByTestId('button')).not.toHaveClass(expected);
+			expect(button).not.toHaveClass(expected);
 		});
 	});
 
-	describe('with transparent backgroundOpacity', function () {
-		it('should have transparent class', function () {
-			const {getByTestId} = render(<ButtonBase data-testid="button" backgroundOpacity="transparent" />);
+	describe('with transparent backgroundOpacity', () => {
+		test('should have transparent class', function () {
+			const {getByRole} = render(<ButtonBase backgroundOpacity="transparent" />);
+			const button = getByRole('button');
 
 			const expected = css.transparent;
 
-			expect(getByTestId('button')).toHaveClass(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should not have have opaque class', function () {
-			const {getByTestId} = render(<ButtonBase data-testid="button" backgroundOpacity="transparent" />);
+		test('should not have have opaque class', () => {
+			const {getByRole} = render(<ButtonBase backgroundOpacity="transparent" />);
+			const button = getByRole('button');
 
 			const expected = css.opaque;
 
-			expect(getByTestId('button')).not.toHaveClass(expected);
+			expect(button).not.toHaveClass(expected);
 		});
 	});
 
 	describe('with icon', function () {
-		it('should have check icon when specified', function () {
+		test('should have check icon when specified', () => {
 			const {getByText} = render(<Button icon="check">abc</Button>);
+			const icon = getByText('✓');
 
-			const actual = getByText('✓');
-			expect(actual).toBeInTheDocument();
-
-			const expectedClassname = 'icon';
-			expect(getByText('✓')).toHaveClass(expectedClassname);
+			expect(icon).toBeInTheDocument();
+			expect(icon).toHaveClass('icon');
 		});
 
-		it('should not have minWidth class with only icon', function () {
-			const {getByTestId} = render(<Button data-testid="button" icon="check" />);
+		test('should not have minWidth class with only icon', () => {
+			const {getByRole} = render(<Button icon="check" />);
+			const button = getByRole('button');
 
 			const expected = css.minWidth;
 
-			expect(getByTestId('button')).not.toHaveClass(expected);
+			expect(button).not.toHaveClass(expected);
 		});
 
-		it('should have iconAfter class with text and icon', function () {
-			const {getByTestId} = render(<Button data-testid="button" icon="check" iconPosition="after">text</Button>);
+		test('should have iconAfter class with text and icon', () => {
+			const {getByRole} = render(<Button icon="check" iconPosition="after">text</Button>);
+			const button = getByRole('button');
 
 			const expected = css.iconAfter;
 
-			expect(getByTestId('button')).toHaveClass(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should have iconBefore class with text and icon', function () {
-			const {getByTestId} = render(<Button data-testid="button" icon="check" iconPosition="before">text</Button>);
+		test('should have iconBefore class with text and icon', () => {
+			const {getByRole} = render(<Button icon="check" iconPosition="before">text</Button>);
+			const button = getByRole('button');
 
 			const expected = css.iconBefore;
 
-			expect(getByTestId('button')).toHaveClass(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should not have iconPosition classes with only icon', function () {
-			const {getByTestId} = render(<Button data-testid="button" icon="check" />);
+		test('should not have iconPosition classes with only icon', () => {
+			const {getByRole} = render(<Button icon="check" />);
+			const button = getByRole('button');
 
-			expect(getByTestId('button')).not.toHaveClass(css.iconBefore);
-			expect(getByTestId('button')).not.toHaveClass(css.iconAfter);
+			expect(button).not.toHaveClass(css.iconBefore);
+			expect(button).not.toHaveClass(css.iconAfter);
 		});
 
-		it('should have iconOnly class when there is no children', function () {
-			const {getByTestId} = render(<Button data-testid="button" icon="check" />);
+		test('should have iconOnly class when there is no children', () => {
+			const {getByRole} = render(<Button icon="check" />);
+			const button = getByRole('button');
 
 			const expected = css.iconOnly;
 
-			expect(getByTestId('button')).toHaveClass(expected);
+			expect(button).toHaveClass(expected);
 		});
 	});
 
 	describe('with color', () => {
-		it('should have hasColor class', () => {
-			const {getByTestId} = render(<Button data-testid="button" color="red">abc</Button>);
+		test('should have hasColor class', () => {
+			const {getByRole} = render(<Button color="red">abc</Button>);
+			const button = getByRole('button');
 
 			const expected = css.hasColor;
-			const actual = getByTestId('button').className;
 
-			expect(actual).toContain(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should have not hasColor class', () => {
+		test('should have not hasColor class', () => {
 			const {getByTestId} = render(<Button data-testid="button">abc</Button>);
+			const button = getByTestId('button');
 
 			const expected = css.hasColor;
-			const actual = getByTestId('button').className;
 
-			expect(actual).not.toContain(expected);
+			expect(button).not.toHaveClass(expected);
 		});
 
-		it('should have red class', () => {
-			const {getByTestId} = render(<Button color="red" data-testid="button">abc</Button>);
+		test('should have red class', () => {
+			const {getByRole} = render(<Button color="red">abc</Button>);
+			const button = getByRole('button');
 
 			const expected = css.red;
-			const actual = getByTestId('button').className;
 
-			expect(actual).toContain(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should have blue class', () => {
-			const {getByTestId} = render(<Button color="blue" data-testid="button">abc</Button>);
+		test('should have blue class', () => {
+			const {getByRole} = render(<Button color="blue">abc</Button>);
+			const button = getByRole('button');
 
 			const expected = css.blue;
-			const actual = getByTestId('button').className;
 
-			expect(actual).toContain(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should have yellow class', () => {
-			const {getByTestId} = render(<Button color="yellow" data-testid="button">abc</Button>);
+		test('should have yellow class', () => {
+			const {getByRole} = render(<Button color="yellow">abc</Button>);
+			const button = getByRole('button');
 
 			const expected = css.yellow;
-			const actual = getByTestId('button').className;
 
-			expect(actual).toContain(expected);
+			expect(button).toHaveClass(expected);
 		});
 
-		it('should have green class', () => {
-			const {getByTestId} = render(<Button color="green" data-testid="button">abc</Button>);
+		test('should have green class', () => {
+			const {getByRole} = render(<Button color="green">abc</Button>);
+			const button = getByRole('button');
 
 			const expected = css.green;
-			const actual = getByTestId('button').className;
 
-			expect(actual).toContain(expected);
+			expect(button).toHaveClass(expected);
 		});
 	});
 
 	describe('events', () => {
 		test('should call onClick when not disabled', () => {
 			const handleClick = jest.fn();
-
 			const {getByText} = render(<Button onClick={handleClick}>I am not a disabled Button</Button>);
+			const button = getByText('I am not a disabled Button');
 
-			fireEvent.click(getByText('I am not a disabled Button'));
+			fireEvent.click(button);
 
 			const expected = 1;
 			const actual = handleClick.mock.calls.length;
@@ -206,8 +216,9 @@ describe('Button', () => {
 		test('should not call onClick when disabled', () => {
 			const handleClick = jest.fn();
 			const {getByText} = render(<Button disabled onClick={handleClick}>I am a disabled Button</Button>);
+			const button = getByText('I am a disabled Button');
 
-			fireEvent.click(getByText('I am a disabled Button'));
+			fireEvent.click(button);
 
 			const expected = 0;
 			const actual = handleClick.mock.calls.length;
@@ -217,8 +228,9 @@ describe('Button', () => {
 
 		test('should have "Select" voice intent in the node of "role=button"', () => {
 			const {getByRole} = render(<Button>Hello</Button>);
+			const button = getByRole('button');
 
-			expect(getByRole('button')).toHaveAttribute('data-webos-voice-intent', 'Select');
+			expect(button).toHaveAttribute('data-webos-voice-intent', 'Select');
 		});
 	});
 });
