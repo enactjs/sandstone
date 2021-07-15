3197c65c3166e88c77a1715a262dcbd9a34a2cf6
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-07-15 12:32:38 +0300

    refactored Item unit tests based on testing-library best practices

--- Item/tests/Item-specs.js
+++ Item/tests/Item-specs.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 import {ItemBase} from '../Item';
 
@@ -9,17 +9,17 @@ describe('Item Specs', () => {
 	test('should support adding text as a child', () => {
 		const expected = 'Hello Item';
 
-		const {getByText} = render(<ItemBase>{expected}</ItemBase>);
-		const actual = getByText(expected).textContent;
+		render(<ItemBase>{expected}</ItemBase>);
+		const actual = screen.getByText(expected);
 
-		expect(actual).toBe(expected);
+		expect(actual).toHaveTextContent(expected);
 	});
 
 	test('should support adding a `label`', () => {
 		const expected = 'Example Label';
 
-		const {getByText} = render(<ItemBase label={expected}>Hello Item</ItemBase>);
-		const label = getByText('Hello Item');
+		render(<ItemBase label={expected}>Hello Item</ItemBase>);
+		const label = screen.getByText('Hello Item');
 
 		expect(label).toBeInTheDocument();
 	});
@@ -27,8 +27,8 @@ describe('Item Specs', () => {
 	test('should support label with 0', () => {
 		const expected = '0';
 
-		const {getByText} = render(<ItemBase label={expected}>Hello Item</ItemBase>);
-		const label = getByText(expected);
+		render(<ItemBase label={expected}>Hello Item</ItemBase>);
+		const label = screen.getByText(expected);
 
 		expect(label).toBeInTheDocument();
 	});
@@ -36,8 +36,8 @@ describe('Item Specs', () => {
 	test('should support adding text as a child when a label is also set', () => {
 		const expected = 'Hello Item';
 
-		const {getByText} = render(<ItemBase label="Example Label">{expected}</ItemBase>);
-		const actual = getByText(expected);
+		render(<ItemBase label="Example Label">{expected}</ItemBase>);
+		const actual = screen.getByText(expected);
 
 		expect(actual).toBeInTheDocument();
 	});
@@ -45,8 +45,8 @@ describe('Item Specs', () => {
 	test('should support `slotBefore`', () => {
 		const expected = 'slot before';
 
-		const {getByText} = render(<ItemBase data-testid="item" slotBefore={expected}>Hello Item</ItemBase>);
-		const actual = getByText(expected);
+		render(<ItemBase data-testid="item" slotBefore={expected}>Hello Item</ItemBase>);
+		const actual = screen.getByText(expected);
 
 		expect(actual).toBeInTheDocument();
 	});
@@ -54,51 +54,52 @@ describe('Item Specs', () => {
 	test('should support `slotAfter`', () => {
 		const expected = 'slot after';
 
-		const {getByText} = render(<ItemBase data-testid="item" slotAfter={expected}>Hello Item</ItemBase>);
-		const actual = getByText(expected);
+		render(<ItemBase data-testid="item" slotAfter={expected}>Hello Item</ItemBase>);
+		const actual = screen.getByText(expected);
 
 		expect(actual).toBeInTheDocument();
 	});
 
 	test('should support repositioning of the label', () => {
-		const {getByTestId} = render(<ItemBase data-testid="item" labelPosition="above" label="my label">Hello Item</ItemBase>);
+		render(<ItemBase data-testid="item" labelPosition="above" label="my label">Hello Item</ItemBase>);
+		const itemLabel = screen.getByTestId('item').children.item(1);
 
-		expect(getByTestId('item').children.item(1).className).toContain(css.labelAbove);
+		expect(itemLabel).toHaveClass(css.labelAbove);
 	});
 
 	test('should not include the selected class when not selected', () => {
-		const {getByTestId} = render(<ItemBase data-testid="item">Hello Item</ItemBase>);
+		render(<ItemBase data-testid="item">Hello Item</ItemBase>);
 
 		const expected = css.selected;
-		const actual = getByTestId('item').className;
+		const actual = screen.getByTestId('item');
 
-		expect(actual).not.toContain(expected);
+		expect(actual).not.toHaveClass(expected);
 	});
 
 	test('should add the selected class when given the selected prop', () => {
-		const {getByTestId} = render(<ItemBase data-testid="item" selected>Hello Item</ItemBase>);
+		render(<ItemBase data-testid="item" selected>Hello Item</ItemBase>);
 
 		const expected = css.selected;
-		const actual = getByTestId('item').className;
+		const actual = screen.getByTestId('item');
 
-		expect(actual).toContain(expected);
+		expect(actual).toHaveClass(expected);
 	});
 
 	test('should have apply small class when small', function () {
-		const {getByTestId} = render(<ItemBase data-testid="item" size="small" />);
+		render(<ItemBase data-testid="item" size="small" />);
 
 		const expected = 'small';
-		const actual = getByTestId('item').className;
+		const actual = screen.getByTestId('item');
 
-		expect(actual).toContain(expected);
+		expect(actual).toHaveClass(expected);
 	});
 
 	test('should support RTL text', () => {
 		const text = 'Hello מצב תמונה';
-		const {getByTestId} = render(<ItemBase data-testid="item">{text}</ItemBase>);
+		render(<ItemBase>{text}</ItemBase>);
 
 		const expected = 'rtl';
-		const actual = getByTestId('item').children.item(1).children.item(0).children.item(0).style;
+		const actual = screen.getByText(text).style;
 
 		expect(actual).toHaveProperty('direction', expected);
 	});
