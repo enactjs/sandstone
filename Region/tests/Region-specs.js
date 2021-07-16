7f993008779ca877be33ece3b93189ab2af5c901
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2021-07-16 08:48:10 +0300

    refactored code according to react-testing recent standards (#1029)

--- Region/tests/Region-specs.js
+++ Region/tests/Region-specs.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 import Region from '../Region';
 
@@ -7,15 +7,15 @@ describe('Region', () => {
 	describe('computed property', () => {
 		describe('aria-label', () => {
 			test('should use aria-label when set', () => {
-				const {getByRole} = render(<Region title="Title" aria-label="ariaLabel" />);
-				const region = getByRole('region');
+				render(<Region title="Title" aria-label="ariaLabel" />);
+				const region = screen.getByRole('region');
 
 				expect(region).toHaveAttribute('aria-label', 'ariaLabel');
 			});
 
 			test('should use title when aria-label is not set', () => {
-				const {getByRole} = render(<Region title="Title" />);
-				const region = getByRole('region');
+				render(<Region title="Title" />);
+				const region = screen.getByRole('region');
 
 				expect(region).toHaveAttribute('aria-label', 'Title');
 			});
