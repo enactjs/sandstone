17d077e9da18d2461d6ed11cba4fb28f621246dc
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-07-16 10:51:43 +0300

    applied best practices of testing library

--- BodyText/tests/BodyText-specs.js
+++ BodyText/tests/BodyText-specs.js
@@ -1,55 +1,51 @@
 import '@testing-library/jest-dom';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 import BodyText from '../BodyText';
 
 describe('BodyText Specs', () => {
 
 	test('should render a text', () => {
-		const {getByTestId} = render(<BodyText data-testid="bodyText" />);
-		const bodyText = getByTestId('bodyText');
+		render(<BodyText data-testid="bodyText" />);
+		const bodyText = screen.getByTestId('bodyText');
 
 		expect(bodyText).toBeInTheDocument();
 	});
 
 	test('should support single-line marqueeing content when `noWrap` is true', () => {
-		const {getByTestId} = render(<BodyText data-testid="bodyText" noWrap />);
-		const bodyText = getByTestId('bodyText');
+		render(<BodyText data-testid="bodyText" noWrap />);
+		const bodyText = screen.getByTestId('bodyText');
 		const marquee = bodyText.children.item(0);
 
 		const expected = 'marquee';
-		const actual = marquee.className;
 
-		expect(actual).toBe(expected);
+		expect(marquee).toHaveClass(expected);
 	});
 
 	test('should include the noWrap class if `noWrap` is true', () => {
-		const {getByTestId} = render(<BodyText data-testid="bodyText" noWrap />);
-		const bodyText = getByTestId('bodyText');
+		render(<BodyText data-testid="bodyText" noWrap />);
+		const bodyText = screen.getByTestId('bodyText');
 
 		const expected = 'noWrap';
-		const actual = bodyText.className;
 
-		expect(actual).toContain(expected);
+		expect(bodyText).toHaveClass(expected);
 	});
 
 	test('should have small class if `size` is true', () => {
-		const {getByTestId} = render(<BodyText data-testid="bodyText" size="small" />);
-		const bodyText = getByTestId('bodyText');
+		render(<BodyText data-testid="bodyText" size="small" />);
+		const bodyText = screen.getByTestId('bodyText');
 
 		const expected = 'small';
-		const actual = bodyText.className
 
-		expect(actual).toContain(expected);
+		expect(bodyText).toHaveClass(expected);
 	});
 
 	test('should have center class if `center` is true', () => {
-		const {getByTestId} = render(<BodyText data-testid="bodyText" centered />);
-		const bodyText = getByTestId('bodyText');
+		render(<BodyText data-testid="bodyText" centered />);
+		const bodyText = screen.getByTestId('bodyText');
 
 		const expected = 'centered';
-		const actual = bodyText.className;
 
-		expect(actual).toContain(expected);
+		expect(bodyText).toHaveClass(expected);
 	});
 });
--- Heading/tests/Heading-specs.js
+++ Heading/tests/Heading-specs.js
@@ -1,4 +1,5 @@
-import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
 
 import {HeadingBase} from '../Heading';
 
@@ -6,49 +7,45 @@ describe('Heading Specs', () => {
 
 	test('should render a Heading with content', () => {
 		const content = 'Hello Heading!';
-		const {getByTestId} = render(<HeadingBase data-testid="heading">{content}</HeadingBase>);
-		const heading = getByTestId('heading').textContent;
+		render(<HeadingBase data-testid="heading">{content}</HeadingBase>);
+		const heading = screen.getByTestId('heading').textContent;
 
 		expect(heading).toBe(content);
 	});
 
 	test('should add the showLine class', () => {
-		const {getByTestId} = render(<HeadingBase data-testid="heading" showLine>check</HeadingBase>);
-		const heading = getByTestId('heading');
+		render(<HeadingBase data-testid="heading" showLine>check</HeadingBase>);
+		const heading = screen.getByTestId('heading');
 
 		const expected = 'showLine';
-		const actual = heading.className;
 
-		expect(actual).toContain(expected);
+		expect(heading).toHaveClass(expected);
 	});
 
 	test('should have large class if `size` is large', () => {
-		const {getByTestId} = render(<HeadingBase data-testid="heading" size="large">Hello Heading</HeadingBase>);
-		const heading = getByTestId('heading');
+		render(<HeadingBase data-testid="heading" size="large">Hello Heading</HeadingBase>);
+		const heading = screen.getByTestId('heading');
 
 		const expected = 'large';
-		const actual = heading.className;
 
-		expect(actual).toContain(expected);
+		expect(heading).toHaveClass(expected);
 	});
 
 	test('should have small class if `size` is small', () => {
-		const {getByTestId} = render(<HeadingBase data-testid="heading" size="small">Hello Heading</HeadingBase>);
-		const heading = getByTestId('heading');
+		render(<HeadingBase data-testid="heading" size="small">Hello Heading</HeadingBase>);
+		const heading = screen.getByTestId('heading');
 
 		const expected = 'small';
-		const actual = heading.className;
 
-		expect(actual).toContain(expected);
+		expect(heading).toHaveClass(expected);
 	});
 
 	test('should have tiny class if `size` is tiny', () => {
-		const {getByTestId} = render(<HeadingBase data-testid="heading" size="tiny">Hello Heading</HeadingBase>);
-		const heading = getByTestId('heading');
+		render(<HeadingBase data-testid="heading" size="tiny">Hello Heading</HeadingBase>);
+		const heading = screen.getByTestId('heading');
 
 		const expected = 'tiny';
-		const actual = heading.className;
 
-		expect(actual).toContain(expected);
+		expect(heading).toHaveClass(expected);
 	});
 });
