5f526fac73d022bf7658cbb2e6f9976be8d06dc7
Sangwook-Lee <sangwook1203.lee@lge.com>
2021-07-16 16:20:33 +0900

    WRN-2465: Date/TimePicker: add prop to hide label (#1001)
    * added prop  to DatePicker and TimePicker
    
    * fixed lint errors and updated sampler
    
    * fixed lint errors
    
    * removed blank line
    
    * fixed changelog
    
    * added unit tests
    
    * added screenshot tests
    
    * updadted jsdoc

--- tests/screenshot/apps/components/TimePicker.js
+++ tests/screenshot/apps/components/TimePicker.js
@@ -3,6 +3,7 @@ import TimePicker from '../../../../TimePicker';
 const TimePickerTests = [
 	<TimePicker defaultValue={new Date(2009, 5, 6)} />,
 	<TimePicker defaultValue={new Date(2009, 5, 6)} disabled />,
+	<TimePicker defaultValue={new Date(2009, 5, 6)} noLabel />,
 	// RTL
 	{
 		locale: 'ar-SA',
