41763c32b66c693873e5123dc3761fece4426cdb
daniel-stoian-lgp <63335068+daniel-stoian-lgp@users.noreply.github.com>
2021-07-16 16:17:51 +0300

    Migrated unit tests for TooltipDecorator to testing-library (#1030)

--- TooltipDecorator/tests/TooltipDecorator-specs.js
+++ TooltipDecorator/tests/TooltipDecorator-specs.js
@@ -1,37 +1,26 @@
-import {shallow} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
 
 import TooltipLabel from '../TooltipLabel';
 
 describe('TooltipDecorator', () => {
-
 	describe('TooltipLabel', () => {
-		test(
-			'should apply alignment when `centered` and `marquee`',
-			() => {
-				const subject = shallow(
-					<TooltipLabel centered marquee>
-						Label
-					</TooltipLabel>
-				);
+		test('should apply alignment when `centered` and `marquee`', () => {
+			render(<TooltipLabel centered marquee>Label</TooltipLabel>);
 
-				const expected = 'center';
-				const actual = subject.prop('alignment');
+			const expected = 'center';
+			const tooltip = screen.getByText('Label');
 
-				expect(actual).toBe(expected);
-			}
-		);
+			expect(tooltip).toHaveStyle({'text-align': expected});
+		});
 
-		test(
-			'should not apply alignment when `centered` but not `marquee`',
-			() => {
-				const subject = shallow(
-					<TooltipLabel centered>
-						Label
-					</TooltipLabel>
-				);
+		test('should not apply alignment when `centered` but not `marquee`', () => {
+			render(<TooltipLabel centered>Label</TooltipLabel>);
 
-				expect(subject).not.toHaveProperty('alignment');
-			}
-		);
+			const unexpected = 'center';
+			const tooltip = screen.getByText('Label');
+
+			expect(tooltip).not.toHaveStyle({'text-align': unexpected});
+		});
 	});
 });
