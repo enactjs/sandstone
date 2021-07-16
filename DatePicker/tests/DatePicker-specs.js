62da4e11e1a582d6f11f022059ec03b668a4007d
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-07-16 12:15:34 +0300

    Changes made based on recent updated standards.

--- DatePicker/tests/DatePicker-specs.js
+++ DatePicker/tests/DatePicker-specs.js
@@ -135,13 +135,11 @@ describe('DatePicker', () => {
 
 	test('should not display Heading', () => {
 		const date = new Date(2000, 0, 1);
-		const subject = mount(
+		render(
 			<DatePicker value={date} locale="en-US" noLabel />
 		);
+		const header = screen.queryByText(dateToLocaleString(date));
 
-		const expected = false;
-		const actual = subject.find('Heading').exists();
-
-		expect(actual).toBe(expected);
+		expect(header).toBeNull();
 	});
 });
