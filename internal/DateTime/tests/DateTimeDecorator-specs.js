06ca9f8b316f5b269e612f3c20a5d07d1e08eb7e
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-07-20 15:41:22 +0300

    partial commit

--- internal/DateTime/tests/DateTimeDecorator-specs.js
+++ internal/DateTime/tests/DateTimeDecorator-specs.js
@@ -1,30 +1,34 @@
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import {mount} from 'enzyme';
 import {DateTimeDecorator} from '../';
+import PickerBase from '../../Picker';
 
 describe('DateTimeDecorator', () => {
 	test(
 		'should accept an updated JavaScript Date for its value prop',
 		() => {
-			const Picker = DateTimeDecorator({}, function PickerBase () {
-				return <div />;
+			const Picker = DateTimeDecorator({}, function PickerBasic () {
+				return <PickerBase />;
 			});
 
-			const subject = mount(
+			render(
 				<Picker
 					title="Date"
 					value={new Date(2000, 0, 1, 12, 30)}
 					locale="en-US"
 				/>
 			);
-
-			subject.setProps({
-				value: new Date(2000, 0, 1, 12, 45)
-			});
-
-			const expected = 45;
-			const actual = subject.find('PickerBase').prop('value').getMinutes();
-
-			expect(actual).toBe(expected);
+screen.debug();
+			// subject.setProps({
+			// 	value: new Date(2000, 0, 1, 12, 45)
+			// });
+			//
+			// const expected = 45;
+			// const actual = subject.find('PickerBase').prop('value').getMinutes();
+			//
+			// expect(actual).toBe(expected);
 		}
 	);
 
--- internal/DebounceDecorator/tests/DebounceDecorator-specs.js
+++ internal/DebounceDecorator/tests/DebounceDecorator-specs.js
@@ -1,3 +1,7 @@
+import '@testing-library/jest-dom';
+import {fireEvent, render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import {mount} from 'enzyme';
 import DebounceDecorator from '../';
 
@@ -5,48 +9,21 @@ describe('DebounceDecorator', () => {
 	test(
 		'should emit the event after the delay',
 		(done) => {
+			const spy = jest.fn();
+
 			const Component = DebounceDecorator(
-				{debounce: 'onChange', delay: 100},
+				{debounce: 'onClick', delay: 100},
 				function Base () {
 					return <div />;
 				}
 			);
 
-			const spy = jest.fn();
-			const subject = mount(<Component onChange={spy} />);
+			render(<div onClick={spy} data-testid="component"><Component onClick={spy} data-testid="component" /></div>);
+			screen.debug();
 
-			subject.find('Base').invoke('onChange')();
+			fireEvent.click(screen.getByTestId("component").children.item(0));
 
 			expect(spy).not.toHaveBeenCalled();
-
-			setTimeout(() => {
-				expect(spy).toHaveBeenCalled();
-				done();
-			}, 150);
-		}
-	);
-
-	test(
-		'should restart the delay if another event occurs before timeout',
-		(done) => {
-			const Component = DebounceDecorator(
-				{debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
-
-			const spy = jest.fn();
-			const subject = mount(<Component onChange={spy} />);
-
-			setTimeout(() => {
-				expect(spy).not.toHaveBeenCalled();
-				subject.find('Base').invoke('onChange')();
-			}, 50);
-
-			setTimeout(() => {
-				expect(spy).not.toHaveBeenCalled();
-			}, 125);
 
 			setTimeout(() => {
 				expect(spy).toHaveBeenCalled();
@@ -55,47 +32,76 @@ describe('DebounceDecorator', () => {
 		}
 	);
 
-	test(
-		'should not emit the event if the cancel event occurs before the delay',
-		(done) => {
-			const Component = DebounceDecorator(
-				{cancel: 'onCancel', debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
+	// test(
+	// 	'should restart the delay if another event occurs before timeout',
+	// 	(done) => {
+	// 		const Component = DebounceDecorator(
+	// 			{debounce: 'onChange', delay: 100},
+	// 			function Base () {
+	// 				return <div />;
+	// 			}
+	// 		);
+	//
+	// 		const spy = jest.fn();
+	// 		const subject = mount(<Component onChange={spy} />);
+	//
+	// 		setTimeout(() => {
+	// 			expect(spy).not.toHaveBeenCalled();
+	// 			subject.find('Base').invoke('onChange')();
+	// 		}, 50);
+	//
+	// 		setTimeout(() => {
+	// 			expect(spy).not.toHaveBeenCalled();
+	// 		}, 125);
+	//
+	// 		setTimeout(() => {
+	// 			expect(spy).toHaveBeenCalled();
+	// 			done();
+	// 		}, 200);
+	// 	}
+	// );
 
-			const spy = jest.fn();
-			const subject = mount(<Component onChange={spy} />);
-
-			subject.find('Base').invoke('onChange')();
-			subject.find('Base').invoke('onCancel')();
-
-			expect(spy).not.toHaveBeenCalled();
-
-			setTimeout(() => {
-				expect(spy).not.toHaveBeenCalled();
-				done();
-			}, 150);
-		}
-	);
-
-	test(
-		'should emit the onCancel event immediately',
-		() => {
-			const Component = DebounceDecorator(
-				{cancel: 'onCancel', debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
-
-			const spy = jest.fn();
-			const subject = mount(<Component onCancel={spy} />);
-
-			subject.find('Base').invoke('onCancel')();
-
-			expect(spy).toHaveBeenCalled();
-		}
-	);
+	// test(
+	// 	'should not emit the event if the cancel event occurs before the delay',
+	// 	(done) => {
+	// 		const Component = DebounceDecorator(
+	// 			{cancel: 'onCancel', debounce: 'onChange', delay: 100},
+	// 			function Base () {
+	// 				return <div />;
+	// 			}
+	// 		);
+	//
+	// 		const spy = jest.fn();
+	// 		const subject = mount(<Component onChange={spy} />);
+	//
+	// 		subject.find('Base').invoke('onChange')();
+	// 		subject.find('Base').invoke('onCancel')();
+	//
+	// 		expect(spy).not.toHaveBeenCalled();
+	//
+	// 		setTimeout(() => {
+	// 			expect(spy).not.toHaveBeenCalled();
+	// 			done();
+	// 		}, 150);
+	// 	}
+	// );
+	//
+	// test(
+	// 	'should emit the onCancel event immediately',
+	// 	() => {
+	// 		const Component = DebounceDecorator(
+	// 			{cancel: 'onCancel', debounce: 'onChange', delay: 100},
+	// 			function Base () {
+	// 				return <div />;
+	// 			}
+	// 		);
+	//
+	// 		const spy = jest.fn();
+	// 		const subject = mount(<Component onCancel={spy} />);
+	//
+	// 		subject.find('Base').invoke('onCancel')();
+	//
+	// 		expect(spy).toHaveBeenCalled();
+	// 	}
+	// );
 });
