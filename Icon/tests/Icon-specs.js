42b709f7e728182ef490c63aef5ba38aa8c2f8d6
daniel-stoian-lgp <63335068+daniel-stoian-lgp@users.noreply.github.com>
2021-08-03 04:41:26 +0300

    WRN-1349: Migrated unit tests for Icon and DateTimeDecorator to testing-library. Removed unit tests for DebounceDecorator (#1051)
    * Migrated unit tests for Icon to testing-library
    
    * partial commit
    
    * partial commit
    
    * migrated DateTimeDecorator unit tests to testing library.
    
    * removed DebounceDecorator unit tests
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * migrated tests for DebounceDecorator
    
    * remove the redundant test
    
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)
    
    Co-authored-by: Mikyung Kim <mikyung27.kim@lge.com>

--- Icon/tests/Icon-specs.js
+++ Icon/tests/Icon-specs.js
@@ -1,159 +1,98 @@
-import {mount} from 'enzyme';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
 import {IconBase as Icon} from '../Icon';
 
 describe('Icon Specs', () => {
-
-	test(
-		'should return the correct Unicode value for named icon \'star\'',
-		() => {
-			const subject = mount(
-				<Icon>star</Icon>
-			);
-
-			const expected = 983080; // decimal converted charCode of Unicode 'star' character
-			const actual = subject.text().codePointAt();
-
-			expect(actual).toBe(expected);
-		}
-	);
-
-	test(
-		'should return the correct Unicode value when provided \'star\' hex value',
-		() => {
-			const subject = mount(
-				<Icon>0x0F0028</Icon>
-			);
-
-			const expected = 983080; // decimal converted charCode of character
-			const actual = subject.text().codePointAt();
-
-			expect(actual).toBe(expected);
-		}
-	);
-
-	test(
-		'should return the correct Unicode value when provided HTML entity as hex value',
-		() => {
-			const subject = mount(
-				<Icon>&#x2605;</Icon>
-			);
-
-			const expected = 9733; // decimal converted charCode of character
-			const actual = subject.text().codePointAt();
-
-			expect(actual).toBe(expected);
-		}
-	);
-
-	test(
-		'should return the correct Unicode value when provided Unicode reference',
-		() => {
-			const subject = mount(
-				<Icon>\u0F0028</Icon>
-			);
-
-			const expected = 983080; // decimal converted charCode of Unicode 'star' character
-			const actual = subject.text().codePointAt();
-
-			expect(actual).toBe(expected);
-		}
-	);
-
-	test('should support high code point Unicode values', () => {
-		const subject = mount(
-			<Icon>{String.fromCodePoint(0x0F0028)}</Icon>
-		);
+	test('should return the correct Unicode value for named icon \'star\'', () => {
+		render(<Icon data-testid="icon">star</Icon>);
 
 		const expected = 983080; // decimal converted charCode of Unicode 'star' character
-		const actual = subject.text().codePointAt();
+		const actual = screen.getByTestId('icon').textContent.codePointAt();
 
 		expect(actual).toBe(expected);
 	});
 
-	test(
-		'should support preset size "large"',
-		() => {
-			const subject = mount(
-				<Icon size="large">star</Icon>
-			);
+	test('should return the correct Unicode value when provided \'star\' hex value', () => {
+		render(<Icon data-testid="icon">0x0F0028</Icon>);
 
-			const expected = 'large';
-			const actual = subject.find('.icon').prop('className');
+		const expected = 983080; // decimal converted charCode of character
+		const actual = screen.getByTestId('icon').textContent.codePointAt();
 
-			expect(actual).toContain(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should support preset size "medium"',
-		() => {
-			const subject = mount(
-				<Icon size="medium">star</Icon>
-			);
+	test('should return the correct Unicode value when provided HTML entity as hex value', () => {
+		render(<Icon data-testid="icon">&#x2605;</Icon>);
 
-			const expected = 'medium';
-			const actual = subject.find('.icon').prop('className');
+		const expected = 9733; // decimal converted charCode of character
+		const actual = screen.getByTestId('icon').textContent.codePointAt();
 
-			expect(actual).toContain(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should support preset size "small"',
-		() => {
-			const subject = mount(
-				<Icon size="small">star</Icon>
-			);
+	test('should return the correct Unicode value when provided Unicode reference', () => {
+		render(<Icon data-testid="icon">\u0F0028</Icon>);
 
-			const expected = 'small';
-			const actual = subject.find('.icon').prop('className');
+		const expected = 983080; // decimal converted charCode of Unicode 'star' character
+		const actual = screen.getByTestId('icon').textContent.codePointAt();
 
-			expect(actual).toContain(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should support preset size "tiny"',
-		() => {
-			const subject = mount(
-				<Icon size="tiny">star</Icon>
-			);
+	test('should support high code point Unicode values', () => {
+		render(<Icon data-testid="icon">{String.fromCodePoint(0x0F0028)}</Icon>);
 
-			const expected = 'tiny';
-			const actual = subject.find('.icon').prop('className');
+		const expected = 983080; // decimal converted charCode of Unicode 'star' character
+		const actual = screen.getByTestId('icon').textContent.codePointAt();
 
-			expect(actual).toContain(expected);
-		}
-	);
+		expect(actual).toBe(expected);
+	});
 
-	test(
-		'should support arbitrary custom numeric sizes',
-		() => {
-			const subject = mount(
-				<Icon size={96}>star</Icon>
-			);
+	test('should support preset size "large"', () => {
+		render(<Icon data-testid="icon" size="large">star</Icon>);
 
-			const expected = '--icon-size';
-			const actual = subject.find('.icon').prop('style');
+		const expected = 'large';
+		const icon = screen.getByTestId('icon');
 
-			expect(actual).toHaveProperty(expected);
-		}
-	);
+		expect(icon).toHaveClass(expected);
+	});
 
-	test(
-		'should support arbitrary custom numeric sizes that scale to the correct value',
-		() => {
-			const subject = mount(
-				<Icon size={96}>star</Icon>
-			);
+	test('should support preset size "medium"', () => {
+		render(<Icon data-testid="icon" size="medium">star</Icon>);
 
-			const expected = '--icon-size';
-			const actual = subject.find('.icon').prop('style');
+		const expected = 'medium';
+		const icon = screen.getByTestId('icon');
 
-			// surprisingly this returns 8rem, instead of what you'd expect
-			// with a base pxToRem value of 48px, which would be 2rem.
-			// Tests must run at a tiny simulated screen size.
-			expect(actual).toHaveProperty(expected, '8rem');
-		}
-	);
+		expect(icon).toHaveClass(expected);
+	});
+
+	test('should support preset size "small"', () => {
+		render(<Icon data-testid="icon" size="small">star</Icon>);
+
+		const expected = 'small';
+		const icon = screen.getByTestId('icon');
+
+		expect(icon).toHaveClass(expected);
+	});
+
+	test('should support preset size "tiny"', () => {
+		render(<Icon data-testid="icon" size="tiny">star</Icon>);
+
+		const expected = 'tiny';
+		const icon = screen.getByTestId('icon');
+
+		expect(icon).toHaveClass(expected);
+	});
+
+	test('should support arbitrary custom numeric sizes that scale to the correct value', () => {
+		render(<Icon data-testid="icon" size={96}>star</Icon>);
+
+		const icon = screen.getByTestId('icon');
+
+		// surprisingly this returns 8rem, instead of what you'd expect
+		// with a base pxToRem value of 48px, which would be 2rem.
+		// Tests must run at a tiny simulated screen size.
+		expect(icon).toHaveStyle({'--icon-size': '8rem'});
+	});
 });
--- internal/DebounceDecorator/tests/DebounceDecorator-specs.js
+++ internal/DebounceDecorator/tests/DebounceDecorator-specs.js
@@ -1,101 +1,143 @@
-import {mount} from 'enzyme';
+import {createEvent, fireEvent, render, screen} from '@testing-library/react';
+import {useEffect, useRef} from 'react';
+
 import DebounceDecorator from '../';
 
 describe('DebounceDecorator', () => {
-	test(
-		'should emit the event after the delay',
-		(done) => {
-			const Component = DebounceDecorator(
-				{debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
+	test('should emit the event after the delay', (done) => {
+		const Component = DebounceDecorator(
+			{debounce: 'onChange', delay: 100},
+			function Base ({onChange}) {
+				const ref = useRef(null);
 
-			const spy = jest.fn();
-			const subject = mount(<Component onChange={spy} />);
+				useEffect(() => {
+					const {current} = ref;
+					current.addEventListener('onChange', onChange);
 
-			subject.find('Base').invoke('onChange')();
+					return () => {
+						current.removeEventListener('onChange', onChange);
+					};
+				}, [onChange]);
 
-			expect(spy).not.toHaveBeenCalled();
+				return <div ref={ref}>Test</div>;
+			}
+		);
 
-			setTimeout(() => {
-				expect(spy).toHaveBeenCalled();
-				done();
-			}, 150);
-		}
-	);
+		const spy = jest.fn();
 
-	test(
-		'should restart the delay if another event occurs before timeout',
-		(done) => {
-			const Component = DebounceDecorator(
-				{debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
+		render(<Component onChange={spy} />);
 
-			const spy = jest.fn();
-			const subject = mount(<Component onChange={spy} />);
+		const elem = screen.getByText('Test');
+		fireEvent(elem, createEvent('onChange', elem));
 
-			setTimeout(() => {
-				expect(spy).not.toHaveBeenCalled();
-				subject.find('Base').invoke('onChange')();
-			}, 50);
+		expect(spy).not.toHaveBeenCalled();
 
-			setTimeout(() => {
-				expect(spy).not.toHaveBeenCalled();
-			}, 125);
-
-			setTimeout(() => {
-				expect(spy).toHaveBeenCalled();
-				done();
-			}, 200);
-		}
-	);
-
-	test(
-		'should not emit the event if the cancel event occurs before the delay',
-		(done) => {
-			const Component = DebounceDecorator(
-				{cancel: 'onCancel', debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
-
-			const spy = jest.fn();
-			const subject = mount(<Component onChange={spy} />);
-
-			subject.find('Base').invoke('onChange')();
-			subject.find('Base').invoke('onCancel')();
-
-			expect(spy).not.toHaveBeenCalled();
-
-			setTimeout(() => {
-				expect(spy).not.toHaveBeenCalled();
-				done();
-			}, 150);
-		}
-	);
-
-	test(
-		'should emit the onCancel event immediately',
-		() => {
-			const Component = DebounceDecorator(
-				{cancel: 'onCancel', debounce: 'onChange', delay: 100},
-				function Base () {
-					return <div />;
-				}
-			);
-
-			const spy = jest.fn();
-			const subject = mount(<Component onCancel={spy} />);
-
-			subject.find('Base').invoke('onCancel')();
-
+		setTimeout(() => {
 			expect(spy).toHaveBeenCalled();
-		}
-	);
+			done();
+		}, 150);
+	});
+
+	test('should restart the delay if another event occurs before timeout', (done) => {
+		const Component = DebounceDecorator(
+			{debounce: 'onChange', delay: 100},
+			function Base ({onChange}) {
+				const ref = useRef(null);
+
+				useEffect(() => {
+					const {current} = ref;
+					current.addEventListener('onChange', onChange);
+
+					return () => {
+						current.removeEventListener('onChange', onChange);
+					};
+				}, [onChange]);
+
+				return <div ref={ref}>Test</div>;
+			}
+		);
+
+		const spy = jest.fn();
+		render(<Component onChange={spy} />);
+
+		setTimeout(() => {
+			expect(spy).not.toHaveBeenCalled();
+
+			const elem = screen.getByText('Test');
+			fireEvent(elem, createEvent('onChange', elem));
+		}, 50);
+
+		setTimeout(() => {
+			expect(spy).not.toHaveBeenCalled();
+		}, 125);
+
+		setTimeout(() => {
+			expect(spy).toHaveBeenCalled();
+			done();
+		}, 200);
+	});
+
+	test('should not emit the event if the cancel event occurs before the delay', (done) => {
+		const Component = DebounceDecorator(
+			{cancel: 'onCancel', debounce: 'onChange', delay: 100},
+			function Base ({onCancel, onChange}) {
+				const ref = useRef(null);
+
+				useEffect(() => {
+					const {current} = ref;
+					current.addEventListener('onChange', onChange);
+					current.addEventListener('onCancel', onCancel);
+
+					return () => {
+						current.removeEventListener('onChange', onChange);
+						current.removeEventListener('onCancel', onCancel);
+					};
+				}, [onCancel, onChange]);
+
+				return <div ref={ref}>Test</div>;
+			}
+		);
+
+		const spy = jest.fn();
+		render(<Component onChange={spy} />);
+
+		const elem = screen.getByText('Test');
+		fireEvent(elem, createEvent('onChange', elem));
+		fireEvent(elem, createEvent('onCancel', elem));
+
+		expect(spy).not.toHaveBeenCalled();
+
+		setTimeout(() => {
+			expect(spy).not.toHaveBeenCalled();
+			done();
+		}, 150);
+	});
+
+	test('should emit the onCancel event immediately', () => {
+		const Component = DebounceDecorator(
+			{cancel: 'onCancel', debounce: 'onChange', delay: 100},
+			function Base ({onCancel}) {
+				const ref = useRef(null);
+
+				useEffect(() => {
+					const {current} = ref;
+					current.addEventListener('onCancel', onCancel);
+
+					return () => {
+						current.removeEventListener('onCancel', onCancel);
+					};
+				}, [onCancel]);
+
+				return <div ref={ref}>Test</div>;
+			}
+		);
+
+		const spy = jest.fn();
+		render(<Component onCancel={spy} />);
+
+		const elem = screen.getByText('Test');
+		fireEvent(elem, createEvent('onCancel', elem));
+
+		expect(spy).toHaveBeenCalled();
+	});
 });
