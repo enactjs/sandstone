302dd6f678dede7ec4e3db2758f84e751805d1bd
Mikyung Kim <mikyung27.kim@lge.com>
2021-08-20 11:35:10 +0900

    WRN-5464: Fixed Scroller to move focus via up/down keys from scroll thumb when the content is short but the scrollbar is visible
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- samples/sampler/stories/qa/Scroller.js
+++ samples/sampler/stories/qa/Scroller.js
@@ -278,26 +278,29 @@ export const WithSpottableComponents = () => (
 WithSpottableComponents.storyName = 'With Spottable Components';
 
 export const WithShortContents = () => (
-	<Scroller
-		direction={select('direction', prop.direction, Config)}
-		focusableScrollbar={
-			prop.focusableScrollbarOption[
-				select('focusableScrollbar', ['false', 'true', '"byEnter"'], Config)
-			]
-		}
-		horizontalScrollbar={select('horizontalScrollbar', prop.scrollbarOption, Config)}
-		key={select('scrollMode', prop.scrollModeOption, Config)}
-		noScrollByWheel={boolean('noScrollByWheel', Config)}
-		onKeyDown={action('onKeyDown')}
-		onScrollStart={action('onScrollStart')}
-		onScrollStop={action('onScrollStop')}
-		scrollMode={select('scrollMode', prop.scrollModeOption, Config)}
-		spotlightDisabled={boolean('spotlightDisabled', Config, false)}
-		style={{height: ri.scaleToRem(600)}}
-		verticalScrollbar={select('verticalScrollbar', prop.scrollbarOption, Config)}
-	>
-		Text
-	</Scroller>
+	<>
+		<Scroller
+			direction={select('direction', prop.direction, Config)}
+			focusableScrollbar={
+				prop.focusableScrollbarOption[
+					select('focusableScrollbar', ['false', 'true', '"byEnter"'], Config)
+				]
+			}
+			horizontalScrollbar={select('horizontalScrollbar', prop.scrollbarOption, Config)}
+			key={select('scrollMode', prop.scrollModeOption, Config)}
+			noScrollByWheel={boolean('noScrollByWheel', Config)}
+			onKeyDown={action('onKeyDown')}
+			onScrollStart={action('onScrollStart')}
+			onScrollStop={action('onScrollStop')}
+			scrollMode={select('scrollMode', prop.scrollModeOption, Config)}
+			spotlightDisabled={boolean('spotlightDisabled', Config, false)}
+			style={{height: ri.scaleToRem(600)}}
+			verticalScrollbar={select('verticalScrollbar', prop.scrollbarOption, Config)}
+		>
+			Text
+		</Scroller>
+		<Button>Button</Button>
+	</>
 );
 
 WithShortContents.storyName = 'With short contents';
