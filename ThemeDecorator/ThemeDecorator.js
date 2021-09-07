5a93beb9ec8834955d84fb6b9e18685f7891f985
Seungcheon Baek <sc.baek@lge.com>
2021-09-07 11:31:57 +0900

    Fixed background color of overlayed Alert when high contrast mode
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- ThemeDecorator/ThemeDecorator.js
+++ ThemeDecorator/ThemeDecorator.js
@@ -282,7 +282,7 @@ const ThemeDecorator = hoc(defaultConfig, (config, Wrapped) => {
 		render () {
 			const {skin: skinProp, ...rest} = this.props;
 			const skinName = skinProp || typeof window === 'object' && window.CUSTOM_SKIN || 'neutral';
-			const className = classNames(css.root, this.props.className, 'sandstone-theme', skinName, 'enact-unselectable', {
+			const className = classNames(css.root, this.props.className, 'sandstone-theme', 'enact-unselectable', {
 				[bgClassName]: !float,
 				'enact-fit': !disableFullscreen
 			});
