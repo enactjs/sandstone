36154a1ed8b82bb107a2b493d0ebb310272c10ba
Juwon Jeong <juwon.jeong@lge.com>
2021-10-18 15:50:23 +0900

    WRN-8939: Add India region font entries to fontGenerator (#1088)
    * WRN-8939: Add India region font entries to fontGenerator
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Add Bengali language text in BodyText qa-sampler
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Update samples/sampler/src/ThemeEnvironment/ThemeEnvironment.js
    
    * Add bengali and telugu text to the qa-sampler
    Enact-DCO-1.0-Signed-off-by: Jeonghee Ahn (jeonghee27.ahn@lge.com)
    
    * minor change
    
    * Revert "Add Bengali language text in BodyText qa-sampler"
    
    This reverts commit 745caea75e77ade6de657da74060ec9af392bbbe.
    
    * add ss test
    
    Co-authored-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 The following is a curated list of changes in the Enact sandstone module, newest changes on the top.
 
+## [unreleased] 
+
+### Fixed
+
+- `sandstone` to support India region font correctly
+
 ## [2.0.2] - 2021-10-07
 
 ### Added
--- ThemeDecorator/fontGenerator.js
+++ ThemeDecorator/fontGenerator.js
@@ -29,8 +29,29 @@ const fontName = 'Sandstone';
 // 	}
 // };
 const fonts = {
+	'bn': {
+		regular: 'LG Smart UI Bengali'
+	},
+	'gu': {
+		regular: 'LG Smart UI Gujarati'
+	},
+	'hi' : {
+		regular: 'LG Smart UI Devanagari'
+	},
 	'ja': {
 		regular: 'LG Smart UI JP'
+	},
+	'kn': {
+		regular: 'LG Smart UI Kannada'
+	},
+	'pa': {
+		regular: 'LG Smart UI Gurmukhi'
+	},
+	'ta': {
+		regular: 'LG Smart UI Tamil'
+	},
+	'te': {
+		regular: 'LG Smart UI Telugu'
 	},
 	'ur': {
 		regular: ['LG Smart UI Urdu', 'LGSmartUIUrdu'] // This needs 2 references because the "full name" differs from the "family name". To target this font file directly in all OSs we must also include the "postscript name" in addition to the "full name".
@@ -41,6 +62,8 @@ const fonts = {
 };
 
 // Duplications and alternate locale names
+fonts['as'] = fonts['bn'];
+fonts['mr'] = fonts['hi'];
 fonts['en-JP'] = fonts['ja'];
 
 addLocalizedFont(fontName, fonts);
