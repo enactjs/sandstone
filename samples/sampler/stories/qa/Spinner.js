17fd2ebb3ee2395a74e4f24a4b7ec19d84487e4e
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-12-21 15:58:32 +0200

    fixed qa-samplers for spinner to fit into smaller screens

--- samples/sampler/stories/qa/Spinner.js
+++ samples/sampler/stories/qa/Spinner.js
@@ -57,7 +57,7 @@ export const WithLongContent = (args) => (
 	<div>
 		<div
 			style={{
-				height: ri.scaleToRem(840),
+				height: 'fit-content',
 				border: ri.scaleToRem(6) + ' dotted red'
 			}}
 		>
@@ -65,8 +65,7 @@ export const WithLongContent = (args) => (
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
 				ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 			</p>
 			<Button onClick={action('Inside Button events')}>Button</Button>
 			<Spinner
@@ -94,7 +93,7 @@ export const BlockingClickEvents = (args) => (
 	<div>
 		<div
 			style={{
-				height: ri.scaleToRem(300),
+				height: 'fit-content',
 				border: ri.scaleToRem(6) + ' dotted red'
 			}}
 		>
@@ -102,7 +101,8 @@ export const BlockingClickEvents = (args) => (
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
 				ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
 			<Button onClick={action('Inside Button events')}>Button</Button>
 			<Spinner
