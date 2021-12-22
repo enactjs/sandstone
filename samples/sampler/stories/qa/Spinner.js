3222d7fe8314b9760f5e2799be553f61460dffd0
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-12-22 18:16:56 +0200

    made the text shorter in qa samplers for spinner in order for the content to fit in smaller mobile screens

--- samples/sampler/stories/qa/Spinner.js
+++ samples/sampler/stories/qa/Spinner.js
@@ -64,8 +64,7 @@ export const WithLongContent = (args) => (
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
 				ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				laboris nisi ut aliquip ex ea commodo consequat.
 			</p>
 			<Button onClick={action('Inside Button events')}>Button</Button>
 			<Spinner
@@ -100,9 +99,7 @@ export const BlockingClickEvents = (args) => (
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
 				ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				laboris nisi ut aliquip ex ea commodo consequat.
 			</p>
 			<Button onClick={action('Inside Button events')}>Button</Button>
 			<Spinner
