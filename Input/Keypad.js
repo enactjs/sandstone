a097244ffbd6534bcb2abd586239acb8de68c1e2
Stanca Pop <stanca.pop@lgepartner.com>
2022-01-13 14:50:14 +0200

    removed unused import

--- Input/Keypad.js
+++ Input/Keypad.js
@@ -5,7 +5,6 @@
 import kind from '@enact/core/kind';
 import {add} from '@enact/core/keymap';
 import {handle, oneOf, forKey, forward, adaptEvent} from '@enact/core/handle';
-import platform from '@enact/core/platform';
 import {spotlightDefaultClass} from '@enact/spotlight/SpotlightContainerDecorator';
 import Layout, {Cell} from '@enact/ui/Layout';
 import PropTypes from 'prop-types';
