41a5f3855aa4bd8f12916f27227fdbf6ee97c617
Vladut Clapou <vladut.clapou@lgepartner.com>
2022-02-10 14:26:54 +0200

    Drawing component Toolbar(brush size, brush color, canvas color, eraser) implemeted

--- Drawing-POC/Drawing-POC.js
+++ Drawing-POC/Drawing-POC.js
@@ -4,6 +4,14 @@ import Slider from '../Slider';
 import css from './Drawing-POC.module.less';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { Panels, Panel, Header } from '../Panels';
+import Layout, { Cell, Column, Row } from '@enact/ui/Layout';
+import SwitchItem from '../SwitchItem';
+import Toggleable from '@enact/ui/Toggleable';
+import BodyText from '@enact/ui/BodyText';
+import Heading from '@enact/ui/Heading';
+import Switch from '../Switch';
+import Button from '@enact/ui/Button';
 
 const DrawingBase = kind({
     name: 'Drawing',
@@ -11,41 +19,90 @@ const DrawingBase = kind({
     functional: true,
 
     propTypes: {
-        brushColor: PropTypes.string,
-        brushSize: PropTypes.number,
-        canvasColor: PropTypes.string,
+        // brushColor: PropTypes.string,
+        // brushSize: PropTypes.number,
+        // canvasColor: PropTypes.string,
         disabled: PropTypes.bool,
+        isErasing: PropTypes.bool,
     },
 
     defaultProps: {
-        brushSize: 5,
-        brushColor: 'green',
-        canvasColor: '#fff',
+        // brushSize: 5,
+        // brushColor: 'green',
+        // canvasColor: '#fff',
+        isErasing: false,
     },
 
-    render: ({ brushColor, brushSize, canvasColor }) => {
-        // const [brushSize, setBrushSize] = useState(5);
-        console.log(brushSize);
+    render: ({ isErasing, onSetErasing }) => {
+        const [brushSize, setBrushSize] = useState(5);
+        const [brushColor, setBrushColor] = useState('green');
+        const [canvasColor, setcanvasColor] = useState('#fff');
         return (
-            <div className={css.app}>
-                <h1>Drawing component</h1>
-                <div>Brush size: {brushSize}</div>
-                <Slider
-                    backgroundProgress={0}
-                    max={30}
-                    min={0}
-                    // onChange={(e) => {
-                    //     setBrushSize(e.value);
-                    //     console.log('fron changeHandler');
-                    // }}
-                    step={1}
-                    tooltip={false}
-                    defaultValue={brushSize}
-                />
-                <Drawing brushSize={brushSize} brushColor={brushColor} canvasColor={canvasColor}/>
-            </div>
+            <Column>
+                <Row>
+                    <Cell>
+                        <Heading size='tiny'>
+                            <Slider
+                                backgroundProgress={0}
+                                max={30}
+                                min={0}
+                                onChange={(e) => {
+                                    setBrushSize(e.value);
+                                }}
+                                step={1}
+                                tooltip={false}
+                                defaultValue={brushSize}
+                            />
+                        </Heading>
+                    </Cell>
+                    <Cell>
+                        <Heading size="tiny">
+                            Brush color{' '}
+                            <input
+                                type="color"
+                                onChange={(e) => {
+                                    setBrushColor(e.target.value);
+                                }}
+                            />
+                        </Heading>
+                    </Cell>
+                    <Cell>
+                        <Heading size="tiny">
+                            Canvas color{' '}
+                            <input
+                                type="color"
+                                onChange={(e) => {
+                                    setcanvasColor(e.target.value);
+                                }}
+                            />
+                        </Heading>
+                    </Cell>
+                    <Cell>
+                        <Heading size="tiny">
+                            Erase
+                            <Switch onClick={onSetErasing} />
+                        </Heading>
+                    </Cell>
+                    <Cell>
+                        <Heading size="tiny">
+                            <Button size='tiny'>Clear all</Button>
+                        </Heading>
+                    </Cell>
+                </Row>
+                <Row>
+                    <Drawing
+                        brushSize={brushSize}
+                        brushColor={brushColor}
+                        canvasColor={canvasColor}
+                        isErasing={isErasing}
+                    />
+                </Row>
+            </Column>
         );
     },
 });
 
-export default DrawingBase;
+export default Toggleable(
+    { prop: 'isErasing', toggle: 'onSetErasing' },
+    DrawingBase
+);
