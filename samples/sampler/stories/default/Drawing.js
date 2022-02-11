e0485aab91072a5534f98aedef05fbaf3f9eeb0f
Alex Morariu <alexandru.morariu@lgepartner.com>
2022-02-11 15:14:05 +0200

    Eliminate useless controls from storybook/Drawing.

--- samples/sampler/stories/default/Drawing.js
+++ samples/sampler/stories/default/Drawing.js
@@ -1,5 +1,5 @@
 import Drawing from '@enact/sandstone/Drawing';
-import {boolean, range, select} from '@enact/storybook-utils/addons/controls';
+import {boolean} from '@enact/storybook-utils/addons/controls';
 
 Drawing.displayName = 'Drawing';
 
@@ -10,17 +10,11 @@ export default {
 
 export const _Drawing = (args) => (
 	<Drawing
-		brushColor={args['brushColor']}
-		brushSize={args['brushSize']}
-		canvasColor={args['canvasColor']}
 		disabled={args['disabled']}
 	/>
 );
 
 boolean('disabled', _Drawing, Drawing);
-range('brushSize', _Drawing, Drawing, {min: 1, max: 20}, 5);
-select('brushColor', _Drawing, ['white', 'red', 'green'], 'green');
-select('canvasColor', _Drawing, ['white', 'black'], 'black');
 
 _Drawing.storyName = 'Drawing';
 _Drawing.parameters = {
