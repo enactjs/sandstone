d56125507e053fa4946993ff7db4851a11a71da3
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-02-23 12:43:40 +0200

    *WIP* updated ss tests and added ui tests for TooltipDecorator

---
+++ tests/ui/specs/TooltipDecorator/TooltipDecoratorPage.js
@@ -0,0 +1,53 @@
+'use strict';
+const {element, getText, Page} = require('@enact/ui-test-utils/utils');
+
+class TooltipButtonInterface {
+	constructor (id) {
+		this.id = id;
+		this.selector = `#${this.id}`;
+		this.marqueeAnimatedSelector = `#${this.id} > div .enact_ui_Marquee_Marquee_animate`;
+	}
+
+	async focus () {
+		return await browser.execute((el) => el.focus(), await $(this.selector));
+	}
+
+	async hover () {
+		return await $(this.selector).moveTo({xOffset: 0, yOffset: 0});
+	}
+
+	get self () {
+		return $(this.selector);
+	}
+
+	get tooltipText () {
+		return getText(element('.enact-fit.enact-clip.enact-untouchable>div', browser));
+	}
+
+	get isTooltipShowing () {
+		return element('.enact-fit.enact-clip.enact-untouchable>div', browser).isExisting();
+	}
+
+	get isMarqueeAnimated () {
+		return $(this.marqueeAnimatedSelector).isExisting();
+	}
+}
+
+class TooltipDecoratorPage extends Page {
+	constructor () {
+		super();
+		this.title = 'TooltipDecorator Test';
+		const tooltipButtonDefault = new TooltipButtonInterface('tooltipButton1');
+		const tooltipButtonDelayed = new TooltipButtonInterface('tooltipButton2');
+		const tooltipButtonDisabled = new TooltipButtonInterface('tooltipButton3');
+		const tooltipButtonWithMarquee = new TooltipButtonInterface('tooltipButton4');
+
+		this.components = {tooltipButtonDefault, tooltipButtonDelayed, tooltipButtonDisabled, tooltipButtonWithMarquee};
+	}
+
+	async open (urlExtra) {
+		await super.open('TooltipDecorator-View', urlExtra);
+	}
+}
+
+module.exports = new TooltipDecoratorPage();
