49f0231216352eaf6eb65509b643c6e546d42ce6
Vladut Clapou <vladut.clapou@lgepartner.com>
2022-02-25 14:25:35 +0200

    Async migration of tests for ContextualPopupDecorator

--- tests/ui/specs/ContextualPopupDecorator/ContextualPopupDecorator-specs.js
+++ tests/ui/specs/ContextualPopupDecorator/ContextualPopupDecorator-specs.js
@@ -7,13 +7,15 @@ describe('ContextualPopupDecorator', function () {
 	});
 
 	const {
-		button1
+		button1,
+		button2,
+		button3
 	} = Page.components;
 
 	describe('not using open', function () {
 
-		beforeEach(function () {
-			button1.focus();
+		beforeEach(async function () {
+			await button1.focus();
 		});
 
 		it('should focus the first button on start', async function () {
@@ -21,16 +23,109 @@ describe('ContextualPopupDecorator', function () {
 		});
 
 		describe('using 5-way', function () {
-			// The menu is open and the first item has focus
-			beforeEach(function () {
-				Page.spotlightSelect();
+			it('should focus second button on 5-way right', async function () {
+				await Page.spotlightRight();
+				expect(await button2.self.isFocused()).to.be.true();
+			});
+		});
+	});
+
+	describe('using open', function () {
+		describe('using 5-way', function () {
+			it('should have Spotlight on button when ContextualPopup1 opens', async function () {
+				let popupButton = $('#popupButton1');
+
+				await Page.spotlightSelect();
+				expect(await popupButton.isFocused()).to.be.true();
 			});
 
-			it('should have Spotlight on close button when ContextualPopup opens - [QWT-2731]', async function () {
-				// 5-waySelectableActivator: Button Retains Spotlight when Popup Hides
-				let popupButton = $('#popupButton');
+			it('should have Spotlight on button when ContextualPopup2 opens', async function () {
+				let popupButton = $('#popupButton2');
 
+				await button2.focus();
+				await Page.spotlightSelect();
 				expect(await popupButton.isFocused()).to.be.true();
+			});
+
+			it('should close popup and move Spotlight on button on Spotlight up', async function () {
+				let popupButton = $('#popupButton3');
+
+				await button3.focus();
+				await Page.spotlightSelect();
+				expect(await popupButton.isFocused()).to.be.true();
+
+				await Page.spotlightUp();
+				expect(await button3.isOpen).to.be.false();
+				expect(await button3.self.isFocused()).to.be.true();
+			});
+
+			it('with SpotlightRestrict="self-only", should not close popup and move Spotlight on button on Spotlight up', async function () {
+				let popupButton = $('#popupButton2');
+
+				await button2.focus();
+				await Page.spotlightSelect();
+				expect(await popupButton.isFocused()).to.be.true();
+
+				await Page.spotlightUp();
+				expect(await button2.isOpen).to.be.true();
+				expect(await button2.self.isFocused()).to.be.false();
+			});
+
+			it('should close the popup and should have Spotlight on button on auto dismiss the ContexualPopup2', async function () {
+				let popupButton = $('#popupButton2');
+
+				await button2.focus();
+				await Page.spotlightSelect();
+				expect(await popupButton.isFocused()).to.be.true();
+
+				await Page.backKey();
+				expect(await button2.isOpen).to.be.false();
+				expect(await button2.self.isFocused()).to.be.true();
+			});
+
+			it('should not close the popup and should not have Spotlight on button on auto dismiss the ContexualPopup3', async function () {
+				let popupButton = $('#popupButton3');
+
+				await button3.focus();
+				await Page.spotlightSelect();
+				expect(await popupButton.isFocused()).to.be.true();
+
+				await Page.backKey();
+				expect(await button1.isOpen).to.be.true();
+				expect(await button1.self.isFocused()).to.be.false();
+			});
+		});
+
+		describe('using pointer', function () {
+			it('should open when clicked', async function () {
+				await button1.self.click();
+				expect(await button1.isOpen).to.be.true();
+			});
+
+			it('should close when clicking twice', async function () {
+				await button1.self.click();
+				await button1.self.click();
+				expect(await button1.isOpen).to.be.false();
+			});
+
+			it('should close when clicking outside', async function () {
+				await button1.self.click();
+
+				// Click in the area outside the ContextualPopupDecorator (in the empty space created by the wrapper)
+				const wrapper = $('.ThemeDecorator_ThemeDecorator_bg');
+				await wrapper.click({x: 0, y: 0});
+
+				expect(await button1.isOpen).to.be.false();
+			});
+
+			it('should not close when clicking outside', async function () {
+				await button3.self.click();
+
+				// Click in the area outside the ContextualPopupDecorator (in the empty space created by the wrapper)
+				const wrapper = $('.ThemeDecorator_ThemeDecorator_bg');
+				await wrapper.click({x: 0, y: 0});
+
+				expect(await button3.isOpen).to.be.true();
 			});
 		});
 	});
