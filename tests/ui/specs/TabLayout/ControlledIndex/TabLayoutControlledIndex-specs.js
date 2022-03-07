9d49c3599d4098761e7ed883c164e5ccd237336d
Daniel Stoian <63335068+daniel-stoian-lgp@users.noreply.github.com>
2022-03-07 09:46:54 +0200

    WRN-18123: Migrated ui tests for TabLayout --> WizardPanels (#1181)
    * migrated ui tests for tablayout
    
    * migrated ui tests for timePicker
    
    * migrated ui tests for WizardPanels
    
    * migrated ui tests for VirtualList
    
    * migrated ui tests for VirtualList
    
    * migrated ui tests for VirtualList
    
    * migrated ui tests for VirtualList
    
    * Update tests/ui/specs/VirtualList/VirtualGridList/ElementSize/VirtualGridList-QWT-2622-specs.js
    
    Co-authored-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- tests/ui/specs/TabLayout/ControlledIndex/TabLayoutControlledIndex-specs.js
+++ tests/ui/specs/TabLayout/ControlledIndex/TabLayoutControlledIndex-specs.js
@@ -1,54 +1,51 @@
 const Page = require('../TabLayoutPage');
 
 describe('TabLayout', function () {
-
-	beforeEach(function () {
-		Page.open('ControlledIndex');
+	beforeEach(async function () {
+		await Page.open('ControlledIndex');
 	});
 
 	describe('with controlled index', function () {
-
 		describe('5-way interaction', function () {
-
-			it('should focus an item in the new panel, remaining collapsed', function () {
+			it('should focus an item in the new panel, remaining collapsed', async function () {
 				// 5-way right to button in panel, collapsing tabs
-				Page.spotlightRight();
-				Page.waitForFocused($('#button2'), {targetName: 'button 2'});
+				await Page.spotlightRight();
+				await Page.waitForFocused($('#button2'), {targetName: 'button 2'});
 				// Click button, programmatically changing tab index
-				Page.spotlightSelect();
+				await Page.spotlightSelect();
 
 				// Wait for new tab to appear, verify focus exists and tabs don't open
-				Page.tabLayout.view(3).waitForExist();
-				Page.waitForFocused($('#button3'), {targetName: 'button 3'});
-				expect(Page.tabLayout.isCollapsed).to.be.true();
+				await (await Page.tabLayout.view(3)).waitForExist();
+				await Page.waitForFocused($('#button3'), {targetName: 'button 3'});
+				expect(await Page.tabLayout.isCollapsed).to.be.true();
 
 				// Spotlight left should return to active tab, not previous tab
-				Page.spotlightLeft();
-				Page.waitForFocused(Page.tabLayout.tabItems[2], {targetName: '3rd tab'});
+				await Page.spotlightLeft();
+				await Page.waitForFocused((await Page.tabLayout.tabItems())[2], {targetName: '3rd tab'});
 
 			});
 
-			it('should not focus an item in the new panel if not collapsed', function () {
+			it('should not focus an item in the new panel if not collapsed', async function () {
 				// 5-way down to 3rd panel
-				Page.spotlightDown();
-				Page.tabLayout.view(3).waitForExist();
+				await Page.spotlightDown();
+				await (await Page.tabLayout.view(3)).waitForExist();
 
 				// Focus delayed index change button and select it
-				Page.spotlightRight();
-				Page.waitForFocused($('#button3'), {targetName: 'button 3'});
-				Page.spotlightSelect();
+				await Page.spotlightRight();
+				await Page.waitForFocused($('#button3'), {targetName: 'button 3'});
+				await Page.spotlightSelect();
 
 				// Focus back to tabs
-				Page.spotlightLeft();
-				Page.waitForFocused(Page.tabLayout.tabItems[2], {targetName: '3rd tab'});
-				expect(Page.tabLayout.isCollapsed).to.be.false();
+				await Page.spotlightLeft();
+				await Page.waitForFocused((await Page.tabLayout.tabItems())[2], {targetName: '3rd tab'});
+				expect(await Page.tabLayout.isCollapsed).to.be.false();
 
 				// Wait for second tab contents to appear
-				Page.tabLayout.view(2).waitForExist();
-				expect(Page.tabLayout.isCollapsed).to.be.false();
+				await (await Page.tabLayout.view(2)).waitForExist();
+				expect(await Page.tabLayout.isCollapsed).to.be.false();
 
 				// Focus should be updated to the new selected item
-				Page.waitForFocused(Page.tabLayout.tabItems[1], {targetName: '2nd tab focused'});
+				await Page.waitForFocused((await Page.tabLayout.tabItems())[1], {targetName: '2nd tab focused'});
 			});
 		});
 	});
