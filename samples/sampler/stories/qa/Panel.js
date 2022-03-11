bf539f99a39d28d9764d249e74dff813962035e9
Juwon Jeong <juwon.jeong@lge.com>
2022-03-11 17:54:50 +0900

    Fix lint errors
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

--- samples/sampler/stories/qa/Panel.js
+++ samples/sampler/stories/qa/Panel.js
@@ -1,22 +1,26 @@
 import Button from '@enact/sandstone/Button';
 import Item from '@enact/sandstone/Item';
-import {useState, useCallback} from 'react';
+import {Header, Panels, Panel} from '@enact/sandstone/Panels';
 import Spotlight from '@enact/spotlight';
 import {action} from '@enact/storybook-utils/addons/actions';
+import {useState, useCallback} from 'react';
 import compose from 'ramda/src/compose';
-import {Header, Panels, Panel} from '@enact/sandstone/Panels';
 
 export const WithAutoFocusControl = () => {
 	// hooks
 	const initialState = 0;
 	const [panelIndex, setState] = useState(initialState);
 
-	const forward = () => setState(panelIndex + 1);
-	const backward = () => setState(panelIndex - 1);
+	const forward = useCallback(() => {
+		setState(panelIndex + 1);
+	}, [panelIndex]);
+
+	const backward = useCallback(() => {
+		setState(panelIndex - 1);
+	}, [panelIndex]);
 
 	const handleTransition = useCallback(() => {
 		setTimeout(() => {
-			console.log(`spotlight focus with ${panelIndex}`);
 			Spotlight.focus(`panel-container-${panelIndex}`);
 		}, 1000);
 	}, [panelIndex]);
@@ -38,7 +42,7 @@ export const WithAutoFocusControl = () => {
 						iconFlip="auto"
 						size="small"
 						slot="slotAfter"
-						onClick={forward} // eslint-disable-line react/jsx-no-bind
+						onClick={forward}
 					/>
 				</Header>
 				<Item onClick={forward}>Item 0</Item>
@@ -50,7 +54,7 @@ export const WithAutoFocusControl = () => {
 						iconFlip="auto"
 						size="small"
 						slot="slotAfter"
-						onClick={forward} // eslint-disable-line react/jsx-no-bind
+						onClick={forward}
 					/>
 				</Header>
 				<Item onClick={forward}>Item 1</Item>
@@ -62,7 +66,7 @@ export const WithAutoFocusControl = () => {
 						iconFlip="auto"
 						size="small"
 						slot="slotAfter"
-						onClick={forward} // eslint-disable-line react/jsx-no-bind
+						onClick={forward}
 					/>
 				</Header>
 				<Item>Item 2</Item>
