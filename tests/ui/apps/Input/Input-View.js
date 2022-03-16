092944b5627038644b8961e529740f494c938b38
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2022-03-16 09:05:23 +0200

    Update tests/ui/apps/Input/Input-View.js
    Co-authored-by: dongsuwon <61816767+dongsuwon@users.noreply.github.com>

--- tests/ui/apps/Input/Input-View.js
+++ tests/ui/apps/Input/Input-View.js
@@ -1,6 +1,8 @@
+import spotlight from '@enact/spotlight';
+
 import {Input} from '../../../../Input';
 import ThemeDecorator from '../../../../ThemeDecorator';
-import spotlight from '@enact/spotlight';
+
 import {Component} from 'react';
 
 // NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
