d15545db21b91612808e7c27418d36e8f6d1175e
Vladut Clapou <vladut.clapou@lgepartner.com>
2022-03-29 11:44:10 +0300

    Lint fixes

--- Drawing/Drawing.js
+++ Drawing/Drawing.js
@@ -258,8 +258,8 @@ const DrawingBase = kind({
 
 		useEffect(() => {
 			setBrushColorValue(brushColor);
-			!showDrawingControls && window.localStorage.setItem('lastBrushColor', JSON.stringify(brushColor)); // eslint-disable-line 
-		}, [brushColor]); // eslint-disable-line react-hooks/exhaustive-deps
+			!showDrawingControls && window.localStorage.setItem('lastBrushColor', JSON.stringify(brushColor));
+		}, [brushColor]);
 
 		useEffect(() => {
 			setBrushSizeValue(brushSize);
@@ -271,13 +271,13 @@ const DrawingBase = kind({
 
 		useEffect(() => {
 			setFillColorValue(fillColor);
-			!showDrawingControls && window.localStorage.setItem('lastFillColor', JSON.stringify(fillColor)); // eslint-disable-line 
-		}, [fillColor]); // eslint-disable-line react-hooks/exhaustive-deps
+			!showDrawingControls && window.localStorage.setItem('lastFillColor', JSON.stringify(fillColor));
+		}, [fillColor]);
 
 		useEffect(() => {
 			setCanvasColorValue(canvasColor);
-			!showDrawingControls && window.localStorage.setItem('lastCanvasColor', JSON.stringify(canvasColor)); // eslint-disable-line 
-		}, [canvasColor]); // eslint-disable-line react-hooks/exhaustive-deps
+			!showDrawingControls && window.localStorage.setItem('lastCanvasColor', JSON.stringify(canvasColor));
+		}, [canvasColor]);
 
 		useEffect(() => {
 			const storedBrushColors = JSON.parse(window.localStorage.getItem('brushColors'));
