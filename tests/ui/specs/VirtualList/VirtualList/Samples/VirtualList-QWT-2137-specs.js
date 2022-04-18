953ccaf21e4c3ba3830ca21ab5503c61fa751d76
dongsu.won <dongsu.won@lgepartner.com>
2022-04-18 09:37:17 +0900

    * Fix ui test(QWT-2137).
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

--- tests/ui/specs/VirtualList/VirtualList/Samples/VirtualList-QWT-2137-specs.js
+++ tests/ui/specs/VirtualList/VirtualList/Samples/VirtualList-QWT-2137-specs.js
@@ -6,6 +6,7 @@ describe('VirtualList Samples', function () {
 		await Page.open();
 	});
 
+	// As Ux change that 5-way and pointer mode had the different UX for restore focus, this test work with 5-way mode.
 	it('should focus last item when entering from outside after scrolling via 5way [QWT-2137]', async function () {
 		// Step 1. Move focus to the first item ('Item 00').
 		await Page.buttonLeft.moveTo();
@@ -14,12 +15,16 @@ describe('VirtualList Samples', function () {
 		// Step2. 5-way Down to the 21st item ('Item 20').
 		await Page.fiveWayToItem(20);
 
-		// Step3. Position the pointer on 'JumpToItem10WithoutFocus' button and select
-		await Page.buttonJumpToItemWithoutFocus.moveTo();
+		// Step3. 5-way spot 'JumpToItem10WithoutFocus' button and select
+		await Page.spotlightLeft();
+		await Page.spotlightUp();
+		await Page.spotlightUp();
+		await Page.spotlightRight();
 		await Page.spotlightSelect();
 
-		// Step4. Move focus to the list
-		await Page.buttonLeft.moveTo();
+		// Step4. Move focus to the list(In this view, Left button -> Item 0)
+		await Page.spotlightDown();
+		await Page.spotlightDown();
 		await Page.spotlightRight();
 
 		// Wait for scroll animation
