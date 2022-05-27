ae5e6d32cda414ba9a8df063761818c460be7777
jeonghee27 <jeonghee27.ahn@lge.com>
2022-05-27 16:42:08 +0900

    minor fix

--- tests/screenshot/apps/components/Header.js
+++ tests/screenshot/apps/components/Header.js
@@ -69,7 +69,7 @@ const LtrTests = [
 	...withProps({type: 'standard', noSubtitle: true}, baseTests),
 	...withProps({type: 'compact', noSubtitle: true}, baseTests),
 	...withProps({type: 'wizard', noSubtitle: true}, baseTests),
-	...withProps({type: 'mini', noSubtitle: true}, baseTests),
+	...withProps({type: 'mini', noSubtitle: true}, baseTests)
 ];
 
 const HeaderTests = [
