8459efef70697b307a7fc827848177bd129635f8
jeonghee27 <jeonghee27.ahn@lge.com>
2022-07-04 13:42:51 +0900

    Add sampler

--- samples/sampler/stories/qa/ProgressBar.js
+++ samples/sampler/stories/qa/ProgressBar.js
@@ -1,6 +1,9 @@
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import {boolean, range, select} from '@enact/storybook-utils/addons/controls';
+import Alert from '@enact/sandstone/Alert';
+import Button from '@enact/sandstone/Button';
 import ProgressBar, {ProgressBarBase} from '@enact/sandstone/ProgressBar';
+import {useCallback, useState} from 'react';
 
 const Config = mergeComponentMetadata('ProgressBar', ProgressBarBase, ProgressBar);
 
@@ -29,5 +32,54 @@ boolean('disabled', TheBasicProgressBar, Config, false);
 
 TheBasicProgressBar.storyName = 'The basic ProgressBar';
 TheBasicProgressBar.parameters = {
+	propTables: [Config]
+};
+
+
+export const inAlert = (args) => {
+	const [open, setOpen] = useState(false);
+
+	const handleOpenAlert = useCallback(() => {
+		setOpen(true);
+	}, []);
+
+	const handleCloseAlert = useCallback(() => {
+		setOpen(false);
+	}, []);
+
+	return (
+		<>
+			<ProgressBar
+				backgroundProgress={args['backgroundProgress']}
+				tooltip={args['tooltip']}
+				highlighted={args['highlighted']}
+				progress={args['progress']}
+				orientation={args['orientation']}
+				disabled={args['disabled']}
+			/>
+			<Button onClick={handleOpenAlert}>open</Button>
+			<Alert open={open} type="overlay" onClose={handleCloseAlert}>
+				<ProgressBar
+					backgroundProgress={args['backgroundProgress']}
+					tooltip={args['tooltip']}
+					highlighted={args['highlighted']}
+					progress={args['progress']}
+					orientation={args['orientation']}
+					disabled={args['disabled']}
+				/>
+			</Alert>
+		</>
+	);
+};
+
+range('backgroundProgress', inAlert, Config, {min: 0, max: 1, step: 0.01}, 0.5);
+boolean('tooltip', inAlert, Config, false);
+boolean('highlighted', inAlert, Config, false);
+range('progress', inAlert, Config, {min: 0, max: 1, step: 0.01}, 0.4);
+select('orientation', inAlert, ['horizontal', 'vertical'], Config, 'horizontal');
+boolean('disabled', inAlert, Config, false);
+
+inAlert.storyName = 'inAlert';
+inAlert.parameters = {
 	propTables: [Config]
 };
