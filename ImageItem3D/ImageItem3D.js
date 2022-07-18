61ec5a617e516674cca83526919f5354aad3d42a
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2022-07-18 15:00:09 +0300

    WRO-8005: [POC] Enact WebXR Components - Gallery3D perspective change. (#1286)
    * Added rotation controls for the gallery.
    
    * Eliminated redundant if statement.
    
    * Merge conflicts.
    
    * Fix
    
    * Lint fix

--- ImageItem3D/ImageItem3D.js
+++ ImageItem3D/ImageItem3D.js
@@ -1,9 +1,10 @@
+import EnactPropTypes from '@enact/core/internal/prop-types';
 import kind from '@enact/core/kind';
-import {OrbitControls, Text} from '@react-three/drei';
+import {Text} from '@react-three/drei';
 import {useLoader} from '@react-three/fiber';
 import {Interactive} from '@react-three/xr';
 import PropTypes from 'prop-types';
-import {useCallback, useRef, useState} from 'react';
+import {useCallback, useImperativeHandle, useRef, useState} from 'react';
 import * as THREE from 'three';
 
 import Skinnable from '../Skinnable';
@@ -16,10 +17,14 @@ const ImageItem3DBase = kind({
 	propTypes: {
 		children: PropTypes.node,
 		disabled: PropTypes.bool,
+		imageItemRef: EnactPropTypes.ref,
 		index: PropTypes.number,
 		label: PropTypes.string,
+		pointerDown: PropTypes.number,
 		position: PropTypes.array,
 		selected: PropTypes.number,
+		setControlled: PropTypes.func,
+		setPointerDown: PropTypes.func,
 		setSelected: PropTypes.func,
 		src: PropTypes.string
 	},
@@ -28,19 +33,23 @@ const ImageItem3DBase = kind({
 		index: 0,
 		position: [0, 0, 0],
 		selected: null,
-		setSelected: null
+		setSelected: null,
+		setControlled: () => {}
 	},
 
-	render: ({children, disabled, label, src, position, index, selected, setSelected, ...rest}) => {
-
+	render: ({children, disabled, imageItemRef, index, label, pointerDown, position, selected, setControlled, setPointerDown, setSelected, src, ...rest}) => {
+		const groupRef = useRef(); // eslint-disable-line react-hooks/rules-of-hooks
 		const mesh = useRef(); // eslint-disable-line react-hooks/rules-of-hooks
 		const textRef = useRef(); // eslint-disable-line react-hooks/rules-of-hooks
 		const [hovered, setHover] = useState(false); // eslint-disable-line react-hooks/rules-of-hooks
+		const [pastState, setPastState] = useState(null); // eslint-disable-line react-hooks/rules-of-hooks
+		const [rotateX, setRotateX] = useState(0); // eslint-disable-line react-hooks/rules-of-hooks
+		const [rotateY, setRotateY] = useState(0); // eslint-disable-line react-hooks/rules-of-hooks
 		const shape = new THREE.Shape();
 
+		let radius = 0.6;
 		let sizeX = 6;
 		let sizeY = 7;
-		let radius = 0.6;
 
 		const texture = useLoader(THREE.TextureLoader, src); // eslint-disable-line react-hooks/rules-of-hooks
 		const image = <mesh>
@@ -57,28 +66,24 @@ const ImageItem3DBase = kind({
 		shape.absarc(halfX, -halfY, radius, baseAngle * 3, baseAngle * 3 + baseAngle);
 
 		const disabledHoverColor = disabled ? '#404040' : '#e6e6e6';
-
-		const lineColor = (hovered || selected === index) ? '#363636' : '#e6e6e6';
 		const imageItemGeometry = new THREE.ExtrudeGeometry(shape, {bevelEnabled: false, depth: 0.3});
+		const lineColor = (hovered || selected === index) ? '#363636' : '#e6e6e6';
 
-		const handlePosition = () => {
-			if (selected === index) {
-				return [position[0], position[1], position[2] + 2];
+		const handlePointerDown = useCallback((ev) => { // eslint-disable-line react-hooks/rules-of-hooks
+			if (pointerDown === -1) {
+				setPastState(ev);
+				setPointerDown(index);
+				setControlled(false);
 			}
+		}, [index, pointerDown, setControlled, setPointerDown]);
 
-			if (hovered) {
-				return [position[0], position[1], position[2] + 3];
-			}
-
-			return position;
-		};
+		const handlePointerOut = useCallback(() => { // eslint-disable-line react-hooks/rules-of-hooks
+			setControlled(true);
+			setHover(false);
+		}, [setControlled]);
 
 		const handlePointerOver = useCallback(() => { // eslint-disable-line react-hooks/rules-of-hooks
 			setHover(true);
-		}, []);
-
-		const handlePointerOut = useCallback(() => { // eslint-disable-line react-hooks/rules-of-hooks
-			setHover(false);
 		}, []);
 
 		const handleSelect = useCallback(() => { // eslint-disable-line react-hooks/rules-of-hooks
@@ -89,7 +94,43 @@ const ImageItem3DBase = kind({
 			}
 		}, [index, selected]); // eslint-disable-line react-hooks/exhaustive-deps
 
+		const handlePosition = () => {
+			if (selected === index) {
+				return [position[0], position[1], position[2] + 2];
+			}
+
+			if ((hovered && pointerDown === -1) || pointerDown === index) {
+				return [position[0], position[1], position[2] + 3];
+			}
+
+			return position;
+		};
+
 		const newPosition = handlePosition();
+
+		useImperativeHandle(imageItemRef, () => ({ // eslint-disable-line react-hooks/rules-of-hooks
+			handlePointerMove: (ev) => {
+				if (pointerDown === index) {
+					if (pastState) {
+						if (pastState.nativeEvent.clientX > ev.nativeEvent.clientX) {
+							setRotateY((state) => state - 0.02);
+						}
+						if (pastState.nativeEvent.clientX < ev.nativeEvent.clientX) {
+							setRotateY((state) => state + 0.02);
+						}
+						if (pastState.nativeEvent.clientY > ev.nativeEvent.clientY) {
+							setRotateX((state) => state - 0.02);
+						}
+						if (pastState.nativeEvent.clientY < ev.nativeEvent.clientY) {
+							setRotateX((state) => state + 0.02);
+						}
+						setPastState(ev);
+					}
+				}
+			},
+
+			handlePointerUp: () => setPointerDown(-1)
+		}));
 
 		return (
 			<Interactive
@@ -100,13 +141,20 @@ const ImageItem3DBase = kind({
 				onSqueezeStart={handlePointerOver}
 				onSqueezeEnd={handlePointerOut}
 			>
-				<group {...rest} position={newPosition} scale={selected === index ? 1.3 : 1} >
+				<group
+					{...rest}
+					ref={groupRef}
+					rotation={new THREE.Euler(rotateX, rotateY, 0)}
+					position={newPosition}
+					scale={selected === index ? 1.3 : 1}
+				>
 					<group position={[0, -0.5, -0.51]}>
 						<mesh
-							ref={mesh}
-							onPointerOver={handlePointerOver}
+							onClick={handleSelect}
+							onPointerDown={handlePointerDown}
 							onPointerOut={handlePointerOut}
-							onPointerDown={handleSelect}
+							onPointerOver={handlePointerOver}
+							ref={mesh}
 						>
 							<lineSegments>
 								<edgesGeometry args={[imageItemGeometry]} />
@@ -114,18 +162,17 @@ const ImageItem3DBase = kind({
 							</lineSegments>
 							<extrudeBufferGeometry args={[shape, {bevelEnabled: false, depth: 0.3}]} />
 							<meshStandardMaterial color={hovered || (selected === index) ? disabledHoverColor : '#282929'} />
-							<OrbitControls />
 						</mesh>
 					</group>
 					<group position={[-2.5, -3.1, -0.15]}>
 						<Text
-							ref={textRef}
 							anchorX="left"
 							anchorY="middle"
 							color={hovered || selected === index ? '#6f7074' : disabledHoverColor}
 							font={'http://fonts.gstatic.com/s/modak/v5/EJRYQgs1XtIEskMA-hI.woff'}
 							fontSize={0.5}
 							maxWidth={15}
+							ref={textRef}
 							textAlign="left"
 						>
 							{children}
@@ -134,7 +181,7 @@ const ImageItem3DBase = kind({
 					<group position={[-2.5, -3.45, -0.15]}>
 						<Text
 							anchorX="left"
-							color={hovered || selected === index ? '#6f7074' : disabled ? '#404040' : '#e6e6e6'} // eslint-disable-line no-nested-ternary
+							color={hovered || selected === index  ? '#6f7074' : disabled ? '#404040' : '#e6e6e6'} // eslint-disable-line no-nested-ternary
 							fontSize={0.3}
 							maxWidth={15}
 							textAlign="left"
