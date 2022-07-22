904daf81f6ccf862a441a795f0a7187ac20be701
dongsuwon <61816767+dongsuwon@users.noreply.github.com>
2022-07-22 19:03:38 +0900

    WRO-7727: Sync TC number in ui Test(QWT>QWTC) (#1287)
    * Sync TC number in ui Test(QWT>QWTC)
    
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)

---
+++ tests/ui/specs/VirtualList/VirtualList/WithMultipleSpottables/VirtualList-QWTC-2260-specs.js
@@ -0,0 +1,25 @@
+const Page = require('../VirtualListPage');
+
+describe('VirtualList with multiple spottables in an item', function () {
+	beforeEach(async function () {
+		await Page.open('WithMultipleSpottables');
+	});
+
+	it('should navigates between the same spottable controls of items [QWTC-2260]', async function () {
+		// Step 1: 5-way Spot a star icon (★) of the first ('Item 000').
+		await Page.spotlightDown();
+		await Page.spotlightRight();
+		await Page.spotlightRight();
+		// STep 1 Verify: Spotlight displays on a star icon(★) of the first item ('item 000').
+		expect((await Page.getElementAttribute('id')).slice(0, 8)).to.equal('starIcon');
+		expect(Number(await Page.getElementAttribute('data-index'))).to.equal(0);
+		// 5-way Down hold for 1 second.
+		await Page.delay(500);
+		await browser.keys('Down Arrow');
+		// Spotlight still on any item's starIcon.
+		// '5-way down' long pressure is too fast to catch focus element in Jenkins. Therefore, test to catch focus properly with 5-way down.
+		await Page.spotlightDown();
+		expect((await Page.getElementAttribute('id')).slice(0, 8)).to.equal('starIcon');
+		expect(Number(await Page.getElementAttribute('data-index'))).to.above(5);
+	});
+});
---
+++ tests/ui/specs/VirtualList/VirtualList/Wrap/VirtualList-QWTC-2052-specs.js
@@ -0,0 +1,53 @@
+const Page = require('../VirtualListPage');
+const {expectFocusedItem, waitUntilFocused} = require('../../VirtualList-utils');
+
+describe('Wrap Knobs', function () {
+	beforeEach(async function () {
+		await Page.open();
+	});
+
+	// Since visual test(Animation, Pointer disappears) is not able to verify by UI Test, This TC is partialy automated.
+	it('should not scroll when leaving list with 5-way up/down [QWTC-2052]', async function () {
+		// Step 3 is 'Set dataSize to 100' but set dataSize to 20 for Speed up Test.
+		await Page.inputfieldNumItems.moveTo();
+		await Page.spotlightSelect();
+		await Page.backSpace();
+		await Page.backSpace();
+		await Page.backSpace();
+		await Page.numPad(2);
+		await Page.numPad(0);
+		await Page.backKey();
+		await Page.spotlightDown();
+		// Step 4: change to 5-way mode
+		await Page.buttonLeft.moveTo();
+		// Step 5: 5-way Spot the first item.
+		await Page.spotlightRight();
+		// Verify Step 5: Spotlight displays on the first item.
+		await expectFocusedItem(0, 'step 5 focus');
+		// Step 6: 5-way Up.
+		await Page.spotlightUp();
+		// Verify Step 6: 1. The list *does not* Scroll to the Bottom. 2. Spotlight is on the close button 'x'.
+		expect(await Page.buttonTop.isFocused(), 'step 6 focus').to.be.true();  // buttonTop replaces the X button
+		// Step 7: 1. Wheel Down on the list to the last item.
+		// Page.mouseWheel(40, Page.item(6));   currently not working as expected so using 5-way Down temporary
+		// Wheeling will not be implemented - see ENYO-6178
+		await Page.spotlightDown();
+		await expectFocusedItem(0);
+		await Page.pageDown();
+		await waitUntilFocused(6, 'focus Item 6');
+		await Page.pageDown();
+		await waitUntilFocused(19, 'focus Item 19');
+		// Step 7: 2. Click the last item.
+		await Page.spotlightSelect();
+		// Verify Step 7: Spotlight is on the last item.
+		await Page.delay(1000);
+		await expectFocusedItem(19, 'step 7 focus');
+		// Step 8: 5-way Down
+		await Page.spotlightDown();
+		await Page.spotlightDown(); // 1 extra 5-way down to check Spotlight does not pass buttonBottom when wrap is off.
+		await Page.delay(1000);
+		// Verify Step 8: 1. The list *does not* Scroll to the Top. 2. Spotlight stays on the last item.
+		// Checking focus is on buttonBottom instead of last item since 5-way Down on last item using this app takes Spotlight to buttonBottom.
+		expect(await Page.buttonBottom.isFocused(), 'step 8 focus').to.be.true();
+	});
+});
