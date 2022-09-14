ab72081b7080cbb1529afbf438b7676b87b99b79
sjro <sj.ro@lge.com>
2022-09-14 13:41:38 +0900

    WRO-11048: Change the image used in the sample to svg (#1312)
    * Change the image source to svg
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Change font size for svg text
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Change the image source to svg
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Change the font size
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Change the font size
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Change the font size
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Update ImageItem.js to change image color
    
    * Update src url
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Update npm-shrinkwrap.json to develop version
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * add helper/svg.js
    Enact-DCO-1.0-Signed-off-by: SJ RO (sj.ro@lge.com)
    
    * Fixed svgGenerator in samples
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fixed typo
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Fixed minor issue
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    Co-authored-by: Juwon Jeong <juwon.jeong@lge.com>

--- samples/qa-virtualgridlist/src/utils/index.js
+++ samples/qa-virtualgridlist/src/utils/index.js
@@ -1,5 +1,11 @@
 import {ITEM_SPACING, RECORD_COUNT, MIN_HEIGHT, MIN_WIDTH, MODIFIER_FREQUENCY} from '../defaultConfiguration';
 
+const svgGenerator = (width, height, bgColor, textColor, customText) => (
+	`data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' width='${width}' height='${height}'%3E` +
+	`%3Crect width='${width}' height='${height}' fill='%23${bgColor}'%3E%3C/rect%3E` +
+	`%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23${textColor}'%3E${customText}%3C/text%3E%3C/svg%3E`
+);
+
 const createRecord = ({
 	recordIndex = 0,
 	modifierFrequency = MODIFIER_FREQUENCY,
@@ -16,7 +22,7 @@ const createRecord = ({
 		subCaption,
 		selected: false,
 		showSelection,
-		src: `http://via.placeholder.com/300x300/${color}/ffffff/png?text=Image+${recordIndex}`
+		src: svgGenerator(300, 300, color, 'ffffff', `Image ${recordIndex}`)
 	};
 };
 
