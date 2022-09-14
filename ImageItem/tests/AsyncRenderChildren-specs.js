098c10be100cd6c2e4bed5ac395a5226571564a2
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-09-14 15:07:38 +0300

    added 1 unit test for asyncRenderChildren

---
+++ ImageItem/tests/AsyncRenderChildren-specs.js
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import {render} from '@testing-library/react';
+
+import AsyncRenderChildren from '../AsyncRenderChildren';
+
+let data = {};
+const Component = (props) => {
+	data = props;
+	return <AsyncRenderChildren {...props} />
+}
+
+describe('AsyncRenderChildren', () => {
+	test('should have a fallback content', () => {
+		render(<Component fallback={<div>Loading...</div>} children="children" index={1} />);
+
+		const fallbackContent = data.fallback.props.children;
+
+		expect(fallbackContent).toBe('Loading...');
+	});
+});
