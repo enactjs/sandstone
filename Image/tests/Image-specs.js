d5922c87aee7b4d6176e6516e0a0cda3d7616fd1
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-09-20 14:09:22 +0300

    test to check window resize

--- Image/tests/Image-specs.js
+++ Image/tests/Image-specs.js
@@ -1,42 +1,41 @@
 import '@testing-library/jest-dom';
-import {render, screen} from '@testing-library/react';
+import {act, render, screen} from '@testing-library/react';
+import matchMediaPolyfill from 'mq-polyfill'
 
 import Image from '../Image';
-import {selectSrc} from '@enact/ui/resolution';
 
-// const src={
-// 	'hd': 'https://dummyimage.com/64/e048e0/0011ff',
-// 	'fhd': 'https://dummyimage.com/128/e048e0/0011ff',
-// 	'uhd': 'https://dummyimage.com/256/e048e0/0011ff'
-// }
 const src = {
-	hd: 'http://via.placeholder.com/200x200',
-	fhd: 'http://via.placeholder.com/300x300',
-	uhd: 'http://via.placeholder.com/600x600'
+	hd: 'https://via.placeholder.com/200x200',
+	fhd: 'https://via.placeholder.com/300x300',
+	uhd: 'https://via.placeholder.com/600x600'
 };
 
-// window.resizeTo = function resizeTo(width, height) {
-// 	Object.assign(this, {
-// 		innerWidth: width,
-// 		innerHeight: height,
-// 		outerWidth: width,
-// 		outerHeight: height
-// 	}).dispatchEvent(new this.Event("resize"));
-// };
 const resizeWindow = (x, y) => {
 	window.innerWidth = x;
 	window.innerHeight = y;
+	window.outerWidth = x;
+	window.outerHeight = y;
 	window.dispatchEvent(new Event('resize'));
 };
 
+matchMediaPolyfill(window)
+window.resizeTo = function resizeTo(width, height) {
+	Object.assign(this, {
+		innerWidth: width,
+		innerHeight: height,
+		outerWidth: width,
+		outerHeight: height,
+	}).dispatchEvent(new this.Event('resize'))
+}
+
 describe('Image', () => {
 	test('should select a src', () => {
-		render(<Image src={selectSrc(src)} />);
+		render(<Image src={src} />);
 
 		const image = screen.getAllByRole('img')[1];
 
 		const actual = image.getAttribute('src');
-		const expected = 'http://via.placeholder.com/300x300';
+		const expected = 'https://via.placeholder.com/300x300';
 
 		expect(actual).toBe(expected);
 	});
@@ -78,27 +77,18 @@ describe('Image', () => {
 		expect(image).toHaveClass(expected);
 	});
 
-
 	// Tried to set the width and height of window with window.resize or global.innerWidth/global.innerHeight
 	// in order for `Image` to get the correct image source based on window resolution.
 	// On each try, Image gets the src.fhd link.
-	test.skip('resize 2', async () => {
-		// act(() => {
-		// 	window.resizeTo(1280, 720);
-		// });
-		// global.innerWidth = 1280;
-		// global.innerHeight = 720;
-		// global.dispatchEvent(new Event('resize'));
-		await resizeWindow(1280, 720);
-		render(<Image src={selectSrc(src)} />);
-		// console.log('1111', window.innerHeight, window.innerWidth);
-		// console.log('121111', window.outerHeight, window.outerWidth);
+	test('resize 2', async () => {
 
-		// act(() => {
-		// 	window.resizeTo(1920, 1080);
-		// });
-		// console.log('2', getScreenType())
-		// console.log('1', window.innerHeight, window.innerWidth);
-		// console.log('12', window.outerHeight, window.outerWidth);
+		const {rerender} = render(<Image src={src.fhd} />);
+		screen.debug()
+		act(() => window.resizeTo(999, 600));
+		rerender(<Image src={src.hd} />);
+		screen.debug()
+		console.log('1111', window.innerHeight, window.innerWidth);
+		console.log('121111', window.outerHeight, window.outerWidth);
+
 	});
 });
