6e6085762ff99933d8a9b6e08bfad52aed962f2f
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2022-11-01 11:04:03 +0200

    WRO-14597: Changed component to use VirtualList. (#1353)
    * Changed component to use Virtual List.
    
    * Fix
    
    * Update TransferList/TransferList.js
    
    Co-authored-by: adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>
    
    Co-authored-by: adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>

--- samples/sampler/stories/default/TransferList.js
+++ samples/sampler/stories/default/TransferList.js
@@ -1,5 +1,5 @@
 import {mergeComponentMetadata} from '@enact/storybook-utils';
-import {boolean} from '@enact/storybook-utils/addons/controls';
+import {boolean, number} from '@enact/storybook-utils/addons/controls';
 
 import TransferList, {TransferListBase, TransferListDecorator} from '@enact/sandstone/TransferList';
 
@@ -16,12 +16,14 @@ export const _TransferList = (args) => (
 	<TransferList
 		allowMultipleDrag={args['allowMultipleDrag']}
 		firstList={['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6', 'Item7', 'Item8']}
+		itemSize={args['itemSize']}
 		moveOnSpotlight={args['moveElementOnSpotlightDirections']}
 		secondList={['Item9', 'Item10', 'Item11', 'Item12', 'Item13', 'Item14', 'Item15', 'Item16']}
 	/>
 );
 
 boolean('allowMultipleDrag', _TransferList, Config, true);
+number('itemSize', _TransferList, Config, 201);
 boolean('moveElementOnSpotlightDirections', _TransferList, Config, false);
 
 _TransferList.storyName = 'TransferList';
