1d4b5110bf093d9e1a7a0ce6db4e0b6cb83039d3
jeonghee27 <jeonghee27.ahn@lge.com>
2022-12-15 14:40:01 +0900

    fix lint error

--- Scroller/tests/Scroller-specs.js
+++ Scroller/tests/Scroller-specs.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {render, fireEvent, screen, waitFor} from '@testing-library/react';
+import {render, fireEvent, screen} from '@testing-library/react';
 
 import Scroller from '../Scroller';
 
