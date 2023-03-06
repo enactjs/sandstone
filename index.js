59c9f2b811885714a3a6172a47caff955c24d704
taeyoung.hong <taeyoung.hong@lge.com>
2023-03-06 17:44:49 +0900

    test: use jsdoc-to-ts

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // This space intentionally left blank
+const jsdocToTs = require('@enact/jsdoc-to-ts');
+const fs = require('fs');
+
+jsdocToTs({
+	package: '.',
+	output: fs.writeFileSync,
+	ignore: ['node_modules', 'ilib', 'build', 'dist', 'samples', 'coverage', 'tests'],
+	importMap: {
+		core: '@enact/core',
+		ui: '@enact/ui',
+		spotlight: '@enact/spotlight',
+		i18n: '@enact/i18n',
+		webos: '@enact/webos',
+		moonstone: '@enact/moonstone',
+		'moonstone-ez': '@enact/moonstone-ez',
+		agate: '@enact/agate',
+		sandstone: '@enact/sandstone'
+	},
+	outputPath: '.'
+});
