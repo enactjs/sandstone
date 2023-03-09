d29280d83bf2a43d31ebe9896c9ca6b37d4c4529
Mikyung Kim <mikyung27.kim@lge.com>
2023-03-09 16:50:41 +0900

    update qa sampler with icons
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- samples/sampler/stories/qa/IconItem.js
+++ samples/sampler/stories/qa/IconItem.js
@@ -8,6 +8,11 @@ import ri from '@enact/ui/resolution';
 import {ScrollerBasic as UiScrollerBasic} from '@enact/ui/Scroller';
 import {useCallback, useLayoutEffect, useRef, useState} from 'react';
 
+import galleryIcon from '../../images/icon_app_gallery.png';
+import gameHomeIcon from '../../images/icon_app_game.png';
+import homeOfficeIcon from '../../images/icon_app_homeoffice.png';
+import mediaDiscoveryIcon from '../../images/icon_app_mediadiscovery.png';
+
 import css from './IconItem.module.less';
 
 const ScrollerConfig = mergeComponentMetadata('Scroller', UiScrollerBasic, Scroller);
@@ -25,10 +30,10 @@ const prop = {
 let itemsArr = [];
 const imageSrcs = [
 	'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI3MDAiIGhlaWdodD0iNzAwIiB2aWV3Qm94PSIwIDAgNzAwIDcwMCI+PGRlZnM+PHN0eWxlPi5hLC5ie2ZpbGw6I2ZmZjt9LmJ7b3BhY2l0eTowLjg7fTwvc3R5bGU+PC9kZWZzPjxwb2x5Z29uIGNsYXNzPSJhIiBwb2ludHM9IjM0OS45IDUyNy44IDE5OS45IDQyOS44IDE5OS45IDM3NC40IDM1MC4xIDQ3Mi43IDM0OS45IDM0OC41IDE5OS45IDI2MS4xIDE5OS45IDIwOS4zIDM1MCAyOTMuNiAzNTAgMjkzLjYgMzUwLjIgMjkzLjcgMzQ5LjkgMTY5LjMgMTAyLjcgNDguNSAxMDIuNyA0NzIuNiAzNTAuMSA2NTEuNiAzNDkuOSA1MjcuOCIvPjxwb2x5Z29uIGNsYXNzPSJiIiBwb2ludHM9IjM1MC4xIDY1MS42IDU5Ny4zIDQ3Mi44IDU5Ny4zIDM2Ni4zIDM0OS45IDUyNy44IDM1MC4xIDY1MS42Ii8+PHBvbHlnb24gY2xhc3M9ImIiIHBvaW50cz0iMzUwLjEgNDcyLjcgNTIzLjUgMzU5LjMgNTIzLjUgMjQ3LjQgMzQ5LjkgMzQ4LjUgMzUwLjEgNDcyLjciLz48cG9seWdvbiBjbGFzcz0iYiIgcG9pbnRzPSIzNTAgMjkzLjYgMzUwIDI5My42IDM1MC4yIDI5My43IDU5Ny4zIDE1NC44IDU5Ny4zIDQ4LjQgMzQ5LjkgMTY5LjMgMzUwIDI5My42Ii8+PC9zdmc+',
-	'https://cdn-icons-png.flaticon.com/512/1384/1384060.png',
-	'https://cdn2.iconfinder.com/data/icons/flat-icons-19/512/Vinyl.png',
-	'https://icons-for-free.com/iconfiles/png/512/google+googleplay+logo+multimedia+play+player+icon-1320193181644092930.png',
-	'https://cdn-icons-png.flaticon.com/512/2111/2111463.png'
+	galleryIcon,
+	gameHomeIcon,
+	homeOfficeIcon,
+	mediaDiscoveryIcon
 ];
 
 const populateItems = ({index}) => {
@@ -50,7 +55,7 @@ const populateItems = ({index}) => {
 		} : null,
 		label: (function () {
 			if (index === 1) return 'USB';
-			else if (index === 6) return 'Youtube';
+			else if (index === 6) return 'Gallery';
 		})(),
 		labelColor: index === 6 ? 'dark' : null,
 		labelOn: index === 6 ? 'focus' : null
