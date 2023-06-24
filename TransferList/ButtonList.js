6891215e861a7804edbe3cebae541b6255208f1c
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2023-06-24 19:31:41 +0300

    WRP-15626: Refactor the component. (#1466)
    * Documentation fix.
    
    * Lint fixes.
    
    * Solved merge conflicts.
    
    * Changed docs for Button List
    
    * Review fix

---
+++ TransferList/ButtonList.js
@@ -0,0 +1,180 @@
+import {Cell} from '@enact/ui/Layout';
+import PropTypes from 'prop-types';
+
+import Button from '../Button';
+
+import {handleSpotlightBounds} from './utils';
+
+import componentCss from './TransferList.module.less';
+
+const ButtonList = ({
+	disabled,
+	firstListMaxCapacity,
+	firstListMinCapacity,
+	handleRemoveSelected,
+	moveIntoFirstSelected,
+	moveIntoSecondSelected,
+	moveOnSpotlight,
+	orientation,
+	secondListMaxCapacity,
+	secondListMinCapacity,
+	selectIntoFirstAll,
+	selectIntoSecondAll,
+	selectedItems
+}) => {
+	return (
+		<Cell className={componentCss.listButtons} style={{flexDirection: orientation === 'vertical' ? 'row' : 'column'}}>
+			{!moveOnSpotlight ?
+				<>
+					<Button
+						disabled={disabled || !!secondListMaxCapacity || !!firstListMinCapacity}
+						icon={orientation === 'vertical' ? 'triangledown' : 'triangleright'}
+						iconOnly
+						onClick={selectIntoSecondAll}
+						onSpotlightLeft={orientation === 'vertical' ? handleSpotlightBounds : null}
+						onSpotlightUp={orientation === 'horizontal' ? handleSpotlightBounds : null}
+						size="small"
+					/>
+					<Button
+						disabled={!(selectedItems?.find((item) => item.list === "first")) || disabled}
+						icon={orientation === 'vertical' ? 'arrowlargedown' : 'arrowsmallright'}
+						iconOnly
+						onClick={moveIntoSecondSelected}
+						size="small"
+					/>
+					<Button
+						disabled={!(selectedItems?.find((item) => item.list === "second")) || disabled}
+						icon={orientation === 'vertical' ? 'arrowlargeup' : 'arrowsmallleft'}
+						iconOnly
+						onClick={moveIntoFirstSelected}
+						size="small"
+					/>
+					<Button
+						disabled={disabled || !!firstListMaxCapacity || !!secondListMinCapacity}
+						icon={orientation === 'vertical' ? 'triangleup' : 'triangleleft'}
+						iconOnly
+						onClick={selectIntoFirstAll}
+						size="small"
+					/>
+					<Button
+						disabled={disabled}
+						icon="refresh"
+						iconOnly
+						onClick={handleRemoveSelected}
+						onSpotlightDown={orientation === 'horizontal' ? handleSpotlightBounds : null}
+						onSpotlightRight={orientation === 'vertical' ? handleSpotlightBounds : null}
+						size="small"
+					/>
+				</> : ''
+			}
+		</Cell>
+	);
+};
+
+ButtonList.propTypes = {
+	/**
+	 * Disables ButtonList and becomes non-interactive.
+	 *
+	 * @type {Boolean}
+	 * @private
+	 */
+	disabled: PropTypes.bool,
+
+	/**
+	 * Sets the maximum number of items for the first list.
+	 *
+	 * @type {Number}
+	 * @private
+	 */
+	firstListMaxCapacity: PropTypes.number,
+
+	/**
+	 * Sets the minimum number of items for the first list.
+	 *
+	 * @type {Number}
+	 * @private
+	 */
+	firstListMinCapacity: PropTypes.number,
+
+	/**
+	 * A function that removes all the items from the selected list.
+	 *
+	 * @type {Function}
+	 * @private
+	 */
+	handleRemoveSelected: PropTypes.func,
+
+	/**
+	 * A function that moves all the selected items into the first list.
+	 *
+	 * @type {Function}
+	 * @private
+	 */
+	moveIntoFirstSelected: PropTypes.func,
+
+	/**
+	 * A function that moves all the selected items into the second list.
+	 *
+	 * @type {Function}
+	 * @private
+	 */
+	moveIntoSecondSelected: PropTypes.func,
+
+	/**
+	 * Removes the buttons.
+	 *
+	 * @type {Boolean}
+	 * @private
+	 */
+	moveOnSpotlight: PropTypes.bool,
+
+	/**
+	 * The orientation for the button list.
+	 *
+	 * @type {('horizontal', 'vertical')}
+	 * @private
+	 */
+	orientation: PropTypes.oneOf(['horizontal', 'vertical']),
+
+	/**
+	 * Sets the maximum number of items for the second list.
+	 *
+	 * @type {Number}
+	 * @private
+	 */
+	secondListMaxCapacity: PropTypes.number,
+
+	/**
+	 * Sets the minimum number of items for the second list.
+	 *
+	 * @type {Number}
+	 * @private
+	 */
+	secondListMinCapacity: PropTypes.number,
+
+	/**
+	 * An array containing all the selected items.
+	 *
+	 * @type {Array}
+	 * @private
+	 */
+	selectedItems: PropTypes.array,
+
+	/**
+	 * A function that moves all the items into the first list.
+	 *
+	 * @type {Function}
+	 * @private
+	 */
+	selectIntoFirstAll: PropTypes.func,
+
+	/**
+	 * A function that moves all the items into the second list.
+	 *
+	 * @type {Function}
+	 * @private
+	 */
+	selectIntoSecondAll: PropTypes.func
+};
+
+export default ButtonList;
---
+++ TransferList/utils.js
@@ -0,0 +1,34 @@
+// Get the transferred item and return the index and the list
+export const getTransferData = (dataTransferObj) => {
+	if (dataTransferObj) {
+		const data = dataTransferObj.getData('text/plain');
+		const [index, list] = data.split('-');
+		return {index, list};
+	}
+	return null;
+};
+
+export const handlePreventDefault = ev => ev.preventDefault();
+
+// Restrict the spotlight only to the component
+export const handleSpotlightBounds = ev => {
+	ev.preventDefault();
+	ev.stopPropagation();
+};
+
+// Rearrange items in the same list
+export const rearrangeList = (dragOverElementIndex, isAbove, itemIndex, list, listName, setNewList) => {
+	const draggedItem = list[itemIndex];
+	// Determines the position for the item if it is dropped above the hovered item
+	const elementAbove = dragOverElementIndex < itemIndex ? dragOverElementIndex : dragOverElementIndex - 1;
+	// Determines the position for the item if it is dropped below the hovered item
+	const elementBelow = dragOverElementIndex > itemIndex ? dragOverElementIndex : dragOverElementIndex + 1;
+	// Determines the exact position for the dropped item
+	const elementPosition = isAbove ? elementAbove : elementBelow;
+
+	// Removes item from its list
+	list.splice(itemIndex, 1);
+	// Add the item on the new position
+	list.splice(elementPosition, 0, draggedItem);
+	setNewList(list);
+};
