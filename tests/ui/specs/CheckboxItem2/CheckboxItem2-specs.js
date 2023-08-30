c7e0b07606a8c56b88db6021d87d8aeb4cf4c41f
taeyoung.hong <taeyoung.hong@lge.com>
2023-08-30 15:24:03 +0900

    test4

--- tests/ui/specs/CheckboxItem2/CheckboxItem2-specs.js
+++
@@ -1,8 +0,0 @@
-describe("CheckboxItem - sandstone-checkboxitem--grouped", () => {
-	it("tests CheckboxItem - sandstone-checkboxitem--grouped", async () => {
-	  await browser.setWindowSize(1183, 931)
-	  await browser.url("http://nebula.lge.com/enact/sandstone/qa-sampler/2.7.5/?path=/story/sandstone-checkboxitem--grouped")
-	  await wdioExpect(browser).toHaveUrl("http://nebula.lge.com/enact/sandstone/qa-sampler/2.7.5/?path=/story/sandstone-checkboxitem--grouped")
-	  await browser.$("//*[@id=\"c_16-viewport\"]/article/section/span/div[1]/div[1]").click()
-	});
-  });
