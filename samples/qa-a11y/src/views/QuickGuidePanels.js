66b863f95c2f498d08d7d7e7c406169f4bb724c9
Jiye Kim <jiye.kim@lge.com>
2023-09-13 15:17:43 +0900

    WRP-25755: [Sandsonte Samples] qa-a11y: Add new component from sandstone(QuickGuide Panels) Enact-DCO-1.0-Signed-off-by: Jiye Kim (jiye.kim@lge.com)

--- samples/qa-a11y/src/views/QuickGuidePanels.js
+++ samples/qa-a11y/src/views/QuickGuidePanels.js
@@ -1,29 +1,27 @@
-/* eslint-disable react/jsx-no-bind */
-
 import QuickGuidePanels from '@enact/sandstone/QuickGuidePanels';
 import {Header} from '@enact/sandstone/Panels';
 
 const QuickGuidePanelsView = () => (
-    <>
-        <Header title="QuickGuidePanels" />
-        <QuickGuidePanels>
-            <QuickGuidePanels.Panel aria-label={'Panel 0'} >
-                <div style={{marginTop: 150}}>
-                    This is Panel 0
-                </div>
-            </QuickGuidePanels.Panel>
-            <QuickGuidePanels.Panel aria-label={'Panel 1'} >
-                <div style={{marginTop: 150}}>
-                    This is Panel 1
-                </div>
-            </QuickGuidePanels.Panel>
-            <QuickGuidePanels.Panel >
-                <div style={{marginTop: 150}}>
-                    This is Panel 2
-                </div>
-            </QuickGuidePanels.Panel>
-        </QuickGuidePanels>
-    </>
+	<>
+		<Header title="QuickGuidePanels" />
+		<QuickGuidePanels>
+			<QuickGuidePanels.Panel aria-label={'Panel 0'} >
+				<div style={{marginTop: 150}}>
+					This is Panel 0
+				</div>
+			</QuickGuidePanels.Panel>
+			<QuickGuidePanels.Panel aria-label={'Panel 1'} >
+				<div style={{marginTop: 150}}>
+					This is Panel 1
+				</div>
+			</QuickGuidePanels.Panel>
+			<QuickGuidePanels.Panel >
+				<div style={{marginTop: 150}}>
+					This is Panel 2
+				</div>
+			</QuickGuidePanels.Panel>
+		</QuickGuidePanels>
+	</>
 );
 
 export default QuickGuidePanelsView;
