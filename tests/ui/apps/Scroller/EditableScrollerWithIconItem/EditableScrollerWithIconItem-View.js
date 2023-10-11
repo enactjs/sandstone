c0f3aa4e64f83efda1db2533abf4080548b93192
Jiye Kim <jiye.kim@lge.com>
2023-10-11 13:57:10 +0900

    WRP-17963: [sandstone] Increase ui-test for editable scroller Enact-DCO-1.0-Signed-off-by: Jiye Kim (jiye.kim@lge.com)

--- tests/ui/apps/Scroller/EditableScrollerWithIconItem/EditableScrollerWithIconItem-View.js
+++ tests/ui/apps/Scroller/EditableScrollerWithIconItem/EditableScrollerWithIconItem-View.js
@@ -6,7 +6,7 @@ import ri from '@enact/ui/resolution';
 import {Component, createRef} from 'react';
 
 import Button from '../../../../../Button/Button';
-import IconItem from '../../../../../IconItem/IconItem';
+import IconItem from '../../../../../IconItem';
 import {InputField} from '../../../../../Input';
 import Scroller from '../../../../../Scroller/Scroller';
 import ThemeDecorator from '../../../../../ThemeDecorator/ThemeDecorator';
