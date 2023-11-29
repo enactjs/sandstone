458879f3ee97d9023276115740f58db00990204e
Seungcheon Baek <5037955+0x64@users.noreply.github.com>
2023-11-29 14:56:37 +0900

    Fixed not to lose focus when pressing directional key on pointer mode
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- Scroller/EditableWrapper.js
+++ Scroller/EditableWrapper.js
@@ -619,6 +619,7 @@ const EditableWrapper = (props) => {
 
 				// Check if focus leaves scroll container.
 				if (nextTarget && !getContainersForNode(nextTarget).includes(mutableRef.current.spotlightId)) {
+					setPointerMode(false);
 					Spotlight.move(getDirection(keyCode));
 
 					const orders = finalizeOrders();
