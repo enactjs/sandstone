9d7c8b424a027ce9a6fd5658d36c26621addbe5c
Jiye Kim <jiye.kim@lge.com>
2024-02-06 18:03:43 +0900

    WRQ-8044: Focus Body Text partially in Scroller with audio guidance ON Enact-DCO-1.0-Signed-off-by: Jiye Kim (jiye.kim@lge.com)

--- samples/qa-a11y/src/views/Scroller.js
+++ samples/qa-a11y/src/views/Scroller.js
@@ -2,17 +2,73 @@
 
 import Scroller from '@enact/sandstone/Scroller';
 import ToggleButton from '@enact/sandstone/SwitchItem';
+import SpotlightContainerDecorator from '@enact/spotlight/SpotlightContainerDecorator';
+import Spottable from '@enact/spotlight/Spottable';
 import Layout, {Cell} from '@enact/ui/Layout';
 import ri from '@enact/ui/resolution';
-import {useState} from 'react';
+import LS2Request from '@enact/webos/LS2Request';
+import {useEffect, useState} from 'react';
+
+import css from './Scroller.module.less';
 
 const ScrollerView = () => {
+	const [audioGuidance, setAudioGuidance] = useState(false);
 	const [native, setNative] = useState(true);
 	const [customAriaLabel, setCustomAriaLabel] = useState(false);
 	const scrollMode = native ? 'native' : 'translate';
 
+	useEffect( () => {
+		if (window.WebOSServiceBridge ?? window.PalmServiceBridge) {
+			new LS2Request().send({
+				service: 'luna://com.webos.settingsservice/',
+				method: 'getSystemSettings',
+				parameters: {
+					category: 'option',
+					keys: ['audioGuidance']
+				},
+				onSuccess: (res) => {
+					setAudioGuidance(res.settings.audioGuidance === 'on');
+				}
+			});
+		}
+	}, []);
+
 	const handleChangeJSNativeButton = () => setNative(!native);
 	const handleChangeAriaLabelButton = () => setCustomAriaLabel(!customAriaLabel);
+
+	const bodyText = [
+		'Foo', 'Bar', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar',
+		'Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.',
+		'Foo', 'Bar', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar',
+		'Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.',
+		'Foo', 'Bar', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar',
+		'Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.',
+		'Foo', 'Bar', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar', 'Boom boom pow',
+		'Foo', 'Bar',
+		'Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.'
+	];
+
+	const Container = SpotlightContainerDecorator('div');
+	const SpottableDiv = Spottable('div');
 
 	return (
 		<Layout orientation="vertical">
@@ -32,31 +88,13 @@ const ScrollerView = () => {
 			</Cell>
 			<Cell
 				component={Scroller}
-				focusableScrollbar="byEnter"
+				focusableScrollbar={audioGuidance ? true : "byEnter"}
 				scrollMode={scrollMode}
 				verticalScrollThumbAriaLabel={customAriaLabel ? 'This is vertical scroll thumb' : null}
 				horizontalScrollThumbAriaLabel={customAriaLabel ? 'This is horizontal scroll thumb' : null}
 			>
 				<div style={{width: ri.scaleToRem(6000)}}>
-					Foo<br />Bar<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />
-					Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. <br />Foo<br />Bar<br />Bar<br />
-					Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />
-					Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. <br />Foo<br />Bar<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />
-					Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />
-					Foo<br />Bar<br />Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. <br />Foo<br />Bar<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />
-					Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow<br />Foo<br />Bar<br />Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
-					Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow. Boom boom pow.
+					{audioGuidance ? <Container>{bodyText.map((x) => <SpottableDiv className={css.focusableBodyText}>{x}<br /></SpottableDiv>)}</Container> : bodyText.map((x) => <>{x}<br /></>)}
 				</div>
 			</Cell>
 		</Layout>
