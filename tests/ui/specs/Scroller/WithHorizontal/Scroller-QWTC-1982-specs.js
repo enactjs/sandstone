e2bb3794b64cade1832938faed1ca7760184ce21
dongsu.won <dongsu.won@lgepartner.com>
2024-02-20 08:17:22 +0900

    * Fix Scroller ui-test(QWTC-1982).
    Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

--- tests/ui/specs/Scroller/WithHorizontal/Scroller-QWTC-1982-specs.js
+++ tests/ui/specs/Scroller/WithHorizontal/Scroller-QWTC-1982-specs.js
@@ -8,6 +8,7 @@ describe('Horizontal Scroller', function () {
 
 	it('should not scroll with 5-way right on the first button [QWTC-1982]', async function () {
 		// Step 3: Position the pointer on 'Button 1'.
+		await ScrollerPage.showPointerByKeycode();
 		await $('#item0').moveTo();
 		// Step 3 Verify: Spotlight is on 'Button 1'.
 		await expectFocusedItem(0);
@@ -17,8 +18,11 @@ describe('Horizontal Scroller', function () {
 		await ScrollerPage.spotlightUp();
 		// Step 4 Verify: The list does not scroll.
 		expect(await ScrollerPage.getScrollThumbPosition().horizontal).to.be.equal(initialThumbPosition);
+		// for initializing
+		await ScrollerPage.spotlightDown();
 
 		// Step 5: Position the pointer on 'Button 1'
+		await ScrollerPage.showPointerByKeycode();
 		await $('#item0').moveTo();
 		// Step 5 Verify: Spotlight is on 'Button 1'.
 		await expectFocusedItem(0);
@@ -28,6 +32,7 @@ describe('Horizontal Scroller', function () {
 		expect(await ScrollerPage.getScrollThumbPosition().horizontal).to.be.equal(initialThumbPosition);
 
 		// Step 7-2: 5-way Select and 5-way Left in the blank area of the viewport to enter 5-way.
+		await ScrollerPage.showPointerByKeycode();
 		await $('#item0').moveTo({xOffset: 0, yOffset: 300});
 		// Step 7-3: 5-way Spot 'Button 1'.
 		await ScrollerPage.spotlightLeft();
@@ -40,6 +45,7 @@ describe('Horizontal Scroller', function () {
 		expect(await ScrollerPage.getScrollThumbPosition().horizontal).to.be.equal(initialThumbPosition);
 
 		// Step 9: 5-way Spot 'Button 1'.
+		await ScrollerPage.spotlightDown();
 		await expectFocusedItem(0);
 		// Step 10: 5-way Down.
 		await ScrollerPage.spotlightDown();
