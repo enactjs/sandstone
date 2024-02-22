5f74380ce0bd9e48cabf9b4f5d98bf3c9fb85a57
Jiye Kim <jiye.kim@lge.com>
2024-02-22 14:42:22 +0900

    Remove audioGuidance call Enact-DCO-1.0-Signed-off-by: Jiye Kim (jiye.kim@lge.com)

--- samples/qa-a11y/src/views/Scroller.js
+++ samples/qa-a11y/src/views/Scroller.js
@@ -6,51 +6,19 @@ import Spottable from '@enact/spotlight/Spottable';
 import ToggleButton from '@enact/sandstone/SwitchItem';
 import Layout, {Cell} from '@enact/ui/Layout';
 import ri from '@enact/ui/resolution';
-import LS2Request from '@enact/webos/LS2Request';
-import {Fragment, useEffect, useState} from 'react';
+import {Fragment, useState} from 'react';
 
 import css from './Scroller.module.less';
 
 const ScrollerView = () => {
-	const [audioGuidance, setAudioGuidance] = useState(false);
-	const [audioGuidanceDisabled, setAudioGuidanceDisabled] = useState(true);
+	const [spottable, setSpottable] = useState(false);
 	const [customAriaLabel, setCustomAriaLabel] = useState(false);
 	const [native, setNative] = useState(true);
 	const scrollMode = native ? 'native' : 'translate';
 
-	useEffect( () => {
-		if (window.WebOSServiceBridge ?? window.PalmServiceBridge) {
-			new LS2Request().send({
-				service: 'luna://com.webos.settingsservice/',
-				method: 'getSystemSettings',
-				parameters: {
-					category: 'option',
-					keys: ['audioGuidance']
-				},
-				onSuccess: () => {
-					setAudioGuidanceDisabled(false);
-				}
-			});
-		}
-	}, []);
-
 	const handleChangeJSNativeButton = () => setNative(!native);
 	const handleChangeAriaLabelButton = () => setCustomAriaLabel(!customAriaLabel);
-	const handleChangeAudioGuidance = () => {
-		if (window.WebOSServiceBridge ?? window.PalmServiceBridge) {
-			setAudioGuidance(!audioGuidance);
-			new LS2Request().send({
-				service: 'luna://com.webos.settingsservice/',
-				method: 'setSystemSettings',
-				parameters: {
-					category: 'option',
-					settings: {
-						audioGuidance: !audioGuidance ? 'on' : 'off'
-					}
-				}
-			});
-		}
-	};
+	const handleChangeSpottableButton = () => setSpottable(!spottable);
 
 	const bodyText = [
 		<>Foo<br />Bar<br />Bar<br />Boom boom pow<br /></>,
@@ -102,22 +70,21 @@ const ScrollerView = () => {
 					Native
 				</ToggleButton>
 				<ToggleButton
-					onClick={handleChangeAudioGuidance}
-					disabled={audioGuidanceDisabled}
-					selected={audioGuidance}
+					onClick={handleChangeSpottableButton}
+					selected={spottable}
 				>
-					Audio Guidance (Scroller with Spottable)
+					Scroller with Spottable
 				</ToggleButton>
 			</Cell>
 			<Cell
 				component={Scroller}
-				focusableScrollbar={audioGuidance ? false : "byEnter"}
+				focusableScrollbar={spottable ? false : "byEnter"}
 				scrollMode={scrollMode}
 				verticalScrollThumbAriaLabel={customAriaLabel ? 'This is vertical scroll thumb' : null}
 				horizontalScrollThumbAriaLabel={customAriaLabel ? 'This is horizontal scroll thumb' : null}
 			>
-				<div style={audioGuidance ? null : {width: ri.scaleToRem(6000)}}>
-					{audioGuidance ?
+				<div style={spottable ? null : {width: ri.scaleToRem(6000)}}>
+					{spottable ?
 						<Container>
 							{bodyText.map((x, i) => <SpottableDiv className={css.focusableBodyText} key={i}>{x}</SpottableDiv>)}
 						</Container> :
