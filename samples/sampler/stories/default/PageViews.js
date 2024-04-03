7ee4bbe99ddc63229daa9310fc2ae39662fbfbef
Juwon Jeong <juwon.jeong@lge.com>
2024-04-03 14:22:08 +0900

    Fix sample
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

--- samples/sampler/stories/default/PageViews.js
+++ samples/sampler/stories/default/PageViews.js
@@ -4,8 +4,6 @@ import Item from '@enact/sandstone/Item';
 import {select} from '@enact/storybook-utils/addons/controls';
 import {Cell, Row, Column} from '@enact/ui/Layout';
 
-import css from './PageViews.module.less';
-
 PageViews.displayName = 'PageViews';
 
 const propOptions = {
@@ -19,14 +17,14 @@ export default {
 
 export const _PageViews = (args) => (
 	<PageViews arranger={BasicArranger} pageIndicatorType={args['pageIndicatorType']}>
-		<PageViews.Page aria-label="This is a description for page 1" css={css}>
-			<div>
+		<PageViews.Page aria-label="This is a description for page 1">
+			<div style={{padding: '24px'}}>
 				<Item>Item 1</Item>
 				<Item>Item 2</Item>
 			</div>
 		</PageViews.Page>
-		<PageViews.Page aria-label="This is a description for page 2" css={css}>
-			<Column>
+		<PageViews.Page aria-label="This is a description for page 2">
+			<Column style={{padding: '24px'}}>
 				<Row style={{padding: '12px'}}>
 					<Cell>Country</Cell>
 					<Cell>City</Cell>
