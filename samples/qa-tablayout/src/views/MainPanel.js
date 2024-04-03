c293614515cea9a37995751843d7f8ddb4cd5c58
Seungcheon Baek <5037955+SeungcheonBaek@users.noreply.github.com>
2024-04-03 19:21:22 +0900

    Update qa-tablayout sample to test app-side back key preventing
    Enact-DCO-1.0-Signed-off-by: Seungcheon Baek (sc.baek@lge.com)

--- samples/qa-tablayout/src/views/MainPanel.js
+++ samples/qa-tablayout/src/views/MainPanel.js
@@ -1,13 +1,29 @@
 import TabLayout, {Tab} from '@enact/sandstone/TabLayout';
 import Button from '@enact/sandstone/Button';
+import Item from '@enact/sandstone/Item';
+
+const handleKeyUpTabLayout = (ev) => {
+	ev.preventDefault();
+};
 
 const MainPanel = () => (
-	<TabLayout>
+	<TabLayout onKeyUp={handleKeyUpTabLayout}>
 		<Tab title="Settings" icon="gear">
 			<Button>Edit</Button>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
 		</Tab>
 		<Tab title="Sound" icon="sound">
 			<Button>Edit</Button>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
+			<Item>Item</Item>
 		</Tab>
 	</TabLayout>
 );
