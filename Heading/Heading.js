2b03f313cc608a8e1ebe6e1e72afb9d8ac38212f
Stanca <stanca.pop@lgepartner.com>
2024-04-15 17:09:13 +0300

    converted Heading style to SCSS

--- Heading/Heading.js
+++ Heading/Heading.js
@@ -24,7 +24,7 @@ import {HeadingBase as UiHeadingBase} from '@enact/ui/Heading';
 import {MarqueeDecorator} from '../Marquee';
 import Skinnable from '../Skinnable';
 
-import componentCss from './Heading.module.less';
+import componentCss from './Heading.module.scss';
 
 /**
  * A labeled Heading component.
