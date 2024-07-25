512687cb9695637238ff703e51d412367a4db616
Juwon Jeong <juwon.jeong@lge.com>
2024-07-25 18:18:00 +0900

    WRQ-27279: Added x of y feature to VirtualList and VirtualGridList qa-a11y samples (#1645)
    * WRQ-27279: Added x of y feature to VirtualList and VirtualGridList qa-a11y samples
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    * Changed menu of the sample
    
    * Update the ubuntu version of travis
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    ---------
    
    Co-authored-by: Seungho Park <seunghoh.park@lge.com>

--- samples/qa-a11y/src/views/VirtualList.js
+++ samples/qa-a11y/src/views/VirtualList.js
@@ -15,15 +15,29 @@ const renderItem = ({index, ...rest}) => (
 	</Item>
 );
 
+// eslint-disable-next-line enact/prop-types
+const renderItemWithRole = ({index, ...rest}) => (
+	<Item
+		aria-posinset={index + 1}
+		aria-setsize={items.length}
+		role="listitem"
+		{...rest}
+	>
+		{items[index]}
+	</Item>
+);
+
 for (let i = 0; i < 100; i++) {
 	items.push('Item ' + ('00' + i).slice(-3));
 }
 
 const VirtualListView = () => {
 	const [native, setNative] = useState(true);
+	const [role, setRole] = useState(false);
 	const scrollMode = native ? 'native' : 'translate';
 
 	const handleToggleScrollMode = () => setNative(!native);
+	const handleToggleRole = () => setRole(!role);
 
 	return (
 		<Layout orientation="vertical">
@@ -34,10 +48,16 @@ const VirtualListView = () => {
 				>
 					Native
 				</CheckboxItem>
+				<CheckboxItem
+					onClick={handleToggleRole}
+					selected={role}
+				>
+					Read X of Y
+				</CheckboxItem>
 			</Cell>
 			<VirtualList
 				dataSize={items.length}
-				itemRenderer={renderItem}
+				itemRenderer={role ? renderItemWithRole : renderItem}
 				itemSize={ri.scale(156)}
 				scrollMode={scrollMode}
 			/>
