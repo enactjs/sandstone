8a64e25bcc90c00ec7cf5b37385de032a7c22150
alexandru-morariu-lgp <alexandru.morariu@lgepartner.com>
2024-08-09 11:56:08 +0300

    Fixed problem with knob size and added a version of HSL for ColorPickerSlider.

--- ColorPickerPOC/ColorPickerSlider.js
+++ ColorPickerPOC/ColorPickerSlider.js
@@ -4,9 +4,60 @@ import {useCallback} from 'react';
 
 import Slider from '../Slider';
 
-import {generateOppositeColor, hexToRGB, rgbObjectToHex} from './utils';
+import {generateOppositeColor, hexToHSL, hexToRGB, hslToHex, rgbObjectToHex} from './utils';
 
 import componentCss from './ColorPickerSlider.module.less';
+
+const hueGradient = (saturation, lightness) => {
+	return `linear-gradient(to right, 
+	hsla(0, ${saturation}%, ${lightness}%, 1),
+	hsla(10, ${saturation}%, ${lightness}%, 1),
+	hsla(20, ${saturation}%, ${lightness}%, 1),
+	hsla(30, ${saturation}%, ${lightness}%, 1),
+	hsla(40, ${saturation}%, ${lightness}%, 1),
+	hsla(50, ${saturation}%, ${lightness}%, 1),
+	hsla(60, ${saturation}%, ${lightness}%, 1),
+	hsla(70, ${saturation}%, ${lightness}%, 1)
+	,hsla(80, ${saturation}%, ${lightness}%, 1),
+	hsla(90, ${saturation}%, ${lightness}%, 1),
+	hsla(100, ${saturation}%, ${lightness}%, 1),
+	hsla(110, ${saturation}%, ${lightness}%, 1),
+	hsla(120, ${saturation}%, ${lightness}%, 1),
+	hsla(130, ${saturation}%, ${lightness}%, 1),
+	hsla(140, ${saturation}%, ${lightness}%, 1),
+	hsla(150, ${saturation}%, ${lightness}%, 1),
+	hsla(160, ${saturation}%, ${lightness}%, 1),
+	hsla(170, ${saturation}%, ${lightness}%, 1),
+	hsla(180, ${saturation}%, ${lightness}%, 1),
+	hsla(190, ${saturation}%, ${lightness}%, 1),
+	hsla(200, ${saturation}%, ${lightness}%, 1),
+	hsla(210, ${saturation}%, ${lightness}%, 1),
+	hsla(220, ${saturation}%, ${lightness}%, 1),
+	hsla(230, ${saturation}%, ${lightness}%, 1),
+	hsla(240, ${saturation}%, ${lightness}%, 1),
+	hsla(250, ${saturation}%, ${lightness}%, 1),
+	hsla(260, ${saturation}%, ${lightness}%, 1),
+	hsla(270, ${saturation}%, ${lightness}%, 1),
+	hsla(280, ${saturation}%, ${lightness}%, 1),
+	hsla(290, ${saturation}%, ${lightness}%, 1),
+	hsla(300, ${saturation}%, ${lightness}%, 1),
+	hsla(310, ${saturation}%, ${lightness}%, 1),
+	hsla(320, ${saturation}%, ${lightness}%, 1),
+	hsla(330, ${saturation}%, ${lightness}%, 1),
+	hsla(340, ${saturation}%, ${lightness}%, 1),
+	hsla(350, ${saturation}%, ${lightness}%, 1),
+	hsla(360, ${saturation}%, ${lightness}%, 1))`;
+};
+
+const lightnessGradient = (hue, saturation) => {
+	return `linear-gradient(to right, 
+	hsla(${hue}, ${saturation}%, 0%, 1),
+	hsla(${hue}, ${saturation}%, 20%, 1),
+	hsla(${hue}, ${saturation}%, 40%, 1),
+	hsla(${hue}, ${saturation}%, 60%, 1),
+	hsla(${hue}, ${saturation}%, 80%, 1),
+	hsla(${hue}, ${saturation}%, 100%, 1))`;
+};
 
 const ColorPickerSlider = ({selectedColor, selectedColorHandler, ...props}) => {
 	const {red, green, blue} = hexToRGB(selectedColor);
@@ -28,16 +79,21 @@ const ColorPickerSlider = ({selectedColor, selectedColorHandler, ...props}) => {
 			<Cell>
 				<Cell className={componentCss.labelText}>Red</Cell>
 				<Row>
-					<Cell size="83%">
+					<Cell
+						className={componentCss.sliderCell}
+						size="80%"
+						style={{backgroundImage: `linear-gradient(to right, rgb(0,${green},${blue}), rgb(255,${green},${blue}))`}}
+					>
 						<Slider
-							className={componentCss.slider}
+							css={componentCss}
 							max={255}
 							min={0}
 							noFill
 							onChange={changeValueRed}
 							style={{
 								'--sand-slider-knob-border-color': generateOppositeColor(selectedColor),
-								backgroundImage: `linear-gradient(to right, rgb(0,${green},${blue}), rgb(255,${green},${blue}))`
+								'--sand-focus-bg-color-rgb': `${red},${green},${blue}`,
+								'--sand-progress-slider-color': selectedColor
 							}}
 							value={red}
 						/>
@@ -50,16 +106,21 @@ const ColorPickerSlider = ({selectedColor, selectedColorHandler, ...props}) => {
 			<Cell className={componentCss.cellElement}>
 				<Cell className={componentCss.labelText}>Green</Cell>
 				<Row>
-					<Cell size="83%">
+					<Cell
+						className={componentCss.sliderCell}
+						size="80%"
+						style={{backgroundImage: `linear-gradient(to right, rgb(${red},0,${blue}), rgb(${red},255,${blue}))`}}
+					>
 						<Slider
-							className={componentCss.slider}
+							css={componentCss}
 							max={255}
 							min={0}
 							noFill
 							onChange={changeValueGreen}
 							style={{
 								'--sand-slider-knob-border-color': generateOppositeColor(selectedColor),
-								backgroundImage: `linear-gradient(to right, rgb(${red},0,${blue}), rgb(${red},255,${blue}))`
+								'--sand-focus-bg-color-rgb': `${red},${green},${blue}`,
+								'--sand-progress-slider-color': selectedColor
 							}}
 							value={green}
 						/>
@@ -70,16 +131,21 @@ const ColorPickerSlider = ({selectedColor, selectedColorHandler, ...props}) => {
 			<Cell className={componentCss.cellElement}>
 				<Cell className={componentCss.labelText}>Blue</Cell>
 				<Row>
-					<Cell size="83%">
+					<Cell
+						className={componentCss.sliderCell}
+						size="80%"
+						style={{backgroundImage: `linear-gradient(to right, rgb(${red},${green},0), rgb(${red},${green},255))`}}
+					>
 						<Slider
-							className={componentCss.slider}
+							css={componentCss}
 							max={255}
 							min={0}
 							noFill
 							onChange={changeValueBlue}
 							style={{
 								'--sand-slider-knob-border-color': generateOppositeColor(selectedColor),
-								backgroundImage: `linear-gradient(to right, rgb(${red},${green},0), rgb(${red},${green},255))`
+								'--sand-focus-bg-color-rgb': `${red},${green},${blue}`,
+								'--sand-progress-slider-color': selectedColor
 							}}
 							value={blue}
 						/>
@@ -91,9 +157,117 @@ const ColorPickerSlider = ({selectedColor, selectedColorHandler, ...props}) => {
 	);
 };
 
+const ColorPickerSliderHSL = ({selectedColor, selectedColorHandler, ...props}) => {
+	const {h: hue, s: saturation, l: lightness} = hexToHSL(selectedColor);
+	const {red, green, blue} = hexToRGB(selectedColor);
+
+	const changeValueHue = useCallback((ev) => {
+		selectedColorHandler(hslToHex({h: ev.value, s: saturation, l: lightness}));
+	}, [saturation, lightness, selectedColorHandler]);
+
+	const changeValueSaturation = useCallback((ev) => {
+		selectedColorHandler(hslToHex({h: hue, s: ev.value, l: lightness}));
+	}, [hue, lightness, selectedColorHandler]);
+
+	const changeValueLightness = useCallback((ev) => {
+		selectedColorHandler(hslToHex({h: hue, s: saturation, l: ev.value}));
+	}, [hue, saturation, selectedColorHandler]);
+
+	return (
+		<div {...props} className={componentCss.sliderContainer}>
+			<Cell>
+				<Cell className={componentCss.labelText}>Hue</Cell>
+				<Row>
+					<Cell
+						className={componentCss.sliderCell}
+						size="80%"
+						style={{backgroundImage: hueGradient(saturation, lightness)}}
+					>
+						<Slider
+							css={componentCss}
+							max={359}
+							min={0}
+							noFill
+							onChange={changeValueHue}
+							style={{
+								'--sand-slider-knob-border-color': generateOppositeColor(selectedColor),
+								'--sand-focus-bg-color-rgb': `${red},${green},${blue}`,
+								'--sand-progress-slider-color': selectedColor
+							}}
+							value={hue}
+						/>
+					</Cell>
+					<Cell
+						className={componentCss.outputText}
+					>{hue}</Cell>
+				</Row>
+			</Cell>
+			<Cell className={componentCss.cellElement}>
+				<Cell className={componentCss.labelText}>Saturation</Cell>
+				<Row>
+					<Cell
+						className={componentCss.sliderCell}
+						size="80%"
+						style={{backgroundImage: `linear-gradient(to right, hsl(${hue}, 0%, ${lightness}%), hsl(${hue}, 100%, ${lightness}%))`}}
+					>
+						<Slider
+							css={componentCss}
+							max={100}
+							min={0}
+							noFill
+							onChange={changeValueSaturation}
+							style={{
+								'--sand-slider-knob-border-color': generateOppositeColor(selectedColor),
+								'--sand-focus-bg-color-rgb': `${red},${green},${blue}`,
+								'--sand-progress-slider-color': selectedColor
+							}}
+							value={saturation}
+						/>
+					</Cell>
+					<Cell className={componentCss.outputTextPercent}>{saturation}%</Cell>
+				</Row>
+			</Cell>
+			<Cell className={componentCss.cellElement}>
+				<Cell className={componentCss.labelText}>Lightness</Cell>
+				<Row>
+					<Cell
+						className={componentCss.sliderCell}
+						size="80%"
+						style={{backgroundImage: lightnessGradient(hue, saturation)}}
+					>
+						<Slider
+							css={componentCss}
+							max={100}
+							min={0}
+							noFill
+							onChange={changeValueLightness}
+							style={{
+								'--sand-slider-knob-border-color': generateOppositeColor(selectedColor),
+								'--sand-focus-bg-color-rgb': `${red},${green},${blue}`,
+								'--sand-progress-slider-color': selectedColor
+							}}
+							value={lightness}
+						/>
+					</Cell>
+					<Cell className={componentCss.outputTextPercent}>{lightness}%</Cell>
+				</Row>
+			</Cell>
+		</div>
+	);
+};
+
 ColorPickerSlider.propTypes = {
 	selectedColor: PropTypes.string,
 	selectedColorHandler: PropTypes.func
 };
 
+ColorPickerSliderHSL.propTypes = {
+	selectedColor: PropTypes.string,
+	selectedColorHandler: PropTypes.func
+};
+
+export {
+	ColorPickerSlider,
+	ColorPickerSliderHSL
+};
 export default ColorPickerSlider;
