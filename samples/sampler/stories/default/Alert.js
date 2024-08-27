d6bc08cc3205e93800d2a42d6c5c5adad665928f
Ion Andrusciac <ion.andrusciac@lgepartner.com>
2024-08-27 14:07:37 +0300

    Fixed `high contrast` overrides colors

--- samples/sampler/stories/default/Alert.js
+++ samples/sampler/stories/default/Alert.js
@@ -1,6 +1,5 @@
 import Alert, {AlertBase, AlertImage} from '@enact/sandstone/Alert';
 import Button from '@enact/sandstone/Button';
-import ProgressBar from '@enact/sandstone/ProgressBar';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import {action} from '@enact/storybook-utils/addons/actions';
 import {boolean, select, text} from '@enact/storybook-utils/addons/controls';
