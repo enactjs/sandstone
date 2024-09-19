8274c1afdc5d2a9698a22fb747af3f281335f13a
Ion Andrusciac <ion.andrusciac@lgepartner.com>
2024-09-19 15:14:23 +0300

    Converted `hex` colors to `rgba` and added specs for Game skin

--- tests/screenshot/apps/Sandstone-View.js
+++ tests/screenshot/apps/Sandstone-View.js
@@ -141,6 +141,10 @@ const ExportedApp = (props) => {
 	let highContrast = url.searchParams.get('highContrast') === 'true';
 	let focusRing = url.searchParams.get('focusRing') === 'true';
 
+	// Test Game Skin
+	let green = url.searchParams.get('green') === 'true';
+	let orange = url.searchParams.get('orange') === 'true';
+
 	// Legacy test parameters
 	let locale = url.searchParams.get('locale');
 	let textSize = url.searchParams.get('textSize') === 'large' ? 'large' : 'normal';
@@ -164,6 +168,15 @@ const ExportedApp = (props) => {
 			delete skinVariants.highContrast;
 			highContrast = true;
 		}
+		// Test Game Skin
+		if (skinVariants.green) {
+			delete skinVariants.highContrast;
+			green = true;
+		}
+		if (skinVariants.orange) {
+			delete skinVariants.highContrast;
+			orange = true;
+		}
 		if (skinVariants.largeText) {
 			delete skinVariants.largeText;
 			textSize = 'large';
@@ -185,7 +198,7 @@ const ExportedApp = (props) => {
 	}, []);
 
 	return (
-		<WrappedApp {...props} skin={skin} highContrast={highContrast} locale={locale} textSize={textSize} focusRing={focusRing} />
+		<WrappedApp {...props} skin={skin} highContrast={highContrast} green={green} orange={orange} locale={locale} textSize={textSize} focusRing={focusRing} />
 	);
 };
 
---
+++ tests/screenshot/specs/game/Green-specs.js
@@ -0,0 +1,12 @@
+const {runTest} = require('@enact/ui-test-utils/utils');
+
+const Page = require('./SandstonePage');
+
+runTest({
+	testName: 'Sandstone Game',
+	Page: Page,
+	skin: 'game',
+	highContrast: false,
+	green: true,
+	concurrency: 1
+});
