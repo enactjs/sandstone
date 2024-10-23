0476111511c60de0795e0cda3e0db4df0694ef06
Mikyung Kim <mikyung27.kim@lge.com>
2024-10-23 13:54:59 +0900

    miscellaneous fix
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- QuickGuidePanels/QuickGuidePanels.js
+++ QuickGuidePanels/QuickGuidePanels.js
@@ -8,7 +8,6 @@ import {Cell, Column, Row} from '@enact/ui/Layout';
 import Changeable from '@enact/ui/Changeable';
 import ForwardRef from '@enact/ui/ForwardRef';
 import ViewManager from '@enact/ui/ViewManager';
-import classNames from 'classnames';
 import IString from 'ilib/lib/IString';
 import PropTypes from 'prop-types';
 import compose from 'ramda/src/compose';
