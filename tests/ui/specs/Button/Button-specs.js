caf2625b176c66f393141b1be645c1b11ae79a62
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-11-04 16:20:21 +0200

    *WIP* apply WebdriverBiDi for automation testing

--- tests/ui/specs/Button/Button-specs.js
+++ tests/ui/specs/Button/Button-specs.js
@@ -13,29 +13,42 @@ describe('Button', function () {
 		iconButton
 	} = Page.components;
 
-	describe('5-way', function () {
-		it('should focus disabled button on 5-way right', async function () {
-			await buttonDefault.focus();
-			await Page.spotlightRight();
-			expect(await buttonDisabled.self.isFocused()).toBe(true);
-		});
-
-		it('should focus buttonSizeSmall button on 5-way left', async function () {
-			await iconButton.focus();
-			await Page.spotlightLeft();
-			expect(await buttonSizeSmall.self.isFocused()).toBe(true);
-		});
-	});
-
-	describe('pointer', function () {
-		it('should focus the disabled when hovered', async function () {
-			await buttonDisabled.hover();
-			expect(await buttonDisabled.self.isFocused()).toBe(true);
-		});
-
-		it('should focus first when hovered', async function () {
-			await buttonDefault.hover();
-			expect(await buttonDefault.self.isFocused()).toBe(true);
+	// describe('5-way', function () {
+	// 	it('should focus disabled button on 5-way right', async function () {
+	// 		await buttonDefault.focus();
+	// 		await Page.spotlightRight();
+	// 		expect(await buttonDisabled.self.isFocused()).toBe(true);
+	// 	});
+	//
+	// 	it('should focus buttonSizeSmall button on 5-way left', async function () {
+	// 		await iconButton.focus();
+	// 		await Page.spotlightLeft();
+	// 		expect(await buttonSizeSmall.self.isFocused()).toBe(true);
+	// 	});
+	// });
+	//
+	// describe('pointer', function () {
+	// 	it('should focus the disabled when hovered', async function () {
+	// 		await buttonDisabled.hover();
+	// 		expect(await buttonDisabled.self.isFocused()).toBe(true);
+	// 	});
+	//
+	// 	it('should focus first when hovered', async function () {
+	// 		await buttonDefault.hover();
+	// 		expect(await buttonDefault.self.isFocused()).toBe(true);
+	// 	});
+	// });
+	describe('touch', function () {
+		it('touch test', async function () {
+			await browser.action('pointer', {
+				parameters: { pointerType: 'touch' } // "mouse" is default value, also possible: "pen" or "touch"
+			})
+				.move({origin: buttonDefault.self})
+				.down()
+				.pause(2000)
+				.up()
+				.perform();
+			// console.log('muie', action);
 		});
 	});
 });
--- tests/ui/specs/VirtualList/VirtualGridList/VirtualGridList-specs.js
+++ tests/ui/specs/VirtualList/VirtualGridList/VirtualGridList-specs.js
@@ -6,30 +6,142 @@ describe('VirtualGridList', function () {
 		await Page.open();
 	});
 
-	it('should meet initial conditions', async function () {
-		expect(await Page.buttonHideScrollbar.isFocused()).toBe(true);
+	it('wheel horizontal', async function () {
+		const initialScrollThumbPosition = await Page.scrollThumbPosition();
+
+		await browser.action('wheel').scroll({
+			x: 700,
+			y: 200,
+			deltaX: 0,
+			deltaY: 1000,
+			duration: 200,
+			pause: 1000,
+		}).perform();
+
+		const currentScrollThumbPosition = await Page.scrollThumbPosition();
+		// verify that the scrolling happened
+		expect(currentScrollThumbPosition > initialScrollThumbPosition).toBe(true);
+
+		// verify scroll returns to original position
+		await browser.action('wheel').scroll({
+			x: 700,
+			y: 200,
+			deltaX: 0,
+			deltaY: -1000,
+			duration: 200,
+			pause: 1000
+		}).perform();
+
+		const finalScrollThumbPosition = await Page.scrollThumbPosition();
+		expect(finalScrollThumbPosition === initialScrollThumbPosition).toBe(true);
 	});
 
-	describe('LTR locale', function () {
-		it('should focus first item on first focus', async function () {
-			await Page.spotlightDown();
-			await Page.spotlightDown();
-			await expectFocusedItem(0);
-		});
+	it('wheel vertical', async function () {
+		await Page.buttonDirectionChange.click();
+
+		const initialScrollThumbPosition = await Page.scrollThumbPosition();
+
+		await browser.action('wheel').scroll({
+			x: 700,
+			y: 200,
+			deltaX: 1000,
+			deltaY: 0,
+			duration: 200,
+			pause: 1000,
+		}).perform();
+		await browser.pause(2000);
+
+		const currentScrollThumbPosition = await Page.scrollThumbPosition();
+		expect(currentScrollThumbPosition > initialScrollThumbPosition).toBe(true);
+
+		await browser.action('wheel').scroll({
+			x: 700,
+			y: 200,
+			deltaX: -1000,
+			deltaY: 0,
+			duration: 200,
+			pause: 1000,
+		}).perform();
+		await browser.pause(2000);
+
+		const finalScrollThumbPosition = await Page.scrollThumbPosition();
+		expect(finalScrollThumbPosition === initialScrollThumbPosition).toBe(true);
 	});
 
-	describe('Minimal DataSize', function () {
-		it('should not display scrollbar when minimal datasize [QWTC-2067]', async function () {
-			// Step 3: Knobs > VirtualGridList > dataSize > 4
-			await Page.inputNumItems.moveTo();
-			await Page.spotlightSelect();
-			await Page.backSpace();
-			await Page.backSpace();
-			await Page.backSpace();
-			await Page.numPad(4);
-			await Page.spotlightLeft();
-			// Step 4 Verify: Scrollbar track does not display to the right as the data size is the minimal size of 4.
-			expect((await Page.scrollBar).error.message.slice(0, 15)).toBe('no such element');
-		});
+	it('touch vertical', async function () {
+		const initialScrollThumbPosition = await Page.scrollThumbPosition();
+
+		await browser.action('pointer', {
+			parameters: {pointerType: 'touch'}
+		})
+			.move({duration: 0, origin: await Page.list, x: 100, y: 100})
+			.down()
+			.move({duration: 3000, y: 200})
+			.up()
+			.move({duration: 1000, origin: await Page.list, x: 100, y: 200})
+			.down()
+			.move({duration: 1000, y: 300})
+			.up()
+			.move({duration: 1000, origin: await Page.list, x: 100, y: 300})
+			.down()
+			.move({duration: 1000, y: 400})
+			.perform();
+
+		const currScrollThumbPosition = await Page.scrollThumbPosition();
+
+		expect(currScrollThumbPosition > initialScrollThumbPosition).toBe(true);
 	});
+
+	it('touch horizontal', async function () {
+		const initialScrollThumbPosition = await Page.scrollThumbPosition();
+
+		await browser.action('pointer', {
+			parameters: {pointerType: 'touch'}
+		})
+			.move({duration: 0, origin: await Page.buttonDirectionChange, x: 0, y: 0})
+			.down()
+			.up()
+			.move({duration: 1000, origin: await Page.list, x: 300, y: 50})
+			.down()
+			.move({duration: 1000, origin: await Page.list, x: 0, y: 50})
+			.up()
+			.move({duration: 1000, origin: await Page.list, x: 300, y: 50})
+			.down()
+			.move({duration: 1000, origin: await Page.list, x: 0, y: 50})
+			.up()
+			.perform()
+
+		const currScrollThumbPosition = await Page.scrollThumbPosition();
+
+		expect(currScrollThumbPosition > initialScrollThumbPosition).toBe(true);
+
+		await browser.pause(3000);
+	});
+
+	// it('should meet initial conditions', async function () {
+	// 	expect(await Page.buttonHideScrollbar.isFocused()).toBe(true);
+	// });
+	//
+	// describe('LTR locale', function () {
+	// 	it('should focus first item on first focus', async function () {
+	// 		await Page.spotlightDown();
+	// 		await Page.spotlightDown();
+	// 		await expectFocusedItem(0);
+	// 	});
+	// });
+	//
+	// describe('Minimal DataSize', function () {
+	// 	it('should not display scrollbar when minimal datasize [QWTC-2067]', async function () {
+	// 		// Step 3: Knobs > VirtualGridList > dataSize > 4
+	// 		await Page.inputNumItems.moveTo();
+	// 		await Page.spotlightSelect();
+	// 		await Page.backSpace();
+	// 		await Page.backSpace();
+	// 		await Page.backSpace();
+	// 		await Page.numPad(4);
+	// 		await Page.spotlightLeft();
+	// 		// Step 4 Verify: Scrollbar track does not display to the right as the data size is the minimal size of 4.
+	// 		expect((await Page.scrollBar).error.message.slice(0, 15)).toBe('no such element');
+	// 	});
+	// });
 });
