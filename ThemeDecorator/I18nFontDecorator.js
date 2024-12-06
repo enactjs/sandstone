cee293ad30d535b1df8f5dc470cf070ce313a86e
alexandru-morariu-lgp <alexandru.morariu@lgepartner.com>
2024-12-06 14:20:52 +0200

    Review fix

--- ThemeDecorator/I18nFontDecorator.js
+++ ThemeDecorator/I18nFontDecorator.js
@@ -1,20 +1,23 @@
 import hoc from '@enact/core/hoc';
 import ilib from '@enact/i18n';
-import {useEffect} from 'react';
 import PropTypes from 'prop-types';
+import {useEffect} from 'react';
 
 import {fontOverrideGenerator} from './fontGenerator';
 
 const I18nFontDecorator = hoc((config, Wrapped) => {
 	const I18nDecorator = (props) => {
+		const ilibLocale = ilib.getLocale();
+
 		useEffect(() => {
-			fontOverrideGenerator(props.locale || ilib.getLocale());
-		}, [props.locale]);
+			fontOverrideGenerator(props.locale || ilibLocale);
+		}, [ilibLocale, props.locale]);
 
 		return <Wrapped {...props} />;
 	};
 
 	I18nDecorator.displayName = 'I18nFontDecorator';
+
 	I18nDecorator.propTypes = {
 		locale: PropTypes.string
 	};
