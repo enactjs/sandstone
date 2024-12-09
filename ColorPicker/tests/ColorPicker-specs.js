659941c346dd45c9a35fc176e65e8843f7ef201b
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-12-09 19:21:19 +0200

    added extra unit tests

--- ColorPicker/tests/ColorPicker-specs.js
+++ ColorPicker/tests/ColorPicker-specs.js
@@ -1,14 +1,115 @@
+import {FloatingLayerDecorator} from '@enact/ui/FloatingLayer';
 import '@testing-library/jest-dom';
 import {fireEvent, render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import ColorPicker from '../ColorPicker';
 import GridColorPicker from '../ColorPickerGrid';
 import SliderColorPicker from '../ColorPickerSlider';
 import SpectrumColorPicker from '../ColorPickerSpectrum';
 
-describe('ColorPickerPOC', () => {
+const FloatingLayerController = FloatingLayerDecorator('div');
+
+describe('ColorPicker', () => {
 	afterEach(() => {
 		jest.clearAllMocks();
+	});
+
+	const colors = ['#eb4034', '#32a852', '#3455eb'];
+
+	test('should render a Grid Color Picker at first render when type="grid"', () => {
+		const onChangeColorHandler = jest.fn();
+		const {container} = render(
+			<FloatingLayerController>
+				<ColorPicker
+					color={colors[0]}
+					colors={colors}
+					onChangeColor={onChangeColorHandler}
+					open
+					type="grid"
+				/>
+			</FloatingLayerController>
+		);
+		const colorPicker = container.querySelector('.colorPicker'); // eslint-disable-line testing-library/no-container
+		const colorBlocks = container.querySelectorAll('.colorBlock'); // eslint-disable-line testing-library/no-container
+
+		expect(colorPicker).toBeInTheDocument();
+		expect(colorBlocks[0]).toBeInTheDocument();
+	});
+
+	test('should render a Grid Color Picker when clicking on "Grid" tab', async () => {
+		const user = userEvent.setup();
+		const onChangeColorHandler = jest.fn();
+		const {container} = render(
+			<FloatingLayerController>
+				<ColorPicker
+					color={colors[0]}
+					colors={colors}
+					onChangeColor={onChangeColorHandler}
+					open
+					type="sliders"
+				/>
+			</FloatingLayerController>
+		);
+		const gridTab = screen.getByText('Grid');
+
+		await user.click(gridTab);
+
+		const colorPicker = container.querySelector('.colorPicker'); // eslint-disable-line testing-library/no-container
+		const colorBlocks = container.querySelectorAll('.colorBlock'); // eslint-disable-line testing-library/no-container
+
+		expect(colorPicker).toBeInTheDocument();
+		expect(colorBlocks[0]).toBeInTheDocument();
+	});
+
+	test('should render a Sliders Color Picker at first render when type="sliders"', () => {
+		const onChangeColorHandler = jest.fn();
+		const {container} = render(
+			<FloatingLayerController>
+				<ColorPicker
+					color={colors[0]}
+					colors={colors}
+					onChangeColor={onChangeColorHandler}
+					open
+					type="sliders"
+				/>
+			</FloatingLayerController>
+		);
+		const colorPicker = container.querySelector('.colorPicker'); // eslint-disable-line testing-library/no-container
+		const sliders = screen.getAllByRole('slider');
+		const [red, green, blue] = sliders;
+
+		expect(colorPicker).toBeInTheDocument();
+		expect(red).toBeInTheDocument();
+		expect(green).toBeInTheDocument();
+		expect(blue).toBeInTheDocument();
+	});
+
+	test('should render a Sliders Color Picker when clicking on "Sliders" tab', async () => {
+		const user = userEvent.setup();
+		const onChangeColorHandler = jest.fn();
+		render(
+			<FloatingLayerController>
+				<ColorPicker
+					color={colors[0]}
+					colors={colors}
+					onChangeColor={onChangeColorHandler}
+					open
+					type="grid"
+				/>
+			</FloatingLayerController>
+		);
+		const sliderTab = screen.getByText('Sliders');
+
+		await user.click(sliderTab);
+
+		const sliders = screen.getAllByRole('slider');
+		const [red, green, blue] = sliders;
+
+		expect(red).toBeInTheDocument();
+		expect(green).toBeInTheDocument();
+		expect(blue).toBeInTheDocument();
+
 	});
 
 	describe('Grid ColorPicker', () => {
@@ -58,10 +159,55 @@ describe('ColorPickerPOC', () => {
 			global.HTMLCanvasElement.prototype.getContext = jest.fn(() => mockContext);
 		});
 
+		test('should render a Spectrum Color Picker at first render when type="spectrum"', () => {
+			const onChangeColorHandler = jest.fn();
+			const {container} = render(
+				<FloatingLayerController>
+					<ColorPicker
+						color={colors[0]}
+						colors={colors}
+						onChangeColor={onChangeColorHandler}
+						open
+						type="spectrum"
+					/>
+				</FloatingLayerController>
+			);
+			const colorPicker = container.querySelector('.colorPicker'); // eslint-disable-line testing-library/no-container
+			const canvas = container.querySelector('.gradientCanvas'); // eslint-disable-line testing-library/no-container
+
+			expect(colorPicker).toBeInTheDocument();
+			expect(canvas).toBeInTheDocument();
+		});
+
+		test('should render a Spectrum Color Picker when clicking on "Spectrum" tab', async () => {
+			const user = userEvent.setup();
+			const onChangeColorHandler = jest.fn();
+			const {container} = render(
+				<FloatingLayerController>
+					<ColorPicker
+						color={colors[0]}
+						colors={colors}
+						onChangeColor={onChangeColorHandler}
+						open
+						type="grid"
+					/>
+				</FloatingLayerController>
+			);
+			const spectrumTab = screen.getByText('Spectrum');
+
+			await user.click(spectrumTab);
+
+			const colorPicker = container.querySelector('.colorPicker'); // eslint-disable-line testing-library/no-container
+			const canvas = container.querySelector('.gradientCanvas'); // eslint-disable-line testing-library/no-container
+
+			expect(colorPicker).toBeInTheDocument();
+			expect(canvas).toBeInTheDocument();
+		});
+
 		test('should render Spectrum Color Picker', () => {
 			const color = "#eb4034";
 			const {container} = render(<SpectrumColorPicker selectedColor={color} />);
-			const canvas = container.querySelector('.gradientCanvas'); // eslint-disable-line
+			const canvas = container.querySelector('.gradientCanvas'); // eslint-disable-line testing-library/no-container
 
 			expect(canvas).toBeInTheDocument();
 		});
@@ -71,9 +217,28 @@ describe('ColorPickerPOC', () => {
 			const selectedColorHandler = jest.fn();
 			const {container} = render(<SpectrumColorPicker selectedColor={color} selectedColorHandler={selectedColorHandler} />);
 
-			const canvas = container.querySelector('.gradientCanvas'); // eslint-disable-line
+			const canvas = container.querySelector('.gradientCanvas'); // eslint-disable-line testing-library/no-container
 			const user = userEvent.setup();
 			await user.click(canvas);
+
+			expect(selectedColorHandler).toHaveBeenCalled();
+		});
+
+		test('should emit a selectedColorHandler event when changing indicator position with 5-way keys', async () => {
+			const color = "#eb4034";
+			const selectedColorHandler = jest.fn();
+			const {container} = render(<SpectrumColorPicker selectedColor={color} selectedColorHandler={selectedColorHandler} />);
+			const indicator = container.querySelector('.circleIndicator'); // eslint-disable-line testing-library/no-container
+
+			// await activate(indicator);
+			await fireEvent.keyDown(indicator, {keyCode: 39});
+			await fireEvent.keyUp(indicator, {keyCode: 39});
+			await fireEvent.keyDown(indicator, {keyCode: 40});
+			await fireEvent.keyUp(indicator, {keyCode: 40});
+			await fireEvent.keyDown(indicator, {keyCode: 37});
+			await fireEvent.keyUp(indicator, {keyCode: 37});
+			await fireEvent.keyDown(indicator, {keyCode: 38});
+			await fireEvent.keyUp(indicator, {keyCode: 38});
 
 			expect(selectedColorHandler).toHaveBeenCalled();
 		});
