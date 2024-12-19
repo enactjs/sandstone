61249a450db205fa86a2ebc6caf0bf0dd933150f
adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>
2024-12-19 11:51:09 +0200

    WRR-12002: ColorPicker - add unit/ui/ss tests (#1778)
    * added JS Docs
    
    * removed @module and indented code
    
    * fixed JS docs
    
    * fixed `too many doclets` error
    
    * fixed js docs warning
    
    * fixed JS docs
    
    * fixed js docs
    
    * added JD docs for SpectrumIndicator.js
    
    * refactored rgbStringToHex function and added `willReadFrequently: true` parameter in ColorPickerSpectrum for optimization
    
    * made ColorPicker main component public and ColorPickerGrid component props private
    
    * fixed qa sampler
    
    * removed forgotten console.log
    
    * added unit tests for Grid and Sliders ColorPickers
    
    * removed transform as it conflicted with 5-way navigation
    
    * review fixes
    
    * modified example in JS docs
    
    * modified JS docs for ColorPicker example
    
    * JS docs fix
    
    * fixed JS docs
    
    * *WIP* add ui tests for ColorPicker
    
    * added default colors
    
    * fixed typo
    
    * fixed JS docs
    
    * changed slider names and adjusted tabLayout width
    
    * updated deprecated max-width value
    
    * added ui tests for sliders color picker and one extra unit test
    
    * moved max-width properties to target tabs class
    
    * corrected selector
    
    * fixed typo
    
    * added screenshot tests for ColorPicker
    
    * fix lint
    
    * added `type` and `disabled` props
    
    * changed type prop to public
    
    * added unit tests for Spectrum
    
    * added disabled style and behavior for each subcomponent of ColorPicker
    
    * added missing dependency
    
    * review fixes
    
    * added missing dependency
    
    * merge with WRR-12000 and update tests
    
    * updated ui/ss tests
    
    * review fix
    
    * removed spotlightDisabled from Dropdown as it is not recognized as a custom prop
    
    * added type to SliderColorPicker and extra unit tests
    
    * added extra unit tests
    
    * restore unit tests file
    
    * added extra tests for FavoriteColors
    
    * added utils unit tests and fix lint
    
    * fix travis
    
    * fix docs
    
    * Update tests/ui/specs/ColorPicker/ColorPickerPage.js
    
    * Apply suggestions from code review
    
    * lint warning fixes
    
    ---------
    
    Co-authored-by: Stanca <stanca.pop@lgepartner.com>
    Co-authored-by: Daniel Stoian <63335068+daniel-stoian-lgp@users.noreply.github.com>
    Co-authored-by: Daniel Stoian <daniel.stoian@lgepartner.com>

---
+++ tests/ui/specs/ColorPicker/ColorPicker-specs.js
@@ -0,0 +1,437 @@
+const Page = require('./ColorPickerPage');
+
+describe('ColorPicker', function () {
+	beforeEach(async function () {
+		await Page.open();
+	});
+
+	const colorPicker = Page.components.colorPicker;
+
+	describe('ColorPicker Grid', function () {
+		it('should focus Grid tab', async function () {
+			expect(await (await colorPicker.tabItems())[0].isFocused()).toBe(true);
+		});
+
+		describe('5-way', function () {
+			it('should change selectedColor with 5-way keys', async function () {
+				// read background color of forth color block form first colors column
+				const colorBlockStyle = await (await colorPicker.colorBlock())[3].getAttribute('style');
+				const colorBlockBgColor = colorBlockStyle.split(';')[0];
+				// Spot forth block from first colors column
+				for (let i = 0; i < 4; i++) {
+					await Page.spotlightDown();
+				}
+				await Page.spotlightSelect();
+
+				const selectedColorStyle = await colorPicker.selectedColor.getAttribute('style');
+				const selectedColor = selectedColorStyle.split(';')[0];
+
+				expect(colorBlockBgColor).toBe(selectedColor);
+			});
+		});
+
+		describe('pointer', function () {
+			it('should change selectedColor after clicking a color block', async function () {
+				const colorBlockStyle = await (await colorPicker.colorBlock())[1].getAttribute('style');
+				const colorBlockBgColor = colorBlockStyle.split(';')[0];
+
+				await (await colorPicker.colorBlock())[1].click();
+
+				const selectedColorStyle = await colorPicker.selectedColor.getAttribute('style');
+				const selectedColor = selectedColorStyle.split(';')[0];
+
+				expect(colorBlockBgColor).toBe(selectedColor);
+			});
+		});
+	});
+
+	describe('ColorPicker Spectrum', function () {
+		beforeEach(async () => {
+			await Page.spotlightRight();
+			await Page.spotlightSelect();
+			await browser.pause(500);
+		});
+
+		it('should focus Spectrum tab on 5-way Right', async function () {
+			expect(await (await colorPicker.tabItems())[1].isFocused()).toBe(true);
+		});
+
+		describe('5-way', function () {
+			it('should change selected color with 5-way keys', async function () {
+				await Page.spotlightDown();
+				await Page.spotlightSelect();
+				for (let i = 0; i < 20; i++) {
+					await Page.spotlightDown();
+				}
+				for (let i = 0; i < 17; i++) {
+					await Page.spotlightRight();
+				}
+
+				const spectrumIndicatorStyle = await colorPicker.spectrumIndicator.getAttribute('style');
+				const spectrumIndicator = String(spectrumIndicatorStyle.split(';').filter(attr => attr.includes('background-color'))).trim();
+				const selectedColorStyle = await colorPicker.selectedColor.getAttribute('style');
+				const selectedColor = String(selectedColorStyle.split(';').filter(attr => attr.includes('background-color'))).trim();
+
+				expect(spectrumIndicator).toBe(selectedColor);
+			});
+		});
+
+		describe('pointer', function () {
+			it('should change color when clicking on the canvas', async function () {
+				await (await colorPicker.tabItems())[1].click();
+				const canvas = await colorPicker.canvas;
+				await canvas.click();
+
+				const spectrumIndicatorStyle = await colorPicker.spectrumIndicator.getAttribute('style');
+				const spectrumIndicator = String(spectrumIndicatorStyle.split(';').filter(attr => attr.includes('background-color'))).trim();
+				const selectedColorStyle = await colorPicker.selectedColor.getAttribute('style');
+				const selectedColor = String(selectedColorStyle.split(';').filter(attr => attr.includes('background-color'))).trim();
+
+				expect(spectrumIndicator).toBe(selectedColor);
+			});
+
+			// TODO: add tests for dragging circle indicator inside canvas
+		});
+	});
+
+	describe('ColorPicker Sliders', function () {
+		beforeEach(async () => {
+			await Page.spotlightRight();
+			await Page.spotlightRight();
+			await Page.spotlightSelect();
+			await browser.pause(500);
+		});
+
+		it('should focus Sliders tab on 5-way Right', async function () {
+			expect(await (await colorPicker.tabItems())[2].isFocused()).toBe(true);
+		});
+
+		describe('RGB Color Picker', function () {
+			it('should display Red/Green/Blue labels', async function () {
+				const firstSliderLabel = await (await colorPicker.slidersLabel())[0].getText();
+				const secondSliderLabel = await (await colorPicker.slidersLabel())[1].getText();
+				const thirdSliderLabel = await (await colorPicker.slidersLabel())[2].getText();
+
+				expect(firstSliderLabel).toBe('Red');
+				expect(secondSliderLabel).toBe('Green');
+				expect(thirdSliderLabel).toBe('Blue');
+			});
+
+			describe('5-way', function () {
+				it('should change first slider output when changing slider value', async function () {
+					await (await colorPicker.sliders())[0].moveTo();
+					await Page.spotlightSelect();
+					const initialSliderOutput =  await (await colorPicker.slidersOutput())[0].getText();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSliderOutput = await (await colorPicker.slidersOutput())[0].getText();
+
+					expect(initialSliderOutput > currentSliderOutput).toBe(true);
+				});
+
+				it('should change the background color of second and third sliders when changing first slider value', async function () {
+					const initialSecondSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+					const initialThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+					await (await colorPicker.sliders())[0].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+
+					const currentSecondSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+					const currentThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+
+					expect(initialSecondSliderBgColor).not.toBe(currentSecondSliderBgColor);
+					expect(initialThirdSliderBgColor).not.toBe(currentThirdSliderBgColor);
+				});
+
+				it('should change selectedColor when changing the value of first slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[0].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change second slider output when changing slider value', async function () {
+					await (await colorPicker.sliders())[1].moveTo();
+					await Page.spotlightSelect();
+					const initialSliderOutput =  await (await colorPicker.slidersOutput())[1].getText();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSliderOutput = await (await colorPicker.slidersOutput())[1].getText();
+
+					expect(initialSliderOutput > currentSliderOutput).toBe(true);
+				});
+
+				it('should change the background color of first and third sliders when changing second slider value', async function () {
+					const initialSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const initialThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+					await (await colorPicker.sliders())[1].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+
+					const currentSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const currentThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+
+					expect(initialSecondSliderBgColor).not.toBe(currentSecondSliderBgColor);
+					expect(initialThirdSliderBgColor).not.toBe(currentThirdSliderBgColor);
+				});
+
+				it('should change selectedColor when changing the value of second slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[1].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change third slider output when changing slider value', async function () {
+					await (await colorPicker.sliders())[2].moveTo();
+					await Page.spotlightSelect();
+					const initialSliderOutput =  await (await colorPicker.slidersOutput())[2].getText();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSliderOutput = await (await colorPicker.slidersOutput())[2].getText();
+
+					expect(initialSliderOutput > currentSliderOutput).toBe(true);
+				});
+
+				it('should change the background color of first and second sliders when changing third slider value', async function () {
+					const initialSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const initialThirdSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+					await (await colorPicker.sliders())[2].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+
+					const currentSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const currentThirdSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+
+					expect(initialSecondSliderBgColor).not.toBe(currentSecondSliderBgColor);
+					expect(initialThirdSliderBgColor).not.toBe(currentThirdSliderBgColor);
+				});
+
+				it('should change selectedColor when changing the value of third slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[2].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+			});
+
+			describe('pointer', function () {
+				it('should change selectedColor when changing the value of first slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[0].click();
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change selectedColor when changing the value of second slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[1].click();
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change selectedColor when changing the value of third slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[2].click();
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+			});
+		});
+
+		describe('HSL Color Picker', function () {
+			beforeEach(async () => {
+				// change the value of dropdown to render HSL color picker
+				await colorPicker.slidersDropdown.moveTo();
+				await Page.spotlightSelect();
+				await Page.spotlightDown();
+				await Page.spotlightSelect();
+			});
+
+			it('should render Hue/Saturation/Lightness color picker', async function () {
+				const firstSliderLabel = await (await colorPicker.slidersLabel())[0].getText();
+				const secondSliderLabel = await (await colorPicker.slidersLabel())[1].getText();
+				const thirdSliderLabel = await (await colorPicker.slidersLabel())[2].getText();
+
+				expect(firstSliderLabel).toBe('Hue');
+				expect(secondSliderLabel).toBe('Saturation');
+				expect(thirdSliderLabel).toBe('Lightness');
+			});
+
+			describe('5-way', function () {
+				it('should change first slider output when changing slider value', async function () {
+					const initialSliderOutput =  await (await colorPicker.slidersOutput())[0].getText();
+					await (await colorPicker.sliders())[0].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightRight();
+					}
+					const currentSliderOutput = await (await colorPicker.slidersOutput())[0].getText();
+
+					expect(initialSliderOutput).not.toBe(currentSliderOutput);
+				});
+
+				it('should change the background color of second and third sliders when changing first slider value', async function () {
+					const initialSecondSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+					const initialThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+					await (await colorPicker.sliders())[0].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightRight();
+					}
+
+					const currentSecondSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+					const currentThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+
+					expect(initialSecondSliderBgColor).not.toBe(currentSecondSliderBgColor);
+					expect(initialThirdSliderBgColor).not.toBe(currentThirdSliderBgColor);
+				});
+
+				it('should change selectedColor when changing the value of first slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[0].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightRight();
+					}
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change second slider output when changing slider value', async function () {
+					await (await colorPicker.sliders())[1].moveTo();
+					await Page.spotlightSelect();
+					const initialSliderOutput =  await (await colorPicker.slidersOutput())[1].getText();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSliderOutput = await (await colorPicker.slidersOutput())[1].getText();
+
+					expect(initialSliderOutput).not.toBe(currentSliderOutput);
+				});
+
+				it('should change the background color of first and third sliders when changing second slider value', async function () {
+					const initialSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const initialThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+					await (await colorPicker.sliders())[1].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+
+					const currentSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const currentThirdSliderBgColor = await (await colorPicker.sliders())[2].getAttribute('style');
+
+					expect(initialSecondSliderBgColor).not.toBe(currentSecondSliderBgColor);
+					expect(initialThirdSliderBgColor).not.toBe(currentThirdSliderBgColor);
+				});
+
+				it('should change selectedColor when changing the value of second slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[1].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change third slider output when changing slider value', async function () {
+					await (await colorPicker.sliders())[2].moveTo();
+					await Page.spotlightSelect();
+					const initialSliderOutput =  await (await colorPicker.slidersOutput())[2].getText();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSliderOutput = await (await colorPicker.slidersOutput())[2].getText();
+
+					expect(initialSliderOutput).not.toBe(currentSliderOutput);
+				});
+
+				it('should change the background color of first and second sliders when changing third slider value', async function () {
+					const initialSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const initialThirdSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+					await (await colorPicker.sliders())[2].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+
+					const currentSecondSliderBgColor = await (await colorPicker.sliders())[0].getAttribute('style');
+					const currentThirdSliderBgColor = await (await colorPicker.sliders())[1].getAttribute('style');
+
+					expect(initialSecondSliderBgColor).not.toBe(currentSecondSliderBgColor);
+					expect(initialThirdSliderBgColor).not.toBe(currentThirdSliderBgColor);
+				});
+
+				it('should change selectedColor when changing the value of third slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[2].moveTo();
+					await Page.spotlightSelect();
+					for (let i = 0; i < 10; i++) {
+						await Page.spotlightLeft();
+					}
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+			});
+
+			describe('pointer', function () {
+				it('should change selectedColor when changing the value of first slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[0].click();
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change selectedColor when changing the value of second slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[1].click();
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+
+				it('should change selectedColor when changing the value of third slider', async function () {
+					const initialSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+					await (await colorPicker.sliders())[2].click();
+					const currentSelectedColor = await colorPicker.selectedColor.getAttribute('style');
+
+					expect(initialSelectedColor).not.toBe(currentSelectedColor);
+				});
+			});
+		});
+	});
+});
---
+++ tests/ui/specs/ColorPicker/ColorPickerPage.js
@@ -0,0 +1,79 @@
+'use strict';
+const {element, getComponent, Page} = require('@enact/ui-test-utils/utils');
+
+const getTabs = getComponent({component: 'TabLayout', child: 'tabs'});
+const colorPicker = getComponent({component: 'ColorPicker', child: 'colorPicker'});
+
+class ColorPickerInterface {
+	constructor (id) {
+		this.id = id;
+		this.selector = `#${this.id}`;
+	}
+
+	get self () {
+		return browser.$(this.selector);
+	}
+
+	async tabItems () {
+		return await (await this.tabs()).$$('.Button_Button_button');
+	}
+
+	async tabs () {
+		return await getTabs(this.self);
+	}
+
+	async colorBlock () {
+		return await (await this.gridColumns()).$$('.ColorPicker_ColorPickerGrid_colorBlock');
+	}
+
+	async gridColumns () {
+		return await colorPicker(this.self);
+	}
+
+	get selectedColor () {
+		return element('.ColorPicker_ColorPicker_selectedColor', browser);
+	}
+
+	get spectrumIndicator () {
+		return element('.ColorPicker_ColorPickerSpectrum_circleIndicator', browser);
+	}
+
+	get canvas () {
+		return element('.ColorPicker_ColorPickerSpectrum_gradientCanvas', browser);
+	}
+
+	async sliders () {
+		return await (await this.sliderColorPicker()).$$('.ColorPicker_ColorPickerSlider_sliderCell');
+	}
+
+	async slidersOutput () {
+		return await (await this.sliderColorPicker()).$$('.ColorPicker_ColorPickerSlider_outputText');
+	}
+
+	async slidersLabel () {
+		return await (await this.sliderColorPicker()).$$('.ColorPicker_ColorPickerSlider_labelText');
+	}
+
+	async sliderColorPicker () {
+		return await colorPicker(this.self);
+	}
+
+	get slidersDropdown () {
+		return element('.ColorPicker_ColorPickerSlider_pickerSelect', browser);
+	}
+}
+
+class ColorPickerPage extends Page {
+	constructor () {
+		super();
+		this.title = 'ColorPicker Test';
+		this.components = {};
+		this.components.colorPicker = new ColorPickerInterface('colorPicker');
+	}
+
+	async open (urlExtra) {
+		await super.open('ColorPicker-View', urlExtra);
+	}
+}
+
+module.exports = new ColorPickerPage();
