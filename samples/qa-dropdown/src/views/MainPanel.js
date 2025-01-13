79e671a3c573c503086fabbdb7f588ba0c117928
myelyn.kim <myelyn.kim@lge.com>
2025-01-13 17:46:50 +0900

    Added a sample to qa-dropdown since the issue cannot reproduced in qa-sampler
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)

--- samples/qa-dropdown/src/views/MainPanel.js
+++ samples/qa-dropdown/src/views/MainPanel.js
@@ -1,23 +1,42 @@
+import BodyText from '@enact/sandstone/BodyText';
 import Button from '@enact/sandstone/Button';
 import Dropdown from '@enact/sandstone/Dropdown';
 import {Panel, WizardPanels} from '@enact/sandstone/WizardPanels';
 import {useCallback, useState} from 'react';
 
 const MainPanel = () => {
-	const [open, setOpen] = useState(false);
+	const [openLanguage, setOpenLanguage] = useState(false);
+	const [openDropdown, setOpenDropdown] = useState(false);
+	const [removed, setRemove] = useState(false);
 
-	const handleOpen = useCallback(() => setOpen(true), []);
-	const handleClose = useCallback(() => setOpen(false), []);
+	const handleOpen = useCallback(() => setOpenLanguage(true), []);
+	const handleClose = useCallback(() => setOpenLanguage(false), []);
+	const handleDropdown = useCallback(() => {
+		setOpenDropdown(true);
+
+		setTimeout(() => {
+			setRemove(true);
+		}, 2000);
+	}, []);
+	const handleDropdownClose = useCallback(() => setOpenDropdown(false), []);
 
 	return (
 		<WizardPanels>
 			<Panel title="QA Sample - Dropdown">
-				<Dropdown onClose={handleClose} onOpen={handleOpen} open={open} size="large" title="language">
+				<Dropdown onClose={handleClose} onOpen={handleOpen} open={openLanguage} size="large" title="language">
 					{['English', 'Korean', 'Spanish', 'Amharic', 'Thai', 'Arabic', 'Urdu', 'Simplified Chinese', 'Traditional Chinese', 'Vietnamese']}
 				</Dropdown>
 				<Button size="large">
 					Enter
 				</Button>
+			</Panel>
+			<Panel>
+				{!removed && (
+					<div style={{margin: '20px'}}>
+						<BodyText>This is the line that will be removed.</BodyText>
+					</div>
+				)}
+				<Dropdown onClose={handleDropdownClose} onOpen={handleDropdown} open={openDropdown}>{['a', 'b', 'c']}</Dropdown>
 			</Panel>
 		</WizardPanels>
 	);
