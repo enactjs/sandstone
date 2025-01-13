f8c360ec49e2cc3d8573726639c99eab21bfc929
myelyn.kim <myelyn.kim@lge.com>
2025-01-13 18:18:48 +0900

    Applied reviews
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)

--- samples/qa-dropdown/src/views/MainPanel.js
+++ samples/qa-dropdown/src/views/MainPanel.js
@@ -2,28 +2,30 @@ import BodyText from '@enact/sandstone/BodyText';
 import Button from '@enact/sandstone/Button';
 import Dropdown from '@enact/sandstone/Dropdown';
 import {Panel, WizardPanels} from '@enact/sandstone/WizardPanels';
+import ri from '@enact/ui/resolution';
 import {useCallback, useState} from 'react';
 
 const MainPanel = () => {
-	const [openLanguage, setOpenLanguage] = useState(false);
-	const [openDropdown, setOpenDropdown] = useState(false);
+	const [open1, setOpen1] = useState(false);
+	const [open2, setOpen2] = useState(false);
 	const [removed, setRemove] = useState(false);
 
-	const handleOpen = useCallback(() => setOpenLanguage(true), []);
-	const handleClose = useCallback(() => setOpenLanguage(false), []);
-	const handleDropdown = useCallback(() => {
-		setOpenDropdown(true);
+	const handleOpen1 = useCallback(() => setOpen1(true), []);
+	const handleClose1 = useCallback(() => setOpen1(false), []);
+
+	const handleOpen2 = useCallback(() => {
+		setOpen2(true);
 
 		setTimeout(() => {
 			setRemove(true);
 		}, 2000);
 	}, []);
-	const handleDropdownClose = useCallback(() => setOpenDropdown(false), []);
+	const handleClose2 = useCallback(() => setOpen2(false), []);
 
 	return (
 		<WizardPanels>
 			<Panel title="QA Sample - Dropdown">
-				<Dropdown onClose={handleClose} onOpen={handleOpen} open={openLanguage} size="large" title="language">
+				<Dropdown onClose={handleClose1} onOpen={handleOpen1} open={open1} size="large" title="language">
 					{['English', 'Korean', 'Spanish', 'Amharic', 'Thai', 'Arabic', 'Urdu', 'Simplified Chinese', 'Traditional Chinese', 'Vietnamese']}
 				</Dropdown>
 				<Button size="large">
@@ -32,11 +34,11 @@ const MainPanel = () => {
 			</Panel>
 			<Panel>
 				{!removed && (
-					<div style={{margin: '20px'}}>
-						<BodyText>This is the line that will be removed.</BodyText>
+					<div style={{margin: ri.scaleToRem(20)}}>
+						<BodyText>This line will remove after opening the dropdown.</BodyText>
 					</div>
 				)}
-				<Dropdown onClose={handleDropdownClose} onOpen={handleDropdown} open={openDropdown}>{['a', 'b', 'c']}</Dropdown>
+				<Dropdown onClose={handleClose2} onOpen={handleOpen2} open={open2}>{['a', 'b', 'c']}</Dropdown>
 			</Panel>
 		</WizardPanels>
 	);
