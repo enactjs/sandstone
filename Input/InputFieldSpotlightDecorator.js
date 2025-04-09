8fa24aed8447a75d0693ff636983087f4b5e5d75
hyelynkim <150755065+mmyelyn@users.noreply.github.com>
2025-04-09 17:05:40 +0900

    WRR-22638: Fixed InputField to reset Spotlight Accelerator instance (#1816)
    * Fixed InputField to reset Spotlight Accelerator instance
    
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)
    
    * Added sample for InputField testing
    
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)
    
    * Applied reviews
    
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)

--- Input/InputFieldSpotlightDecorator.js
+++ Input/InputFieldSpotlightDecorator.js
@@ -304,7 +304,7 @@ const InputSpotlightDecorator = hoc(defaultConfig, (config, Wrapped) => {
 
 				if (shouldSpotlightMove) {
 					const direction = getDirection(keyCode);
-					const {getPointerMode, move, setPointerMode} = Spotlight;
+					const {getPointerMode, move, resetSpotlightAccelerator, setPointerMode} = Spotlight;
 
 					if (getPointerMode()) {
 						setPointerMode(false);
@@ -317,6 +317,7 @@ const InputSpotlightDecorator = hoc(defaultConfig, (config, Wrapped) => {
 					if (move(direction)) {
 						// if successful, reset the internal state
 						this.blur();
+						resetSpotlightAccelerator();
 					} else {
 						// if there is no other spottable elements, focus `InputDecorator` instead
 						this.focusDecorator(currentTarget);
